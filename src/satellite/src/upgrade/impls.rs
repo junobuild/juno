d47334ca9b26175506f97cb92982dd5b9bce425a
David Dal Busco <david.dalbusco@outlook.com>
2023-02-10 10:09:17 +0100

    feat: upgrade satellite state v0.0.1 -> v0.0.2 (#14)

---
+++ src/satellite/src/upgrade/impls.rs
@@ -0,0 +1,30 @@
+use std::collections::HashMap;
+use crate::storage::types::domain::CustomDomain;
+use crate::storage::types::state::StorageStableState;
+use crate::types::state::StableState;
+use crate::upgrade::types::upgrade::UpgradeStableState;
+
+impl From<&UpgradeStableState> for StableState {
+    fn from(state: &UpgradeStableState) -> Self {
+        let mut custom_domains = HashMap::new();
+
+        for (domain_name, custom_domain) in state.storage.custom_domains.clone().into_iter() {
+            custom_domains.insert(domain_name, CustomDomain {
+                updated_at: custom_domain.updated_at,
+                created_at: custom_domain.created_at,
+                bn_id: Some(custom_domain.bn_id)
+            });
+        }
+
+        StableState {
+            controllers: state.controllers.clone(),
+            db: state.db.clone(),
+            storage: StorageStableState {
+                assets: state.storage.assets.clone(),
+                rules: state.storage.rules.clone(),
+                config: state.storage.config.clone(),
+                custom_domains
+            }
+        }
+    }
+}

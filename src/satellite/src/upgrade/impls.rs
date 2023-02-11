041e80da1fbe5f41d7075d836b0026769a9a3271
David Dal Busco <david.dalbusco@outlook.com>
2023-02-11 13:16:30 +0100

    feat: next upgrade (#21)

--- src/satellite/src/upgrade/impls.rs
+++ src/satellite/src/upgrade/impls.rs
@@ -1,25 +1,10 @@
 use crate::storage::types::config::StorageConfig;
-use crate::storage::types::domain::CustomDomain;
 use crate::storage::types::state::StorageStableState;
 use crate::types::state::StableState;
-use crate::upgrade::types::upgrade::{UpgradeStableState, UpgradeTrailingSlash};
-use std::collections::HashMap;
+use crate::upgrade::types::upgrade::{UpgradeStableState};
 
 impl From<&UpgradeStableState> for StableState {
     fn from(state: &UpgradeStableState) -> Self {
-        let mut custom_domains = HashMap::new();
-
-        for (domain_name, custom_domain) in state.storage.custom_domains.clone().into_iter() {
-            custom_domains.insert(
-                domain_name,
-                CustomDomain {
-                    updated_at: custom_domain.updated_at,
-                    created_at: custom_domain.created_at,
-                    bn_id: Some(custom_domain.bn_id),
-                },
-            );
-        }
-
         StableState {
             controllers: state.controllers.clone(),
             db: state.db.clone(),
@@ -27,14 +12,8 @@ impl From<&UpgradeStableState> for StableState {
                 assets: state.storage.assets.clone(),
                 rules: state.storage.rules.clone(),
                 config: StorageConfig::default(),
-                custom_domains,
+                custom_domains: state.storage.custom_domains.clone(),
             },
         }
-    }
-}
-
-impl Default for UpgradeTrailingSlash {
-    fn default() -> Self {
-        UpgradeTrailingSlash::Never
     }
 }

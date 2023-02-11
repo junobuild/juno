416288c475c543466cc7f5a38026b558e904b370
David Dal Busco <david.dalbusco@outlook.com>
2023-02-11 08:51:04 +0100

    style: rounded copy

--- src/frontend/src/lib/components/icons/IconCopy.svelte
+++ src/frontend/src/lib/components/icons/IconCopy.svelte
@@ -1,4 +1,4 @@
-<!-- source: https://fonts.google.com/icons?selected=Material%20Icons%20Sharp%3Acontent_copy%3A -->
+<!-- source: https://fonts.google.com/icons?selected=Material%20Icons%20Outlined%3Acontent_copy%3A -->
 <svg
 	xmlns="http://www.w3.org/2000/svg"
 	height="16px"
@@ -6,6 +6,6 @@
 	width="16px"
 	fill="currentColor"
 	><path d="M0 0h24v24H0V0z" fill="none" /><path
-		d="M16 1H2v16h2V3h12V1zm5 4H6v18h15V5zm-2 16H8V7h11v14z"
+		d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"
 	/></svg
 >

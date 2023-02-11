c093eac5de53387e22d11b92cf6639f13cc3cd51
David Dal Busco <david.dalbusco@outlook.com>
2023-02-11 11:56:07 +0100

    feat: next upgrade

--- src/satellite/src/upgrade/types.rs
+++ src/satellite/src/upgrade/types.rs
@@ -1,52 +1,20 @@
 ///
-/// Breaking changes:
+/// Upgrade structure:
 ///
-/// v0.0.1 -> v0.0.3
-///
-/// CustomDomain.bn_id from String to Option<String>
+/// v0.0.3 -> v0.0.x
 ///
 pub mod upgrade {
     use crate::db::types::state::DbStableState;
-    use crate::rules::types::rules::Rules;
-    use crate::storage::types::domain::DomainName;
-    use crate::storage::types::state::Assets;
+    use crate::storage::types::state::{StorageStableState};
     use candid::CandidType;
     use serde::Deserialize;
     use shared::types::interface::Controllers;
-    use std::collections::HashMap;
 
     #[derive(Default, CandidType, Deserialize, Clone)]
     pub struct UpgradeStableState {
         pub controllers: Controllers,
         pub db: DbStableState,
-        pub storage: UpgradeStorageStableState,
+        pub storage: StorageStableState,
     }
 
-    #[derive(Default, CandidType, Deserialize, Clone)]
-    pub struct UpgradeStorageStableState {
-        pub assets: Assets,
-        pub rules: Rules,
-        pub config: UpgradeStorageConfig,
-        pub custom_domains: UpgradeCustomDomains,
-    }
-
-    pub type UpgradeCustomDomains = HashMap<DomainName, UpgradeCustomDomain>;
-
-    #[derive(CandidType, Deserialize, Clone)]
-    pub struct UpgradeCustomDomain {
-        pub bn_id: String,
-        pub created_at: u64,
-        pub updated_at: u64,
-    }
-
-    #[derive(Default, CandidType, Deserialize, Clone)]
-    pub struct UpgradeStorageConfig {
-        pub trailing_slash: UpgradeTrailingSlash,
-    }
-
-    #[derive(CandidType, Deserialize, Clone)]
-    pub enum UpgradeTrailingSlash {
-        Never,
-        Always,
-    }
 }

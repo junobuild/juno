bb1e52cfd34e2ca0ef5ca8b2d13807247bc325a0
David Dal Busco <david.dalbusco@outlook.com>
2023-02-16 09:14:40 +0100

    feat: custom domain BN registration (#32)

--- src/frontend/src/lib/components/hosting/CustomDomain.svelte
+++ src/frontend/src/lib/components/hosting/CustomDomain.svelte
@@ -1,25 +1,61 @@
 <script lang="ts">
 	import ExternalLink from '$lib/components/ui/ExternalLink.svelte';
+	import type { CustomDomain } from '$declarations/satellite/satellite.did';
+	import type { CustomDomainRegistrationState } from '$lib/types/custom-domain';
+	import { isNullish, nonNullish } from '$lib/utils/utils';
+	import { getCustomDomainRegistration } from '$lib/services/hosting.services';
 
 	export let url: string;
 	export let ariaLabel = '';
 	export let type: 'default' | 'custom' = 'default';
+	export let customDomain: CustomDomain | undefined = undefined;
 
 	let host = '';
 	$: ({ host } = new URL(url));
+
+	let registrationState: CustomDomainRegistrationState | undefined = undefined;
+
+	const loadRegistrationState = async () => {
+		if (isNullish(customDomain)) {
+			registrationState = undefined;
+			return;
+		}
+
+		try {
+			const registration = await getCustomDomainRegistration(customDomain);
+			registrationState = registration?.state;
+		} catch (err: unknown) {
+			registrationState = 'Failed';
+
+			// Error is display with state, we do not use a toast here
+			console.error(err);
+		}
+	};
+
+	$: customDomain, (async () => await loadRegistrationState())();
 </script>
 
-<div>
+<td colspan={nonNullish(registrationState) ? 2 : undefined} class="domain">
 	<ExternalLink href={url} {ariaLabel}>{host}</ExternalLink>
 	<span>{type}</span>
-</div>
+</td>
+
+{#if nonNullish(registrationState)}
+	<td>{registrationState}</td>
+{/if}
 
 <style lang="scss">
 	@use '../../styles/mixins/fonts';
+	@use '../../styles/mixins/text';
 
-	div {
+	.domain {
 		display: flex;
 		flex-direction: column;
+
+		:global(*) {
+			@include text.truncate;
+			display: inline-block;
+		}
 	}
 
 	span {
--- src/frontend/src/lib/services/hosting.services.ts
+++ src/frontend/src/lib/services/hosting.services.ts
@@ -3,8 +3,14 @@ import {
 	listCustomDomains as listCustomDomainsApi,
 	setCustomDomain
 } from '$lib/api/satellites.api';
+import type { CustomDomainRegistration } from '$lib/types/custom-domain';
+import { fromNullable } from '$lib/utils/did.utils';
+import { isNullish } from '$lib/utils/utils';
 import type { Principal } from '@dfinity/principal';
 
+/**
+ * https://github.com/r-birkner/portal/blob/rjb/update-custom-domains-docs/docs/developer-docs/deploy/custom-domain.md
+ */
 export const addCustomDomain = async ({
 	satelliteId,
 	domainName
@@ -19,8 +25,8 @@ export const addCustomDomain = async ({
 		boundaryNodesId: undefined
 	});
 
-	// TODO: POST call BN ic0.app/registrations
-	const boundaryNodesId = '123';
+	// Register domain name with BN
+	const boundaryNodesId = await registerDomain({ domainName });
 
 	// Save above request ID provided in previous step
 	await setCustomDomain({
@@ -42,4 +48,53 @@ export const listCustomDomains = async ({
 	// TODO: GET ic0.app/registrations/REQUEST_ID
 
 	return customDomains;
+};
+
+const BN_REGISTRATIONS_URL = import.meta.env.VITE_BN_REGISTRATIONS_URL;
+
+const registerDomain = async ({ domainName }: { domainName: string }): Promise<string> => {
+	const response = await fetch(BN_REGISTRATIONS_URL, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({ name: domainName })
+	});
+
+	if (!response.ok) {
+		throw new Error(`Registering ${domainName} with the boundary nodes failed.`);
+	}
+
+	const result: { id: string } = await response.json();
+
+	console.log('BN POST result', result);
+
+	return result.id;
+};
+
+export const getCustomDomainRegistration = async ({
+	bn_id
+}: CustomDomain): Promise<CustomDomainRegistration | undefined> => {
+	const id = fromNullable(bn_id);
+
+	if (isNullish(id) || id === '') {
+		return undefined;
+	}
+
+	const response = await fetch(`${BN_REGISTRATIONS_URL}/${id}`, {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
+
+	if (!response.ok) {
+		throw new Error(`Fetching custom domain state from the boundary nodes failed.`);
+	}
+
+	const result: CustomDomainRegistration = await response.json();
+
+	console.log('BN GET result', result);
+
+	return result;
 };

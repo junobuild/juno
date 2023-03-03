80ca25b40b180d58014938e802d9d78c51162420
David Dal Busco <david.dalbusco@outlook.com>
2023-03-03 15:41:59 +0100

    build: sveltekit and split rest for workers

--- src/frontend/src/lib/services/hosting.services.ts
+++ src/frontend/src/lib/services/hosting.services.ts
@@ -4,9 +4,9 @@ import {
 	listCustomDomains as listCustomDomainsApi,
 	setCustomDomain as setCustomDomainApi
 } from '$lib/api/satellites.api';
-import type { CustomDomainRegistration } from '$lib/types/custom-domain';
+import { deleteDomain, registerDomain } from '$lib/rest/bn.rest';
 import { fromNullable } from '$lib/utils/did.utils';
-import { isNullish, nonNullish } from '$lib/utils/utils';
+import { nonNullish } from '$lib/utils/utils';
 import type { Principal } from '@dfinity/principal';
 
 /**
@@ -66,70 +66,3 @@ export const listCustomDomains = async ({
 	listCustomDomainsApi({
 		satelliteId
 	});
-
-const BN_REGISTRATIONS_URL = import.meta.env.VITE_BN_REGISTRATIONS_URL;
-
-const registerDomain = async ({ domainName }: { domainName: string }): Promise<string> => {
-	const response = await fetch(BN_REGISTRATIONS_URL, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify({ name: domainName })
-	});
-
-	if (!response.ok) {
-		const text = await response.text();
-		throw new Error(`Registering ${domainName} with the boundary nodes failed. ${text}`);
-	}
-
-	const result: { id: string } = await response.json();
-
-	return result.id;
-};
-
-export const getCustomDomainRegistration = async ({
-	bn_id
-}: CustomDomain): Promise<CustomDomainRegistration | undefined> => {
-	const id = fromNullable(bn_id);
-
-	if (isNullish(id) || id === '') {
-		return undefined;
-	}
-
-	const response = await fetch(`${BN_REGISTRATIONS_URL}/${id}`, {
-		method: 'GET',
-		headers: {
-			'Content-Type': 'application/json'
-		}
-	});
-
-	if (!response.ok) {
-		const text = await response.text();
-		throw new Error(`Fetching custom domain state from the boundary nodes failed. ${text}`);
-	}
-
-	const result: CustomDomainRegistration = await response.json();
-
-	return result;
-};
-
-const deleteDomain = async ({ bn_id }: CustomDomain): Promise<void> => {
-	const id = fromNullable(bn_id);
-
-	if (isNullish(id) || id === '') {
-		throw new Error(`No existing BN id provided to delete custom domain.`);
-	}
-
-	const response = await fetch(`${BN_REGISTRATIONS_URL}/${id}`, {
-		method: 'DELETE',
-		headers: {
-			'Content-Type': 'application/json'
-		}
-	});
-
-	if (!response.ok) {
-		const text = await response.text();
-		throw new Error(`Deleting custom domain in the boundary nodes failed. ${text}`);
-	}
-};

0604a3e8daefb6454196758c5230268d7d6439d7
David Dal Busco <david.dalbusco@outlook.com>
2023-03-12 14:17:00 +0100

    feat: 10 per min

--- src/console/src/constants.rs
+++ src/console/src/constants.rs
@@ -5,7 +5,8 @@ use ic_ledger_types::Tokens;
 pub const SATELLITE_CREATION_FEE_ICP: Tokens = Tokens::from_e8s(50_000_000);
 
 // Rates
+// 60000000000 = 1min
 pub const DEFAULT_RATE_CONFIG: RateConfig = RateConfig {
-    max_tokens: 1,
-    time_per_token_ns: 600000000000,
+    max_tokens: 10,
+    time_per_token_ns: 60000000000,
 };

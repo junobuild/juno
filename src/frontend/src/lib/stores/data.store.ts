ec965e46449f4a6759d55cb8ca94c0d920e1d2f8
David Dal Busco <david.dalbusco@outlook.com>
2023-03-12 15:39:42 +0100

    feat: preserve order selection (#65)

--- src/frontend/src/lib/stores/data.store.ts
+++ src/frontend/src/lib/stores/data.store.ts
@@ -1,5 +1,27 @@
-import { DEFAULT_LIST_ORDER } from '$lib/constants/data.constants';
 import type { ListOrder } from '$lib/types/list';
+import { getLocalListOrder, setLocalStorageItem } from '$lib/utils/local-storage.utils';
+import type { Readable } from 'svelte/store';
 import { writable } from 'svelte/store';
 
-export const listOrderStore = writable<ListOrder>(DEFAULT_LIST_ORDER);
+const saveListOrder = (order: ListOrder) =>
+	setLocalStorageItem({ key: 'order', value: JSON.stringify(order) });
+
+export interface ListOrderStore extends Readable<ListOrder> {
+	set: (order: ListOrder) => void;
+}
+
+const initListOrderStore = (): ListOrderStore => {
+	const { subscribe, set } = writable<ListOrder>(getLocalListOrder());
+
+	return {
+		subscribe,
+
+		set: (order: ListOrder) => {
+			set(order);
+
+			saveListOrder(order);
+		}
+	};
+};
+
+export const listOrderStore = initListOrderStore();

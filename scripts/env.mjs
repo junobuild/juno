22d2e4d320134c79957ca81932695b28e36352ad
David Dal Busco <david.dalbusco@outlook.com>
2023-03-29 08:45:10 +0200

    chore: format

--- scripts/env.mjs
+++ scripts/env.mjs
@@ -1,5 +1,5 @@
-export const CONSOLE_CANISTER_ID_LOCAL = "rno2w-sqaaa-aaaaa-aaacq-cai";
-export const CONSOLE_CANISTER_ID_MAINNET = "cokmz-oiaaa-aaaal-aby6q-cai";
+export const CONSOLE_CANISTER_ID_LOCAL = 'rno2w-sqaaa-aaaaa-aaacq-cai';
+export const CONSOLE_CANISTER_ID_MAINNET = 'cokmz-oiaaa-aaaal-aby6q-cai';
 
 export const LEDGER_CANISTER_ID_LOCAL = 'r7inp-6aaaa-aaaaa-aaabq-cai';
 export const LEDGER_CANISTER_ID_MAINNET = 'ryjl3-tyaaa-aaaaa-aaaba-cai';

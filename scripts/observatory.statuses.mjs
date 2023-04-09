25cb9c997d84591089e21c802e38798c90559997
David Dal Busco <david.dalbusco@outlook.com>
2023-04-09 08:59:42 +0200

    feat: collect

--- scripts/observatory.statuses.mjs
+++ scripts/observatory.statuses.mjs
@@ -2,10 +2,40 @@
 
 import { observatoryActorLocal } from './actor.mjs';
 
-(async () => {
+const filterSegmentStatus = ({segmentStatus, cron_jobs}) => {
+	// If there was an error we want to inform
+	if ('Err' in segmentStatus) {
+		return true;
+	}
+
+	const { Ok: status } = segmentStatus;
+
+	return status.status.cycles < cron_jobs.statuses.cycles_threshold;
+}
+
+const filterStatuses = ({ statuses, cron_jobs }) => {
+	// If there was an error we want to inform
+	if ('Err' in statuses) {
+		return true;
+	}
+
+	const { Ok: {mission_control, satellites} } = statuses;
+
+	// Mission control needs to be reported
+	if (filterSegmentStatus({segmentStatus: mission_control, cron_jobs})) {
+		return true;
+	}
+
+	const satellite = (satellites[0] ?? []).find((satellite) => filterSegmentStatus({segmentStatus: satellite, cron_jobs}));
+
+	return satellite !== undefined;
+};
+
+const collect = async () => {
 	const actor = await observatoryActorLocal();
+	const statuses = await actor.list_last_statuses();
 
-	const result = await actor.list_last_statuses();
+	console.log('Statuses:', statuses.filter(filterStatuses));
+};
 
-	console.log('Statuses:', result);
-})();
+await collect();

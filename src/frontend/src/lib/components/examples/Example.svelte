0b775780d6ce68fada7863a2b4b878db5e555978
David Dal Busco <david.dalbusco@outlook.com>
2023-04-15 14:32:11 +0200

    feat: examples (#94)

---
+++ src/frontend/src/lib/components/examples/Example.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import IconArrowOutward from '$lib/components/icons/IconArrowOutward.svelte';
+
+	export let href: string;
+</script>
+
+<a rel="external noopener noreferrer" target="_blank" class="article">
+	<div class="summary">
+		<slot name="icon" />
+
+		<h5><slot name="title" /></h5>
+	</div>
+
+	<p><slot /></p>
+
+	<div class="icon">
+		<IconArrowOutward />
+	</div>
+</a>
+
+<style lang="scss">
+	.summary {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: var(--padding-4x) var(--padding-4x) var(--padding);
+		gap: var(--padding-2x);
+	}
+
+	p {
+		padding: var(--padding) var(--padding-4x) var(--padding);
+		min-height: 100px;
+	}
+
+	.icon {
+		position: absolute;
+		top: 0;
+		right: 0;
+		padding: var(--padding);
+	}
+
+	h5 {
+		margin: 0;
+	}
+</style>

691fd478fc6a1bb8712272f292a0c047a0679f9f
David Dal Busco <david.dalbusco@outlook.com>
2023-05-01 16:41:45 +0200

    build: transfer icp
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- scripts/ledger.transfer.mjs
+++ scripts/ledger.transfer.mjs
@@ -1,20 +1,19 @@
-import { AccountIdentifier, LedgerCanister } from '@dfinity/nns';
+import { IcrcLedgerCanister } from '@dfinity/ledger';
+import { Principal } from '@dfinity/principal';
 import { localAgent } from './actor.mjs';
 import { LEDGER_CANISTER_ID_LOCAL } from './env.mjs';
 
 const transfer = async () => {
 	const agent = await localAgent();
 
-	const ledger = LedgerCanister.create({
+	const ledger = IcrcLedgerCanister.create({
 		agent,
 		canisterId: LEDGER_CANISTER_ID_LOCAL
 	});
 
 	return ledger.transfer({
 		amount: 5_500_010_000n,
-		to: AccountIdentifier.fromHex(
-			'f3a58ea11bc128ab8a455dd7bce0a29b0a20f400625d1a46871fbfe82efed38d'
-		)
+		to: { owner: Principal.fromText('qaa6y-5yaaa-aaaaa-aaafa-cai'), subaccount: [] }
 	});
 };
 

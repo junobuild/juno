1dc4e6f91d55420c6bfefccf0b592a5ed8b69784
David Dal Busco <david.dalbusco@outlook.com>
2023-05-01 16:11:42 +0200

    build: deploy more recent version of the ledger
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- scripts/ledger.sh
+++ scripts/ledger.sh
@@ -3,7 +3,7 @@
 # Install ledger locally as documented in:
 # https://internetcomputer.org/docs/current/developer-docs/integrations/ledger/ledger-local-setup
 
-IC_VERSION=dd3a710b03bd3ae10368a91b255571d012d1ec2f
+IC_VERSION=f02cc38677905e24a9016637fddc697039930808
 curl -o ledger.wasm.gz "https://download.dfinity.systems/ic/$IC_VERSION/canisters/ledger-canister_notify-method.wasm.gz"
 gunzip ledger.wasm.gz
 curl -o ledger.private.did "https://raw.githubusercontent.com/dfinity/ic/$IC_VERSION/rs/rosetta-api/ledger.did"
@@ -18,7 +18,7 @@ dfx identity use default
 # LEDGER_ACC=$(dfx ledger account-id)
 LEDGER_ACC=1fac02b103220f2fe5ad314b98767db6ea7443cafc6a0a34c8adba4198a14d03
 
-dfx deploy ledger --argument '(record {minting_account = "'${MINT_ACC}'"; initial_values = vec { record { "'${LEDGER_ACC}'"; record { e8s=100_000_000_000 } }; }; send_whitelist = vec {}})'
+dfx deploy ledger --argument '(record {minting_account = "'${MINT_ACC}'"; initial_values = vec { record { "'${LEDGER_ACC}'"; record { e8s=100_000_000_000 } }; }; send_whitelist = vec {}})' --mode reinstall
 
 # Rust example to transfer ICP
 # https://github.com/dfinity/examples/tree/master/rust/tokens_transfer

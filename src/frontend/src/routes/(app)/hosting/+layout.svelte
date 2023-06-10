8031b9eba8ffb5fdb0193919303a28060903959e
David Dal Busco <david.dalbusco@outlook.com>
2023-06-10 09:20:48 +0200

    feat: i18n keys
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/routes/(app)/hosting/+layout.svelte
+++ src/frontend/src/routes/(app)/hosting/+layout.svelte
@@ -3,9 +3,10 @@
 	import { applyColor } from '$lib/utils/theme.utils';
 	import { Color } from '$lib/types/theme';
 	import { layoutTitle } from '$lib/stores/layout.store';
+	import { i18n } from '$lib/stores/i18n.store';
 
 	onMount(() => {
-		layoutTitle.set('Hosting');
+		layoutTitle.set($i18n.hosting.title);
 		applyColor(Color.BABY_PINK);
 	});
 </script>

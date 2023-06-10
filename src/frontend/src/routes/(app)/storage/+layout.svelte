70becdfa4b47fbcd577f1b891bb17b62178c93d0
David Dal Busco <david.dalbusco@outlook.com>
2023-06-10 09:21:25 +0200

    feat: display satellite name in overview (#139)
    * feat: display satellite name in title of overview page
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * feat: remove glitchy transition
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * feat: transition title
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * feat: i18n keys
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * chore: format
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    ---------
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/routes/(app)/storage/+layout.svelte
+++ src/frontend/src/routes/(app)/storage/+layout.svelte
@@ -3,9 +3,10 @@
 	import { applyColor } from '$lib/utils/theme.utils';
 	import { Color } from '$lib/types/theme';
 	import { layoutTitle } from '$lib/stores/layout.store';
+	import { i18n } from '$lib/stores/i18n.store';
 
 	onMount(() => {
-		layoutTitle.set('Storage');
+		layoutTitle.set($i18n.storage.title);
 		applyColor(Color.PINK_LACE);
 	});
 </script>

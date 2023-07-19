88b4cbddfb1c1e6bed34e82fa7517a4982a96a1f
David Dal Busco <david.dalbusco@outlook.com>
2023-07-19 19:09:11 +0200

    fix: type
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/lib/types/qr-creator.ts
+++ src/frontend/src/lib/types/qr-creator.ts
@@ -1,6 +1,4 @@
-import type QrCreator from 'qr-creator';
-
-export type QrCreatorConfig = QrCreator.Config;
+export type QrCreatorConfig = Object;
 export type QrCreateClass = {
 	render: (config: QrCreatorConfig, $element: HTMLElement) => void;
 };

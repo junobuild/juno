921665677b3195a608a227755631c5106310e085
David Dal Busco <david.dalbusco@outlook.com>
2023-07-26 14:00:42 +0200

    feat: display bounce rates and sessions
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/lib/components/analytics/Analytics.svelte
+++ src/frontend/src/lib/components/analytics/Analytics.svelte
@@ -6,6 +6,7 @@
 	import { getPageViews } from '$lib/api/orbiter.api';
 	import { onMount } from 'svelte';
 	import type { AnalyticKey, PageView } from '$declarations/orbiter/orbiter.did';
+	import Value from '$lib/components/ui/Value.svelte';
 
 	export let satelliteId: Principal;
 
@@ -32,14 +33,44 @@
 
 	onMount(load);
 
-	let uniqueVisitors: number = 0;
-	$: uniqueVisitors = [...new Set(data.map(([key, _]) => key.session_id))].length;
+	let uniqueSessions: number = 0;
+	$: uniqueSessions = [...new Set(data.map(([key, _]) => key.session_id))].length;
+
+	let sessionsViews: Record<string, number> = {};
+	$: sessionsViews = data.reduce(
+		(acc, [{ session_id }, _]) => ({
+			...acc,
+			[session_id]: (acc[session_id] ?? 0) + 1
+		}),
+		{}
+	);
+
+	let bounceRate = 0;
+	$: bounceRate = Object.entries(sessionsViews).filter(([key, value]) => value === 1).length;
 </script>
 
 <div class="card-container">
 	{#if loading}
 		<SpinnerParagraph>{$i18n.analytics.loading}</SpinnerParagraph>
 	{:else}
-		Unique visitors: {uniqueVisitors}
+		<Value>
+			<svelte:fragment slot="label">Number of Sessions</svelte:fragment>
+			<p>{uniqueSessions}</p>
+		</Value>
+
+		<Value>
+			<svelte:fragment slot="label">Unique page views</svelte:fragment>
+			<p>{data.length}</p>
+		</Value>
+
+		<Value>
+			<svelte:fragment slot="label">Average page views per session</svelte:fragment>
+			<p>{uniqueSessions / data.length}</p>
+		</Value>
+
+		<Value>
+			<svelte:fragment slot="label">Bounce rate</svelte:fragment>
+			<p>{bounceRate}</p>
+		</Value>
 	{/if}
 </div>

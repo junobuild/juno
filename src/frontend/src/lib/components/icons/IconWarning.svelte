68d6a4702ec634c50377e219b28b2fc309b8729e
David Dal Busco <david.dalbusco@outlook.com>
2023-08-03 18:00:04 +0200

    style: update icon rounded
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/lib/components/icons/IconWarning.svelte
+++ src/frontend/src/lib/components/icons/IconWarning.svelte
@@ -1,7 +1,7 @@
-<!-- source: https://fonts.google.com/icons?selected=Material%20Symbols%20Sharp%3Awarning%3AFILL%401%3Bwght%40400%3BGRAD%400%3Bopsz%4024-->
-<svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" fill="currentColor"
+<!-- source: https://fonts.google.com/icons?selected=Material%20Symbols%20Rounded%3Awarning%3AFILL%401%3Bwght%40400%3BGRAD%400%3Bopsz%4024 -->
+<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" fill="currentColor"
 	><path
-		d="M1 21 12 2l11 19Zm11-3q.425 0 .713-.288Q13 17.425 13 17t-.287-.712Q12.425 16 12 16t-.712.288Q11 16.575 11 17t.288.712Q11.575 18 12 18Zm-1-3h2v-5h-2Z"
+		d="M109-120q-11 0-20-5.5T75-140q-5-9-5.5-19.5T75-180l370-640q6-10 15.5-15t19.5-5q10 0 19.5 5t15.5 15l370 640q6 10 5.5 20.5T885-140q-5 9-14 14.5t-20 5.5H109Zm371-120q17 0 28.5-11.5T520-280q0-17-11.5-28.5T480-320q-17 0-28.5 11.5T440-280q0 17 11.5 28.5T480-240Zm0-120q17 0 28.5-11.5T520-400v-120q0-17-11.5-28.5T480-560q-17 0-28.5 11.5T440-520v120q0 17 11.5 28.5T480-360Z"
 	/></svg
 >
 

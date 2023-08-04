c96a6eb973ad1beea25ca88b096bc4e93d56ed9f
David Dal Busco <david.dalbusco@outlook.com>
2023-08-04 16:43:30 +0200

    refactor: extract and rename canister functions
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/console/src/factory/satellite.rs
+++ src/console/src/factory/satellite.rs
@@ -1,23 +1,12 @@
-use crate::constants::SATELLITE_CREATION_FEE_ICP;
 use crate::controllers::remove_console_controller_from_satellite;
-use crate::store::{
-    get_existing_mission_control, has_credits, increment_satellites_rate, insert_new_payment,
-    is_known_payment, update_payment_completed, update_payment_refunded, use_credits,
-};
-use crate::types::ledger::Payment;
+use crate::factory::canister::{create_canister_with_credits, create_canister_with_payment};
+use crate::store::{get_existing_mission_control, has_credits, increment_satellites_rate};
 use crate::wasm::satellite_wasm_arg;
 use candid::Principal;
-use ic_ledger_types::BlockIndex;
-use shared::constants::{
-    CREATE_SATELLITE_CYCLES, IC_TRANSACTION_FEE_ICP, MEMO_SATELLITE_CREATE_REFUND,
-};
+use shared::constants::CREATE_SATELLITE_CYCLES;
 use shared::ic::create_canister_install_code;
-use shared::ledger::{
-    find_payment, principal_to_account_identifier, transfer_payment, SUB_ACCOUNT,
-};
 use shared::types::interface::CreateSatelliteArgs;
 use shared::types::state::{MissionControlId, UserId};
-use std::future::Future;
 
 pub async fn create_satellite(
     console: Principal,
@@ -34,7 +23,7 @@ pub async fn create_satellite(
                 // Guard too many requests
                 increment_satellites_rate()?;
 
-                return create_satellite_with_credits(
+                return create_canister_with_credits(
                     create_satellite_wasm,
                     console,
                     mission_control_id,
@@ -43,7 +32,7 @@ pub async fn create_satellite(
                 .await;
             }
 
-            create_satellite_with_payment(
+            create_canister_with_payment(
                 create_satellite_wasm,
                 console,
                 caller,
@@ -51,106 +40,6 @@ pub async fn create_satellite(
                 CreateSatelliteArgs { user, block_index },
             )
             .await
-        }
-    }
-}
-
-async fn create_satellite_with_credits<F, Fut>(
-    create: F,
-    console: Principal,
-    mission_control_id: MissionControlId,
-    user: UserId,
-) -> Result<Principal, String>
-where
-    F: FnOnce(Principal, MissionControlId, UserId) -> Fut,
-    Fut: Future<Output = Result<Principal, String>>,
-{
-    // Create the satellite
-    let create_canister_result = create(console, mission_control_id, user).await;
-
-    match create_canister_result {
-        Err(_) => Err("Satellite creation with credits failed.".to_string()),
-        Ok(satellite_id) => {
-            // Satellite is created we can use the credits
-            let credits = use_credits(&user);
-
-            match credits {
-                Err(e) => Err(e.to_string()),
-                Ok(_) => Ok(satellite_id),
-            }
-        }
-    }
-}
-
-async fn create_satellite_with_payment<F, Fut>(
-    create: F,
-    console: Principal,
-    caller: Principal,
-    mission_control_id: MissionControlId,
-    CreateSatelliteArgs { user, block_index }: CreateSatelliteArgs,
-) -> Result<Principal, String>
-where
-    F: FnOnce(Principal, MissionControlId, UserId) -> Fut,
-    Fut: Future<Output = Result<Principal, String>>,
-{
-    let mission_control_account_identifier = principal_to_account_identifier(&caller, &SUB_ACCOUNT);
-    let console_account_identifier = principal_to_account_identifier(&console, &SUB_ACCOUNT);
-
-    if block_index.is_none() {
-        return Err("No block index provided to verify payment.".to_string());
-    }
-
-    // User should have processed a payment from the mission control center
-    let mission_control_payment_block_index: BlockIndex = block_index.unwrap();
-    let block = find_payment(
-        mission_control_account_identifier,
-        console_account_identifier,
-        SATELLITE_CREATION_FEE_ICP,
-        mission_control_payment_block_index,
-    )
-    .await;
-
-    match block {
-        None => Err([
-            "No valid payment found to create satellite.",
-            &format!(" Mission control: {}", mission_control_account_identifier),
-            &format!(" Console: {}", console_account_identifier),
-            &format!(" Amount: {}", SATELLITE_CREATION_FEE_ICP),
-            &format!(" Block index: {}", block_index.unwrap()),
-        ]
-        .join("")),
-        Some(_) => {
-            if is_known_payment(&mission_control_payment_block_index) {
-                return Err("Payment has been or is being processed.".to_string());
-            }
-
-            // We acknowledge the new payment
-            insert_new_payment(&user, &mission_control_payment_block_index)?;
-
-            // Create the satellite
-            let create_canister_result = create(console, mission_control_id, user).await;
-
-            match create_canister_result {
-                Err(error) => {
-                    refund_satellite_creation(
-                        &mission_control_id,
-                        &mission_control_payment_block_index,
-                    )
-                    .await?;
-
-                    Err([
-                        "Satellite creation failed. Mission control center has been refunded.",
-                        &error,
-                    ]
-                    .join(" - "))
-                }
-                Ok(satellite_id) => {
-                    // Satellite is created we can update the payment has being processed
-                    update_payment_completed(&mission_control_payment_block_index)?;
-
-                    Ok(satellite_id)
-                }
-            }
         }
     }
 }
@@ -176,28 +65,4 @@ async fn create_satellite_wasm(
             Ok(satellite_id)
         }
     }
-}
-
-async fn refund_satellite_creation(
-    mission_control_id: &MissionControlId,
-    mission_control_payment_block_index: &BlockIndex,
-) -> Result<Payment, String> {
-    // We refund the satellite creation fee minus the ic fee - i.e. user pays the fee
-    let refund_amount = SATELLITE_CREATION_FEE_ICP - IC_TRANSACTION_FEE_ICP;
-
-    // Refund on error
-    let refund_block_index = transfer_payment(
-        mission_control_id,
-        &SUB_ACCOUNT,
-        MEMO_SATELLITE_CREATE_REFUND,
-        refund_amount,
-        IC_TRANSACTION_FEE_ICP,
-    )
-    .await
-    .map_err(|e| format!("failed to call ledger: {:?}", e))?
-    .map_err(|e| format!("ledger transfer error {:?}", e))?;
-
-    // We record the refund in the payment list
-    update_payment_refunded(mission_control_payment_block_index, &refund_block_index)
-        .map_err(|e| format!("Insert refund transaction error {:?}", e))
 }

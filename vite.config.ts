7e5038e01860d34e56de2135a1bfd119441e9935
David Dal Busco <david.dalbusco@outlook.com>
2023-08-08 18:30:33 +0200

    build: remove unused
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
-import dsv from '@rollup/plugin-dsv';
 import inject from '@rollup/plugin-inject';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { readFileSync } from 'fs';
@@ -47,7 +46,7 @@ const readCanisterIds = ({ prefix }: { prefix?: string }): Record<string, string
 };
 
 const config: UserConfig = {
-	plugins: [sveltekit(), dsv()],
+	plugins: [sveltekit()],
 	resolve: {
 		alias: {
 			$declarations: resolve('./src/declarations')

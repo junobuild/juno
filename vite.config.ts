7724f040f15d8989d09f8b4796881f6c9aaf196c
David Dal Busco <david.dalbusco@outlook.com>
2023-08-18 15:13:44 +0200

    chore: no dev
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- vite.config.ts
+++ vite.config.ts
@@ -100,9 +100,6 @@ const config: UserConfig = {
 	server: {
 		proxy: {
 			'/api': 'http://localhost:8000'
-		},
-		watch: {
-			ignored: ['.dfx']
 		}
 	},
 	// Node polyfill agent-js. Thanks solution shared by chovyfu on the Discord channel.

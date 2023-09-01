5f800b023f517b5a183afb8a7266c7521db5fc28
David Dal Busco <david.dalbusco@outlook.com>
2023-09-01 16:09:56 +0200

    fix: upgrade observatory (#201)
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/observatory/src/upgrade/types.rs
+++ src/observatory/src/upgrade/types.rs
@@ -1,13 +1,39 @@
 pub mod upgrade {
-    use crate::types::state::{Archive, CronTabs};
+    use crate::types::state::Archive;
     use candid::CandidType;
     use serde::Deserialize;
-    use shared::types::state::Controllers;
+    use shared::types::cronjob::CronJobStatusesSegments;
+    use shared::types::state::{Controllers, Metadata, MissionControlId, UserId};
+    use std::collections::HashMap;
 
     #[derive(Default, CandidType, Deserialize, Clone)]
     pub struct UpgradeStableState {
         pub controllers: Controllers,
-        pub cron_tabs: CronTabs,
+        pub cron_tabs: UpgradeCronTabs,
         pub archive: Archive,
+    }
+
+    pub type UpgradeCronTabs = HashMap<UserId, UpgradeCronTab>;
+
+    #[derive(CandidType, Deserialize, Clone)]
+    pub struct UpgradeCronTab {
+        pub mission_control_id: MissionControlId,
+        pub cron_jobs: UpgradeCronJobs,
+        pub created_at: u64,
+        pub updated_at: u64,
+    }
+
+    #[derive(Default, CandidType, Deserialize, Clone)]
+    pub struct UpgradeCronJobs {
+        pub metadata: Metadata,
+        pub statuses: UpgradeCronJobStatuses,
+    }
+
+    #[derive(Default, CandidType, Deserialize, Clone)]
+    pub struct UpgradeCronJobStatuses {
+        pub enabled: bool,
+        pub cycles_threshold: Option<u64>,
+        pub mission_control_cycles_threshold: Option<u64>,
+        pub satellites: CronJobStatusesSegments,
     }
 }

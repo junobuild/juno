bc31996034df5a4d51f2d82cf708698bc37e09ee
David Dal Busco <david.dalbusco@outlook.com>
2023-09-11 20:53:44 +0200

    chore: remove print
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/lib/api/ledger.api.ts
+++ src/frontend/src/lib/api/ledger.api.ts
@@ -1,9 +1,11 @@
+import { localIdentityCanisterId } from '$lib/constants/constants';
 import { authStore } from '$lib/stores/auth.store';
 import { getAgent } from '$lib/utils/agent.utils';
+import { nonNullish } from '$lib/utils/utils';
 import type { Identity } from '@dfinity/agent';
 import { IcrcLedgerCanister } from '@dfinity/ledger';
 import type { Principal } from '@dfinity/principal';
-import { AccountIdentifier } from '@junobuild/ledger';
+import { AccountIdentifier, balance as balanceApi, transactions } from '@junobuild/ledger';
 import { get } from 'svelte/store';
 
 export const getAccountIdentifier = (principal: Principal): AccountIdentifier =>
@@ -22,6 +24,28 @@ export const getBalance = async (owner: Principal): Promise<bigint> => {
 		agent,
 		canisterId: import.meta.env.VITE_LEDGER_CANISTER_ID
 	});
+
+	// TODO: this is a test
+	console.log(
+		await balanceApi({
+			index: {
+				...(nonNullish(localIdentityCanisterId) && { env: 'dev' }),
+				identity
+			},
+			accountIdentifier: getAccountIdentifier(owner).toHex()
+		}),
+		await transactions({
+			index: {
+				...(nonNullish(localIdentityCanisterId) && { env: 'dev' }),
+				identity
+			},
+			args: {
+				start: undefined,
+				max_results: 100n,
+				account_identifier: getAccountIdentifier(owner).toHex()
+			}
+		})
+	);
 
 	return balance({
 		owner,

1086aa753035fb3da8092d3b478d3996bd50ce6f
David Dal Busco <david.dalbusco@outlook.com>
2023-09-18 19:41:09 +0200

    build: use candid-explorer
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- scripts/did.sh
+++ scripts/did.sh
@@ -6,17 +6,12 @@ function generate_did() {
 
   cargo build --manifest-path="$canister_root/Cargo.toml" \
       --target wasm32-unknown-unknown \
-      --release --package "$canister" \
-      --features "ic-cdk/wasi"
+      --release --package "$canister"
 
-  # Installation https://docs.wasmtime.dev/cli-install.html
-  wasmtime "target/wasm32-unknown-unknown/release/$canister.wasm" > "$canister_root/$canister.did"
+  candid-extractor "target/wasm32-unknown-unknown/release/$canister.wasm" > "$canister_root/$canister.did"
 }
 
-# TODO: ic_cdk v.10.0 incompatible with wasmtime and stable-structures
-# Add orbiter to generate list
-
-CANISTERS=console,observatory,mission_control,satellite
+CANISTERS=console,observatory,mission_control,satellite,orbiter
 
 for canister in $(echo $CANISTERS | sed "s/,/ /g")
 do

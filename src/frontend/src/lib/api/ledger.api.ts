fc397c2b11b863825da58e83ff34608d7a3d9694
David Dal Busco <david.dalbusco@outlook.com>
2023-09-24 18:50:26 +0200

    feat: use juno ledger
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/lib/api/ledger.api.ts
+++ src/frontend/src/lib/api/ledger.api.ts
@@ -1,54 +1,51 @@
 import { localIdentityCanisterId } from '$lib/constants/constants';
 import { authStore } from '$lib/stores/auth.store';
-import { getAgent } from '$lib/utils/agent.utils';
 import { nonNullish } from '$lib/utils/utils';
 import type { Identity } from '@dfinity/agent';
-import { IcrcLedgerCanister } from '@dfinity/ledger';
 import type { Principal } from '@dfinity/principal';
-import { AccountIdentifier, balance as balanceApi, transactions } from '@junobuild/ledger';
+import type { GetAccountIdentifierTransactionsResponse } from '@junobuild/ledger';
+import { AccountIdentifier, balance, transactions } from '@junobuild/ledger';
 import { get } from 'svelte/store';
 
 export const getAccountIdentifier = (principal: Principal): AccountIdentifier =>
 	AccountIdentifier.fromPrincipal({ principal, subAccount: undefined });
 
-export const getBalance = async (owner: Principal): Promise<bigint> => {
+export const getBalance = (owner: Principal): Promise<bigint> => {
 	const identity: Identity | undefined | null = get(authStore).identity;
 
 	if (!identity) {
 		throw new Error('No internet identity.');
 	}
 
-	const agent = await getAgent({ identity });
-
-	const { balance } = IcrcLedgerCanister.create({
-		agent,
-		canisterId: import.meta.env.VITE_LEDGER_CANISTER_ID
-	});
-
-	// TODO: this is a test
-	console.log(
-		await balanceApi({
-			index: {
-				...(nonNullish(localIdentityCanisterId) && { env: 'dev' }),
-				identity
-			},
-			accountIdentifier: getAccountIdentifier(owner).toHex()
-		}),
-		await transactions({
-			index: {
-				...(nonNullish(localIdentityCanisterId) && { env: 'dev' }),
-				identity
-			},
-			args: {
-				start: undefined,
-				max_results: 100n,
-				account_identifier: getAccountIdentifier(owner).toHex()
-			}
-		})
-	);
-
 	return balance({
-		owner,
-		certified: false
+		index: {
+			...(nonNullish(localIdentityCanisterId) && { env: 'dev' }),
+			identity
+		},
+		accountIdentifier: getAccountIdentifier(owner).toHex()
+	});
+};
+
+export const getTransactions = ({
+	owner
+}: {
+	owner: Principal;
+}): Promise<GetAccountIdentifierTransactionsResponse> => {
+	const identity: Identity | undefined | null = get(authStore).identity;
+
+	if (!identity) {
+		throw new Error('No internet identity.');
+	}
+
+	return transactions({
+		index: {
+			...(nonNullish(localIdentityCanisterId) && { env: 'dev' }),
+			identity
+		},
+		args: {
+			start: undefined,
+			max_results: 100n,
+			account_identifier: getAccountIdentifier(owner).toHex()
+		}
 	});
 };

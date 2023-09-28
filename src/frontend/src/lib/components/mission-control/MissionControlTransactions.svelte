f1943bf220cb5c06221e2d94e255d4481ac3fd25
David Dal Busco <david.dalbusco@outlook.com>
2023-09-28 09:22:04 +0200

    feat: constant
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/lib/components/mission-control/MissionControlTransactions.svelte
+++ src/frontend/src/lib/components/mission-control/MissionControlTransactions.svelte
@@ -1,45 +1,52 @@
 <script lang="ts">
-    import type {TransactionWithId} from "@junobuild/ledger";
-	import {formatE8sICP} from "$lib/utils/icp.utils";
-	import { fade } from "svelte/transition";
+	import type { TransactionWithId } from '@junobuild/ledger';
+	import { formatE8sICP } from '$lib/utils/icp.utils';
+	import { fade } from 'svelte/transition';
+	import {
+		MEMO_CANISTER_CREATE,
+		MEMO_CANISTER_TOP_UP,
+		MEMO_ORBITER_CREATE_REFUND,
+		MEMO_SATELLITE_CREATE_REFUND
+	} from '$lib/constants/wallet.constants';
 
-    export let transactions: TransactionWithId[];
+	export let transactions: TransactionWithId[];
 </script>
 
 <div class="table-container">
-    <table>
-        <thead>
-        <tr>
-            <th class="block_index"> ID </th>
-            <th class="memo"> Memo </th>
-            <th class="amount"> Amount </th>
-        </tr>
-        </thead>
+	<table>
+		<thead>
+			<tr>
+				<th class="block_index"> ID </th>
+				<th class="memo"> Memo </th>
+				<th class="amount"> Amount </th>
+			</tr>
+		</thead>
 
-        		<tbody>
-        			{#each transactions as {id, transaction}}
-        				<tr transition:fade>
-        					<td>{`${id}`}</td>
-        					<td
-        						>{transaction.memo === 1095062083n
-        							? 'Create satellite'
-        							: transaction.memo === 19217561964265811n
-        							? 'Create satellite refund'
-        							: transaction.memo === 1347768404n
-        							? 'Top-up satellite'
-        							: 'Received'}</td
-        					>
-        					<td>
-        						{#if 'Transfer' in transaction.operation}
-        							{formatE8sICP(transaction.operation.Transfer.amount.e8s)} ICP
-        						{/if}
-        					</td>
-        				</tr>
-        			{/each}
-        		</tbody>
-    </table>
+		<tbody>
+			{#each transactions as { id, transaction }}
+				<tr in:fade>
+					<td>{`${id}`}</td>
+					<td
+						>{transaction.memo === MEMO_CANISTER_CREATE
+							? 'Create satellite'
+							: transaction.memo === MEMO_SATELLITE_CREATE_REFUND
+							? 'Create satellite refund'
+							: transaction.memo === MEMO_ORBITER_CREATE_REFUND
+							? 'Create orbiter refund'
+							: transaction.memo === MEMO_CANISTER_TOP_UP
+							? 'Top-up'
+							: 'Received'}</td
+					>
+					<td>
+						{#if 'Transfer' in transaction.operation}
+							{formatE8sICP(transaction.operation.Transfer.amount.e8s)} ICP
+						{/if}
+					</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
 </div>
-
 
 <style lang="scss">
 	.table-container {

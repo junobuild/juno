e4f7ac0d0bb1aadb334e81bb1bd72f3ce82d2ff4
David Dal Busco <david.dalbusco@outlook.com>
2023-11-02 21:19:54 +0100

    feat: default redirect
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/satellite/src/storage/constants.rs
+++ src/satellite/src/storage/constants.rs
@@ -7,7 +7,7 @@ pub static ENCODING_CERTIFICATION_ORDER: &[&str] = &[
     "br",
 ];
 pub static BN_WELL_KNOWN_CUSTOM_DOMAINS: &str = "/.well-known/ic-domains";
-pub static REWRITE_TO_ROOT_INDEX_HTML: (&str, &str) = ("**", "/index.html");
+pub static REWRITE_TO_ROOT_INDEX_HTML: (&str, &str) = ("/**", "/");
 
 pub static RESPONSE_STATUS_CODE_200: u16 = 200;
 pub static RESPONSE_STATUS_CODE_404: u16 = 404;

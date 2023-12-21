e17633dc4ca255844577db2e8d116a39e0d3e450
David Dal Busco <david.dalbusco@outlook.com>
2023-12-21 22:05:03 +0100

    feat: total of files deployed
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/lib/components/hosting/Hosting.svelte
+++ src/frontend/src/lib/components/hosting/Hosting.svelte
@@ -8,10 +8,12 @@
 	import type { CustomDomain as CustomDomainType } from '$declarations/satellite/satellite.did';
 	import { i18n } from '$lib/stores/i18n.store';
 	import { listCustomDomains } from '$lib/services/hosting.services';
+	import HostingCount from '$lib/components/hosting/HostingCount.svelte';
+	import type { SatelliteIdText } from '$lib/types/satellite';
 
 	export let satellite: Satellite;
 
-	let satelliteId: string;
+	let satelliteId: SatelliteIdText;
 	$: satelliteId = satellite.satellite_id.toText();
 
 	let customDomains: [string, CustomDomainType][] = [];
@@ -72,7 +74,11 @@
 	</table>
 </div>
 
-<AddCustomDomain {satellite} />
+<div class="footer">
+	<AddCustomDomain {satellite} />
+
+	<HostingCount {satellite} />
+</div>
 
 <style lang="scss">
 	@use '../../styles/mixins/media';
@@ -84,6 +90,18 @@
 	.domain {
 		@include media.min-width(small) {
 			width: 60%;
+		}
+	}
+
+	.footer {
+		display: flex;
+		flex-direction: column-reverse;
+		gap: var(--padding);
+
+		@include media.min-width(small) {
+			display: flex;
+			flex-direction: row;
+			justify-content: space-between;
 		}
 	}
 </style>

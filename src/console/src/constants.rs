5cc8c934a65ea09ecedd2bf4f1f983702ffb3e04
David Dal Busco <david.dalbusco@outlook.com>
2023-12-22 15:18:21 +0100

    feat: review user credits (#373)
    * feat: review user credits
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * chore: remove unused
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    ---------
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/console/src/constants.rs
+++ src/console/src/constants.rs
@@ -7,6 +7,10 @@ pub const SATELLITE_CREATION_FEE_ICP: Tokens = Tokens::from_e8s(50_000_000);
 // 0.5 ICP
 pub const ORBITER_CREATION_FEE_ICP: Tokens = Tokens::from_e8s(50_000_000);
 
+// 1 ICP but also the default credit - i.e. a mission control starts with one credit.
+// A credit which can be used to start one satellite or one orbiter.
+pub const E8S_PER_ICP: Tokens = Tokens::from_e8s(100_000_000);
+
 // Rates
 // 60000000000 = 1min
 pub const DEFAULT_RATE_CONFIG: RateConfig = RateConfig {

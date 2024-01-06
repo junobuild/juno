95a5e7a77783427b12172a123ca93c68f44a9a45
David Dal Busco <david.dalbusco@outlook.com>
2024-01-06 12:23:39 +0100

    feat: few functions of the orbiter can actually be accessed by read+write controllers not just admin
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/orbiter/src/guards.rs
+++ src/orbiter/src/guards.rs
@@ -1,6 +1,6 @@
 use crate::STATE;
 use ic_cdk::caller;
-use shared::controllers::is_admin_controller;
+use shared::controllers::{is_admin_controller, is_controller};
 use shared::types::state::Controllers;
 
 pub fn caller_is_admin_controller() -> Result<(), String> {
@@ -8,6 +8,17 @@ pub fn caller_is_admin_controller() -> Result<(), String> {
     let controllers: Controllers = STATE.with(|state| state.borrow().heap.controllers.clone());
 
     if is_admin_controller(caller, &controllers) {
+        Ok(())
+    } else {
+        Err("Caller is not an admin controller of the orbiter.".to_string())
+    }
+}
+
+pub fn caller_is_controller() -> Result<(), String> {
+    let caller = caller();
+    let controllers: Controllers = STATE.with(|state| state.borrow().heap.controllers.clone());
+
+    if is_controller(caller, &controllers) {
         Ok(())
     } else {
         Err("Caller is not a controller of the orbiter.".to_string())

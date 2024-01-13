23cbc772be3c082430696f8eb059848142ddd034
David Dal Busco <david.dalbusco@outlook.com>
2024-01-13 16:42:58 +0100

    feat: move more code to macro
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/satellite/src/lib.rs
+++ src/satellite/src/lib.rs
@@ -1,7 +1,5 @@
 use ic_cdk::print;
-use ic_cdk_macros::export_candid;
 use junobuild_macros::init_satellite;
-use junobuild_satellite::include_satellite;
 use junobuild_satellite::types::hooks::SatelliteHooks;
 
 struct MyHooks;
@@ -14,10 +12,5 @@ impl SatelliteHooks for MyHooks {
 
 #[init_satellite]
 fn init() -> Box<dyn SatelliteHooks> {
-    print("Crate register");
     Box::new(MyHooks)
 }
-
-include_satellite!();
-
-export_candid!();

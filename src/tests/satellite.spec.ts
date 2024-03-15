070526d0595f4d57beca1b2f99d28d8b20b1b33f
David Dal Busco <david.dalbusco@outlook.com>
2024-03-15 12:57:20 +0100

    test: more satellite test
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/tests/satellite.spec.ts
+++ src/tests/satellite.spec.ts
@@ -1,13 +1,17 @@
-import type { _SERVICE as SatelliteActor, SetRule } from '$declarations/satellite/satellite.did';
+import type {
+	_SERVICE as SatelliteActor,
+	SetRule,
+	StorageConfig
+} from '$declarations/satellite/satellite.did';
 import { idlFactory as idlFactorSatellite } from '$declarations/satellite/satellite.factory.did';
 import { IDL } from '@dfinity/candid';
 import { Ed25519KeyIdentity } from '@dfinity/identity';
-import { toNullable } from '@dfinity/utils';
+import { arrayBufferToUint8Array, toNullable } from '@dfinity/utils';
 import { PocketIc, type Actor } from '@hadronous/pic';
 import { parse } from '@ltd/j-toml';
 import { existsSync, readFileSync } from 'node:fs';
 import { join, resolve } from 'node:path';
-import { afterAll, beforeAll, describe, expect } from 'vitest';
+import { afterAll, beforeAll, beforeEach, describe, expect } from 'vitest';
 
 const WASM_PATH_LOCAL = resolve(
 	process.cwd(),
@@ -86,6 +90,199 @@ describe('Satellite', () => {
 			expect(updated_at).toBeGreaterThan(0n);
 
 			testRuleUpdatedAt = updated_at;
+		});
+
+		it('should  list collections', async () => {
+			const { list_rules, set_rule } = actor;
+
+			const [
+				[collection, { updated_at, created_at, memory, mutable_permissions, read, write }],
+				_
+			] = await list_rules({ Db: null });
+
+			expect(collection).toEqual('test');
+			expect(memory).toEqual(toNullable({ Heap: null }));
+			expect(read).toEqual({ Managed: null });
+			expect(write).toEqual({ Managed: null });
+			expect(created_at).toBeGreaterThan(0n);
+			expect(updated_at).toBeGreaterThan(0n);
+		});
+
+		it('should add and remove collections', async () => {
+			const { list_rules, set_rule, del_rule } = actor;
+
+			await set_rule({ Db: null }, 'test2', setRule);
+
+			const rules = await list_rules({ Db: null });
+			expect(rules).toHaveLength(2);
+
+			const [_, { updated_at }] = rules[1];
+
+			await del_rule({ Db: null }, 'test2', {
+				updated_at: toNullable(updated_at)
+			});
+
+			expect(await list_rules({ Db: null })).toHaveLength(1);
+		});
+
+		it('should add and remove additional controller', async () => {
+			const { set_controllers, del_controllers, list_controllers } = actor;
+
+			const newController = Ed25519KeyIdentity.generate();
+
+			const controllers = await set_controllers({
+				controllers: [newController.getPrincipal()],
+				controller: {
+					expires_at: toNullable(),
+					metadata: [],
+					scope: { Admin: null }
+				}
+			});
+
+			expect(controllers).toHaveLength(2);
+
+			expect(
+				controllers.find(([p]) => p.toText() === controller.getPrincipal().toText())
+			).not.toBeUndefined();
+
+			expect(
+				controllers.find(([p]) => p.toText() === newController.getPrincipal().toText())
+			).not.toBeUndefined();
+
+			await del_controllers({
+				controllers: [newController.getPrincipal()]
+			});
+
+			const updatedControllers = await list_controllers();
+			expect(updatedControllers).toHaveLength(1);
+			expect(updatedControllers[0][0].toText()).toEqual(controller.getPrincipal().toText());
+		});
+
+		it('should set and get config', async () => {
+			const { set_config, get_config } = actor;
+
+			const storage: StorageConfig = {
+				headers: [['*', [['Cache-Control', 'no-cache']]]],
+				iframe: toNullable({ Deny: null }),
+				redirects: [
+					[
+						[
+							'/*',
+							{
+								location: '/hello.html',
+								status_code: 302
+							}
+						]
+					]
+				],
+				rewrites: [['/hello.html', '/hello.html']]
+			};
+
+			await set_config({
+				storage
+			});
+
+			const configs = await get_config();
+
+			expect(configs).toEqual({
+				storage
+			});
+		});
+
+		it('should deploy asset to dapp', async () => {
+			const { http_request, commit_asset_upload, upload_asset_chunk, init_asset_upload } = actor;
+
+			const file = await init_asset_upload({
+				collection: '#dapp',
+				description: toNullable(),
+				encoding_type: [],
+				full_path: '/hello.html',
+				name: 'hello.html',
+				token: toNullable()
+			});
+
+			const HTML = '<html><body>Hello</body></html>';
+
+			const blob = new Blob([HTML], {
+				type: 'text/plain; charset=utf-8'
+			});
+
+			const chunk = await upload_asset_chunk({
+				batch_id: file.batch_id,
+				content: arrayBufferToUint8Array(await blob.arrayBuffer()),
+				order_id: [0n]
+			});
+
+			await commit_asset_upload({
+				batch_id: file.batch_id,
+				chunk_ids: [chunk.chunk_id],
+				headers: []
+			});
+
+			const { headers, body } = await http_request({
+				body: [],
+				certificate_version: toNullable(),
+				headers: [],
+				method: 'GET',
+				url: '/hello.html'
+			});
+
+			const rest = headers.filter(([header, _]) => header !== 'IC-Certificate');
+
+			expect(rest).toEqual([
+				['accept-ranges', 'bytes'],
+				['etag', '"03ee66f1452916b4f91a504c1e9babfa201b6d64c26a82b2cf03c3ed49d91585"'],
+				['X-Content-Type-Options', 'nosniff'],
+				['Strict-Transport-Security', 'max-age=31536000 ; includeSubDomains'],
+				['Referrer-Policy', 'same-origin'],
+				['X-Frame-Options', 'DENY'],
+				['Cache-Control', 'no-cache']
+			]);
+
+			const certificate = headers.find(([header, _]) => header === 'IC-Certificate');
+
+			expect(certificate).not.toBeUndefined();
+
+			const [_, value] = certificate as [string, string];
+			expect(value.substring(value.indexOf('tree=:'))).toEqual(
+				'tree=:2dn3gwGDAktodHRwX2Fzc2V0c4MBggRYIDmN5doHXoiKCtNGBOZIdmQ+WGqYjcmdRB1MPuJBK2oXgwJLL2hlbGxvLmh0bWyCA1ggA+5m8UUpFrT5GlBMHpur+iAbbWTCaoKyzwPD7UnZFYWCBFggu+vgJ40baoH6QaigqUGb3VrUkopUK4LJuugRxX7g6qc=:'
+			);
+
+			const decoder = new TextDecoder();
+			expect(decoder.decode(body as ArrayBuffer)).toEqual(HTML);
+		});
+
+		it('should add and remove custom domain', async () => {
+			const { list_custom_domains, del_custom_domain, set_custom_domain } = actor;
+
+			await set_custom_domain('hello.com', toNullable('12345'));
+
+			const domains = await list_custom_domains();
+			expect(domains[0][0]).toEqual('hello.com');
+			expect(domains[0][1].bn_id).toEqual(toNullable('12345'));
+
+			await set_custom_domain('world.com', toNullable());
+			expect(await list_custom_domains()).toHaveLength(2);
+
+			await del_custom_domain('world.com');
+			const result = await list_custom_domains();
+			expect(result).toHaveLength(1);
+			expect(result[0][0]).toEqual('hello.com');
+		});
+
+		it('should expose /.well-known/ic-domains', async () => {
+			const { http_request } = actor;
+
+			const { body } = await http_request({
+				body: [],
+				certificate_version: toNullable(),
+				headers: [],
+				method: 'GET',
+				url: '/.well-known/ic-domains'
+			});
+
+			const decoder = new TextDecoder();
+			expect(decoder.decode(body as ArrayBuffer)).toEqual('hello.com');
 		});
 	});
 
@@ -230,6 +427,81 @@ describe('Satellite', () => {
 			const { memory_size } = actor;
 
 			await expect(memory_size()).rejects.toThrow(CONTROLLER_ERROR_MSG);
+		});
+
+		it('should throw errors on trying to deploy dapp', async () => {
+			const { init_asset_upload } = actor;
+
+			await expect(
+				init_asset_upload({
+					collection: '#dapp',
+					description: toNullable(),
+					encoding_type: [],
+					full_path: '/hello.html',
+					name: 'hello.html',
+					token: toNullable()
+				})
+			).rejects.toThrow('Caller not allowed to upload data.');
+		});
+	});
+
+	describe('hacking', () => {
+		const hacker = Ed25519KeyIdentity.generate();
+
+		beforeEach(() => {
+			actor.setIdentity(controller);
+		});
+
+		it('should throw errors on upload chunk to admin batch', async () => {
+			const { init_asset_upload, upload_asset_chunk } = actor;
+
+			const batch = await init_asset_upload({
+				collection: '#dapp',
+				description: toNullable(),
+				encoding_type: [],
+				full_path: '/hello.html',
+				name: 'hello.html',
+				token: toNullable()
+			});
+
+			actor.setIdentity(hacker);
+
+			await expect(
+				upload_asset_chunk({
+					batch_id: batch.batch_id,
+					content: [],
+					order_id: [0n]
+				})
+			).rejects.toThrow('Bach initializer does not match chunk uploader.');
+		});
+
+		it('should throw errors on upload chunk to admin batch', async () => {
+			const { init_asset_upload, commit_asset_upload, upload_asset_chunk } = actor;
+
+			const batch = await init_asset_upload({
+				collection: '#dapp',
+				description: toNullable(),
+				encoding_type: [],
+				full_path: '/hello.html',
+				name: 'hello.html',
+				token: toNullable()
+			});
+
+			const chunk = await upload_asset_chunk({
+				batch_id: batch.batch_id,
+				content: [],
+				order_id: [0n]
+			});
+
+			actor.setIdentity(hacker);
+
+			await expect(
+				commit_asset_upload({
+					batch_id: batch.batch_id,
+					chunk_ids: [chunk.chunk_id],
+					headers: []
+				})
+			).rejects.toThrow('Cannot commit batch.');
 		});
 	});
 

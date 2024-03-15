a5825b34aacad6cd52c279de26b03b01c76a8ad0
David Dal Busco <david.dalbusco@outlook.com>
2024-03-15 07:36:57 +0100

    feat: test rules (#456)
    * test: collections
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * build: ci tests
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * build: remove
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    ---------
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/tests/satellite.test.ts
+++ src/tests/satellite.test.ts
@@ -1,12 +1,13 @@
-import type { _SERVICE as SatelliteActor } from '$declarations/satellite/satellite.did';
+import type { _SERVICE as SatelliteActor, SetRule } from '$declarations/satellite/satellite.did';
 import { idlFactory as idlFactorSatellite } from '$declarations/satellite/satellite.factory.did';
 import { IDL } from '@dfinity/candid';
 import { Ed25519KeyIdentity } from '@dfinity/identity';
+import { toNullable } from '@dfinity/utils';
 import { PocketIc, type Actor } from '@hadronous/pic';
 import { parse } from '@ltd/j-toml';
 import { readFileSync } from 'node:fs';
 import { join, resolve } from 'node:path';
-import { afterAll, beforeAll, expect } from 'vitest';
+import { afterAll, beforeAll, describe, expect } from 'vitest';
 
 const WASM_PATH = resolve(
 	process.cwd(),
@@ -21,10 +22,10 @@ describe('Satellite', () => {
 	let pic: PocketIc;
 	let actor: Actor<SatelliteActor>;
 
+	const controller = Ed25519KeyIdentity.generate();
+
 	beforeAll(async () => {
 		pic = await PocketIc.create();
-
-		const identity = Ed25519KeyIdentity.generate();
 
 		const arg = IDL.encode(
 			[
@@ -32,31 +33,89 @@ describe('Satellite', () => {
 					controllers: IDL.Vec(IDL.Principal)
 				})
 			],
-			[{ controllers: [identity.getPrincipal()] }]
+			[{ controllers: [controller.getPrincipal()] }]
 		);
 
-		const fixture = await pic.setupCanister<SatelliteActor>({
+		const { actor: c } = await pic.setupCanister<SatelliteActor>({
 			idlFactory: idlFactorSatellite,
 			wasm: WASM_PATH,
 			arg
 		});
 
-		actor = fixture.actor;
+		actor = c;
 	});
 
 	afterAll(async () => {
 		await pic?.tearDown();
 	});
 
-	it('should expose version of the consumer', async () => {
-		const tomlFile = readFileSync(join(process.cwd(), 'src', 'satellite', 'Cargo.toml'));
+	const setRule: SetRule = {
+		memory: toNullable(),
+		updated_at: toNullable(),
+		max_size: toNullable(),
+		read: { Managed: null },
+		mutable_permissions: toNullable(),
+		write: { Managed: null }
+	};
 
-		type Toml = { package: { version: string } } | undefined;
+	describe('admin', () => {
+		beforeAll(() => {
+			actor.setIdentity(controller);
+		});
 
-		const result: Toml = parse(tomlFile.toString()) as unknown as Toml;
+		it('should create a collection', async () => {
+			const { set_rule, list_rules, list_controllers } = actor;
 
-		expect(result?.package?.version).not.toBeNull();
+			await set_rule({ Db: null }, 'test', setRule);
 
-		expect(await actor.version()).toEqual(result?.package?.version);
+			const [[collection, { memory, created_at, updated_at, read, write }], _] = await list_rules({
+				Db: null
+			});
+
+			expect(collection).toEqual('test');
+			expect(memory).toEqual(toNullable({ Heap: null }));
+			expect(read).toEqual({ Managed: null });
+			expect(write).toEqual({ Managed: null });
+			expect(created_at).toBeGreaterThan(0n);
+			expect(updated_at).toBeGreaterThan(0n);
+		});
+	});
+
+	describe('user', () => {
+		const user = Ed25519KeyIdentity.generate();
+
+		beforeAll(() => {
+			actor.setIdentity(user);
+		});
+
+		it('should throw errors on creating collections', async () => {
+			const { set_rule, list_controllers } = actor;
+
+			await expect(set_rule({ Db: null }, 'user-test', setRule)).rejects.toThrow(
+				'Caller is not an admin controller of the satellite.'
+			);
+		});
+
+		it('should throw errors on list collections', async () => {
+			const { list_rules, list_controllers } = actor;
+
+			await expect(list_rules({ Db: null })).rejects.toThrow(
+				'Caller is not an admin controller of the satellite.'
+			);
+		});
+	});
+
+	describe('public', () => {
+		it('should expose version of the consumer', async () => {
+			const tomlFile = readFileSync(join(process.cwd(), 'src', 'satellite', 'Cargo.toml'));
+
+			type Toml = { package: { version: string } } | undefined;
+
+			const result: Toml = parse(tomlFile.toString()) as unknown as Toml;
+
+			expect(result?.package?.version).not.toBeNull();
+
+			expect(await actor.version()).toEqual(result?.package?.version);
+		});
 	});
 });

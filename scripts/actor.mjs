c49eea28fc745fca1079451bca5830c8a598120d
David Dal Busco <david.dalbusco@outlook.com>
2024-03-19 21:04:26 +0100

    feat: query logs
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- scripts/actor.mjs
+++ scripts/actor.mjs
@@ -3,6 +3,7 @@ import pkgPrincipal from '@dfinity/principal';
 import { readFileSync } from 'fs';
 import fetch from 'node-fetch';
 import { idlFactory } from '../src/declarations/console/console.factory.did.mjs';
+import { idlFactory as icIdlFactory } from '../src/declarations/ic/ic.factory.did.mjs';
 import { idlFactory as observatoryIdlFactory } from '../src/declarations/observatory/observatory.factory.did.mjs';
 import { idlFactory as orbiterIdlFactory } from '../src/declarations/orbiter/orbiter.factory.did.mjs';
 import { initIdentity } from './identity.utils.mjs';
@@ -113,5 +114,31 @@ export const orbiterActorLocal = async (canisterId) => {
 	return Actor.createActor(orbiterIdlFactory, {
 		agent,
 		canisterId
+	});
+};
+
+const MANAGEMENT_CANISTER_ID = Principal.fromText('aaaaa-aa');
+
+// Source nns-dapp - dart -> JS bridge
+const transform = (_methodName, args, _callConfig) => {
+	const first = args[0];
+	let effectiveCanisterId = MANAGEMENT_CANISTER_ID;
+	if (first && typeof first === 'object' && first.canister_id) {
+		effectiveCanisterId = Principal.from(first.canister_id);
+	}
+
+	return { effectiveCanisterId };
+};
+
+export const icActorIC = async () => {
+	const agent = icAgent();
+
+	return Actor.createActor(icIdlFactory, {
+		agent,
+		canisterId: MANAGEMENT_CANISTER_ID,
+		config: {
+			callTransform: transform,
+			queryTransform: transform
+		}
 	});
 };

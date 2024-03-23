3c101aab7619c854712141be157b8f978eda2ca3
David Dal Busco <david.dalbusco@outlook.com>
2024-03-23 10:24:04 +0100

    feat: expose Log types
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/libs/satellite/src/lib.rs
+++ src/libs/satellite/src/lib.rs
@@ -49,6 +49,7 @@ pub use crate::logs::loggers::{
     debug, debug_with_data, error, error_with_data, info, info_with_data, log, log_with_data, warn,
     warn_with_data,
 };
+pub use crate::logs::types::logs::{Log, LogLevel};
 pub use crate::storage::store::{
     count_assets_store, delete_asset_store, get_asset_store, get_content_chunks_store,
 };

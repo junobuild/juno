8d1242fd702812ab6ffaa2be21290a0f197293bd
David Dal Busco <david.dalbusco@outlook.com>
2024-03-24 20:41:09 +0100

    refactor: extract popover apply cmp
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

---
+++ src/frontend/src/lib/components/ui/PopoverApply.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import { i18n } from '$lib/stores/i18n.store';
+	import Popover from '$lib/components/ui/Popover.svelte';
+
+	export let ariaLabel: string;
+	export let visible: boolean | undefined;
+
+	let button: HTMLButtonElement | undefined;
+</script>
+
+<button
+	class="icon"
+	aria-label={ariaLabel}
+	type="button"
+	on:click={() => (visible = true)}
+	bind:this={button}><slot name="icon" /></button
+>
+
+<Popover bind:visible anchor={button} direction="rtl">
+	<div class="container">
+		<slot />
+
+		<button class="apply" type="button" on:click|stopPropagation>
+			{$i18n.core.apply}
+		</button>
+	</div>
+</Popover>
+
+<style lang="scss">
+	button.icon {
+		padding: 0;
+	}
+
+	.container {
+		display: flex;
+		flex-direction: column;
+
+		width: 100%;
+
+		padding: var(--padding);
+	}
+
+	.apply {
+		margin: var(--padding-1_5x) 0 var(--padding);
+	}
+</style>

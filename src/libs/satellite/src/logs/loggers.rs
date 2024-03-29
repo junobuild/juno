95f19e8416c2055132ce7a6906e56ab1545056c2
David Dal Busco <david.dalbusco@outlook.com>
2024-03-29 09:20:00 +0100

    docs: logs
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/libs/satellite/src/logs/loggers.rs
+++ src/libs/satellite/src/logs/loggers.rs
@@ -8,42 +8,71 @@ use junobuild_utils::encode_doc_data;
 use rand::Rng;
 use serde::Serialize;
 
+/// Logs a message at the `Info` level.
+///
+/// # Arguments
+///
+/// * `message` - A string slice that holds the message to be logged.
+///
+/// # Returns
+///
+/// A result indicating success (`Ok(())`) or containing an error message (`Err(String)`).
 pub fn log(message: String) -> Result<(), String> {
     set_log::<()>(LogLevel::Info, message, None)
 }
 
+/// Logs a message at the `Info` level with additional serialized data.
+///
+/// # Arguments
+///
+/// * `message` - The message to be logged.
+/// * `data` - A reference to the data to be logged. The data must implement the `Serialize` trait.
+///
+/// # Returns
+///
+/// A result indicating success or containing an error message.
 pub fn log_with_data<T: Serialize>(message: String, data: &T) -> Result<(), String> {
     set_log::<T>(LogLevel::Info, message, Some(data))
 }
 
+/// Logs an informational message.
+///
+/// This function is a convenience wrapper for `log`, setting the log level to `Info`.
 pub fn info(message: String) -> Result<(), String> {
     set_log::<()>(LogLevel::Info, message, None)
 }
 
+/// Logs an informational message with additional serialized data.
 pub fn info_with_data<T: Serialize>(message: String, data: &T) -> Result<(), String> {
     set_log::<T>(LogLevel::Info, message, Some(data))
 }
 
+/// Logs a debug-level message.
 pub fn debug(message: String) -> Result<(), String> {
     set_log::<()>(LogLevel::Debug, message, None)
 }
 
+/// Logs a debug-level message with additional serialized data.
 pub fn debug_with_data<T: Serialize>(message: String, data: &T) -> Result<(), String> {
     set_log::<T>(LogLevel::Debug, message, Some(data))
 }
 
+/// Logs a warning message.
 pub fn warn(message: String) -> Result<(), String> {
     set_log::<()>(LogLevel::Warning, message, None)
 }
 
+/// Logs a warning message with additional serialized data.
 pub fn warn_with_data<T: Serialize>(message: String, data: &T) -> Result<(), String> {
     set_log::<T>(LogLevel::Warning, message, Some(data))
 }
 
+/// Logs an error message.
 pub fn error(message: String) -> Result<(), String> {
     set_log::<()>(LogLevel::Error, message, None)
 }
 
+/// Logs an error message with additional serialized data.
 pub fn error_with_data<T: Serialize>(message: String, data: &T) -> Result<(), String> {
     set_log::<T>(LogLevel::Error, message, Some(data))
 }

9ff744fe0d1b632e8bb1af1f43ee235a93387cb5
David Dal Busco <david.dalbusco@outlook.com>
2024-04-21 09:38:52 +0200

    test: storage and custom domains including fix
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/tests/satellite.spec.ts
+++ src/tests/satellite.spec.ts
@@ -1,18 +1,15 @@
-import type {
-	_SERVICE as SatelliteActor,
-	SetRule,
-	StorageConfig
-} from '$declarations/satellite/satellite.did';
+import type { _SERVICE as SatelliteActor, SetRule } from '$declarations/satellite/satellite.did';
 import { idlFactory as idlFactorSatellite } from '$declarations/satellite/satellite.factory.did';
 import { AnonymousIdentity } from '@dfinity/agent';
 import { Ed25519KeyIdentity } from '@dfinity/identity';
-import { arrayBufferToUint8Array, toNullable } from '@dfinity/utils';
+import { toNullable } from '@dfinity/utils';
 import { PocketIc, type Actor } from '@hadronous/pic';
 import { toArray } from '@junobuild/utils';
 import { parse } from '@ltd/j-toml';
 import { readFileSync } from 'node:fs';
 import { join } from 'node:path';
-import { afterAll, beforeAll, beforeEach, describe, expect } from 'vitest';
+import { afterAll, beforeAll, describe, expect } from 'vitest';
+import { ADMIN_ERROR_MSG, CONTROLLER_ERROR_MSG } from './constants/satellite-tests.constants';
 import { WASM_PATH, satelliteInitArgs } from './utils/satellite-tests.utils';
 
 describe('Satellite', () => {
@@ -140,140 +137,10 @@ describe('Satellite', () => {
 			expect(updatedControllers).toHaveLength(1);
 			expect(updatedControllers[0][0].toText()).toEqual(controller.getPrincipal().toText());
 		});
-
-		it('should set and get config', async () => {
-			const { set_config, get_config } = actor;
-
-			const storage: StorageConfig = {
-				headers: [['*', [['Cache-Control', 'no-cache']]]],
-				iframe: toNullable({ Deny: null }),
-				redirects: [
-					[
-						[
-							'/*',
-							{
-								location: '/hello.html',
-								status_code: 302
-							}
-						]
-					]
-				],
-				rewrites: [['/hello.html', '/hello.html']]
-			};
-
-			await set_config({
-				storage
-			});
-
-			const configs = await get_config();
-
-			expect(configs).toEqual({
-				storage
-			});
-		});
-
-		it('should deploy asset to dapp', async () => {
-			const { http_request, commit_asset_upload, upload_asset_chunk, init_asset_upload } = actor;
-
-			const file = await init_asset_upload({
-				collection: '#dapp',
-				description: toNullable(),
-				encoding_type: [],
-				full_path: '/hello.html',
-				name: 'hello.html',
-				token: toNullable()
-			});
-
-			const HTML = '<html><body>Hello</body></html>';
-
-			const blob = new Blob([HTML], {
-				type: 'text/plain; charset=utf-8'
-			});
-
-			const chunk = await upload_asset_chunk({
-				batch_id: file.batch_id,
-				content: arrayBufferToUint8Array(await blob.arrayBuffer()),
-				order_id: [0n]
-			});
-
-			await commit_asset_upload({
-				batch_id: file.batch_id,
-				chunk_ids: [chunk.chunk_id],
-				headers: []
-			});
-
-			const { headers, body } = await http_request({
-				body: [],
-				certificate_version: toNullable(),
-				headers: [],
-				method: 'GET',
-				url: '/hello.html'
-			});
-
-			const rest = headers.filter(([header, _]) => header !== 'IC-Certificate');
-
-			expect(rest).toEqual([
-				['accept-ranges', 'bytes'],
-				['etag', '"03ee66f1452916b4f91a504c1e9babfa201b6d64c26a82b2cf03c3ed49d91585"'],
-				['X-Content-Type-Options', 'nosniff'],
-				['Strict-Transport-Security', 'max-age=31536000 ; includeSubDomains'],
-				['Referrer-Policy', 'same-origin'],
-				['X-Frame-Options', 'DENY'],
-				['Cache-Control', 'no-cache']
-			]);
-
-			const certificate = headers.find(([header, _]) => header === 'IC-Certificate');
-
-			expect(certificate).not.toBeUndefined();
-
-			const [_, value] = certificate as [string, string];
-			expect(value.substring(value.indexOf('tree=:'))).toEqual(
-				'tree=:2dn3gwGDAktodHRwX2Fzc2V0c4MBggRYIDmN5doHXoiKCtNGBOZIdmQ+WGqYjcmdRB1MPuJBK2oXgwJLL2hlbGxvLmh0bWyCA1ggA+5m8UUpFrT5GlBMHpur+iAbbWTCaoKyzwPD7UnZFYWCBFggu+vgJ40baoH6QaigqUGb3VrUkopUK4LJuugRxX7g6qc=:'
-			);
-
-			const decoder = new TextDecoder();
-			expect(decoder.decode(body as ArrayBuffer)).toEqual(HTML);
-		});
-
-		it('should add and remove custom domain', async () => {
-			const { list_custom_domains, del_custom_domain, set_custom_domain } = actor;
-
-			await set_custom_domain('hello.com', toNullable('12345'));
-
-			const domains = await list_custom_domains();
-			expect(domains[0][0]).toEqual('hello.com');
-			expect(domains[0][1].bn_id).toEqual(toNullable('12345'));
-
-			await set_custom_domain('world.com', toNullable());
-			expect(await list_custom_domains()).toHaveLength(2);
-
-			await del_custom_domain('world.com');
-			const result = await list_custom_domains();
-			expect(result).toHaveLength(1);
-			expect(result[0][0]).toEqual('hello.com');
-		});
-
-		it('should expose /.well-known/ic-domains', async () => {
-			const { http_request } = actor;
-
-			const { body } = await http_request({
-				body: [],
-				certificate_version: toNullable(),
-				headers: [],
-				method: 'GET',
-				url: '/.well-known/ic-domains'
-			});
-
-			const decoder = new TextDecoder();
-			expect(decoder.decode(body as ArrayBuffer)).toEqual('hello.com');
-		});
 	});
 
 	describe('admin guard', () => {
 		const user = Ed25519KeyIdentity.generate();
-
-		const ADMIN_ERROR_MSG = 'Caller is not an admin controller of the satellite.';
-		const CONTROLLER_ERROR_MSG = 'Caller is not a controller of the satellite.';
 
 		beforeAll(() => {
 			actor.setIdentity(user);
@@ -330,45 +197,6 @@ describe('Satellite', () => {
 					controllers: [controller.getPrincipal()]
 				})
 			).rejects.toThrow(ADMIN_ERROR_MSG);
-		});
-
-		it('should throw errors on setting config', async () => {
-			const { set_config } = actor;
-
-			await expect(
-				set_config({
-					storage: {
-						headers: [],
-						iframe: toNullable(),
-						redirects: toNullable(),
-						rewrites: []
-					}
-				})
-			).rejects.toThrow(ADMIN_ERROR_MSG);
-		});
-
-		it('should throw errors on getting config', async () => {
-			const { get_config } = actor;
-
-			await expect(get_config()).rejects.toThrow(ADMIN_ERROR_MSG);
-		});
-
-		it('should throw errors on setting custom domain', async () => {
-			const { set_custom_domain } = actor;
-
-			await expect(set_custom_domain('hello.com', toNullable())).rejects.toThrow(ADMIN_ERROR_MSG);
-		});
-
-		it('should throw errors on listing custom domains', async () => {
-			const { list_custom_domains } = actor;
-
-			await expect(list_custom_domains()).rejects.toThrow(ADMIN_ERROR_MSG);
-		});
-
-		it('should throw errors on deleting custom domains', async () => {
-			const { del_custom_domain } = actor;
-
-			await expect(del_custom_domain('hello.com')).rejects.toThrow(ADMIN_ERROR_MSG);
 		});
 
 		it('should throw errors on deleting docs', async () => {
@@ -477,66 +305,6 @@ describe('Satellite', () => {
 					updated_at: toNullable()
 				})
 			).rejects.toThrow('Cannot write.');
-		});
-	});
-
-	describe('hacking', () => {
-		const hacker = Ed25519KeyIdentity.generate();
-
-		beforeEach(() => {
-			actor.setIdentity(controller);
-		});
-
-		it('should throw errors on upload chunk to admin batch', async () => {
-			const { init_asset_upload, upload_asset_chunk } = actor;
-
-			const batch = await init_asset_upload({
-				collection: '#dapp',
-				description: toNullable(),
-				encoding_type: [],
-				full_path: '/hello.html',
-				name: 'hello.html',
-				token: toNullable()
-			});
-
-			actor.setIdentity(hacker);
-
-			await expect(
-				upload_asset_chunk({
-					batch_id: batch.batch_id,
-					content: [],
-					order_id: [0n]
-				})
-			).rejects.toThrow('Bach initializer does not match chunk uploader.');
-		});
-
-		it('should throw errors on trying to commit admin batch', async () => {
-			const { init_asset_upload, commit_asset_upload, upload_asset_chunk } = actor;
-
-			const batch = await init_asset_upload({
-				collection: '#dapp',
-				description: toNullable(),
-				encoding_type: [],
-				full_path: '/hello.html',
-				name: 'hello.html',
-				token: toNullable()
-			});
-
-			const chunk = await upload_asset_chunk({
-				batch_id: batch.batch_id,
-				content: [],
-				order_id: [0n]
-			});
-
-			actor.setIdentity(hacker);
-
-			await expect(
-				commit_asset_upload({
-					batch_id: batch.batch_id,
-					chunk_ids: [chunk.chunk_id],
-					headers: []
-				})
-			).rejects.toThrow('Cannot commit batch.');
 		});
 	});
 

24c034f9553946750d22ec30c15231bfb507c1f4
David Dal Busco <david.dalbusco@outlook.com>
2024-04-24 18:20:22 +0200

    feat: custom domain into well-known
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/libs/satellite/src/storage/well_known.rs
+++ src/libs/satellite/src/storage/well_known.rs
@@ -1,33 +1,37 @@
 use crate::rules::constants::{ASSET_COLLECTION_KEY, DEFAULT_ASSETS_COLLECTIONS};
-use crate::storage::constants::{ASSET_ENCODING_NO_COMPRESSION, WELL_KNOWN_II_ALTERNATIVE_ORIGINS};
+use crate::storage::constants::{ASSET_ENCODING_NO_COMPRESSION, BN_WELL_KNOWN_CUSTOM_DOMAINS, WELL_KNOWN_II_ALTERNATIVE_ORIGINS};
 use crate::storage::http::types::HeaderField;
-use crate::storage::runtime::{delete_certified_asset, update_certified_asset};
-use crate::storage::state::{delete_asset, get_asset, get_rule, insert_asset};
+use crate::storage::runtime::{delete_certified_asset, update_certified_asset as update_runtime_certified_asset};
+use crate::storage::state::{delete_asset, get_asset as get_state_asset, get_domains, get_rule, insert_asset as insert_state_asset};
 use crate::storage::store::get_config_store;
 use crate::storage::types::store::{Asset, AssetEncoding, AssetKey};
 use ic_cdk::api::time;
 use ic_cdk::id;
 use std::collections::HashMap;
+use crate::storage::custom_domains::map_custom_domains_asset;
+use crate::types::core::DomainName;
 
 pub fn update_alternative_origins_asset(alternative_origins: &String) -> Result<(), String> {
     let full_path = WELL_KNOWN_II_ALTERNATIVE_ORIGINS.to_string();
 
-    let collection = DEFAULT_ASSETS_COLLECTIONS[0].0.to_string();
+    update_asset(&full_path, alternative_origins, &map_alternative_origins_asset)
+}
 
-    // #app collection rule
-    let rule = get_rule(&collection)?;
+pub fn update_custom_domains_asset() -> Result<(), String> {
+    let custom_domains = get_custom_domains_as_content();
 
-    let existing_asset = get_asset(&collection, &full_path, &rule);
+    let full_path = BN_WELL_KNOWN_CUSTOM_DOMAINS.to_string();
 
-    let asset = map_alternative_origins_asset(alternative_origins, existing_asset);
+    update_asset(&full_path, &custom_domains, &map_custom_domains_asset)
+}
 
-    insert_asset(&collection, &full_path, &asset, &rule);
+fn get_custom_domains_as_content() -> String {
+    let custom_domains = get_domains();
 
-    let config = get_config_store();
-
-    update_certified_asset(&asset, &config);
-
-    Ok(())
+    custom_domains
+        .into_keys()
+        .collect::<Vec<DomainName>>()
+        .join("\n")
 }
 
 pub fn delete_alternative_origins_asset() -> Result<(), String> {
@@ -86,4 +90,23 @@ fn map_alternative_origins_asset(
     );
 
     asset
+}
+
+fn update_asset(full_path: &String, content: &String, f: &dyn Fn(&String, Option<Asset>) -> Asset) -> Result<(), String> {
+    let collection = DEFAULT_ASSETS_COLLECTIONS[0].0.to_string();
+
+    // #app collection rule
+    let rule = get_rule(&collection)?;
+
+    let existing_asset = get_state_asset(&collection, &full_path, &rule);
+
+    let asset = f(content, existing_asset);
+
+    insert_state_asset(&collection, &full_path, &asset, &rule);
+
+    let config = get_config_store();
+
+    update_runtime_certified_asset(&asset, &config);
+
+    Ok(())
 }

d888bd1c12260fafd268bcc29953e2ef9cbec854
David Dal Busco <david.dalbusco@outlook.com>
2024-04-24 17:33:37 +0200

    chore: derive default
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/libs/satellite/src/auth/impl.rs
+++
@@ -1,9 +0,0 @@
-use crate::auth::types::state::AuthenticationConfig;
-
-impl Default for AuthenticationConfig {
-    fn default() -> Self {
-        AuthenticationConfig {
-            internet_identity: None,
-        }
-    }
-}

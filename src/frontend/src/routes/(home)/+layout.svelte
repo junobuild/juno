28571e42aa302cfd72827e80aff2c4511a8a0e59
David Dal Busco <david.dalbusco@outlook.com>
2024-04-25 17:41:52 +0200

    feat: remove illustration
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/routes/(home)/+layout.svelte
+++ src/frontend/src/routes/(home)/+layout.svelte
@@ -3,14 +3,11 @@
 	import Navbar from '$lib/components/core/Navbar.svelte';
 	import { onMount } from 'svelte';
 	import { layoutTitle } from '$lib/stores/layout.store';
-	import Illustration from '$lib/components/presentation/Illustration.svelte';
 	import { authSignedInStore } from '$lib/stores/auth.store';
 	import Footer from '$lib/components/ui/Footer.svelte';
 
 	onMount(() => layoutTitle.set(''));
 </script>
-
-<Illustration sticky={$authSignedInStore} />
 
 <Layout centered title={false}>
 	<Navbar

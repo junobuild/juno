3e02cc8c5eeb3487116f1fc9d0a36fc1861e2d40
David Dal Busco <david.dalbusco@outlook.com>
2024-04-26 17:14:13 +0200

    feat: extract cmp
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

---
+++ src/frontend/src/lib/components/hosting/AddCustomDomainDns.svelte
@@ -0,0 +1,97 @@
+<script lang="ts">
+	import { i18n } from '$lib/stores/i18n.store.js';
+	import { i18nFormat } from '$lib/utils/i18n.utils.js';
+	import { nonNullish } from '@dfinity/utils';
+	import Copy from '$lib/components/ui/Copy.svelte';
+	import type { CustomDomainDns } from '$lib/types/custom-domain';
+	import { createEventDispatcher } from 'svelte';
+
+	export let domainNameInput: string;
+	export let dns: CustomDomainDns | undefined;
+	export let edit = false;
+
+	const dispatch = createEventDispatcher();
+</script>
+
+<h2>{$i18n.hosting.configure}</h2>
+
+<p>
+	{@html i18nFormat($i18n.hosting.add_records, [
+		{
+			placeholder: '{0}',
+			value: domainNameInput ?? ''
+		}
+	])}
+</p>
+
+<section>
+	<p class="title">{$i18n.hosting.type}</p>
+	<p class="title">{$i18n.hosting.host}</p>
+	<p class="title value">{$i18n.hosting.value}</p>
+
+	{#each dns?.entries ?? [] as { type, host, value }}
+		<p class="td">{type}</p>
+		<p class="td">
+			{#if nonNullish(host)}
+				<span>{host}</span>
+				<Copy value={host} />
+			{/if}
+		</p>
+		<p class="value td">
+			<span>{value}</span>
+			<Copy {value} />
+		</p>
+	{/each}
+</section>
+
+<p class="notes">{@html $i18n.hosting.dns_notes}</p>
+
+<div class="toolbar">
+	{#if !edit}
+		<button on:click={() => dispatch('junoBack')}>{$i18n.core.back}</button>
+		<button on:click={() => dispatch('junoSubmit')}>{$i18n.core.ready}</button>
+	{:else}
+		<button on:click={() => dispatch('junoClose')}>{$i18n.core.close}</button>
+		<button on:click={() => dispatch('junoSubmit')}>{$i18n.core.submit}</button>
+	{/if}
+</div>
+
+<style lang="scss">
+	@use '../../styles/mixins/collections';
+	@use '../../styles/mixins/shadow';
+	@use '../../styles/mixins/text';
+
+	section {
+		display: grid;
+		grid-template-columns: repeat(4, auto);
+
+		@include shadow.shadow;
+
+		p {
+			display: inline-flex;
+			align-items: center;
+			gap: var(--padding);
+			margin: 0;
+		}
+
+		span {
+			@include text.truncate;
+		}
+	}
+
+	.title {
+		@include collections.title-style;
+	}
+
+	.value {
+		grid-column: span 2;
+	}
+
+	.toolbar {
+		padding: var(--padding) 0 0 0;
+	}
+
+	.notes {
+		margin: var(--padding-4x) 0 var(--padding-2x);
+	}
+</style>
--- src/frontend/src/lib/components/modals/CustomDomainModal.svelte
+++ src/frontend/src/lib/components/modals/CustomDomainModal.svelte
@@ -2,9 +2,8 @@
 	import type { JunoModalCustomDomainDetail, JunoModalDetail } from '$lib/types/modal';
 	import type { Satellite } from '$declarations/mission_control/mission_control.did';
 	import Modal from '$lib/components/ui/Modal.svelte';
-	import { isNullish, nonNullish, notEmptyString } from '@dfinity/utils';
+	import { isNullish } from '@dfinity/utils';
 	import { toasts } from '$lib/stores/toasts.store';
-	import Copy from '$lib/components/ui/Copy.svelte';
 	import type { CustomDomainDns } from '$lib/types/custom-domain';
 	import { toCustomDomainDns } from '$lib/utils/custom-domain.utils';
 	import SpinnerModal from '$lib/components/ui/SpinnerModal.svelte';
@@ -12,12 +11,12 @@
 	import { createEventDispatcher, onMount } from 'svelte';
 	import { emit } from '$lib/utils/events.utils';
 	import { i18n } from '$lib/stores/i18n.store';
-	import { i18nFormat } from '$lib/utils/i18n.utils';
 	import { setCustomDomain } from '$lib/services/hosting.services';
 	import { wizardBusy } from '$lib/stores/busy.store';
 	import AddCustomDomainForm from '$lib/components/hosting/AddCustomDomainForm.svelte';
 	import AddCustomDomainAuth from '$lib/components/hosting/AddCustomDomainAuth.svelte';
 	import type { AuthenticationConfig } from '$declarations/satellite/satellite.did';
+	import AddCustomDomainDns from '$lib/components/hosting/AddCustomDomainDns.svelte';
 
 	export let detail: JunoModalDetail;
 
@@ -47,7 +46,7 @@
 	const onAuth = ({ detail }: CustomEvent<AuthenticationConfig | undefined>) => {
 		editConfig = detail;
 
-		steps = "dns";
+		steps = 'dns';
 	};
 
 	const setupCustomDomain = async () => {
@@ -95,48 +94,14 @@
 			<button on:click={close}>{$i18n.core.close}</button>
 		</div>
 	{:else if steps === 'dns'}
-		<h2>{$i18n.hosting.configure}</h2>
-
-		<p>
-			{@html i18nFormat($i18n.hosting.add_records, [
-				{
-					placeholder: '{0}',
-					value: domainNameInput ?? ''
-				}
-			])}
-		</p>
-
-		<section>
-			<p class="title">{$i18n.hosting.type}</p>
-			<p class="title">{$i18n.hosting.host}</p>
-			<p class="title value">{$i18n.hosting.value}</p>
-
-			{#each dns?.entries ?? [] as { type, host, value }}
-				<p class="td">{type}</p>
-				<p class="td">
-					{#if nonNullish(host)}
-						<span>{host}</span>
-						<Copy value={host} />
-					{/if}
-				</p>
-				<p class="value td">
-					<span>{value}</span>
-					<Copy {value} />
-				</p>
-			{/each}
-		</section>
-
-		<p class="notes">{@html $i18n.hosting.dns_notes}</p>
-
-		<div class="toolbar">
-			{#if !edit}
-				<button on:click={() => (steps = 'init')}>{$i18n.core.back}</button>
-				<button on:click={setupCustomDomain}>{$i18n.core.ready}</button>
-			{:else}
-				<button on:click={() => dispatch('junoClose')}>{$i18n.core.close}</button>
-				<button on:click={setupCustomDomain}>{$i18n.core.submit}</button>
-			{/if}
-		</div>
+		<AddCustomDomainDns
+			{domainNameInput}
+			{dns}
+			{edit}
+			on:junoSubmit={async () => await setupCustomDomain()}
+			on:junoBack={() => (steps = 'init')}
+			on:junoClose
+		/>
 	{:else if steps === 'in_progress'}
 		<SpinnerModal>
 			<p>{$i18n.hosting.config_in_progress}</p>
@@ -154,46 +119,9 @@
 </Modal>
 
 <style lang="scss">
-	@use '../../styles/mixins/collections';
-	@use '../../styles/mixins/shadow';
-	@use '../../styles/mixins/text';
 	@use '../../styles/mixins/overlay';
 
 	.msg {
 		@include overlay.message;
-	}
-
-	section {
-		display: grid;
-		grid-template-columns: repeat(4, auto);
-
-		@include shadow.shadow;
-
-		p {
-			display: inline-flex;
-			align-items: center;
-			gap: var(--padding);
-			margin: 0;
-		}
-
-		span {
-			@include text.truncate;
-		}
-	}
-
-	.title {
-		@include collections.title-style;
-	}
-
-	.value {
-		grid-column: span 2;
-	}
-
-	.toolbar {
-		padding: var(--padding) 0 0 0;
-	}
-
-	.notes {
-		margin: var(--padding-4x) 0 var(--padding-2x);
 	}
 </style>

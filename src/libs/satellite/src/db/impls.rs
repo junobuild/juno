71134c22a610f61883818c2670b3141d83650320
David Dal Busco <david.dalbusco@outlook.com>
2024-04-30 14:16:32 +0200

    feat: replace updated_at by version for datastore
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/libs/satellite/src/db/impls.rs
+++ src/libs/satellite/src/db/impls.rs
@@ -1,8 +1,12 @@
 use crate::db::types::state::{Doc, StableKey};
 use crate::types::core::Compare;
+use crate::SetDoc;
+use candid::Principal;
+use ic_cdk::api::time;
 use ic_stable_structures::storable::Bound;
 use ic_stable_structures::Storable;
 use junobuild_shared::serializers::{deserialize_from_bytes, serialize_to_bytes};
+use junobuild_shared::types::state::{Timestamp, UserId, Version};
 use std::borrow::Cow;
 use std::cmp::Ordering;
 
@@ -38,4 +42,36 @@ impl Storable for StableKey {
     }
 
     const BOUND: Bound = Bound::Unbounded;
+}
+
+impl Doc {
+    pub fn prepare(caller: Principal, current_doc: &Option<Doc>, user_doc: SetDoc) -> Self {
+        let now = time();
+
+        let created_at: Timestamp = match current_doc {
+            None => now,
+            Some(current_doc) => current_doc.created_at,
+        };
+
+        let version: Version = match current_doc {
+            None => 1,
+            Some(current_doc) => current_doc.version.unwrap_or_default() + 1,
+        };
+
+        let owner: UserId = match current_doc {
+            None => caller,
+            Some(current_doc) => current_doc.owner,
+        };
+
+        let updated_at: Timestamp = now;
+
+        Doc {
+            owner,
+            data: user_doc.data,
+            description: user_doc.description,
+            created_at,
+            updated_at,
+            version: Some(version),
+        }
+    }
 }

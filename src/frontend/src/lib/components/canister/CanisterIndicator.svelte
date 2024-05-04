99b53e7d2c90aee85d62f2540b963c54cb4516a6
David Dal Busco <david.dalbusco@outlook.com>
2024-05-04 09:00:30 +0200

    feat: canister status indicator
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

---
+++ src/frontend/src/lib/components/canister/CanisterIndicator.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import type { CanisterData, CanisterStatus } from '$lib/types/canister';
+	import { isNullish } from '@dfinity/utils';
+	import { fade } from 'svelte/transition';
+
+	export let data: CanisterData | undefined = undefined;
+
+	let warning: boolean;
+	$: warning = data?.warning?.cycles === true ?? false;
+
+	let status: CanisterStatus | undefined;
+	$: status = data?.canister?.status;
+</script>
+
+{#if isNullish(status)}
+	<div in:fade />
+{:else if warning || status === 'stopping'}
+	<div class="warning" in:fade />
+{:else if status === 'stopped'}
+	<div class="stopped" in:fade />
+{:else}
+	<div class="running" in:fade />
+{/if}
+
+<style lang="scss">
+	div {
+		width: var(--padding-2x);
+		height: var(--padding-2x);
+
+		border-radius: 50%;
+	}
+
+	.warning {
+		background: var(--color-warning);
+	}
+
+	.running {
+		background: var(--color-success);
+	}
+
+	.stopped {
+		background: var(--color-error);
+	}
+</style>

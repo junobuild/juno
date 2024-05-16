d6128fd7232b9f33b52d7cbfa9f03f2b383ca2da
David Dal Busco <david.dalbusco@outlook.com>
2024-05-16 15:19:23 +0200

    chore: remove cmc declarations
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/lib/api/cmc.api.ts
+++ src/frontend/src/lib/api/cmc.api.ts
@@ -1,13 +1,19 @@
-import type { _SERVICE as CMCActor } from '$declarations/cmc/cmc.did';
-import { getCMCActor } from '$lib/utils/actor.ic.utils';
+import { CMC_CANISTER_ID } from '$lib/constants/constants';
+import { getAgent } from '$lib/utils/agent.utils';
+import { AnonymousIdentity } from '@dfinity/agent';
+import { CMCCanister } from '@dfinity/cmc';
 
 const NUMBER_XDR_PER_ONE_ICP = 10_000;
 
 export const icpXdrConversionRate = async (): Promise<bigint> => {
-	const actor: CMCActor = await getCMCActor();
+	const agent = await getAgent({ identity: new AnonymousIdentity() });
 
-	const { data } = await actor.get_icp_xdr_conversion_rate();
-	const { xdr_permyriad_per_icp } = data;
+	const { getIcpToCyclesConversionRate } = CMCCanister.create({
+		agent,
+		canisterId: CMC_CANISTER_ID
+	});
+
+	const xdr_permyriad_per_icp = await getIcpToCyclesConversionRate();
 
 	const CYCLES_PER_XDR = BigInt(1_000_000_000_000);
 

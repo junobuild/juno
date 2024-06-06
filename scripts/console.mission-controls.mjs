02aec63ab8c20a146488beebb0230a4e47f26671
David Dal Busco <david.dalbusco@outlook.com>
2024-06-06 16:30:48 +0200

    feat: save mission controls locally in JSON file
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

---
+++ scripts/console.mission-controls.mjs
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+import { jsonReplacer } from '@dfinity/utils';
+import { writeFile } from 'fs/promises';
+import { join } from 'node:path';
+import { consoleActorIC, consoleActorLocal } from './actor.mjs';
+
+/**
+ * A temporary script used to backup the mission controls of the console as we aim to migrate those from heap to stable.
+ * @param mainnet
+ * @returns {Promise<void>}
+ */
+const savePayments = async (mainnet) => {
+	const { list_user_mission_control_centers } = await (mainnet ? consoleActorIC() : consoleActorLocal());
+
+	const users = await list_user_mission_control_centers();
+
+	await writeFile(
+		join(process.cwd(), 'console.mission-controls.json'),
+		JSON.stringify(users, jsonReplacer, 2)
+	);
+
+	console.log(`👩‍🚀🧑‍🚀 ${users.length} mission controls saved locally successfully.`);
+};
+
+const mainnet = process.argv.find((arg) => arg.indexOf(`--mainnet`) > -1) !== undefined;
+
+await savePayments(mainnet);

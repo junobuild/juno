ae3a5c7ff6087aab357d52881b260b4d0c03a887
David Dal Busco <david.dalbusco@outlook.com>
2024-06-15 14:59:53 +0200

    feat: upload assets
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- scripts/console.deploy.assets.mjs
+++ scripts/console.deploy.assets.mjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { uint8ArrayToHexString } from '@dfinity/utils';
+import { toNullable, uint8ArrayToHexString } from '@dfinity/utils';
 import {
 	deploy as cliDeploy,
 	nextArg,
@@ -31,11 +31,76 @@ const readJunoConfig = async (env) => {
 	});
 };
 
-const { init_assets_upload_group, propose_assets_upload_group } = await consoleActorLocal();
+const {
+	init_assets_upload_group,
+	propose_assets_upload_group,
+	init_asset_upload,
+	commit_asset_upload,
+	upload_asset_chunk
+} = await consoleActorLocal();
 
-const uploadFile = async (file) => {
-	// TODO: upload
+const batchGroupId = await init_assets_upload_group();
+
+const uploadFile = async ({ collection, encoding, filename, fullPath, headers, data }) => {
+	const { batch_id: batchId } = await init_asset_upload({
+		collection,
+		full_path: fullPath,
+		name: filename,
+		token: toNullable(),
+		encoding_type: toNullable(encoding),
+		description: toNullable()
+	}, batchGroupId);
+
+	const chunkSize = 1900000;
+
+	const uploadChunks = [];
+
+	let orderId = 0n;
+	for (let start = 0; start < data.size; start += chunkSize) {
+		const chunk = data.slice(start, start + chunkSize);
+
+		uploadChunks.push({
+			batchId,
+			chunk,
+			orderId
+		});
+
+		orderId++;
+	}
+
+	let chunkIds = [];
+	for await (const results of batchUploadChunks({ uploadChunks })) {
+		chunkIds = [...chunkIds, ...results];
+	}
+
+	const contentType =
+		headers.find(([type, _]) => type.toLowerCase() === 'content-type') === undefined &&
+		data.type !== undefined &&
+		data.type !== ''
+			? [['Content-Type', data.type]]
+			: undefined;
+
+	await commit_asset_upload({
+		batch_id: batchId,
+		chunk_ids: chunkIds.map(({ chunk_id }) => chunk_id),
+		headers: [...headers, ...(contentType ? contentType : [])]
+	});
 };
+
+async function* batchUploadChunks({ uploadChunks, limit = 12 }) {
+	for (let i = 0; i < uploadChunks.length; i = i + limit) {
+		const batch = uploadChunks.slice(i, i + limit);
+		const result = await Promise.all(batch.map((params) => uploadChunk(params)));
+		yield result;
+	}
+}
+
+const uploadChunk = async ({ batchId, chunk, actor, orderId }) =>
+	upload_asset_chunk({
+		batch_id: batchId,
+		content: new Uint8Array(await chunk.arrayBuffer()),
+		order_id: toNullable(orderId)
+	});
 
 const deploy = async () => {
 	const config = await readJunoConfig(env);
@@ -49,8 +114,6 @@ const deploy = async () => {
 		uploadFile
 	});
 };
-
-const batchGroupId = await init_assets_upload_group();
 
 await deploy();
 

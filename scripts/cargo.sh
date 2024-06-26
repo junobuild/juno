a502d54049920da72c2f67286148a40fc3be4fe0
David Dal Busco <david.dalbusco@outlook.com>
2024-06-26 19:04:42 +0200

    feat: candid and ic-wasm option to cargo local build script
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- scripts/cargo.sh
+++ scripts/cargo.sh
@@ -5,25 +5,64 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
+CARGO_HOME=${CARGO_HOME:-$HOME/.cargo}
+
 MODULE=$1
 WASM_MODULE="${MODULE}.wasm"
 
-cargo build --target wasm32-unknown-unknown -p $MODULE  --release --locked
+TARGET="wasm32-unknown-unknown"
+RUSTFLAGS="--remap-path-prefix $CARGO_HOME=/cargo -C link-args=-zstack-size=3000000"
 
-# Metadata for Docker image
+# Build module WASM
+RUSTFLAGS="$RUSTFLAGS" cargo build --target "$TARGET" -p "$MODULE" --release --locked
+
+# Generate metadata for Docker image
 VERSION=$(cargo metadata --format-version=1 --no-deps | jq -r '.packages[] | select(.name == "'"$MODULE"'") | .version')
 node ./scripts/cargo.metadata.mjs "$MODULE" "$VERSION"
 
-# TODO: did
+RELEASE_DIR="./target/${TARGET}/release"
 
-# TODO: ic-wasm
+# Clean and create temporary and output folder
+WASM_DIR="./target/wasm"
+DID_DIR="./target/did"
+DEPLOY_DIR="./target/deploy"
 
-mkdir -p ./target/deploy
+rm -rf "${WASM_DIR}"
+mkdir -p "${WASM_DIR}"
 
-gzip -c "./target/wasm32-unknown-unknown/release/${WASM_MODULE}" > "./target/deploy/${WASM_MODULE}.tmp.gz"
+rm -rf "${DID_DIR}"
+mkdir -p "${DID_DIR}"
 
-mv "./target/deploy/${WASM_MODULE}.tmp.gz" "./target/deploy/${WASM_MODULE}.gz"
+mkdir -p "${DEPLOY_DIR}"
+
+# Generate did
+candid-extractor "${RELEASE_DIR}/${WASM_MODULE}" > "${DID_DIR}/${WASM_MODULE}.did"
+
+# Optimize WASM and set metadata
+ic-wasm \
+    "${RELEASE_DIR}/${WASM_MODULE}" \
+    -o "${WASM_DIR}/${WASM_MODULE}" \
+    shrink
+
+# adds the content of did to the `icp:public candid:service` custom section of the public metadata in the wasm
+ic-wasm "${WASM_DIR}/${WASM_MODULE}" -o "${WASM_DIR}/${WASM_MODULE}" metadata candid:service -f "${DID_DIR}/${WASM_MODULE}.did" -v public
+
+if [ "${WASM_MODULE}" == "satellite" ]
+then
+  # add the type of build "stock" to the satellite. This way, we can identify whether it's the standard canister ("stock") or a custom build ("extended") of the developer.
+  ic-wasm "${WASM_DIR}/${WASM_MODULE}" -o "${WASM_DIR}/${WASM_MODULE}" metadata juno:build -d "stock" -v public
+fi
+
+if [ "${MODULE}" == "satellite" ] || [ "${MODULE}" == "console" ];
+then
+  # indicate support for certificate version 1 and 2 in the canister metadata
+  ic-wasm "${WASM_DIR}/${WASM_MODULE}" -o "${WASM_DIR}/${WASM_MODULE}" metadata supported_certificate_versions -d "1,2" -v public
+fi
+
+gzip -c "${WASM_DIR}/${WASM_MODULE}" > "${DEPLOY_DIR}/${WASM_MODULE}.tmp.gz"
+
+mv "${DEPLOY_DIR}/${WASM_MODULE}.tmp.gz" "${DEPLOY_DIR}/${WASM_MODULE}.gz"
 
 echo ""
-echo "👉 ./target/deploy/${WASM_MODULE}.gz"
+echo "👉 ${DEPLOY_DIR}/${WASM_MODULE}.gz"
 echo ""

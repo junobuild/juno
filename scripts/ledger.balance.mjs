b7b83bc7ce546403af617a3b7487708afb3e51e4
David Dal Busco <david.dalbusco@outlook.com>
2024-06-26 20:43:24 +0200

    feat: deprecate reading identity for scripts from dfx files
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- scripts/ledger.balance.mjs
+++ scripts/ledger.balance.mjs
@@ -1,7 +1,7 @@
 import { IcrcLedgerCanister } from '@dfinity/ledger-icrc';
 import { icAgent, localAgent } from './actor.mjs';
+import { getIdentity } from './console.config.utils.mjs';
 import { CONSOLE_ID, LEDGER_ID } from './constants.mjs';
-import { initIdentity } from './identity.utils.mjs';
 import { accountIdentifier } from './ledger.utils.mjs';
 
 const getBalance = async (mainnet, console_) => {
@@ -12,7 +12,7 @@ const getBalance = async (mainnet, console_) => {
 		canisterId: LEDGER_ID
 	});
 
-	const owner = console_ ? CONSOLE_ID : initIdentity(mainnet).getPrincipal();
+	const owner = console_ ? CONSOLE_ID : getIdentity(mainnet).getPrincipal();
 
 	const e8sBalance = await balance({
 		owner,
@@ -24,7 +24,7 @@ const getBalance = async (mainnet, console_) => {
 
 	console.log(formatE8sICP(e8sBalance), '|', e8sBalance);
 
-	const identifier = accountIdentifier(mainnet, owner);
+	const identifier = await accountIdentifier(mainnet, owner);
 
 	console.log(identifier.toHex());
 };

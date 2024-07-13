f919ab28d2be98c093faf09bd061a323e8dada48
David Dal Busco <david.dalbusco@outlook.com>
2024-07-13 11:00:45 +0200

    feat: utils to decode doc data to string
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/libs/utils/src/lib.rs
+++ src/libs/utils/src/lib.rs
@@ -3,7 +3,7 @@
 mod serializers;
 
 use serde::{Deserialize, Serialize};
-use serde_json::{from_slice, to_vec};
+use serde_json::{from_slice, to_string, to_vec};
 
 pub use crate::serializers::types::{DocDataBigInt, DocDataPrincipal};
 
@@ -83,4 +83,45 @@ pub fn decode_doc_data<T: for<'a> Deserialize<'a>>(data: &[u8]) -> Result<T, Str
 /// ```
 pub fn encode_doc_data<T: Serialize>(data: &T) -> Result<Vec<u8>, String> {
     to_vec(data).map_err(|e| e.to_string())
+}
+
+/// Encodes a Rust struct into a JSON string, designed for use with Juno hooks.
+///
+/// This function facilitates the serialization
+/// of a Rust data structure representing a document data into a JSON string.
+/// It leverages Serde's serialization capabilities,
+/// ensuring that any Rust type implementing the `Serialize` trait can be efficiently converted
+/// into a format compatible with Juno's document requirements.
+///
+/// # Parameters
+/// - `data`: A reference to the Rust data structure to be serialized.
+///
+/// # Returns
+/// - `Ok(String)`: A JSON string containing the serialized data.
+/// - `Err(String)`: An error string if serialization fails.
+///
+/// # Examples
+/// In a Juno hook, you might want to modify and then store updated document data:
+/// ```rust
+/// #[derive(Serialize, Deserialize)]
+/// struct Person {
+///     name: String,
+///     age: u32,
+/// }
+///
+/// #[on_set_doc(collections = ["people"])]
+/// async fn handle_set_person_doc(context: OnSetDocContext) -> Result<(), String> {
+///     let mut person: Person = decode_doc_data(&context.data.data.after.data)?;
+///     person.age += 1; // Increment the person's age
+///
+///     let updated_data = encode_doc_data_to_string(&person)
+///         .expect("Failed to serialize updated document data");
+///
+///     // Use `updated_data` to store the modified document
+///
+///     Ok(())
+/// }
+/// ```
+pub fn encode_doc_data_to_string<T: Serialize>(data: &T) -> Result<String, String> {
+    to_string(data).map_err(|e| e.to_string())
 }

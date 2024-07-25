d9c8d8f05ee5b2a815b3f0cba3c81b5d09ec6501
David Dal Busco <david.dalbusco@outlook.com>
2024-07-25 13:23:37 +0200

    feat: use en-US native platform formatting for numbers
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/lib/utils/number.utils.ts
+++ src/frontend/src/lib/utils/number.utils.ts
@@ -1,16 +1,17 @@
 export const formatNumber = (
 	value: number,
-	options?: { minFraction: number; maxFraction: number }
+	options?: { minFraction?: number; maxFraction?: number } & Pick<
+		Intl.NumberFormatOptions,
+		'notation'
+	>
 ): string => {
-	const { minFraction = 2, maxFraction = 2 } = options || {};
+	const { minFraction = 2, maxFraction = 2, notation } = options || {};
 
-	return new Intl.NumberFormat('fr-FR', {
+	return new Intl.NumberFormat('en-US', {
+		notation,
 		minimumFractionDigits: minFraction,
 		maximumFractionDigits: maxFraction
-	})
-		.format(value)
-		.replace(/\s/g, '’')
-		.replace(',', '.');
+	}).format(value);
 };
 
 export const bigintStringify = (_key: string, value: unknown): unknown =>

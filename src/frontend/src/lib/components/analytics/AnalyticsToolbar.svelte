f5dd71cd824e24ddf07d719ae2913ffae0175ffc
David Dal Busco <david.dalbusco@outlook.com>
2024-08-23 12:07:46 +0200

    chore: format
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/lib/components/analytics/AnalyticsToolbar.svelte
+++ src/frontend/src/lib/components/analytics/AnalyticsToolbar.svelte
@@ -1,40 +1,40 @@
 <div class="filters">
-    <div class="start">
-        <slot name="start" />
-    </div>
+	<div class="start">
+		<slot name="start" />
+	</div>
 
-    <div>
-        <slot name="center" />
-    </div>
+	<div>
+		<slot name="center" />
+	</div>
 
-    <div>
-        <slot name="end" />
-    </div>
+	<div>
+		<slot name="end" />
+	</div>
 </div>
 
 <style lang="scss">
-  @use '../../styles/mixins/grid';
-  @use '../../styles/mixins/media';
+	@use '../../styles/mixins/grid';
+	@use '../../styles/mixins/media';
 
-  .filters {
-    display: grid;
-    grid-template-columns: repeat(2, calc((100% - var(--padding-1_5x)) / 2));
+	.filters {
+		display: grid;
+		grid-template-columns: repeat(2, calc((100% - var(--padding-1_5x)) / 2));
 
-    column-gap: var(--padding-1_5x);
-    padding: var(--padding-2x) 0;
+		column-gap: var(--padding-1_5x);
+		padding: var(--padding-2x) 0;
 
-    @include media.min-width(large) {
-      grid-template-columns: repeat(3, calc((100% - (2 * var(--padding-4x))) / 3));
+		@include media.min-width(large) {
+			grid-template-columns: repeat(3, calc((100% - (2 * var(--padding-4x))) / 3));
 
-      gap: var(--padding-4x);
-    }
-  }
+			gap: var(--padding-4x);
+		}
+	}
 
-  .start {
-    grid-column: 1 / 3;
+	.start {
+		grid-column: 1 / 3;
 
-    @include media.min-width(large) {
-      grid-column: 1 / 2;
-    }
-  }
+		@include media.min-width(large) {
+			grid-column: 1 / 2;
+		}
+	}
 </style>

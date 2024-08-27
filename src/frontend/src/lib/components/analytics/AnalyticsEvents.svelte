8d69036fc9073b8bd4248924a3c9ba71508300e7
David Dal Busco <david.dalbusco@outlook.com>
2024-08-27 21:07:03 +0200

    feat: sort track events
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/lib/components/analytics/AnalyticsEvents.svelte
+++ src/frontend/src/lib/components/analytics/AnalyticsEvents.svelte
@@ -5,7 +5,15 @@
 	export let trackEvents: AnalyticsTrackEvents;
 
 	let total: Array<[string, number]>;
-	$: total = trackEvents.total.sort(([keyA, _], [keyB, __]) => keyA.localeCompare(keyB));
+
+	const setTotal = () => {
+		const { total: t } = trackEvents;
+		t.sort(([keyA, _], [keyB, __]) => keyA.localeCompare(keyB));
+
+		total = t;
+	};
+
+	$: trackEvents, setTotal();
 </script>
 
 <div class="table-container">

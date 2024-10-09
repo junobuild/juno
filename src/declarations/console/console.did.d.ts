f0a227d5909156cc75243aac76f77099e148fae7
David Dal Busco <david.dalbusco@outlook.com>
2024-10-09 18:48:42 +0200

    feat: generate did
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/declarations/console/console.did.d.ts
+++ src/declarations/console/console.did.d.ts
@@ -261,6 +261,7 @@ export interface _SERVICE {
 	update_rate_config: ActorMethod<[SegmentType, RateConfig], undefined>;
 	upload_asset_chunk: ActorMethod<[UploadChunk], UploadChunkResult>;
 	version: ActorMethod<[], string>;
+	withdraw_payments: ActorMethod<[], bigint>;
 }
 export declare const idlFactory: IDL.InterfaceFactory;
 export declare const init: (args: { IDL: typeof IDL }) => IDL.Type[];

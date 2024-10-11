624b1bdcb87955e7d27a024ed6c1a8406ae04a76
David Dal Busco <david.dalbusco@outlook.com>
2024-10-11 14:46:41 +0200

    chore: cleanup
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/tests/mission-control.user.spec.ts
+++ src/tests/mission-control.user.spec.ts
@@ -1,7 +1,5 @@
 import type { _SERVICE as MissionControlActor } from '$declarations/mission_control/mission_control.did';
 import { idlFactory as idlFactorMissionControl } from '$declarations/mission_control/mission_control.factory.did';
-import type { _SERVICE as OrbiterActor } from '$declarations/orbiter/orbiter.did';
-import { idlFactory as idlFactorOrbiter } from '$declarations/orbiter/orbiter.factory.did';
 import type { _SERVICE as SatelliteActor } from '$declarations/satellite/satellite.did';
 import { idlFactory as idlFactorSatellite } from '$declarations/satellite/satellite.factory.did';
 import { IDL } from '@dfinity/candid';
@@ -12,7 +10,6 @@ import { afterAll, beforeAll, describe, expect, inject } from 'vitest';
 import { CONTROLLER_ERROR_MSG } from './constants/mission-control-tests.constants';
 import {
 	MISSION_CONTROL_WASM_PATH,
-	ORBITER_WASM_PATH,
 	SATELLITE_WASM_PATH,
 	controllersInitArgs
 } from './utils/setup-tests.utils';
@@ -21,7 +18,6 @@ describe('Mission Control', () => {
 	let pic: PocketIc;
 	let actor: Actor<MissionControlActor>;
 
-	let orbiterId: Principal;
 	let satelliteId: Principal;
 
 	const incorrectUser = Ed25519KeyIdentity.generate();
@@ -50,15 +46,6 @@ describe('Mission Control', () => {
 		);
 
 		actor = c;
-
-		const { canisterId } = await pic.setupCanister<OrbiterActor>({
-			idlFactory: idlFactorOrbiter,
-			wasm: ORBITER_WASM_PATH,
-			arg: controllersInitArgs([controller.getPrincipal(), missionControlId]),
-			sender: controller.getPrincipal()
-		});
-
-		orbiterId = canisterId;
 
 		const { canisterId: cId } = await pic.setupCanister<SatelliteActor>({
 			idlFactory: idlFactorSatellite,

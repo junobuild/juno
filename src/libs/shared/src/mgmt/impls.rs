576f16ceaf06dac25e228c70d25eb93325e43518
David Dal Busco <david.dalbusco@outlook.com>
2024-10-12 13:12:31 +0200

    fix: disply canister error
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/libs/shared/src/mgmt/impls.rs
+++ src/libs/shared/src/mgmt/impls.rs
@@ -1,32 +1,15 @@
-use crate::mgmt::types::NotifyError;
+use crate::mgmt::types::CreateCanisterError;
 
-impl std::fmt::Display for NotifyError {
+impl std::fmt::Display for CreateCanisterError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::Refunded {
-                reason,
-                block_index: Some(b),
-            } => write!(f, "The payment was refunded in block {}: {}", b, reason),
-            Self::Refunded {
-                reason,
-                block_index: None,
-            } => write!(f, "The payment was refunded: {}", reason),
-            Self::InvalidTransaction(err) => write!(f, "Failed to verify transaction: {}", err),
-            Self::TransactionTooOld(bh) => write!(
-                f,
-                "The payment is too old, you cannot notify blocks older than block {}",
-                bh
-            ),
-            Self::Processing => {
-                write!(f, "Another notification of this transaction is in progress")
-            }
-            Self::Other {
-                error_code,
-                error_message,
+                refund_amount,
+                create_error,
             } => write!(
                 f,
-                "Notification failed with code {}: {}",
-                error_code, error_message
+                "The payment for the canister creation was refunded {}: {}",
+                refund_amount, create_error
             ),
         }
     }

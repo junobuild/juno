4502602283ea8ca924c9286b794adbf85ee2c552
David Dal Busco <david.dalbusco@outlook.com>
2024-10-15 21:58:37 +0200

    feat: load subnets
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/lib/components/canister/CanisterSubnets.svelte
+++ src/frontend/src/lib/components/canister/CanisterSubnets.svelte
@@ -4,12 +4,37 @@
 	import { i18n } from '$lib/stores/i18n.store';
 	import type { PrincipalText } from '$lib/types/itentity';
 	import { shortenWithMiddleEllipsis } from '$lib/utils/format.utils';
-	import { JUNO_SUBNET_ID } from '$lib/constants/constants';
+	import { DEV, JUNO_SUBNET_ID } from '$lib/constants/constants';
+	import { arrayBufferToUint8Array } from '@dfinity/utils';
+	import { getAgent } from '$lib/utils/agent.utils';
+	import { AnonymousIdentity, getDefaultAgent } from '@dfinity/agent';
+	import { Principal } from '@dfinity/principal';
+	import { getDefaultSubnets } from '$lib/api/cmc.api';
 
 	export let subnetId: PrincipalText | undefined;
 
 	let filteredSubnets: PrincipalText[];
-	$: filteredSubnets = subnets; // TODO: uncomment.filter(({ subnetId }) => subnetId !== JUNO_SUBNET_ID);
+	$: filteredSubnets = subnets.filter(({ subnetId }) => subnetId !== JUNO_SUBNET_ID);
+
+	let extendedSubnets: PrincipalText[] = [];
+
+	const extendSubnets = async () => {
+		if (!DEV) {
+			extendedSubnets = subnets;
+			return;
+		}
+
+		// The local docker image supports only a single subnet which is not a mainnet ID.
+		// So we add it at the top and we still display the other subnets that way we can assert visually it looks ok while still being able to use the facture on one specific subnet.
+		const localSubnets = await getDefaultSubnets();
+
+		extendedSubnets = [
+			...localSubnets.map((subnet) => ({ subnetId: subnet.toText() })),
+			...subnets
+		];
+	};
+
+	$: filteredSubnets, (async () => extendSubnets())();
 </script>
 
 <div class="subnet">
@@ -18,7 +43,7 @@
 		<select name="subnet" bind:value={subnetId}>
 			<option value={undefined}>{$i18n.canisters.default_subnet}</option>
 
-			{#each filteredSubnets as { subnetId }}
+			{#each extendedSubnets as { subnetId }}
 				<option value={subnetId}>{shortenWithMiddleEllipsis(subnetId)}</option>
 			{/each}
 		</select>

9dc1376bff08d65aa9c00e1ace61746d0b90e1a7
David Dal Busco <david.dalbusco@outlook.com>
2024-10-18 09:06:16 +0200

    style: animation
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/lib/components/ui/QRCodeContainer.svelte
+++ src/frontend/src/lib/components/ui/QRCodeContainer.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
 	import QRCode from '$lib/components/ui/QRCode.svelte';
+	import { slide } from 'svelte/transition';
+	import { quintOut } from 'svelte/easing';
 
 	export let ariaLabel: string | undefined = undefined;
 	export let value: string;
 </script>
 
-<div class="qrcode">
+<div
+	class="qrcode"
+	in:slide={{ delay: 0, duration: 150, easing: quintOut, axis: 'y' }}
+	out:slide={{ delay: 0, duration: 50, easing: quintOut, axis: 'y' }}
+>
 	<QRCode {value} {ariaLabel} />
 </div>
 

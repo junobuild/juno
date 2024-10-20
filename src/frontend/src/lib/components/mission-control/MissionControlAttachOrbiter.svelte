2ab92ddd4f349af866468b8937250c1a6325f596
David Dal Busco <david.dalbusco@outlook.com>
2024-10-20 13:03:55 +0200

    feat: move attach orbiter to mission control (#751)
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/lib/components/mission-control/MissionControlAttachOrbiter.svelte
+++ src/frontend/src/lib/components/mission-control/MissionControlAttachOrbiter.svelte
@@ -4,6 +4,9 @@
 	import IconLink from '$lib/components/icons/IconLink.svelte';
 	import { attachOrbiter } from '$lib/services/mission-control.services';
 	import CanisterAttach from '$lib/components/canister/CanisterAttach.svelte';
+	import { createEventDispatcher } from 'svelte';
+	import { toasts } from '$lib/stores/toasts.store';
+	import { i18nCapitalize, i18nFormat } from '$lib/utils/i18n.utils';
 
 	let visible: boolean | undefined;
 
@@ -19,11 +22,28 @@
 			orbiterId: canisterId
 		});
 	};
+	const dispatch = createEventDispatcher();
+
+	const onSuccess = () => {
+		toasts.success(
+			i18nCapitalize(
+				i18nFormat($i18n.canisters.attach_success, [
+					{
+						placeholder: '{0}',
+						value: 'orbiter'
+					}
+				])
+			)
+		);
+
+		dispatch('junoAttach');
+	};
 </script>
 
-<button on:click={() => (visible = true)} class="menu"><IconLink /> {$i18n.core.attach}</button>
+<button on:click={() => (visible = true)} class="menu"><IconLink /> {$i18n.analytics.attach}</button
+>
 
-<CanisterAttach on:junoAttach bind:visible setFn={setOrbiter}>
+<CanisterAttach on:junoAttach={onSuccess} bind:visible setFn={setOrbiter}>
 	<svelte:fragment slot="title">{$i18n.analytics.attach}</svelte:fragment>
 	<svelte:fragment slot="input">{$i18n.analytics.attach_id}</svelte:fragment>
 </CanisterAttach>

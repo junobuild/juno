2c065024c87d6927ba23c1ed129dd3f17e49f8e5
David Dal Busco <david.dalbusco@outlook.com>
2024-11-08 08:28:41 +0100

    test: rate users
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/tests/satellite.rate.spec.ts
+++ src/tests/satellite.rate.spec.ts
@@ -1,9 +1,9 @@
 import type { Doc, _SERVICE as SatelliteActor } from '$declarations/satellite/satellite.did';
 import { idlFactory as idlFactorSatellite } from '$declarations/satellite/satellite.factory.did';
 import { Ed25519KeyIdentity } from '@dfinity/identity';
-import { toNullable } from '@dfinity/utils';
+import { fromNullable, toNullable } from '@dfinity/utils';
 import { PocketIc, type Actor } from '@hadronous/pic';
-import { toArray } from '@junobuild/utils';
+import { assertNonNullish, toArray } from '@junobuild/utils';
 import { afterAll, beforeAll, describe, expect, inject } from 'vitest';
 import { SATELLITE_WASM_PATH, controllersInitArgs } from './utils/setup-tests.utils';
 
@@ -30,41 +30,98 @@ describe('Satellite rate', () => {
 		await pic?.tearDown();
 	});
 
-	const initUser = async (): Promise<Doc> => {
-		const { set_doc } = actor;
+	describe('user', () => {
+		const initUser = async (): Promise<Doc> => {
+			const { set_doc } = actor;
 
-		const user = Ed25519KeyIdentity.generate();
-		actor.setIdentity(user);
+			const user = Ed25519KeyIdentity.generate();
+			actor.setIdentity(user);
 
-		const doc = await set_doc('#user', user.getPrincipal().toText(), {
-			data: await toArray({
-				provider: 'internet_identity'
-			}),
-			description: toNullable(),
-			version: toNullable()
-		});
+			const doc = await set_doc('#user', user.getPrincipal().toText(), {
+				data: await toArray({
+					provider: 'internet_identity'
+				}),
+				description: toNullable(),
+				version: toNullable()
+			});
 
-		return doc;
-	};
+			return doc;
+		};
 
-	it('should throw error if user rate is reached', async () => {
-		const length = 13;
+		const testUsers = async (length: number): Promise<number> => {
+			const keys = Array.from({ length });
 
-		const keys = Array.from({ length });
+			const docs = [];
 
-		const docs = [];
-
-		try {
 			for (const _ of keys) {
 				const doc = await initUser();
 				docs.push(doc);
-
-				await pic.advanceTime(60000 / length);
 			}
 
-			expect(docs).toHaveLength(length);
-		} catch (error: unknown) {
-			expect((error as Error).message).toContain('Rate limit reached, try again later.');
-		}
+			return docs.length;
+		};
+
+		it('should not throw error if there is a delay', async () => {
+			await testUsers(101);
+
+			// Observed this advance time in comparison to last updated_at of 600 milliseconds
+			await pic.advanceTime(200);
+
+			await testUsers(1);
+		});
+
+		it('should throw error if user rate is reached', async () => {
+			await pic.advanceTime(60600);
+
+			try {
+				// DEFAULT 100 per minutes. So 1 for very first token + 100
+				await testUsers(102);
+
+				expect(true).toBe(false);
+			} catch (error: unknown) {
+				expect((error as Error).message).toContain('Rate limit reached, try again later.');
+			}
+		});
+
+		it('should set config and accept more users', async () => {
+			await pic.advanceTime(60600);
+
+			actor.setIdentity(controller);
+
+			const { get_rule, set_rule } = actor;
+
+			const result = await get_rule({ Db: null }, '#user');
+
+			const rule = fromNullable(result);
+
+			assertNonNullish(rule);
+
+			await set_rule({ Db: null }, '#user', {
+				...rule,
+				rate_config: [
+					{
+						max_tokens: 201n,
+						time_per_token_ns: 600_000_000n
+					}
+				]
+			});
+
+			await pic.advanceTime(600000);
+
+			const length = 202;
+
+			const count = await testUsers(length);
+
+			expect(count).toBe(length);
+
+			try {
+				// One too many
+				await testUsers(1);
+
+				expect(true).toBe(false);
+			} catch (error: unknown) {
+				expect((error as Error).message).toContain('Rate limit reached, try again later.');
+			}
+		});
 	});
 });

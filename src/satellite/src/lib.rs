72d26547f4ba26dc2c720ac294bb7b731c7bfe21
David Dal Busco <david.dalbusco@outlook.com>
2024-11-15 10:07:18 +0100

    feat: include a default index.html in stock satellites
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/satellite/src/lib.rs
+++ src/satellite/src/lib.rs
@@ -1,3 +1,12 @@
+mod assets;
+
+use crate::assets::init_asset;
+use junobuild_macros::on_init;
 use junobuild_satellite::include_satellite;
+
+#[on_init]
+fn on_init() -> Result<(), String> {
+    init_asset()
+}
 
 include_satellite!();

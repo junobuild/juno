8158ebf892c53292c52f06ba7ed82c845c9856fe
David Dal Busco <david.dalbusco@outlook.com>
2024-11-15 19:04:52 +0100

    feat: download template before build if needed
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

---
+++ scripts/satellite.template.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+URL="https://github.com/junobuild/satellite-template/releases/download/v0.0.1/index.html"
+TARGET_DIR="src/satellite/resources"
+TARGET_FILE="${TARGET_DIR}/index.html"
+
+mkdir -p "$TARGET_DIR"
+
+if [ ! -f "$TARGET_FILE" ]; then
+  echo "Satellite template does not exist. Downloading..."
+  curl -o "$TARGET_FILE" -L "$URL"
+
+  if [ $? -eq 0 ]; then
+    echo "Download completed successfully: $TARGET_FILE"
+  else
+    echo "Error downloading template. Please check the URL and try again."
+    exit 1
+  fi
+fi

93b71c73a74391d0f9790f03369b4edde9c10ee3
David Dal Busco <david.dalbusco@outlook.com>
2024-11-18 19:37:07 +0100

    feat: present only enabled satellites in the filter (#822)
    * feat: present only enabled satellites in the filter
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * chore: revert
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    ---------
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/lib/components/analytics/AnalyticsSatellitesPicker.svelte
+++ src/frontend/src/lib/components/analytics/AnalyticsSatellitesPicker.svelte
@@ -2,11 +2,11 @@
 	import { Principal } from '@dfinity/principal';
 	import { nonNullish } from '@dfinity/utils';
 	import { onMount } from 'svelte';
+	import { orbiterSatellitesConfig } from '$lib/derived/orbiter-satellites.derived';
 	import { i18n } from '$lib/stores/i18n.store';
-	import { satelliteIdStore, satellitesStore } from '$lib/stores/satellite.store';
+	import { satelliteIdStore } from '$lib/stores/satellite.store';
 	import type { SatelliteIdText } from '$lib/types/satellite';
 	import { navigateToAnalytics } from '$lib/utils/nav.utils';
-	import { satelliteName } from '$lib/utils/satellite.utils';
 
 	const navigate = async () =>
 		await navigateToAnalytics(
@@ -16,14 +16,30 @@
 	let satelliteIdText: SatelliteIdText | undefined = $state();
 
 	onMount(() => (satelliteIdText = $satelliteIdStore));
+
+	let satellites: { satelliteId: string; satName: string }[] = $derived(
+		Object.entries($orbiterSatellitesConfig).reduce(
+			(acc, [satelliteId, { name: satName, enabled }]) => [
+				...acc,
+				...(enabled
+					? [
+							{
+								satelliteId,
+								satName
+							}
+						]
+					: [])
+			],
+			// eslint-disable-next-line @typescript-eslint/prefer-reduce-type-parameter
+			[] as { satelliteId: string; satName: string }[]
+		)
+	);
 </script>
 
 <select id="satellite" name="satellite" bind:value={satelliteIdText} onchange={navigate}>
 	<option value={undefined}>{$i18n.analytics.all_satellites}</option>
 
-	{#each $satellitesStore ?? [] as satellite}
-		{@const satName = satelliteName(satellite)}
-
-		<option value={satellite.satellite_id.toText()}>{satName}</option>
+	{#each satellites as { satelliteId, satName }}
+		<option value={satelliteId}>{satName}</option>
 	{/each}
 </select>

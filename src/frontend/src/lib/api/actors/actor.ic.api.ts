2ef3c8c1bd543b649b4a3c936068d0f333fc1560
David Dal Busco <david.dalbusco@outlook.com>
2024-11-27 13:43:27 +0100

    feat: update transform for ic actor (#848)
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/lib/api/actors/actor.ic.api.ts
+++ src/frontend/src/lib/api/actors/actor.ic.api.ts
@@ -2,29 +2,40 @@ import type { _SERVICE as ICActor } from '$declarations/ic/ic.did';
 import { idlFactory as idlFactorIC } from '$declarations/ic/ic.factory.did';
 import { ActorApi } from '$lib/api/actors/actor.api';
 import type { GetAgentParams } from '$lib/api/agent/agent.api';
-import type { CallConfig } from '@dfinity/agent';
+import type { ActorConfig, CallConfig } from '@dfinity/agent';
 import { Principal } from '@dfinity/principal';
+import { nonNullish } from '@dfinity/utils';
 
 const MANAGEMENT_CANISTER_ID = Principal.fromText('aaaaa-aa');
 
-/* eslint-disable */
+type CallTransform = Required<ActorConfig>['callTransform'];
 
-// Source nns-dapp - dart -> JS bridge
-const transform = (
-	_methodName: string,
-	args: unknown[],
+type QueryTransform = Required<ActorConfig>['queryTransform'];
+
+// Source: `@dfinity/ic-management` function `transform`
+// eslint-disable-next-line local-rules/prefer-object-params
+const transform: CallTransform | QueryTransform = (
+	methodName: string,
+	args: (Record<string, unknown> & {
+		canister_id?: unknown;
+		target_canister?: unknown;
+	})[],
 	_callConfig: CallConfig
 ): { effectiveCanisterId: Principal } => {
-	const first = args[0] as any;
-	let effectiveCanisterId = MANAGEMENT_CANISTER_ID;
-	if (first && typeof first === 'object' && first.canister_id) {
-		effectiveCanisterId = Principal.from(first.canister_id as unknown);
+	const first = args[0];
+
+	if (nonNullish(first) && typeof first === 'object') {
+		if (methodName === 'install_chunked_code' && nonNullish(first.target_canister)) {
+			return { effectiveCanisterId: Principal.from(first.target_canister) };
+		}
+
+		if (nonNullish(first.canister_id)) {
+			return { effectiveCanisterId: Principal.from(first.canister_id) };
+		}
 	}
 
-	return { effectiveCanisterId };
+	return { effectiveCanisterId: MANAGEMENT_CANISTER_ID };
 };
-
-/* eslint-enable */
 
 const icActor = new ActorApi<ICActor>();
 

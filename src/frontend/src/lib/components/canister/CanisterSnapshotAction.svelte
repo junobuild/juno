ec9fd71f36e3978c2b8f3f4b22e38356cee96a24
David Dal Busco <david.dalbusco@outlook.com>
2024-11-29 15:01:10 +0100

    feat: backup restore and replace in popover (#858)
    * feat: backup restore and replace in popover
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * chore: lint
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    ---------
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

---
+++ src/frontend/src/lib/components/canister/CanisterSnapshotAction.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	import IconEdit from '$lib/components/icons/IconEdit.svelte';
+	import IconHistory from '$lib/components/icons/IconHistory.svelte';
+	import IconRefresh from '$lib/components/icons/IconRefresh.svelte';
+	import Popover from '$lib/components/ui/Popover.svelte';
+	import { i18n } from '$lib/stores/i18n.store';
+
+	interface Props {
+		onrestore: () => void;
+		onreplace: () => void;
+	}
+
+	let { onrestore, onreplace }: Props = $props();
+
+	let visible = $state(false);
+
+	let button: HTMLButtonElement | undefined = $state();
+
+	const restore = () => {
+		onrestore();
+		visible = false;
+	};
+
+	const replace = () => {
+		onreplace();
+		visible = false;
+	};
+</script>
+
+<button
+	class="square"
+	bind:this={button}
+	onclick={() => (visible = true)}
+	aria-label={$i18n.canisters.edit_snapshot}><IconEdit size="20px" /></button
+>
+
+<Popover bind:visible anchor={button} direction="ltr">
+	<div class="container">
+		<button onclick={restore} class="menu"><IconHistory /> {$i18n.core.restore}</button>
+		<button onclick={replace} class="menu"><IconRefresh /> {$i18n.core.replace}</button>
+	</div>
+</Popover>
+
+<style lang="scss">
+	@use '../../styles/mixins/overlay';
+
+	@include overlay.popover-container;
+
+	button {
+		vertical-align: middle;
+	}
+</style>

d839e3078b8c815754327043ce8ed144b0732350
David Dal Busco <david.dalbusco@outlook.com>
2024-12-03 09:46:25 +0100

    feat: move preferences and mic to single layout
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/routes/(single)/mission-control/+page.ts
+++ src/frontend/src/routes/(single)/mission-control/+page.ts
@@ -1,5 +1,5 @@
 import { loadRouteTab, type RouteTab } from '$lib/utils/nav.utils';
 import type { LoadEvent } from '@sveltejs/kit';
-import type { PageLoad } from './$types';
+import type { PageLoad } from '../../../../../../.svelte-kit/types/src/frontend';
 
 export const load: PageLoad = ($event: LoadEvent): RouteTab => loadRouteTab($event);
---
+++ src/frontend/src/routes/(single)/preferences/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { onMount, type Snippet } from 'svelte';
+	import IconRaygun from '$lib/components/icons/IconRaygun.svelte';
+	import { i18n } from '$lib/stores/i18n.store';
+	import { layoutTitle } from '$lib/stores/layout.store';
+
+	interface Props {
+		children: Snippet;
+	}
+
+	let { children }: Props = $props();
+
+	onMount(() =>
+		layoutTitle.set({
+			title: $i18n.preferences.title,
+			icon: IconRaygun
+		})
+	);
+</script>
+
+{@render children()}

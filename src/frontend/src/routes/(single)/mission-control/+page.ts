224ee5e537a6ae10b62745e374cd38dbaf7d19f9
David Dal Busco <david.dalbusco@outlook.com>
2024-12-03 09:52:59 +0100

    fix: webstorm incorrect auto refactoring
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/routes/(single)/mission-control/+page.ts
+++ src/frontend/src/routes/(single)/mission-control/+page.ts
@@ -1,5 +1,5 @@
 import { loadRouteTab, type RouteTab } from '$lib/utils/nav.utils';
 import type { LoadEvent } from '@sveltejs/kit';
-import type { PageLoad } from '../../../../../../.svelte-kit/types/src/frontend';
+import type { PageLoad } from './$types';
 
 export const load: PageLoad = ($event: LoadEvent): RouteTab => loadRouteTab($event);

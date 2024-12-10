0bd33eeffb6699deab30149acf183a0cda4ab7ae
David Dal Busco <david.dalbusco@outlook.com>
2024-12-10 17:39:49 +0100

    feat: use data store
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/lib/stores/mission-control.store.ts
+++ src/frontend/src/lib/stores/mission-control.store.ts
@@ -1,9 +1,8 @@
 import type { MissionControlSettings } from '$declarations/mission_control/mission_control.did';
+import { initDataStore } from '$lib/stores/data.store';
 import type { Principal } from '@dfinity/principal';
 import { writable } from 'svelte/store';
 
 export const missionControlStore = writable<Principal | undefined | null>(undefined);
 
-export const missionControlSettingsStore = writable<MissionControlSettings | undefined | null>(
-	undefined
-);
+export const missionControlSettingsStore = initDataStore<MissionControlSettings>();

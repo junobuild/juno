86d3ea9dc95a360031a2050d6d2276ac85895cf7
David Dal Busco <david.dalbusco@outlook.com>
2024-12-10 18:25:39 +0100

    chore: format and lint
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/routes/+layout.svelte
+++ src/frontend/src/routes/+layout.svelte
@@ -5,7 +5,6 @@
 	import { browser } from '$app/environment';
 	import Overlays from '$lib/components/core/Overlays.svelte';
 	import Spinner from '$lib/components/ui/Spinner.svelte';
-	import { missionControlDataStore } from '$lib/stores/mission-control.store';
 	import { displayAndCleanLogoutMsg, signOut } from '$lib/services/auth.services';
 	import { initMissionControl } from '$lib/services/console.services';
 	import { syncSnapshots } from '$lib/services/snapshots.services';
@@ -13,6 +12,7 @@
 	import { initAuthWorker } from '$lib/services/worker.auth.services';
 	import { type AuthStoreData, authStore } from '$lib/stores/auth.store';
 	import { i18n } from '$lib/stores/i18n.store';
+	import { missionControlDataStore } from '$lib/stores/mission-control.store';
 	import { toasts } from '$lib/stores/toasts.store';
 	import '$lib/styles/global.scss';
 

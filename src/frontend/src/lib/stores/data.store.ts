ed842053bbbf8e46672509a26715895161f47df7
David Dal Busco <david.dalbusco@outlook.com>
2024-12-10 17:23:30 +0100

    feat: extract data store
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

---
+++ src/frontend/src/lib/stores/data.store.ts
@@ -0,0 +1,25 @@
+import type { Option } from '$lib/types/utils';
+import { writable, type Readable } from 'svelte/store';
+
+type Data<T> = Option<T>;
+
+interface DataStore<T> extends Readable<Data<T>> {
+	set: (data: T) => void;
+	reset: () => void;
+}
+
+export const initDataStore = <T>(): DataStore<T> => {
+	const { subscribe, set } = writable<Data<T>>(undefined);
+
+	return {
+		subscribe,
+
+		set(data) {
+			set(data);
+		},
+
+		reset: () => {
+			set(null);
+		}
+	};
+};

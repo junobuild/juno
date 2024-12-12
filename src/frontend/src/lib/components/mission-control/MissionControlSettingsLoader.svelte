4a6fbfd7c0f05f6d410be18a23c535dea824a4a9
David Dal Busco <david.dalbusco@outlook.com>
2024-12-12 11:56:07 +0100

    chore: fmt
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/lib/components/mission-control/MissionControlSettingsLoader.svelte
+++ src/frontend/src/lib/components/mission-control/MissionControlSettingsLoader.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	import type { Principal } from '@dfinity/principal';
 	import { onMount, type Snippet } from 'svelte';
-	import { authStore } from '$lib/stores/auth.store';
 	import { loadSettings } from '$lib/services/mission-control.services';
+	import { authStore } from '$lib/stores/auth.store';
 
 	interface Props {
 		missionControlId: Principal;

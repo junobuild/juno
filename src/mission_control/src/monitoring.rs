47b6ffcac827f6ad807ad1541b6a5168677c50cb
David Dal Busco <david.dalbusco@outlook.com>
2024-12-13 14:50:46 +0100

    chore: fmt
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/mission_control/src/monitoring.rs
+++ src/mission_control/src/monitoring.rs
@@ -1,11 +1,11 @@
-use ic_cdk::spawn;
-use std::time::Duration;
-use ic_cdk::trap;
-use ic_cdk_timers::set_timer;
 use crate::cycles_monitoring::restart::restart_cycles_monitoring;
 use crate::cycles_monitoring::start::start_cycles_monitoring;
 use crate::cycles_monitoring::stop::stop_cycles_monitoring;
 use crate::types::interface::{MonitoringStartConfig, MonitoringStopConfig};
+use ic_cdk::spawn;
+use ic_cdk::trap;
+use ic_cdk_timers::set_timer;
+use std::time::Duration;
 
 pub fn defer_restart_monitoring() {
     set_timer(Duration::ZERO, || spawn(restart_monitoring()));

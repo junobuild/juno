65d4b05222b619443c0602eb826a4e66863fe9ad
David Dal Busco <david.dalbusco@outlook.com>
2024-12-17 18:21:16 +0100

    chore: fmt
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/lib/components/modals/StopMonitoringStrategyModal.svelte
+++ src/frontend/src/lib/components/modals/StopMonitoringStrategyModal.svelte
@@ -11,12 +11,12 @@
 	import MonitoringStopReview from '$lib/components/monitoring/MonitoringStopReview.svelte';
 	import ProgressMonitoring from '$lib/components/monitoring/ProgressMonitoring.svelte';
 	import Modal from '$lib/components/ui/Modal.svelte';
+	import { stopMonitoringCyclesStrategy } from '$lib/services/monitoring.services';
+	import { authStore } from '$lib/stores/auth.store';
+	import { wizardBusy } from '$lib/stores/busy.store';
 	import { i18n } from '$lib/stores/i18n.store';
 	import type { JunoModalDetail, JunoModalMonitoringStrategyDetail } from '$lib/types/modal';
 	import type { MonitoringStrategyProgress } from '$lib/types/strategy';
-	import { wizardBusy } from '$lib/stores/busy.store';
-	import { stopMonitoringCyclesStrategy } from '$lib/services/monitoring.services';
-	import { authStore } from '$lib/stores/auth.store';
 
 	interface Props {
 		detail: JunoModalDetail;

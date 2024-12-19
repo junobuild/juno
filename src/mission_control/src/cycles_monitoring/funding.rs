132b545b53db7481ebd62d2902dbb4f1b94b8d99
David Dal Busco <david.dalbusco@outlook.com>
2024-12-19 09:35:21 +0100

    refactor: extract one more function
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/mission_control/src/cycles_monitoring/funding.rs
+++ src/mission_control/src/cycles_monitoring/funding.rs
@@ -50,31 +50,35 @@ fn obtain_cycles_options() -> ObtainCyclesOptions {
     }
 }
 
-pub fn funding_callback() -> ObserverCallback {
+fn funding_callback() -> ObserverCallback {
     Rc::new(|records: HashMap<CanisterId, CanisterRecord>| {
-        for (canister_id, record) in records.iter() {
-            if let Some(cycles) = record.get_cycles() {
-                let last_deposited_cycles =
-                    record
-                        .get_last_deposited_cycles()
-                        .clone()
-                        .map(|last_deposited| CyclesBalance {
-                            amount: last_deposited.amount,
-                            timestamp: last_deposited.timestamp,
-                        });
-
-                let history_entry: MonitoringHistoryCycles = MonitoringHistoryCycles {
-                    cycles: CyclesBalance {
-                        amount: cycles.amount,
-                        timestamp: cycles.timestamp,
-                    },
-                    last_deposited_cycles,
-                };
-
-                insert_cycles_monitoring_history(&canister_id, &history_entry);
-            }
-        }
+        save_monitoring_history(records);
     })
+}
+
+fn save_monitoring_history(records: HashMap<CanisterId, CanisterRecord>) {
+    for (canister_id, record) in records.iter() {
+        if let Some(cycles) = record.get_cycles() {
+            let last_deposited_cycles =
+                record
+                    .get_last_deposited_cycles()
+                    .clone()
+                    .map(|last_deposited| CyclesBalance {
+                        amount: last_deposited.amount,
+                        timestamp: last_deposited.timestamp,
+                    });
+
+            let history_entry: MonitoringHistoryCycles = MonitoringHistoryCycles {
+                cycles: CyclesBalance {
+                    amount: cycles.amount,
+                    timestamp: cycles.timestamp,
+                },
+                last_deposited_cycles,
+            };
+
+            insert_cycles_monitoring_history(&canister_id, &history_entry);
+        }
+    }
 }
 
 pub fn register_cycles_monitoring(

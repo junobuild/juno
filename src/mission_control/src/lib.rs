85ef3923c148ede24024355f84ff97b90d55ef1d
David Dal Busco <david.dalbusco@outlook.com>
2024-12-29 10:48:54 +0100

    feat: remove deprecated unused segment status crontab
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/mission_control/src/lib.rs
+++ src/mission_control/src/lib.rs
@@ -33,9 +33,7 @@ use crate::segments::satellite::{
 use crate::segments::store::get_orbiters;
 use crate::store::{
     get_settings as get_settings_store, get_user as get_user_store,
-    list_mission_control_statuses as list_mission_control_statuses_store,
-    list_orbiter_statuses as list_orbiter_statuses_store,
-    list_satellite_statuses as list_satellite_statuses_store, set_metadata as set_metadata_store,
+    set_metadata as set_metadata_store,
 };
 use crate::types::interface::{
     CreateCanisterConfig, GetMonitoringHistory, MonitoringStartConfig, MonitoringStatus,
@@ -391,37 +389,6 @@ async fn deposit_cycles(args: DepositCyclesArgs) {
 #[query]
 fn version() -> String {
     env!("CARGO_PKG_VERSION").to_string()
-}
-
-// ---------------------------------------------------------
-// Observatory
-// ---------------------------------------------------------
-
-#[deprecated(
-    since = "0.0.14",
-    note = "Deprecated with the introduction of monitoring features that include auto top-up capabilities."
-)]
-#[query(guard = "caller_is_user_or_admin_controller")]
-fn list_mission_control_statuses() -> Statuses {
-    list_mission_control_statuses_store()
-}
-
-#[deprecated(
-    since = "0.0.14",
-    note = "Deprecated with the introduction of monitoring features that include auto top-up capabilities."
-)]
-#[query(guard = "caller_is_user_or_admin_controller")]
-fn list_satellite_statuses(satellite_id: SatelliteId) -> Option<Statuses> {
-    list_satellite_statuses_store(&satellite_id)
-}
-
-#[deprecated(
-    since = "0.0.14",
-    note = "Deprecated with the introduction of monitoring features that include auto top-up capabilities."
-)]
-#[query(guard = "caller_is_user_or_admin_controller")]
-fn list_orbiter_statuses(orbiter_id: OrbiterId) -> Option<Statuses> {
-    list_orbiter_statuses_store(&orbiter_id)
 }
 
 // ---------------------------------------------------------

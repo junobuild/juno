5ca0754ed61ce30c43a03caa3c7c93088970e880
David Dal Busco <david.dalbusco@outlook.com>
2024-12-29 10:47:55 +0100

    feat: remove deprecated unused segment status crontab
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/mission_control/src/store.rs
+++ src/mission_control/src/store.rs
@@ -4,7 +4,7 @@ use crate::types::state::{
 };
 use candid::Principal;
 use ic_cdk::api::time;
-use junobuild_shared::types::state::{Metadata, OrbiterId, SatelliteId, UserId};
+use junobuild_shared::types::state::{Metadata, UserId};
 
 pub fn get_user() -> UserId {
     STATE.with(|state| state.borrow().heap.user.user).unwrap()
@@ -29,34 +29,4 @@ fn set_metadata_impl(metadata: &Metadata, state: &mut HeapState) {
     };
 
     state.user = updated_user;
-}
-
-// ---------------------------------------------------------
-// Statuses
-// ---------------------------------------------------------
-
-#[deprecated]
-pub fn list_mission_control_statuses() -> Statuses {
-    STATE.with(|state| state.borrow().heap.archive.statuses.mission_control.clone())
-}
-
-#[deprecated]
-pub fn list_satellite_statuses(satellite_id: &SatelliteId) -> Option<Statuses> {
-    STATE.with(|state| {
-        list_segment_statuses_impl(
-            satellite_id,
-            &state.borrow().heap.archive.statuses.satellites,
-        )
-    })
-}
-
-#[deprecated]
-pub fn list_orbiter_statuses(orbiter_id: &OrbiterId) -> Option<Statuses> {
-    STATE.with(|state| {
-        list_segment_statuses_impl(orbiter_id, &state.borrow().heap.archive.statuses.orbiters)
-    })
-}
-
-fn list_segment_statuses_impl(id: &Principal, state: &ArchiveStatusesSegments) -> Option<Statuses> {
-    state.get(id).cloned()
 }

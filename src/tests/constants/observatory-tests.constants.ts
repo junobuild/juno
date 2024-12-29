fab3cc5cc2408e1f6eb98cd16df62f878e2585d6
David Dal Busco <david.dalbusco@outlook.com>
2024-12-29 10:56:25 +0100

    chore: fmt
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/tests/constants/observatory-tests.constants.ts
+++ src/tests/constants/observatory-tests.constants.ts
@@ -1,1 +1,2 @@
-export const CALLER_NOT_CONTROLLER_OBSERVATORY_MSG = 'Caller is not a controller of the observatory.';
+export const CALLER_NOT_CONTROLLER_OBSERVATORY_MSG =
+	'Caller is not a controller of the observatory.';

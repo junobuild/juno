6c539f1663ea0760ac52c337c0a70b83560a9346
David Dal Busco <david.dalbusco@outlook.com>
2024-12-30 18:58:26 +0100

    feat: simplify code
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/libs/shared/src/random.rs
+++ src/libs/shared/src/random.rs
@@ -3,16 +3,13 @@ use ic_cdk::api::call::CallResult;
 use ic_cdk::call;
 use rand::{rngs::StdRng, SeedableRng};
 
-pub async fn set_random_seed<F>(with_state: F)
-where
-    F: FnOnce(Option<StdRng>),
-{
+pub async fn get_random_seed() -> Option<StdRng> {
     let result: CallResult<([u8; 32],)> =
         call(Principal::management_canister(), "raw_rand", ()).await;
 
     match result {
         // We do nothing in case of error to not block initialization but, getrandom will throw errors
-        Err(_) => (),
-        Ok((seed,)) => with_state(Some(StdRng::from_seed(seed))),
+        Err(_) => None,
+        Ok((seed,)) => Some(StdRng::from_seed(seed)),
     }
 }

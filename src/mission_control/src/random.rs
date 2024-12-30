c5efaed8390104abb9a034c93c89341250e62c23
David Dal Busco <david.dalbusco@outlook.com>
2024-12-30 20:47:33 +0100

    feat: use utils
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/mission_control/src/random.rs
+++ src/mission_control/src/random.rs
@@ -1,11 +1,10 @@
 use crate::memory::RUNTIME_STATE;
-use candid::Principal;
 use getrandom::register_custom_getrandom;
 use getrandom::Error;
-use ic_cdk::api::call::CallResult;
-use ic_cdk::{call, spawn};
+use ic_cdk::spawn;
 use ic_cdk_timers::set_timer;
-use rand::{rngs::StdRng, Rng, RngCore, SeedableRng};
+use junobuild_shared::random::get_random_seed;
+use rand::{Rng, RngCore};
 use std::num::NonZeroU32;
 use std::time::Duration;
 
@@ -15,16 +14,11 @@ pub fn defer_init_random_seed() {
 }
 
 async fn set_random_seed() {
-    let result: CallResult<([u8; 32],)> =
-        call(Principal::management_canister(), "raw_rand", ()).await;
+    let seed = get_random_seed().await;
 
-    match result {
-        // We do nothing in case of error to not block initialization but, getrandom will throw errors
-        Err(_) => (),
-        Ok((seed,)) => RUNTIME_STATE.with(|state| {
-            state.borrow_mut().rng = Some(StdRng::from_seed(seed));
-        }),
-    }
+    RUNTIME_STATE.with(|state| {
+        state.borrow_mut().rng = seed;
+    });
 }
 
 fn custom_getrandom(buf: &mut [u8]) -> Result<(), Error> {

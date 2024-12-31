d26783c7253d723d7d95b06bf94bd97c13228905
David Dal Busco <david.dalbusco@outlook.com>
2024-12-31 17:40:09 +0100

    feat: generate did
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/declarations/observatory/observatory.did.d.ts
+++ src/declarations/observatory/observatory.did.d.ts
@@ -3,9 +3,38 @@ import type { IDL } from '@dfinity/candid';
 import type { Principal } from '@dfinity/principal';
 
 export type ControllerScope = { Write: null } | { Admin: null };
+export interface CyclesBalance {
+	timestamp: bigint;
+	amount: bigint;
+}
 export interface DeleteControllersArgs {
 	controllers: Array<Principal>;
 }
+export interface Env {
+	email_api_key: [] | [string];
+}
+export interface HttpHeader {
+	value: string;
+	name: string;
+}
+export interface HttpResponse {
+	status: bigint;
+	body: Uint8Array | number[];
+	headers: Array<HttpHeader>;
+}
+export interface NotifyArgs {
+	user: Principal;
+	segment_id: Principal;
+	notification: SendNotification;
+}
+export interface SendDepositedCyclesEmailNotification {
+	to: string;
+	deposited_cycles: CyclesBalance;
+	metadata: [] | [Array<[string, string]>];
+}
+export type SendNotification = {
+	DepositedCyclesEmail: SendDepositedCyclesEmailNotification;
+};
 export interface SetController {
 	metadata: Array<[string, string]>;
 	scope: ControllerScope;
@@ -15,9 +44,16 @@ export interface SetControllersArgs {
 	controller: SetController;
 	controllers: Array<Principal>;
 }
+export interface TransformArgs {
+	context: Uint8Array | number[];
+	response: HttpResponse;
+}
 export interface _SERVICE {
 	del_controllers: ActorMethod<[DeleteControllersArgs], undefined>;
+	notify: ActorMethod<[NotifyArgs], undefined>;
 	set_controllers: ActorMethod<[SetControllersArgs], undefined>;
+	set_env: ActorMethod<[Env], undefined>;
+	transform: ActorMethod<[TransformArgs], HttpResponse>;
 	version: ActorMethod<[], string>;
 }
 export declare const idlFactory: IDL.InterfaceFactory;
--- src/declarations/observatory/observatory.factory.did.js
+++ src/declarations/observatory/observatory.factory.did.js
@@ -3,6 +3,23 @@ export const idlFactory = ({ IDL }) => {
 	const DeleteControllersArgs = IDL.Record({
 		controllers: IDL.Vec(IDL.Principal)
 	});
+	const CyclesBalance = IDL.Record({
+		timestamp: IDL.Nat64,
+		amount: IDL.Nat
+	});
+	const SendDepositedCyclesEmailNotification = IDL.Record({
+		to: IDL.Text,
+		deposited_cycles: CyclesBalance,
+		metadata: IDL.Opt(IDL.Vec(IDL.Tuple(IDL.Text, IDL.Text)))
+	});
+	const SendNotification = IDL.Variant({
+		DepositedCyclesEmail: SendDepositedCyclesEmailNotification
+	});
+	const NotifyArgs = IDL.Record({
+		user: IDL.Principal,
+		segment_id: IDL.Principal,
+		notification: SendNotification
+	});
 	const ControllerScope = IDL.Variant({
 		Write: IDL.Null,
 		Admin: IDL.Null
@@ -16,9 +33,23 @@ export const idlFactory = ({ IDL }) => {
 		controller: SetController,
 		controllers: IDL.Vec(IDL.Principal)
 	});
+	const Env = IDL.Record({ email_api_key: IDL.Opt(IDL.Text) });
+	const HttpHeader = IDL.Record({ value: IDL.Text, name: IDL.Text });
+	const HttpResponse = IDL.Record({
+		status: IDL.Nat,
+		body: IDL.Vec(IDL.Nat8),
+		headers: IDL.Vec(HttpHeader)
+	});
+	const TransformArgs = IDL.Record({
+		context: IDL.Vec(IDL.Nat8),
+		response: HttpResponse
+	});
 	return IDL.Service({
 		del_controllers: IDL.Func([DeleteControllersArgs], [], []),
+		notify: IDL.Func([NotifyArgs], [], []),
 		set_controllers: IDL.Func([SetControllersArgs], [], []),
+		set_env: IDL.Func([Env], [], []),
+		transform: IDL.Func([TransformArgs], [HttpResponse], ['query']),
 		version: IDL.Func([], [IDL.Text], ['query'])
 	});
 };

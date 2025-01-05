4cfbac276d7798d8de782bf73e0a7ff286615c41
David Dal Busco <david.dalbusco@outlook.com>
2025-01-05 15:30:53 +0100

    chore: lint
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/lib/components/modals/SatelliteCreateModal.svelte
+++ src/frontend/src/lib/components/modals/SatelliteCreateModal.svelte
@@ -2,7 +2,6 @@
 	import { isNullish } from '@dfinity/utils';
 	import type { Satellite } from '$declarations/mission_control/mission_control.did';
 	import CanisterAdvancedOptions from '$lib/components/canister/CanisterAdvancedOptions.svelte';
-	import ProgressCreate from '$lib/components/canister/ProgressCreate.svelte';
 	import CreditsGuard from '$lib/components/guards/CreditsGuard.svelte';
 	import Confetti from '$lib/components/ui/Confetti.svelte';
 	import Modal from '$lib/components/ui/Modal.svelte';
@@ -14,7 +13,6 @@
 	import { authStore } from '$lib/stores/auth.store';
 	import { wizardBusy } from '$lib/stores/busy.store';
 	import { i18n } from '$lib/stores/i18n.store';
-	import type { CanisterCreateProgress } from '$lib/types/canister-create';
 	import type { PrincipalText } from '$lib/types/itentity';
 	import type { JunoModalDetail } from '$lib/types/modal';
 	import { navigateToSatellite } from '$lib/utils/nav.utils';

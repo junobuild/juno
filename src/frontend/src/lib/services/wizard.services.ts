780cd822b1f2704120e1839e1374574316ed8e33
David Dal Busco <david.dalbusco@outlook.com>
2025-01-05 15:28:01 +0100

    feat: extract and reuse code to create module
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/lib/services/wizard.services.ts
+++ src/frontend/src/lib/services/wizard.services.ts
@@ -1,12 +1,24 @@
+import type { Orbiter, Satellite } from '$declarations/mission_control/mission_control.did';
 import { getOrbiterFee, getSatelliteFee } from '$lib/api/console.api';
 import { getMissionControlBalance } from '$lib/services/balance.services';
+import {
+	createOrbiter,
+	createOrbiterWithConfig,
+	loadOrbiters
+} from '$lib/services/orbiters.services';
+import {
+	createSatellite,
+	createSatelliteWithConfig,
+	loadSatellites
+} from '$lib/services/satellites.services';
 import { i18n } from '$lib/stores/i18n.store';
 import { toasts } from '$lib/stores/toasts.store';
-import type { OptionIdentity } from '$lib/types/itentity';
+import type { OptionIdentity, PrincipalText } from '$lib/types/itentity';
 import type { JunoModalCreateSegmentDetail } from '$lib/types/modal';
 import type { Option } from '$lib/types/utils';
-import type { Principal } from '@dfinity/principal';
-import { isNullish, nonNullish } from '@dfinity/utils';
+import type { Identity } from '@dfinity/agent';
+import { Principal } from '@dfinity/principal';
+import { assertNonNullish, isNullish, nonNullish } from '@dfinity/utils';
 import { get } from 'svelte/store';
 
 export interface GetFeeBalance {
@@ -69,4 +81,126 @@ const getCreateFeeBalance = async ({
 			}
 		}
 	};
+};
+
+interface CreateWizardParams {
+	missionControlId: Option<Principal>;
+	identity: OptionIdentity;
+	subnetId: PrincipalText | undefined;
+}
+
+export const createSatelliteWizard = async ({
+	missionControlId,
+	identity,
+	subnetId,
+	satelliteName
+}: CreateWizardParams & {
+	satelliteName: string | undefined;
+}): Promise<
+	| {
+			success: 'ok';
+			segment: Satellite;
+	  }
+	| { success: 'error'; err?: unknown }
+> => {
+	if (isNullish(satelliteName)) {
+		toasts.error({
+			text: get(i18n).errors.satellite_name_missing
+		});
+		return { success: 'error' };
+	}
+
+	const createFn = async ({ identity }: { identity: Identity }): Promise<Satellite> => {
+		const fn = nonNullish(subnetId) ? createSatelliteWithConfig : createSatellite;
+
+		return await fn({
+			identity,
+			missionControlId,
+			config: {
+				name: satelliteName,
+				...(nonNullish(subnetId) && { subnetId: Principal.fromText(subnetId) })
+			}
+		});
+	};
+
+	return await createWizard({
+		identity,
+		missionControlId,
+		createFn,
+		reloadFn: loadSatellites,
+		errorLabel: 'satellite_unexpected_error'
+	});
+};
+
+export const createOrbiterWizard = async ({
+	missionControlId,
+	identity,
+	subnetId
+}: CreateWizardParams): Promise<
+	| {
+			success: 'ok';
+			segment: Orbiter;
+	  }
+	| { success: 'error'; err?: unknown }
+> => {
+	const createFn = async ({ identity }: { identity: Identity }): Promise<Orbiter> => {
+		const fn = nonNullish(subnetId) ? createOrbiterWithConfig : createOrbiter;
+
+		return await fn({
+			identity,
+			missionControlId,
+			config: {
+				...(nonNullish(subnetId) && { subnetId: Principal.fromText(subnetId) })
+			}
+		});
+	};
+
+	return await createWizard({
+		identity,
+		missionControlId,
+		createFn,
+		reloadFn: loadOrbiters,
+		errorLabel: 'orbiter_unexpected_error'
+	});
+};
+
+const createWizard = async <T>({
+	missionControlId,
+	identity,
+	errorLabel,
+	createFn,
+	reloadFn
+}: Omit<CreateWizardParams, 'subnetId'> & {
+	errorLabel: keyof I18nErrors;
+	createFn: (params: { identity: Identity }) => Promise<T>;
+	reloadFn: (params: {
+		missionControlId: Option<Principal>;
+		reload: boolean;
+	}) => Promise<{ result: 'skip' | 'success' | 'error' }>;
+}): Promise<
+	| {
+			success: 'ok';
+			segment: T;
+	  }
+	| { success: 'error'; err?: unknown }
+> => {
+	try {
+		assertNonNullish(identity, get(i18n).core.not_logged_in);
+
+		const segment = await createFn({
+			identity
+		});
+
+		// Reload list of segments before navigation
+		await reloadFn({ missionControlId, reload: true });
+
+		return { success: 'ok', segment };
+	} catch (err: unknown) {
+		toasts.error({
+			text: get(i18n).errors[errorLabel],
+			detail: err
+		});
+
+		return { success: 'error', err };
+	}
 };

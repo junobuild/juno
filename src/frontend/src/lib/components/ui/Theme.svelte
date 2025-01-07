dc946fff6429a3ad877573d00c19c298e5bba94d
David Dal Busco <david.dalbusco@outlook.com>
2025-01-07 12:24:54 +0100

    feat: extract theme toggle
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/lib/components/ui/Theme.svelte
+++ src/frontend/src/lib/components/ui/Theme.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
-	import IconLightOff from '$lib/components/icons/IconLightOff.svelte';
-	import IconLightOn from '$lib/components/icons/IconLightOn.svelte';
 	import { i18n } from '$lib/stores/i18n.store';
 	import { theme } from '$lib/stores/theme.store';
 	import { Theme } from '$lib/types/theme';
+	import Toggle from "$lib/components/ui/Toggle.svelte";
 
 	interface Props {
 		inline?: boolean;
@@ -11,46 +10,29 @@
 
 	let { inline = false }: Props = $props();
 
-	let dark: boolean = $derived($theme === Theme.DARK);
-
-	let Icon = $derived(dark ? IconLightOn : IconLightOff);
+	let dark = $derived($theme === Theme.DARK);
 </script>
 
-<button class="text" onclick={() => theme.select(dark ? Theme.LIGHT : Theme.DARK)}>
-	<Icon />
+<Toggle toggle={dark} onclick={() => theme.select(dark ? Theme.LIGHT : Theme.DARK)} nomargin={!inline}>
 	<span class:inline>
 		{#if dark}
-			{$i18n.core.light_on}
-		{:else}
 			{$i18n.core.light_off}
+		{:else}
+			{$i18n.core.light_on}
 		{/if}
 	</span>
-</button>
+</Toggle>
 
 <style lang="scss">
 	@use '../../styles/mixins/media';
-
-	.text {
-		display: inline-flex;
-		justify-content: flex-start;
-		gap: var(--padding);
-
-		min-width: var(--menu-width);
-
-		text-decoration: none;
-
-		margin: var(--padding) 0;
-
-		&:active {
-			transform: none;
-		}
-	}
 
 	span {
 		font-size: var(--font-size-ultra-small);
 
 		&.inline {
 			font-size: inherit;
+
+			margin: 0 0 var(--padding-0_5x);
 		}
 
 		color: var(--color-primary-contrast);

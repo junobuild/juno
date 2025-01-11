584ffff9a34da11f7df0a1b3bc2cb5e485d011c5
David Dal Busco <david.dalbusco@outlook.com>
2025-01-11 09:04:30 +0100

    style: use purple on standalone routes (#1054)
    * style: use purple on standalone routes
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * chore: fmt
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    ---------
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/routes/(single)/preferences/+layout.svelte
+++ src/frontend/src/routes/(single)/preferences/+layout.svelte
@@ -3,6 +3,8 @@
 	import IconRaygun from '$lib/components/icons/IconRaygun.svelte';
 	import { i18n } from '$lib/stores/i18n.store';
 	import { layoutNavigation } from '$lib/stores/layout-navigation.store';
+	import { Color } from '$lib/types/theme';
+	import { applyColor } from '$lib/utils/theme.utils';
 
 	interface Props {
 		children: Snippet;
@@ -10,12 +12,14 @@
 
 	let { children }: Props = $props();
 
-	onMount(() =>
+	onMount(() => {
+		applyColor(Color.LAVENDER_BLUE);
+
 		layoutNavigation.set({
 			title: $i18n.preferences.title,
 			icon: IconRaygun
-		})
-	);
+		});
+	});
 </script>
 
 {@render children()}
--- src/frontend/src/routes/(standalone)/cli/+layout.svelte
+++ src/frontend/src/routes/(standalone)/cli/+layout.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
 	import { onMount, type Snippet } from 'svelte';
 	import Navbar from '$lib/components/core/Navbar.svelte';
+	import IconRaygun from '$lib/components/icons/IconRaygun.svelte';
 	import IconUser from '$lib/components/icons/IconUser.svelte';
 	import Footer from '$lib/components/ui/Footer.svelte';
 	import Layout from '$lib/components/ui/Layout.svelte';
 	import { authSignedIn } from '$lib/derived/auth.derived';
 	import { i18n } from '$lib/stores/i18n.store';
 	import { layoutNavigation } from '$lib/stores/layout-navigation.store';
+	import { Color } from '$lib/types/theme';
+	import { applyColor } from '$lib/utils/theme.utils';
 
 	interface Props {
 		children: Snippet;
@@ -14,12 +17,14 @@
 
 	let { children }: Props = $props();
 
-	onMount(() =>
+	onMount(() => {
+		applyColor(Color.LAVENDER_BLUE);
+
 		layoutNavigation.set({
 			title: $i18n.cli.title,
 			icon: IconUser
-		})
-	);
+		});
+	});
 </script>
 
 <Layout centered={true}>

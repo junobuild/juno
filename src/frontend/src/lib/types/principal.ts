a729204ddc674c73ef8d2f338a26fae191ff6174
David Dal Busco <david.dalbusco@outlook.com>
2025-01-12 14:52:35 +0100

    feat: zod validator for principal text (#1062)
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/lib/types/principal.ts
+++ src/frontend/src/lib/types/principal.ts
@@ -1,1 +1,18 @@
-export type PrincipalText = string;
+import { Principal } from '@dfinity/principal';
+import { z } from 'zod';
+
+export const PrincipalTextSchema = z.string().refine(
+	(principal) => {
+		try {
+			Principal.fromText(principal);
+			return true;
+		} catch (err: unknown) {
+			return false;
+		}
+	},
+	{
+		message: 'Invalid textual representation of a Principal.'
+	}
+);
+
+export type PrincipalText = z.infer<typeof PrincipalTextSchema>;

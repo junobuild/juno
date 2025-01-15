18f65b63b2f074060326f3a070a0a19750e05946
David Dal Busco <david.dalbusco@outlook.com>
2025-01-15 12:12:16 +0100

    feat: review warning messages
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

---
+++ src/frontend/src/lib/components/warning/VersionWarning.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import IconNewReleases from '$lib/components/icons/IconNewReleases.svelte';
+	import Html from '$lib/components/ui/Html.svelte';
+	import { i18n } from '$lib/stores/i18n.store';
+
+	interface Props {
+		text: string;
+		onclick: () => Promise<void>;
+	}
+
+	let { text, onclick }: Props = $props();
+</script>
+
+<div class="container">
+	<div class="icon"><IconNewReleases /></div>
+
+	<div>
+		<p><Html {text} /></p>
+
+		<button class="primary" {onclick}>{$i18n.canisters.upgrade}</button>
+	</div>
+</div>
+
+<style lang="scss">
+	@use '../../styles/mixins/info';
+
+	.container {
+		@include info.warning;
+
+		button {
+			@include info.warning-button;
+		}
+	}
+
+	p {
+		margin: 0 0 var(--padding-1_5x);
+	}
+
+	button {
+		margin: 0 0 var(--padding-0_5x);
+	}
+
+	.icon {
+		min-width: var(--padding-4x);
+	}
+</style>

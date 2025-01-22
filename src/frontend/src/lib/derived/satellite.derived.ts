58f2851b2de86b1c50e69759cc3ee9cf9d1f5294
David Dal Busco <david.dalbusco@outlook.com>
2025-01-22 08:30:32 +0100

    feat: use page satellite id in satellite derived (#1130)
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/lib/derived/satellite.derived.ts
+++ src/frontend/src/lib/derived/satellite.derived.ts
@@ -1,25 +1,24 @@
-import { page } from '$app/stores';
 import type { Satellite } from '$declarations/mission_control/mission_control.did';
+import { pageSatelliteId } from '$lib/derived/page.derived.svelte';
 import { satellitesStore } from '$lib/derived/satellites.derived';
+import type { Option } from '$lib/types/utils';
 import { isNullish } from '@dfinity/utils';
 import { derived, type Readable } from 'svelte/store';
 
-export const satelliteStore: Readable<Satellite | undefined | null> = derived(
-	[satellitesStore, page],
-	([satellites, page]) => {
-		const { data } = page;
-
-		if (isNullish(data.satellite)) {
+export const satelliteStore: Readable<Option<Satellite>> = derived(
+	[satellitesStore, pageSatelliteId],
+	([$satellites, $pageSatelliteId]) => {
+		if (isNullish($pageSatelliteId)) {
 			return undefined;
 		}
 
 		// Satellites not loaded yet
-		if (satellites === undefined) {
+		if ($satellites === undefined) {
 			return undefined;
 		}
 
-		const satellite = (satellites ?? []).find(
-			({ satellite_id }) => satellite_id.toText() === data.satellite
+		const satellite = ($satellites ?? []).find(
+			({ satellite_id }) => satellite_id.toText() === $pageSatelliteId
 		);
 
 		// eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing

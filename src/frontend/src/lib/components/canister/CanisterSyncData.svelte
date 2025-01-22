8d1e4a59e0790c528fbffb67a318a63748a3c63d
David Dal Busco <david.dalbusco@outlook.com>
2025-01-22 17:25:27 +0100

    chore: lint

--- src/frontend/src/lib/components/canister/CanisterSyncData.svelte
+++ src/frontend/src/lib/components/canister/CanisterSyncData.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
 	import type { Principal } from '@dfinity/principal';
 	import type { Snippet } from 'svelte';
-	import { canisterMonitoringUncertifiedStore } from '$lib/stores/canister-monitoring.store';
 	import { canisterSyncDataUncertifiedStore } from '$lib/stores/canister-sync-data.store';
-	import type { CanisterMonitoringData, CanisterSyncData } from '$lib/types/canister';
+	import type { CanisterSyncData } from '$lib/types/canister';
 
 	interface Props {
 		canisterId: Principal;

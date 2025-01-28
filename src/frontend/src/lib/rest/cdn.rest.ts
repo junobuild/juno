1dae95d4491a9a527c292b9d57c3028dbd1fbb07
David Dal Busco <david.dalbusco@outlook.com>
2025-01-28 15:56:25 +0100

    feat: import from utils

--- src/frontend/src/lib/rest/cdn.rest.ts
+++ src/frontend/src/lib/rest/cdn.rest.ts
@@ -1,6 +1,6 @@
 import { last } from '$lib/utils/utils';
 import type { ReleaseMetadata, ReleasesMetadata } from '@junobuild/admin';
-import { assertNonNullish } from '@junobuild/utils';
+import { assertNonNullish } from '@dfinity/utils';
 import { compare } from 'semver';
 
 export const getReleasesMetadata = async (): Promise<ReleasesMetadata> => {

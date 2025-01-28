e873cb07b32df9d7717b811edd6b257e2ece2e9a
David Dal Busco <david.dalbusco@outlook.com>
2025-01-28 17:58:18 +0100

    feat: refresh icon copy

--- src/frontend/src/lib/components/icons/IconCopy.svelte
+++ src/frontend/src/lib/components/icons/IconCopy.svelte
@@ -1,11 +1,19 @@
-<!-- source: https://fonts.google.com/icons?selected=Material%20Icons%20Outlined%3Acontent_copy%3A -->
+<!-- source: https://fonts.google.com/icons?selected=Material+Symbols+Outlined:content_copy:FILL@0;wght@100;GRAD@0;opsz@24&icon.size=24&icon.color=%23000000&icon.query=copy&icon.set=Material+Symbols -->
+<script lang="ts">
+	interface Props {
+		size?: string;
+	}
+
+	let { size = '16px' }: Props = $props();
+</script>
+
 <svg
 	xmlns="http://www.w3.org/2000/svg"
-	height="16px"
-	viewBox="0 0 24 24"
-	width="16px"
+	height={size}
+	width={size}
+	viewBox="0 -960 960 960"
 	fill="currentColor"
-	><path d="M0 0h24v24H0V0z" fill="none" /><path
-		d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"
+	><path
+		d="M362.31-260Q332-260 311-281q-21-21-21-51.31v-455.38Q290-818 311-839q21-21 51.31-21h335.38Q728-860 749-839q21 21 21 51.31v455.38Q770-302 749-281q-21 21-51.31 21H362.31Zm0-60h335.38q4.62 0 8.46-3.85 3.85-3.84 3.85-8.46v-455.38q0-4.62-3.85-8.46-3.84-3.85-8.46-3.85H362.31q-4.62 0-8.46 3.85-3.85 3.84-3.85 8.46v455.38q0 4.62 3.85 8.46 3.84 3.85 8.46 3.85Zm-140 200Q192-120 171-141q-21-21-21-51.31v-515.38h60v515.38q0 4.62 3.85 8.46 3.84 3.85 8.46 3.85h395.38v60H222.31ZM350-320v-480 480Z"
 	/></svg
 >

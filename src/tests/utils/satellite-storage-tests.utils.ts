c0f02bef8585ed098935b6b7249ba49c2b87c8d0
David Dal Busco <david.dalbusco@outlook.com>
2025-02-02 09:35:41 +0100

    refactor: extract test mocks and utils for storage (#1173)

---
+++ src/tests/utils/satellite-storage-tests.utils.ts
@@ -0,0 +1,39 @@
+import type { _SERVICE as SatelliteActor } from '$declarations/satellite/satellite.did';
+import { arrayBufferToUint8Array, toNullable } from '@dfinity/utils';
+import type { Actor } from '@hadronous/pic';
+import { mockBlob } from '../mocks/storage.mocks';
+
+export const uploadAsset = async ({
+	full_path,
+	name,
+	collection,
+	actor
+}: {
+	full_path: string;
+	name: string;
+	collection: string;
+	actor: Actor<SatelliteActor>;
+}) => {
+	const { commit_asset_upload, upload_asset_chunk, init_asset_upload } = actor;
+
+	const file = await init_asset_upload({
+		collection,
+		description: toNullable(),
+		encoding_type: [],
+		full_path,
+		name,
+		token: toNullable()
+	});
+
+	const chunk = await upload_asset_chunk({
+		batch_id: file.batch_id,
+		content: arrayBufferToUint8Array(await mockBlob.arrayBuffer()),
+		order_id: [0n]
+	});
+
+	await commit_asset_upload({
+		batch_id: file.batch_id,
+		chunk_ids: [chunk.chunk_id],
+		headers: []
+	});
+};

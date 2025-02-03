69b56e83ec23f234079ffecddb9acbf3b89b2efb
David Dal Busco <david.dalbusco@outlook.com>
2025-02-03 21:38:35 +0100

    feat: increment and assert

--- src/libs/satellite/src/usage/user_usage.rs
+++ src/libs/satellite/src/usage/user_usage.rs
@@ -1,17 +1,9 @@
 use crate::types::state::CollectionType;
-use crate::usage::state::{set_user_usage, update_user_usage};
+use crate::usage::state::set_user_usage;
 use crate::usage::types::state::UserUsage;
 use crate::usage::utils::is_storage_collection_no_usage;
 use junobuild_collections::types::core::CollectionKey;
 use junobuild_shared::types::state::UserId;
-
-pub fn increase_storage_usage(collection: &CollectionKey, user_id: &UserId) {
-    if is_storage_collection_no_usage(collection) {
-        return;
-    }
-
-    update_user_usage(collection, &CollectionType::Storage, user_id, None);
-}
 
 pub fn set_storage_usage(
     collection: &CollectionKey,
@@ -28,20 +20,4 @@ pub fn set_storage_usage(
     let usage = set_user_usage(collection, &CollectionType::Storage, user_id, count);
 
     Ok(usage)
-}
-
-pub fn decrease_storage_usage(collection: &CollectionKey, user_id: &UserId) {
-    if is_storage_collection_no_usage(collection) {
-        return;
-    }
-
-    update_user_usage(collection, &CollectionType::Storage, user_id, None);
-}
-
-pub fn decrease_storage_usage_by(collection: &CollectionKey, user_id: &UserId, count: u32) {
-    if is_storage_collection_no_usage(collection) {
-        return;
-    }
-
-    update_user_usage(collection, &CollectionType::Storage, user_id, Some(count));
 }

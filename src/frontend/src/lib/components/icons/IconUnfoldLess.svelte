8080482441c94754d0af02cd4e0fcce56891fa66
David Dal Busco <david.dalbusco@outlook.com>
2025-02-05 08:51:50 +0100

    feat: review collapsible icons and alignment

---
+++ src/frontend/src/lib/components/icons/IconUnfoldLess.svelte
@@ -0,0 +1,19 @@
+<!-- source: https://fonts.google.com/icons?selected=Material+Symbols+Rounded:unfold_less:FILL@0;wght@400;GRAD@0;opsz@24&icon.size=24&icon.color=%23000000&icon.query=collapse&icon.style=Rounded -->
+<script lang="ts">
+	interface Props {
+		size?: string;
+	}
+
+	let { size = '24px' }: Props = $props();
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	height={size}
+	width={size}
+	fill="currentColor"
+	viewBox="0 -960 960 960"
+	><path
+		d="m480-284-96 96q-11 11-28 11t-28-11q-11-11-11-28t11-28l124-124q6-6 13-8.5t15-2.5q8 0 15 2.5t13 8.5l124 124q11 11 11 28t-11 28q-11 11-28 11t-28-11l-96-96Zm0-392 96-96q11-11 28-11t28 11q11 11 11 28t-11 28L508-592q-6 6-13 8.5t-15 2.5q-8 0-15-2.5t-13-8.5L328-716q-11-11-11-28t11-28q11-11 28-11t28 11l96 96Z"
+	/></svg
+>

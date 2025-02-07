176c8bed8b887fc2baed1972d6a97b48c338c600
David Dal Busco <david.dalbusco@outlook.com>
2025-02-07 15:13:38 +0100

    build: ic-wasm certification and type a bit more generic and set for test_satellite plus adds it for docker

--- scripts/cargo.sh
+++ scripts/cargo.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ -z "$1" ]; then
-  echo "Usage: $0 <module_name>"
+  echo "Usage: $0 <module_name> [with_certification] [--build-type=stock|extended]"
   exit 1
 fi
 
@@ -9,6 +9,28 @@ CARGO_HOME=${CARGO_HOME:-$HOME/.cargo}
 
 MODULE=$1
 WASM_MODULE="${MODULE}.wasm"
+WITH_CERTIFICATION=0
+BUILD_TYPE=
+
+# Parse optional arguments
+shift
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --with-certification)
+      WITH_CERTIFICATION=1
+      shift
+      ;;
+    --build-type=*)
+      BUILD_TYPE="${1#--build-type=}"
+      shift
+      ;;
+    *)
+      echo "ERROR: unknown argument $1"
+      echo "Usage: $0 <module_name> [--with-certification] [--build-type=stock|extended]"
+      exit 1
+      ;;
+  esac
+done
 
 TARGET="wasm32-unknown-unknown"
 RUSTFLAGS="--remap-path-prefix $CARGO_HOME=/cargo -C link-args=-zstack-size=3000000"
@@ -48,13 +70,13 @@ ic-wasm \
 # adds the content of did to the `icp:public candid:service` custom section of the public metadata in the wasm
 ic-wasm "${WASM_DIR}/${WASM_MODULE}" -o "${WASM_DIR}/${WASM_MODULE}" metadata candid:service -f "${DID_DIR}/${WASM_MODULE}.did" -v public --keep-name-section
 
-if [ "${WASM_MODULE}" == "satellite" ]
+if [ -n "$BUILD_TYPE" ]
 then
   # add the type of build "stock" to the satellite. This way, we can identify whether it's the standard canister ("stock") or a custom build ("extended") of the developer.
-  ic-wasm "${WASM_DIR}/${WASM_MODULE}" -o "${WASM_DIR}/${WASM_MODULE}" metadata juno:build -d "stock" -v public --keep-name-section
+  ic-wasm "${WASM_DIR}/${WASM_MODULE}" -o "${WASM_DIR}/${WASM_MODULE}" metadata juno:build -d "$BUILD_TYPE" -v public --keep-name-section
 fi
 
-if [ "${MODULE}" == "satellite" ] || [ "${MODULE}" == "console" ];
+if [ "$WITH_CERTIFICATION" -eq 1 ];
 then
   # indicate support for certificate version 1 and 2 in the canister metadata
   ic-wasm "${WASM_DIR}/${WASM_MODULE}" -o "${WASM_DIR}/${WASM_MODULE}" metadata supported_certificate_versions -d "1,2" -v public --keep-name-section

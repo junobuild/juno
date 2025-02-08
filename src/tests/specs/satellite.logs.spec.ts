f2dc288ba1d710be4320c4f3400c5a5e12ed440d
David Dal Busco <david.dalbusco@outlook.com>
2025-02-08 08:35:32 +0100

    test: log error, keys and capacity

--- src/tests/specs/satellite.logs.spec.ts
+++ src/tests/specs/satellite.logs.spec.ts
@@ -6,6 +6,7 @@ import { type Actor, PocketIc } from '@hadronous/pic';
 import { fromArray } from '@junobuild/utils';
 import { afterAll, beforeAll, describe, expect, inject } from 'vitest';
 import { mockSetRule } from './mocks/collection.mocks';
+import { mockListParams } from './mocks/list.mocks';
 import { tick } from './utils/pic-tests.utils';
 import { createDoc as createDocUtils } from './utils/satellite-doc-tests.utils';
 import { controllersInitArgs, TEST_SATELLITE_WASM_PATH } from './utils/setup-tests.utils';
@@ -51,23 +52,26 @@ describe('Satellite Logging', () => {
 		await pic?.tearDown();
 	});
 
-	it('should log an info when on_set_doc hook is fired', async () => {
-		await createDocUtils({
+	const waitServerlessFunction = async () => {
+		// Wait for the serverless function to being fired
+		await tick(pic);
+	};
+
+	const createDoc = async (): Promise<string> => {
+		return createDocUtils({
 			actor,
 			collection: TEST_COLLECTION
 		});
+	};
 
-		// Wait for the serverless function to being fired
-		await tick(pic);
+	it('should log an info when on_set_doc hook is fired', async () => {
+		await createDoc();
+
+		await waitServerlessFunction();
 
 		const { list_docs } = actor;
 
-		const { items: logs } = await list_docs('#log', {
-			matcher: toNullable(),
-			order: toNullable(),
-			owner: toNullable(),
-			paginate: toNullable()
-		});
+		const { items: logs } = await list_docs('#log', mockListParams);
 
 		expect(logs).toHaveLength(1);
 
@@ -81,5 +85,72 @@ describe('Satellite Logging', () => {
 			level: 'Info',
 			message: 'Hello world'
 		});
+	});
+
+	it('should log an error when on_delete_doc hook is fired', async () => {
+		const { list_docs, del_doc } = actor;
+
+		const { items } = await list_docs(TEST_COLLECTION, mockListParams);
+
+		const [item] = items;
+		const [key, doc] = item;
+
+		await del_doc(TEST_COLLECTION, key, doc);
+
+		await waitServerlessFunction();
+
+		const { items: logs } = await list_docs('#log', {
+			...mockListParams,
+			order: toNullable({
+				desc: true,
+				field: { CreatedAt: null }
+			})
+		});
+
+		expect(logs).toHaveLength(2);
+
+		const [log, _] = logs;
+		const [__, docLog] = log;
+
+		const data = await fromArray(docLog.data);
+
+		expect(data).toEqual({
+			data: null,
+			level: 'Error',
+			message: 'Delete Hello world'
+		});
+	});
+
+	it('should create logs with different random keys', async () => {
+		await Promise.all(Array.from({ length: 10 }).map(createDoc));
+
+		await waitServerlessFunction();
+
+		const { list_docs } = actor;
+
+		const { items: logs } = await list_docs('#log', mockListParams);
+
+		expect(logs).toHaveLength(12);
+
+		const keys = new Set([...logs.map(([key, _]) => key)]);
+		expect(keys).toHaveLength(12);
+
+		// Log key is format!("{}-{}", time(), nonce)
+		// When we create doc and log with serverless without tick, the time should be the same
+		// Therefore, without nonce, the count of entry should be smaller than the effective count of time we logged
+		const trimmedKey = new Set([...logs.map(([key, _]) => key.split('-')[0])]);
+		expect(trimmedKey.size).toBeLessThan(12);
+	});
+
+	it('should limit log entries to hundred', async () => {
+		await Promise.all(Array.from({ length: 101 }).map(createDoc));
+
+		await waitServerlessFunction();
+
+		const { list_docs } = actor;
+
+		const { items: logs } = await list_docs('#log', mockListParams);
+
+		expect(logs).toHaveLength(100);
 	});
 });

ae08e17fdc1d537edabc0c3ce12a3fb525f2c997
David Dal Busco <david.dalbusco@outlook.com>
2025-02-10 08:30:09 +0100

    refactor: rename collection const

--- src/libs/collections/src/constants.rs
+++ src/libs/collections/src/constants.rs
@@ -5,9 +5,9 @@ use junobuild_shared::rate::constants::DEFAULT_RATE_CONFIG;
 
 pub const SYS_COLLECTION_PREFIX: char = '#';
 
-pub const LOG_COLLECTION_KEY: &str = "#log";
+pub const COLLECTION_LOG_KEY: &str = "#log";
 
-pub const DEFAULT_DB_LOG_RULE: SetRule = SetRule {
+pub const COLLECTION_LOG_DEFAULT_RULE: SetRule = SetRule {
     read: Controllers,
     write: Controllers,
     memory: Some(Memory::Stable),
@@ -34,26 +34,26 @@ pub const DEFAULT_DB_COLLECTIONS: [(&str, SetRule); 2] = [
             rate_config: Some(DEFAULT_RATE_CONFIG),
         },
     ),
-    (LOG_COLLECTION_KEY, DEFAULT_DB_LOG_RULE),
+    (COLLECTION_LOG_KEY, COLLECTION_LOG_DEFAULT_RULE),
 ];
 
-pub const DB_COLLECTIONS_NO_USER_USAGE: [&str; 1] = [LOG_COLLECTION_KEY];
+pub const DB_COLLECTIONS_WITHOUT_USER_USAGE: [&str; 1] = [COLLECTION_LOG_KEY];
 
-pub const ASSET_COLLECTION_KEY: &str = "#dapp";
+pub const COLLECTION_ASSET_KEY: &str = "#dapp";
 
-pub const DEFAULT_ASSETS_COLLECTIONS: [(&str, SetRule); 1] = [(
-    ASSET_COLLECTION_KEY,
-    SetRule {
-        read: Controllers,
-        write: Controllers,
-        memory: Some(Memory::Heap),
-        mutable_permissions: Some(false),
-        max_size: None,
-        max_capacity: None,
-        max_changes_per_user: None,
-        version: None,
-        rate_config: None,
-    },
-)];
+const COLLECTION_ASSET_DEFAULT_RULE: SetRule = SetRule {
+    read: Controllers,
+    write: Controllers,
+    memory: Some(Memory::Heap),
+    mutable_permissions: Some(false),
+    max_size: None,
+    max_capacity: None,
+    max_changes_per_user: None,
+    version: None,
+    rate_config: None,
+};
 
-pub const ASSETS_COLLECTIONS_NO_USER_USAGE: [&str; 1] = [ASSET_COLLECTION_KEY];
+pub const DEFAULT_ASSETS_COLLECTIONS: [(&str, SetRule); 1] =
+    [(COLLECTION_ASSET_KEY, COLLECTION_ASSET_DEFAULT_RULE)];
+
+pub const ASSETS_COLLECTIONS_WITHOUT_USER_USAGE: [&str; 1] = [COLLECTION_ASSET_KEY];

248716dfa67b909e54591823a6fed8e53d0af865
David Dal Busco <david.dalbusco@outlook.com>
2025-02-11 06:59:25 +0100

    feat: cleanup

--- src/libs/satellite/src/usage/types.rs
+++ src/libs/satellite/src/usage/types.rs
@@ -1,17 +1,12 @@
 pub mod state {
     use crate::types::state::CollectionType;
-    use candid::CandidType;
     use junobuild_collections::types::core::CollectionKey;
     use junobuild_shared::types::state::UserId;
     use serde::{Deserialize, Serialize};
 
     /// A unique key for identifying user usage within a collection.
-    ///
-    /// It consists of:
-    /// - `user_id`: The unique identifier for the user which is matched to the caller.
-    /// - `collection_key`: The collection where usage is tracked.
-    /// - `collection_type`: The type of collection (`Db` for datastore, `Storage` for assets).
-    #[derive(CandidType, Serialize, Deserialize, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+    /// The key will be parsed to `user-id#db|storage#collection`.
+    #[derive(Serialize, Deserialize)]
     pub struct UserUsageKey {
         pub user_id: UserId,
         pub collection_key: CollectionKey,
@@ -19,14 +14,8 @@ pub mod state {
     }
 
     /// Tracks the usage (create, set and delete) of a user in a collection.
-    ///
-    ///
-    /// Fields:
-    /// - `changes_count`: The total number of changes (create/update/delete) by the user.
-    /// - `created_at`: The timestamp when this user usage entry was first recorded.
-    /// - `updated_at`: The timestamp of the last update to this user usage entry.
-    /// - `version`: An optional field representing the version of this usage entry. In the future we might implement checks to avoid overwrite but, this is not the case currently.
-    #[derive(CandidType, Serialize, Deserialize, Clone)]
+    #[derive(Serialize, Deserialize)]
+    #[serde(deny_unknown_fields)]
     pub struct UserUsageData {
         pub changes_count: u32,
     }

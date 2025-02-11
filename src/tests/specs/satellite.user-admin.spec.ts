54266cea4153230adec184dc38da770d86be1267
David Dal Busco <david.dalbusco@outlook.com>
2025-02-11 15:10:14 +0100

    test: wip

--- src/tests/specs/satellite.user-admin.spec.ts
+++ src/tests/specs/satellite.user-admin.spec.ts
@@ -1,11 +1,14 @@
 import type { _SERVICE as SatelliteActor, SetDoc } from '$declarations/satellite/satellite.did';
 import { idlFactory as idlFactorSatellite } from '$declarations/satellite/satellite.factory.did';
-import { AnonymousIdentity } from '@dfinity/agent';
+import { AnonymousIdentity, type Identity } from '@dfinity/agent';
 import { Ed25519KeyIdentity } from '@dfinity/identity';
-import { toNullable } from '@dfinity/utils';
+import { fromNullable, toNullable } from '@dfinity/utils';
 import { type Actor, PocketIc } from '@hadronous/pic';
 import { toArray } from '@junobuild/utils';
+import { nanoid } from 'nanoid';
 import { beforeAll, describe, expect, inject } from 'vitest';
+import { USER_CANNOT_WRITE } from './constants/satellite-tests.constants';
+import { mockSetRule } from './mocks/collection.mocks';
 import { controllersInitArgs, SATELLITE_WASM_PATH } from './utils/setup-tests.utils';
 
 describe('Satellite User Usage', () => {
@@ -54,7 +57,7 @@ describe('Satellite User Usage', () => {
 						version: toNullable()
 					};
 
-					await expect(set_doc('#user-admin', key, doc)).rejects.toThrow('Cannot write.');
+					await expect(set_doc('#user-admin', key, doc)).rejects.toThrow(USER_CANNOT_WRITE);
 				});
 			}
 		);
@@ -101,6 +104,77 @@ describe('Satellite User Usage', () => {
 				await expect(set_doc('#user-admin', key, doc)).rejects.toThrow(
 					'Invalid user admin data: unknown field `unknown`, expected `banned` at line 1 column 32.'
 				);
+			});
+		});
+	});
+
+	describe.only('Ban', () => {
+		const collection = 'test_banned';
+
+		beforeAll(async () => {
+			actor.setIdentity(controller);
+
+			const { set_rule } = actor;
+
+			await set_rule({ Db: null }, collection, mockSetRule);
+			await set_rule({ Storage: null }, collection, mockSetRule);
+		});
+
+		const createUser = async (user: Identity) => {
+			actor.setIdentity(user);
+
+			const { set_doc } = actor;
+
+			return await set_doc('#user', user.getPrincipal().toText(), {
+				data: await toArray({
+					provider: 'internet_identity'
+				}),
+				description: toNullable(),
+				version: toNullable()
+			});
+		};
+
+		const banUser = async (user: Identity) => {
+			actor.setIdentity(controller);
+
+			const { set_doc } = actor;
+
+			await set_doc('#user-admin', user.getPrincipal().toText(), {
+				data: await toArray({
+					banned: 'indefinite'
+				}),
+				description: toNullable(),
+				version: toNullable()
+			});
+		};
+
+		describe('get document', () => {
+			it.only('should not get document if banned', async () => {
+				const user = Ed25519KeyIdentity.generate();
+
+				await createUser(user);
+
+				const { set_doc } = actor;
+
+				const key = nanoid();
+
+				await set_doc(collection, key, {
+					data: await toArray({
+						hello: 'world'
+					}),
+					description: toNullable(),
+					version: toNullable()
+				});
+
+				await banUser(user);
+
+				actor.setIdentity(user);
+				const { get_doc } = actor;
+
+				const result = await get_doc(collection, key);
+				const doc = fromNullable(result);
+
+				expect(doc).toBeUndefined();
 			});
 		});
 	});

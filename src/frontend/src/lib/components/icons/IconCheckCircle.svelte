773f65ebffc6c135a416ccee93c056e2c742f40e
David Dal Busco <david.dalbusco@outlook.com>
2025-02-13 08:29:09 +0100

    feat: outline icon

--- src/frontend/src/lib/components/icons/IconCheckCircle.svelte
+++ src/frontend/src/lib/components/icons/IconCheckCircle.svelte
@@ -1,4 +1,4 @@
-<!-- source: https://fonts.google.com/icons?selected=Material%20Icons%20Sharp%3Acheck_circle%3A -->
+<!-- source: https://fonts.google.com/icons?selected=Material+Icons+Sharp:check_circle_outline:&icon.size=24&icon.color=%23000000&icon.query=check -->
 <script lang="ts">
 	interface Props {
 		size?: string;
@@ -14,7 +14,7 @@
 	viewBox="0 0 24 24"
 	fill="currentColor"
 	><path d="M0 0h24v24H0V0z" fill="none" /><path
-		d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"
+		d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm4.59-12.42L10 14.17l-2.59-2.58L6 13l4 4 8-8z"
 	/></svg
 >
 

b680151026df22957ef3a4d6ad954168f7cdd42e
David Dal Busco <david.dalbusco@outlook.com>
2025-02-14 12:13:19 +0100

    feat: align icons sharpness

--- src/frontend/src/lib/components/icons/IconCheckCircle.svelte
+++ src/frontend/src/lib/components/icons/IconCheckCircle.svelte
@@ -1,4 +1,4 @@
-<!-- source: https://fonts.google.com/icons?selected=Material+Icons+Sharp:check_circle_outline:&icon.size=24&icon.color=%23000000&icon.query=check -->
+<!-- source: https://fonts.google.com/icons?selected=Material+Symbols+Outlined:check_circle:FILL@0;wght@200;GRAD@0;opsz@24&icon.size=24&icon.color=%23000000&icon.query=check&icon.set=Material+Symbols -->
 <script lang="ts">
 	interface Props {
 		size?: string;
@@ -11,10 +11,10 @@
 	xmlns="http://www.w3.org/2000/svg"
 	height={size}
 	width={size}
-	viewBox="0 0 24 24"
+	viewBox="0 -960 960 960"
 	fill="currentColor"
-	><path d="M0 0h24v24H0V0z" fill="none" /><path
-		d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm4.59-12.42L10 14.17l-2.59-2.58L6 13l4 4 8-8z"
+	><path
+		d="m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"
 	/></svg
 >
 

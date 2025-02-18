1fc10422a2cf51be474cc53bb1a84fa88da41ae8
David Dal Busco <david.dalbusco@outlook.com>
2025-02-18 07:31:17 +0100

    chore: lint

--- src/frontend/src/routes/(home)/+layout.svelte
+++ src/frontend/src/routes/(home)/+layout.svelte
@@ -3,7 +3,6 @@
 	import Navbar from '$lib/components/core/Navbar.svelte';
 	import Footer from '$lib/components/ui/Footer.svelte';
 	import Layout from '$lib/components/ui/Layout.svelte';
-	import { authNotSignedIn, authSignedOut } from '$lib/derived/auth.derived';
 	import { layoutNavigation } from '$lib/stores/layout-navigation.store';
 	import { Color } from '$lib/types/theme';
 	import { applyColor } from '$lib/utils/theme.utils';

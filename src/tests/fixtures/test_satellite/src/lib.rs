439b8d3e3b26a432265bc2775513a7c6fe6bc4e0
David Dal Busco <david.dalbusco@outlook.com>
2025-02-27 17:39:34 +0100

    feat: expose random for serverless function

--- src/tests/fixtures/test_satellite/src/lib.rs
+++ src/tests/fixtures/test_satellite/src/lib.rs
@@ -1,5 +1,6 @@
+use ic_cdk::{update};
 use junobuild_macros::{on_delete_doc, on_init_sync, on_post_upgrade_sync, on_set_doc};
-use junobuild_satellite::{error, include_satellite, info, OnDeleteDocContext, OnSetDocContext};
+use junobuild_satellite::{error, include_satellite, info, random, OnDeleteDocContext, OnSetDocContext};
 
 #[on_set_doc]
 fn on_set_doc(_context: OnSetDocContext) -> Result<(), String> {
@@ -23,6 +24,11 @@ fn on_init_sync() {
 #[on_post_upgrade_sync]
 fn on_post_upgrade_sync() {
     ic_cdk::print("On post upgrade sync was executed");
+}
+
+#[update]
+fn get_random() -> Result<i32, String> {
+    random()
 }
 
 include_satellite!();

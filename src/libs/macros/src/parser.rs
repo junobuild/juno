06abc1cd0ee27535c0bc84b12d635276777a4170
David Dal Busco <david.dalbusco@outlook.com>
2025-02-27 20:54:43 +0100

    fix: accept result for on_init_random_seed because it's deferred

--- src/libs/macros/src/parser.rs
+++ src/libs/macros/src/parser.rs
@@ -112,8 +112,8 @@ fn parse_hook(hook: &Hook, attr: TokenStream, item: TokenStream) -> Result<Token
     let hook_fn = Ident::new(&map_hook_name(hook.clone()), proc_macro2::Span::call_site());
 
     match hook {
-        Hook::OnPostUpgrade | Hook::OnInit => parse_lifecycle_hook(&ast, signature, &hook_fn),
-        Hook::OnPostUpgradeSync | Hook::OnInitSync | Hook::OnInitRandomSeed => {
+        Hook::OnPostUpgrade | Hook::OnInit | Hook::OnInitRandomSeed => parse_lifecycle_hook(&ast, signature, &hook_fn),
+        Hook::OnPostUpgradeSync | Hook::OnInitSync => {
             parse_lifecycle_sync_hook(&ast, signature, &hook_fn)
         }
         _ => parse_doc_hook(&ast, signature, &hook_fn, hook, attr),

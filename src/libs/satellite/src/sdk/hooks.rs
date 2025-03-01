ebc8a456c45f722132da7fd0f4e309ffc10b611f
David Dal Busco <david.dalbusco@outlook.com>
2025-03-01 11:46:51 +0100

    refactor: extract re-exported function to a sdk module

---
+++ src/libs/satellite/src/sdk/hooks.rs
@@ -0,0 +1,6 @@
+pub use crate::types::hooks::{
+    AssertDeleteAssetContext, AssertDeleteDocContext, AssertSetDocContext,
+    AssertUploadAssetContext, HookContext, OnDeleteAssetContext, OnDeleteDocContext,
+    OnDeleteFilteredAssetsContext, OnDeleteFilteredDocsContext, OnDeleteManyAssetsContext,
+    OnDeleteManyDocsContext, OnSetDocContext, OnSetManyDocsContext, OnUploadAssetContext,
+};
---
+++ src/libs/satellite/src/sdk/mod.rs
@@ -0,0 +1,15 @@
+mod controllers;
+mod db;
+mod hooks;
+mod logs;
+mod memory;
+mod random;
+mod storage;
+
+pub use controllers::*;
+pub use db::*;
+pub use hooks::*;
+pub use logs::*;
+pub use memory::*;
+pub use random::*;
+pub use storage::*;

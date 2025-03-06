169b404b2cf3efc8e91a3bfc4ee9c97958c9a745
David Dal Busco <david.dalbusco@outlook.com>
2025-03-06 07:18:12 +0100

    feat: init rules context

--- src/frontend/src/lib/stores/rules.store.ts
+++ src/frontend/src/lib/stores/rules.store.ts
@@ -1,5 +1,4 @@
 import type { CollectionType } from '$declarations/satellite/satellite.did';
-import { DbCollectionType } from '$lib/constants/rules.constants';
 import { authStore } from '$lib/stores/auth.store';
 import type { RulesContext, RulesData } from '$lib/types/rules.context';
 import { reloadContextRules } from '$lib/utils/rules.utils';
@@ -7,29 +6,40 @@ import type { Principal } from '@dfinity/principal';
 import { get, writable } from 'svelte/store';
 
 export const initRulesContext = ({
-	satelliteId,
+	satelliteId: initialSatelliteId,
 	type
 }: {
 	satelliteId: Principal;
 	type: CollectionType;
 }): RulesContext => {
 	const store = writable<RulesData>({
-		satelliteId,
+		satelliteId: initialSatelliteId,
 		rules: undefined,
 		rule: undefined
 	});
 
 	const reloadRules = async () =>
 		await reloadContextRules({
-			satelliteId,
+			satelliteId: get(store).satelliteId,
 			type,
 			store,
 			// TODO: pass auth as argument
 			identity: get(authStore).identity
 		});
 
+	const initRules = async (satelliteId: Principal) => {
+		store.set({
+			satelliteId,
+			rules: undefined,
+			rule: undefined
+		});
+
+		await reloadRules();
+	};
+
 	return {
 		store,
-		reload: reloadRules
+		reload: reloadRules,
+		init: initRules
 	};
 };

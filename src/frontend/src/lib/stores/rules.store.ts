feab0f2e88cd57800aa0dfa3e86f8d2f515b4911
David Dal Busco <david.dalbusco@outlook.com>
2025-03-06 06:51:58 +0100

    refactor: extract init rules context and resolve svelte v5 leftover (#1307)
    * refactor: extract init rules context and resolve svelte v5 leftover
    
    * chore: add todo

---
+++ src/frontend/src/lib/stores/rules.store.ts
@@ -0,0 +1,34 @@
+import type { CollectionType } from '$declarations/satellite/satellite.did';
+import { DbCollectionType } from '$lib/constants/rules.constants';
+import { authStore } from '$lib/stores/auth.store';
+import type { RulesContext, RulesData } from '$lib/types/rules.context';
+import { reloadContextRules } from '$lib/utils/rules.utils';
+import type { Principal } from '@dfinity/principal';
+import { get, writable } from 'svelte/store';
+
+export const initRulesContext = ({
+	satelliteId
+}: {
+	satelliteId: Principal;
+	type: CollectionType;
+}): RulesContext => {
+	const store = writable<RulesData>({
+		satelliteId,
+		rules: undefined,
+		rule: undefined
+	});
+
+	const reloadRules = async () =>
+		await reloadContextRules({
+			satelliteId,
+			type: DbCollectionType,
+			store,
+			// TODO: pass auth as argument
+			identity: get(authStore).identity
+		});
+
+	return {
+		store,
+		reload: reloadRules
+	};
+};

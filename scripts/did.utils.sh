6160db834d2b2cac8823a6c639d8809d671439f9
David Dal Busco <david.dalbusco@outlook.com>
2025-03-07 14:25:19 +0100

    feat: make did script easier to extend

--- scripts/did.utils.sh
+++ scripts/did.utils.sh
@@ -11,3 +11,12 @@ function generate_did() {
     candid-extractor "target/wasm/$canister.wasm" > "$canister_root/$did_filename.did"
   fi
 }
+
+function prepend_import_did() {
+  local crate=$1
+  local canister_root=$2
+  local canister=$3
+  local canister_extension=$4
+
+  echo -e "import service \"$canister_extension.did\";\n\n$(cat  "$crate")" >  "$canister_root/$canister.did"
+}

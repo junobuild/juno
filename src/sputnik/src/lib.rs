85e2ffa77ae0b5728281d5cdb52c05a7ee87e481
David Dal Busco <david.dalbusco@outlook.com>
2025-03-12 06:24:32 +0100

    refactor: move polyfill

--- src/sputnik/src/lib.rs
+++ src/sputnik/src/lib.rs
@@ -2,12 +2,12 @@
 
 mod errors;
 mod js;
+mod polyfill;
 mod state;
-mod wasi_polyfill;
 
-use crate::wasi_polyfill::{init_wasi, update_wasi_seed};
 use junobuild_macros::{on_init_random_seed, on_init_sync, on_post_upgrade_sync};
 use junobuild_satellite::include_satellite;
+use polyfill::wasi::{init_wasi, update_wasi_seed};
 
 #[on_init_sync]
 fn on_init_sync() {

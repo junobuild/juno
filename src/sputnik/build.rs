e350ac3608e54c90add20e8aa3ec5a9c67e1fe00
David Dal Busco <david.dalbusco@outlook.com>
2025-03-12 12:33:16 +0100

    fix: re-run on environment variable change

--- src/sputnik/build.rs
+++ src/sputnik/build.rs
@@ -1,8 +1,8 @@
 use std::env;
 
 fn main() {
-    // Always re-rerun a build. Useful if nothing changes but the environment variable did.
-    println!("cargo:rerun-if-changed=");
+    // Re-rerun a build if env variable DEV_SCRIPT_PATH change.
+    println!("cargo:rerun-if-env-changed=DEV_SCRIPT_PATH");
 
     let default_path = "../../resources/index.mjs";
     let script_path = env::var("DEV_SCRIPT_PATH").unwrap_or_else(|_| default_path.to_string());

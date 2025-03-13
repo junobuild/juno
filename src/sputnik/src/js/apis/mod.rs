414b1e6791208b91936f373a0e16c453805ab35b
David Dal Busco <david.dalbusco@outlook.com>
2025-03-13 08:17:17 +0100

    feat: ic_cdk::print for JS and console usage (#1348)

--- src/sputnik/src/js/apis/mod.rs
+++ src/sputnik/src/js/apis/mod.rs
@@ -1,9 +1,12 @@
+mod ic_cdk;
 mod node;
 
+use crate::js::apis::ic_cdk::init_ic_cdk_apis;
 use crate::js::apis::node::init_node_apis;
 use rquickjs::{Ctx, Error as JsError};
 
 pub fn init_apis(ctx: &Ctx) -> Result<(), JsError> {
+    init_ic_cdk_apis(ctx)?;
     init_node_apis(ctx)?;
 
     Ok(())

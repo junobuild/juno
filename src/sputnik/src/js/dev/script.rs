09ec2624ac5d0bb9dfe8a8ee34a32b9a1fce20b7
David Dal Busco <david.dalbusco@outlook.com>
2025-03-13 08:50:15 +0100

    feat(sputnik): bubble jserror instead of string (#1350)

--- src/sputnik/src/js/dev/script.rs
+++ src/sputnik/src/js/dev/script.rs
@@ -1,7 +1,7 @@
 use crate::js::constants::{DEV_MODULE_NAME, DEV_SCRIPT};
 use crate::js::module::engine::declare_module;
-use rquickjs::{Ctx, Module};
+use rquickjs::{Ctx, Error as JsError, Module};
 
-pub fn declare_dev_script<'js>(ctx: &Ctx<'js>) -> Result<Module<'js>, String> {
+pub fn declare_dev_script<'js>(ctx: &Ctx<'js>) -> Result<Module<'js>, JsError> {
     declare_module(ctx, DEV_MODULE_NAME, DEV_SCRIPT)
 }
--- src/sputnik/src/js/runtime.rs
+++ src/sputnik/src/js/runtime.rs
@@ -21,7 +21,7 @@ where
     async_with!(ctx => |ctx|{
         init_apis(&ctx).map_err(|e| format!("{} ({})", JUNO_SPUTNIK_ERROR_RUNTIME_API_INIT, e))?;
 
-        declare_dev_script(&ctx)?;
+        declare_dev_script(&ctx).map_err(|e| e.to_string())?;
 
         f.run(&ctx).await.map_err(|e| format!("{} ({})", JUNO_SPUTNIK_ERROR_RUNTIME_ASYNC_EXECUTE, e))?;
 
@@ -46,7 +46,8 @@ where
     ctx.with(|ctx| -> Result<(), String> {
         init_apis(&ctx).map_err(|e| format!("{} ({})", JUNO_SPUTNIK_ERROR_RUNTIME_API_INIT, e))?;
 
-        declare_dev_script(&ctx)?;
+        // TODO: find a way to bubble the content of the error
+        declare_dev_script(&ctx).map_err(|e| e.to_string())?;
 
         f(&ctx)
     })

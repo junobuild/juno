b91632895e24457b51922c1a04a5f82eb6e4f3a0
David Dal Busco <david.dalbusco@outlook.com>
2025-03-28 13:28:18 +0100

    test(sputnik): sdk deleteDocStore (#1390)

--- src/tests/fixtures/test_sputnik/resources/on-set-doc/sdk.ts
+++ src/tests/fixtures/test_sputnik/resources/on-set-doc/sdk.ts
@@ -1,10 +1,15 @@
 import type { OnSetDocContext } from '@junobuild/functions';
 import { id } from '@junobuild/functions/ic-cdk';
-import { decodeDocData, encodeDocData, setDocStore } from '@junobuild/functions/sdk';
+import {
+	decodeDocData,
+	deleteDocStore,
+	encodeDocData,
+	setDocStore
+} from '@junobuild/functions/sdk';
 import type { SputnikMock } from '../../../../mocks/sputnik.mocks';
 
 // eslint-disable-next-line require-await
-export const testDocUpdate = async (context: OnSetDocContext) => {
+export const testSdkSetDocStore = async (context: OnSetDocContext) => {
 	const sourceData = decodeDocData<SputnikMock>(context.data.data.after.data);
 
 	const updateData = {
@@ -22,6 +27,21 @@ export const testDocUpdate = async (context: OnSetDocContext) => {
 		doc: {
 			version: context.data.data.after.version,
 			data: encodedData
+		}
+	});
+};
+
+// eslint-disable-next-line require-await
+export const testSdkDeleteDocStore = async ({
+	caller,
+	data: { collection, key, data }
+}: OnSetDocContext) => {
+	deleteDocStore({
+		caller,
+		collection,
+		key,
+		doc: {
+			version: data.after.version
 		}
 	});
 };
---
+++ src/tests/specs/sputnik/sputnik.sdk.delete-doc-store.spec.ts
@@ -0,0 +1,53 @@
+import type { _SERVICE as SputnikActor } from '$declarations/sputnik/sputnik.did';
+import { fromNullable, toNullable } from '@dfinity/utils';
+import { type Actor, PocketIc } from '@hadronous/pic';
+import { toArray } from '@junobuild/utils';
+import { nanoid } from 'nanoid';
+import { afterAll, beforeAll, describe, inject } from 'vitest';
+import { mockSetRule } from '../../mocks/collection.mocks';
+import { mockSputnikObj } from '../../mocks/sputnik.mocks';
+import { setupTestSputnik } from '../../utils/fixtures-tests.utils';
+import { waitServerlessFunction } from '../../utils/satellite-extended-tests.utils';
+
+describe('Sputnik > sdk > deleteDocStore', () => {
+	let pic: PocketIc;
+	let actor: Actor<SputnikActor>;
+
+	const TEST_COLLECTION = 'test-deletedoc';
+
+	beforeAll(async () => {
+		pic = await PocketIc.create(inject('PIC_URL'));
+
+		const { pic: p, actor: a } = await setupTestSputnik();
+
+		pic = p;
+		actor = a;
+
+		const { set_rule } = actor;
+		await set_rule({ Db: null }, TEST_COLLECTION, mockSetRule);
+	});
+
+	afterAll(async () => {
+		await pic?.tearDown();
+	});
+
+	it('should delete document', async () => {
+		const { set_doc, get_doc } = actor;
+
+		const key = nanoid();
+
+		await set_doc(TEST_COLLECTION, key, {
+			data: await toArray(mockSputnikObj),
+			description: toNullable(),
+			version: toNullable()
+		});
+
+		await waitServerlessFunction(pic);
+
+		const result = await get_doc(TEST_COLLECTION, key);
+
+		const doc = fromNullable(result);
+
+		expect(doc).toBeUndefined();
+	});
+});

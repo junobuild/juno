4914f3852c9a269e2bb128acac5e8c2d5fd6914b
David Dal Busco <david.dalbusco@outlook.com>
2025-03-28 09:15:28 +0100

    feat(sputnik): delete doc store sdk (#1389)

--- src/sputnik/src/hooks/js/sdk/db/mod.rs
+++ src/sputnik/src/hooks/js/sdk/db/mod.rs
@@ -1,10 +1,13 @@
+mod delete_doc_store;
+mod set_doc_store;
+
+use crate::hooks::js::sdk::db::delete_doc_store::init_delete_doc_store;
 use crate::hooks::js::sdk::db::set_doc_store::init_set_doc_store;
 use rquickjs::{Ctx, Error as JsError};
 
-mod set_doc_store;
-
 pub fn init_db_sdk(ctx: &Ctx) -> Result<(), JsError> {
     init_set_doc_store(ctx)?;
+    init_delete_doc_store(ctx)?;
 
     Ok(())
 }

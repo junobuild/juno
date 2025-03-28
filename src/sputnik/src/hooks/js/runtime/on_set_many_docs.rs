302d50f4ff19112322c9e9c9d563659f33600257
David Dal Busco <david.dalbusco@outlook.com>
2025-03-28 08:08:55 +0100

    refactor(sputnik): extract hook runner (#1387)

--- src/sputnik/src/hooks/js/runtime/on_set_many_docs.rs
+++ src/sputnik/src/hooks/js/runtime/on_set_many_docs.rs
@@ -1,26 +1,14 @@
+use crate::hooks::js::runtime::runner::{execute_hook, make_loader_code};
 use crate::hooks::js::runtime::types::{JsHook, OnJsHook};
 use crate::hooks::js::types::hooks::JsHookContext;
-use crate::js::constants::{DEV_MODULE_NAME, HOOKS_MODULE_NAME};
-use crate::js::module::engine::evaluate_async_module;
 use junobuild_satellite::OnSetManyDocsContext;
-use rquickjs::{Ctx, Error as JsError, IntoJs};
+use rquickjs::{Ctx, Error as JsError};
 
 pub struct OnSetManyDocs;
 
-// TODO: refactor duplicate code
-
 impl JsHook for OnSetManyDocs {
     fn get_loader_code(&self) -> String {
-        format!(
-            r#"const {{ onSetManyDocs }} = await import("{}");
-
-            if (typeof onSetManyDocs !== 'undefined') {{
-                const config = typeof onSetManyDocs === 'function' ? onSetManyDocs({{}}) : onSetManyDocs;
-                __juno_satellite_on_set_many_docs_loader(config.collections);
-            }}
-            "#,
-            DEV_MODULE_NAME
-        )
+        make_loader_code("onSetManyDocs", "__juno_satellite_on_set_many_docs_loader")
     }
 }
 
@@ -31,21 +19,6 @@ impl OnJsHook<OnSetManyDocsContext> for OnSetManyDocs {
         context: OnSetManyDocsContext,
     ) -> Result<(), JsError> {
         let js_context = JsHookContext::from_on_set_many_docs(context, ctx)?;
-        let js_obj = js_context.into_js(ctx)?;
-
-        ctx.globals().set("jsContext", js_obj)?;
-
-        let code = format!(
-            r#"const {{ onSetManyDocs }} = await import("{}");
-
-            if (typeof onSetManyDocs !== 'undefined') {{
-                const config = typeof onSetManyDocs === 'function' ? onSetManyDocs({{}}) : onSetManyDocs;
-                await config.run(jsContext);
-            }}
-            "#,
-            DEV_MODULE_NAME
-        );
-
-        evaluate_async_module(ctx, HOOKS_MODULE_NAME, &code).await
+        execute_hook(ctx, js_context, "onSetManyDocs").await
     }
 }

84b4de7908de5cb41fd35c008219a9eab8f0354e
David Dal Busco <david.dalbusco@outlook.com>
2025-03-29 15:45:00 +0100

    test: on delete asset

--- src/tests/fixtures/test_sputnik/resources/index.ts
+++ src/tests/fixtures/test_sputnik/resources/index.ts
@@ -2,6 +2,7 @@ export * from './assert-delete-asset';
 export * from './assert-delete-doc';
 export * from './assert-set-doc';
 export * from './assert-upload-asset';
+export * from './on-delete-asset';
 export * from './on-delete-doc';
 export * from './on-delete-filtered-docs';
 export * from './on-delete-many-docs';

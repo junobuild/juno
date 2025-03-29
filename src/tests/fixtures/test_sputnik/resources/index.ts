e1595d500c9f6cbe23dcf2972a5554b396500de4
David Dal Busco <david.dalbusco@outlook.com>
2025-03-29 15:24:51 +0100

    test: upload and throw

--- src/tests/fixtures/test_sputnik/resources/index.ts
+++ src/tests/fixtures/test_sputnik/resources/index.ts
@@ -7,3 +7,4 @@ export * from './on-delete-filtered-docs';
 export * from './on-delete-many-docs';
 export { onSetDoc } from './on-set-doc';
 export * from './on-set-many-docs';
+export * from './on-upload-asset';
---
+++ src/tests/fixtures/test_sputnik/resources/on-upload-asset.ts
@@ -0,0 +1,23 @@
+import { defineHook, type OnUploadAsset } from '@junobuild/functions';
+
+/* eslint-disable no-console */
+
+export const onUploadAsset = defineHook<OnUploadAsset>({
+	collections: ['test-on-upload-asset'],
+	run: async ({ data }) => {
+		console.log('onUploadAsset called');
+
+		const asset = data;
+
+		if (asset.key.name === 'test.html') {
+			const thr = async () => {
+				throw new Error('test-async-and-trap');
+			};
+
+			await thr();
+			return;
+		}
+	}
+});
+
+/* eslint-enable */

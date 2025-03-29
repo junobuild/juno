7cdefc5ca9017b89ef9a117051fba2c53d3978c3
David Dal Busco <david.dalbusco@outlook.com>
2025-03-29 11:32:51 +0100

    feat: load loader on post_upgrade

--- src/sputnik/src/hooks/js/runtime/lifecycle/on_post_upgrade.rs
+++ src/sputnik/src/hooks/js/runtime/lifecycle/on_post_upgrade.rs
@@ -9,6 +9,7 @@ use crate::hooks::js::runtime::types::JsHook;
 use crate::js::constants::HOOKS_MODULE_NAME;
 use crate::js::module::engine::evaluate_module;
 use rquickjs::{Ctx, Error as JsError};
+use crate::hooks::js::runtime::storage::on_upload_asset::OnUploadAsset;
 
 pub fn execute_on_post_upgrade<'js>(ctx: &Ctx<'js>) -> Result<(), JsError> {
     evaluate_loaders(ctx)
@@ -21,6 +22,9 @@ fn evaluate_loaders<'js>(ctx: &Ctx<'js>) -> Result<(), JsError> {
         OnDeleteDoc.get_loader_code(),
         OnDeleteManyDocs.get_loader_code(),
         OnDeleteFilteredDocs.get_loader_code(),
+
+        OnUploadAsset.get_loader_code(),
+
         AssertSetDoc.get_loader_code(),
         AssertDeleteDoc.get_loader_code(),
     ];

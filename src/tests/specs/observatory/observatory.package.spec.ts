ad983a60b752b76c1c04090a4045ecbfb5add77b
David Dal Busco <david.dalbusco@outlook.com>
2025-04-07 21:18:51 +0200

    test: juno packages

---
+++ src/tests/specs/observatory/observatory.package.spec.ts
@@ -0,0 +1,15 @@
+import {MISSION_CONTROL_WASM_PATH, OBSERVATORY_WASM_PATH, readWasmVersion} from '../../utils/setup-tests.utils';
+import { customSectionJunoPackage } from '../../utils/wasm-tests.utils';
+
+describe('Observatory > Package', () => {
+	it('should expose public custom section juno:package', async () => {
+		const junoPkg = await customSectionJunoPackage({ path: OBSERVATORY_WASM_PATH });
+
+		const expectedPkg = {
+			name: '@junobuild/observatory',
+			version: readWasmVersion('observatory')
+		};
+
+		expect(junoPkg).toEqual(expectedPkg);
+	});
+});

acbd9b6f101485119421df09c70b04685d8fa186
David Dal Busco <david.dalbusco@outlook.com>
2025-04-08 06:10:50 +0200

    test: Wasm modules icp:public juno:package (#1418)
    * test: Wasm modules icp:public juno:package
    
    * test: juno packages
    
    * test: pkgs
    
    * chore: fmt

---
+++ src/tests/specs/orbiter/orbiter.package.spec.ts
@@ -0,0 +1,15 @@
+import { ORBITER_WASM_PATH, readWasmVersion } from '../../utils/setup-tests.utils';
+import { customSectionJunoPackage } from '../../utils/wasm-tests.utils';
+
+describe('Orbiter > Package', () => {
+	it('should expose public custom section juno:package', async () => {
+		const junoPkg = await customSectionJunoPackage({ path: ORBITER_WASM_PATH });
+
+		const expectedPkg = {
+			name: '@junobuild/orbiter',
+			version: readWasmVersion('orbiter')
+		};
+
+		expect(junoPkg).toEqual(expectedPkg);
+	});
+});

50217be93f218c9ce7a4cf0cb28a103cea429d5a
David Dal Busco <david.dalbusco@outlook.com>
2025-04-14 07:24:54 +0200

    fix: mode

--- src/frontend/tests/lib/components/wallet/WalletGetICP.test.ts
+++ src/frontend/tests/lib/components/wallet/WalletGetICP.test.ts
@@ -1,5 +1,6 @@
-import WalletGetICP from '$lib/components/wallet/WalletGetICP.svelte';
+import { describe, it, expect } from 'vitest';
 import { render } from '@testing-library/svelte';
+import WalletGetICP from '$lib/components/wallet/WalletGetICP.svelte';
 import { mockMissionControlId } from '../../../mocks/modules.mock';
 
 describe('WalletGetICP', () => {
@@ -8,16 +9,25 @@ describe('WalletGetICP', () => {
 		vi.unstubAllEnvs();
 	});
 
-	it.only('should not display button when mode is production', () => {
-		vi.stubEnv('MODE', 'production');
+	it('should not display button when mode is not dev', () => {
+		vi.stubEnv('DEV', false);
 
 		const { container } = render(WalletGetICP, { missionControlId: mockMissionControlId });
 
 		expect(container.querySelector('button')).not.toBeInTheDocument();
 	});
 
+	it('should display button when in skylab mode', () => {
+		vi.stubEnv('DEV', false);
+		vi.stubEnv('MODE', "skylab");
+
+		const { getByRole } = render(WalletGetICP, { missionControlId: mockMissionControlId });
+
+		expect(getByRole('button')).toBeInTheDocument();
+	});
+
 	it('should display button when in dev mode', () => {
-		vi.stubEnv('MODE', 'development');
+		vi.stubEnv('DEV', true);
 
 		const { getByRole } = render(WalletGetICP, { missionControlId: mockMissionControlId });
 

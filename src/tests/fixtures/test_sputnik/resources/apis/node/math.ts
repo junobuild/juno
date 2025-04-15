83fd6eeb439fe7d562ea211ed3bab51b33919cea
David Dal Busco <david.dalbusco@outlook.com>
2025-04-15 17:15:50 +0200

    test: init

---
+++ src/tests/fixtures/test_sputnik/resources/apis/node/math.ts
@@ -0,0 +1,9 @@
+/* eslint-disable require-await, no-console */
+
+export const testMathRandom = async () => {
+	console.log('One:', Math.random());
+	console.log('Two:', Math.random());
+	console.log('Three:', Math.random());
+};
+
+/* eslint-enable */

d92d09806598cbc993c4dadb4fb265a10dc20dd7
David Dal Busco <david.dalbusco@outlook.com>
2025-04-15 17:32:37 +0200

    test: random

--- src/tests/fixtures/test_sputnik/resources/apis/node/math.ts
+++ src/tests/fixtures/test_sputnik/resources/apis/node/math.ts
@@ -1,9 +1,9 @@
 /* eslint-disable require-await, no-console */
 
 export const testMathRandom = async () => {
-	console.log('One:', Math.random());
-	console.log('Two:', Math.random());
-	console.log('Three:', Math.random());
+	console.log('Random:', Math.random());
+	console.log('Random:', Math.random());
+	console.log('Random:', Math.random());
 };
 
 /* eslint-enable */
---
+++ src/tests/specs/sputnik/sputnik.math-random.spec.ts
@@ -0,0 +1,56 @@
+import type { _SERVICE as SputnikActor } from '$declarations/sputnik/sputnik.did';
+import type { Identity } from '@dfinity/agent';
+import type { Principal } from '@dfinity/principal';
+import { type Actor, PocketIc } from '@hadronous/pic';
+import { inject } from 'vitest';
+import { mockSetRule } from '../../mocks/collection.mocks';
+import { setupTestSputnik } from '../../utils/fixtures-tests.utils';
+import { setDocAndFetchLogs } from '../../utils/sputnik-tests.utils';
+import {tick} from "../../utils/pic-tests.utils";
+
+describe('Sputnik > math', () => {
+	let pic: PocketIc;
+	let actor: Actor<SputnikActor>;
+	let canisterId: Principal;
+	let controller: Identity;
+
+	const TEST_COLLECTION = 'test-mathrandom';
+
+	beforeAll(async () => {
+		pic = await PocketIc.create(inject('PIC_URL'));
+
+		const { pic: p, actor: a, canisterId: cId, controller: c } = await setupTestSputnik();
+
+		pic = p;
+		actor = a;
+		canisterId = cId;
+		controller = c;
+
+		const { set_rule } = actor;
+		await set_rule({ Db: null }, TEST_COLLECTION, mockSetRule);
+	});
+
+	afterAll(async () => {
+		await pic?.tearDown();
+	});
+
+	it('should generate random numbers', async () => {
+		await tick(pic);
+
+		const { logs } = await setDocAndFetchLogs({
+			collection: TEST_COLLECTION,
+			actor,
+			controller,
+			canisterId,
+			pic
+		});
+
+		const values = logs
+			.filter(([_, { message }]) => message.includes('Random:'))
+			.map(([_, { message }]) => message.replace('Random:', '').trim());
+
+		console.log(values);
+
+		expect(new Set(values).size).toEqual(logs.length);
+	});
+});

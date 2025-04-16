ff12c4b88d4bc04ba4edb5af773a42f7aae65483
David Dal Busco <david.dalbusco@outlook.com>
2025-04-16 12:18:24 +0200

    feat(sputnik): parse more details on potential error on post_upgrade (#1456)

--- src/sputnik/src/hooks/lifecycle.rs
+++ src/sputnik/src/hooks/lifecycle.rs
@@ -2,13 +2,16 @@ use crate::hooks::js::loaders::init_loaders;
 use crate::hooks::js::runtime::lifecycle::on_post_upgrade::execute_on_post_upgrade;
 use crate::js::runtime::execute_sync_js;
 use junobuild_macros::on_post_upgrade;
+use rquickjs::CatchResultExt;
 
 #[on_post_upgrade]
 fn on_post_upgrade() -> Result<(), String> {
     execute_sync_js(|ctx| {
-        init_loaders(ctx).map_err(|e| e.to_string())?;
+        init_loaders(ctx).catch(ctx).map_err(|e| e.to_string())?;
 
-        execute_on_post_upgrade(ctx).map_err(|e| e.to_string())?;
+        execute_on_post_upgrade(ctx)
+            .catch(ctx)
+            .map_err(|e| e.to_string())?;
 
         Ok(())
     })

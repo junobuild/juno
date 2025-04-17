4de8992958f88eb7f34c1e4ebf8a7b1706bd1e62
David Dal Busco <david.dalbusco@outlook.com>
2025-04-17 21:36:51 +0200

    feat(sputnik): ic_cdk::time()

--- src/sputnik/src/js/apis/ic_cdk/mod.rs
+++ src/sputnik/src/js/apis/ic_cdk/mod.rs
@@ -3,16 +3,19 @@ mod id;
 mod impls;
 mod print;
 pub mod types;
+mod time;
 
 use crate::js::apis::ic_cdk::call::init_ic_cdk_call_raw;
 use crate::js::apis::ic_cdk::id::init_ic_cdk_id;
 use crate::js::apis::ic_cdk::print::init_ic_cdk_print;
 use rquickjs::{Ctx, Error as JsError};
+use crate::js::apis::ic_cdk::time::init_ic_cdk_time;
 
 pub fn init_ic_cdk_apis(ctx: &Ctx) -> Result<(), JsError> {
     init_ic_cdk_id(ctx)?;
     init_ic_cdk_print(ctx)?;
     init_ic_cdk_call_raw(ctx)?;
+    init_ic_cdk_time(ctx)?;
 
     Ok(())
 }

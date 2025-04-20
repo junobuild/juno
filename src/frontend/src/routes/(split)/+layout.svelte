efc45c2be51b64c7dc00c81b4210c815574de62c
David Dal Busco <david.dalbusco@outlook.com>
2025-04-20 21:08:08 +0200

    feat: functions page full width

--- src/frontend/src/routes/(split)/+layout.svelte
+++ src/frontend/src/routes/(split)/+layout.svelte
@@ -3,15 +3,19 @@
 	import Navbar from '$lib/components/core/Navbar.svelte';
 	import Navmenu from '$lib/components/core/Navmenu.svelte';
 	import Layout from '$lib/components/ui/Layout.svelte';
+	import {page} from "$app/state";
 
 	interface Props {
 		children: Snippet;
 	}
 
 	let { children }: Props = $props();
+
+	let routeId: string | null = $derived(page.route.id);
+	let fullWidth = $derived(routeId?.includes("functions"));
 </script>
 
-<Layout topMargin="wide">
+<Layout topMargin="wide" {fullWidth}>
 	{#snippet menu()}
 		<Navmenu />
 	{/snippet}

ae087343ba357c8273a649a2328d9ce45a41bb60
David Dal Busco <david.dalbusco@outlook.com>
2025-04-24 13:52:42 +0200

    feat(orbiter): CORS

--- src/orbiter/src/http/utils.rs
+++ src/orbiter/src/http/utils.rs
@@ -1,6 +1,9 @@
 use ic_http_certification::{HttpResponse, StatusCode};
 
 pub fn create_json_response(status_code: StatusCode, body: Vec<u8>) -> HttpResponse<'static> {
+    
+    ic_cdk::print("DA FUQ!!!!!!");
+    
     HttpResponse::builder()
         .with_status_code(status_code)
         .with_headers(vec![
@@ -16,7 +19,20 @@ pub fn create_json_response(status_code: StatusCode, body: Vec<u8>) -> HttpRespo
                 "no-store, max-age=0".to_string(),
             ),
             ("pragma".to_string(), "no-cache".to_string()),
+            ("Access-Control-Allow-Origin".to_string(), "*".to_string()),
         ])
         .with_body(body)
+        .build()
+}
+
+pub fn create_cors_preflight_response() -> HttpResponse<'static> {
+    HttpResponse::builder()
+        .with_status_code(StatusCode::NO_CONTENT)
+        .with_headers(vec![
+            ("Access-Control-Allow-Origin".to_string(), "*".to_string()),
+            ("Access-Control-Allow-Methods".to_string(), "GET, POST, OPTIONS".to_string()),
+            ("Access-Control-Allow-Headers".to_string(), "Content-Type".to_string()),
+        ])
+        .with_body(vec![])
         .build()
 }

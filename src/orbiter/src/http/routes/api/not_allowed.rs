9bc3a07fc20815c6871389c54dc65f9f2078a1f3
David Dal Busco <david.dalbusco@outlook.com>
2025-04-27 14:21:26 +0200

    feat(orbiter): not_allowed response only certification

--- src/orbiter/src/http/routes/api/not_allowed.rs
+++ src/orbiter/src/http/routes/api/not_allowed.rs
@@ -1,7 +1,7 @@
 use crate::http::routes::api::init::init_certified_response;
 use crate::http::routes::api::routes::{
-    EVENTS_FULL_ROUTE, EVENT_FULL_ROUTE, METRICS_FULL_ROUTE, METRIC_FULL_ROUTE, VIEWS_FULL_ROUTE,
-    VIEW_FULL_ROUTE,
+    EVENTS_RESPONSE_ONLY_ROUTE, EVENT_RESPONSE_ONLY_ROUTE, METRICS_RESPONSE_ONLY_ROUTE,
+    METRIC_RESPONSE_ONLY_ROUTE, VIEWS_RESPONSE_ONLY_ROUTE, VIEW_RESPONSE_ONLY_ROUTE,
 };
 use crate::http::routes::api::types::CertifiedExactRoute;
 use crate::http::types::response::ErrorResponse;
@@ -9,12 +9,12 @@ use crate::http::utils::create_json_response;
 use ic_http_certification::{HttpResponse, Method, StatusCode};
 
 pub fn init_certified_not_allowed_responses() {
-    init_not_allowed_responses(&VIEW_FULL_ROUTE);
-    init_not_allowed_responses(&VIEWS_FULL_ROUTE);
-    init_not_allowed_responses(&EVENT_FULL_ROUTE);
-    init_not_allowed_responses(&EVENTS_FULL_ROUTE);
-    init_not_allowed_responses(&METRIC_FULL_ROUTE);
-    init_not_allowed_responses(&METRICS_FULL_ROUTE);
+    init_not_allowed_responses(&VIEW_RESPONSE_ONLY_ROUTE);
+    init_not_allowed_responses(&VIEWS_RESPONSE_ONLY_ROUTE);
+    init_not_allowed_responses(&EVENT_RESPONSE_ONLY_ROUTE);
+    init_not_allowed_responses(&EVENTS_RESPONSE_ONLY_ROUTE);
+    init_not_allowed_responses(&METRIC_RESPONSE_ONLY_ROUTE);
+    init_not_allowed_responses(&METRICS_RESPONSE_ONLY_ROUTE);
 }
 
 fn init_not_allowed_responses(certified_route: &CertifiedExactRoute) {

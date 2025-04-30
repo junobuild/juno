5547f5c495d9d0622fff3dc5cfd00f0f75219909
David Dal Busco <david.dalbusco@outlook.com>
2025-04-30 12:06:09 +0200

    chore: fmt

--- src/frontend/src/lib/components/analytics/AnalyticsDevices.svelte
+++ src/frontend/src/lib/components/analytics/AnalyticsDevices.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { i18n } from '$lib/stores/i18n.store';
 	import type { AnalyticsPageViews } from '$lib/types/orbiter';
-	import {nonNullish} from "@dfinity/utils";
 
 	interface Props {
 		pageViews: AnalyticsPageViews;

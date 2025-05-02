9bc83708c4a045cfd1b2eede6c94a4e9f2c5142d
David Dal Busco <david.dalbusco@outlook.com>
2025-05-02 12:34:04 +0200

    chore: lint

--- src/frontend/src/lib/components/analytics/AnalyticsEventsExport.svelte
+++ src/frontend/src/lib/components/analytics/AnalyticsEventsExport.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-	import { isNullish, jsonReplacer } from '@dfinity/utils';
+	import { isNullish } from '@dfinity/utils';
 	import type { Orbiter } from '$declarations/mission_control/mission_control.did';
-	import { getTrackEvents } from '$lib/api/orbiter.api';
 	import { satelliteStore } from '$lib/derived/satellite.derived';
+	import { exportTrackEvents } from '$lib/services/orbiter.export.services';
 	import { authStore } from '$lib/stores/auth.store';
 	import { busy } from '$lib/stores/busy.store';
 	import { i18n } from '$lib/stores/i18n.store';
@@ -12,8 +12,6 @@
 		PageViewsOptionPeriod,
 		AnalyticsPeriodicity
 	} from '$lib/types/orbiter';
-	import { filenameTimestamp, JSON_PICKER_OPTIONS, saveToFileSystem } from '$lib/utils/save.utils';
-	import { exportTrackEvents } from '$lib/services/orbiter.export.services';
 
 	interface Props {
 		period?: PageViewsOptionPeriod;

c28989572e91464519d51ee32a84d412f5d54210
David Dal Busco <david.dalbusco@outlook.com>
2025-05-02 09:30:40 +0200

    feat(frontend): move visitors at the top and inline (#1511)

--- src/frontend/src/lib/components/analytics/AnalyticsMetrics.svelte
+++ src/frontend/src/lib/components/analytics/AnalyticsMetrics.svelte
@@ -2,7 +2,7 @@
 	import Value from '$lib/components/ui/Value.svelte';
 	import { i18n } from '$lib/stores/i18n.store';
 	import type { AnalyticsPageViews } from '$lib/types/orbiter';
-	import { formatNumber } from '$lib/utils/number.utils';
+	import { formatCompactNumber, formatNumber } from '$lib/utils/number.utils';
 
 	interface Props {
 		pageViews: AnalyticsPageViews;
@@ -17,42 +17,76 @@
 	<span class="title">{$i18n.analytics.visitors}</span>
 
 	<div class="content">
-		<Value>
-			{#snippet label()}
-				{$i18n.analytics.number_of_sessions}
-			{/snippet}
-			<p>{metrics.unique_sessions}</p>
-		</Value>
+		<div class="cell">
+			<Value>
+				{#snippet label()}
+					{$i18n.analytics.number_of_sessions}
+				{/snippet}
+				<p>{formatCompactNumber(Number(metrics.unique_sessions ?? 0n))}</p>
+			</Value>
+		</div>
 
-		<Value>
-			{#snippet label()}
-				{$i18n.analytics.unique_page_views}
-			{/snippet}
-			<p>{metrics.unique_page_views}</p>
-		</Value>
+		<div class="cell">
+			<Value>
+				{#snippet label()}
+					{$i18n.analytics.unique_page_views}
+				{/snippet}
+				<p>{formatCompactNumber(Number(metrics.unique_page_views ?? 0n))}</p>
+			</Value>
+		</div>
 
-		<Value>
-			{#snippet label()}
-				{$i18n.analytics.total_page_views}
-			{/snippet}
-			<p>{metrics.total_page_views}</p>
-		</Value>
+		<div class="cell">
+			<Value>
+				{#snippet label()}
+					{$i18n.analytics.total_page_views}
+				{/snippet}
+				<p>{formatCompactNumber(metrics.total_page_views ?? 0n)}</p>
+			</Value>
+		</div>
 
-		<Value>
-			{#snippet label()}
-				{$i18n.analytics.average_page_views_per_session}
-			{/snippet}
-			<p>{formatNumber(metrics.average_page_views_per_session)}</p>
-		</Value>
+		<div class="cell">
+			<Value>
+				{#snippet label()}
+					{$i18n.analytics.average_page_views_per_session}
+				{/snippet}
+				<p>{formatNumber(metrics.average_page_views_per_session)}</p>
+			</Value>
+		</div>
 
-		<Value>
-			{#snippet label()}
-				{$i18n.analytics.bounce_rate}
-			{/snippet}
-			<p>
-				{formatNumber(metrics.bounce_rate * 100, { minFraction: 0, maxFraction: 0 })}<small>%</small
-				>
-			</p>
-		</Value>
+		<div class="cell">
+			<Value>
+				{#snippet label()}
+					{$i18n.analytics.bounce_rate}
+				{/snippet}
+				<p>
+					{formatNumber(metrics.bounce_rate * 100, { minFraction: 0, maxFraction: 0 })}<small
+						>%</small
+					>
+				</p>
+			</Value>
+		</div>
 	</div>
 </div>
+
+<style lang="scss">
+	@use '../../styles/mixins/media';
+	@use '../../styles/mixins/grid';
+
+	.content {
+		@include media.min-width(medium) {
+			@include grid.two-columns;
+		}
+
+		@include media.min-width(large) {
+			@include grid.three-columns;
+		}
+	}
+
+	.cell {
+		@include media.min-width(large) {
+			display: inline-flex;
+			gap: var(--padding);
+			height: var(--padding-4x);
+		}
+	}
+</style>

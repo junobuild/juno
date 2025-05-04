a0eed68600f0fba807c8d0fcaa17ce141a6408f9
David Dal Busco <david.dalbusco@outlook.com>
2025-05-04 14:26:40 +0200

    feat: display progression behind tracked events

--- src/frontend/src/lib/components/analytics/AnalyticsEvents.svelte
+++ src/frontend/src/lib/components/analytics/AnalyticsEvents.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import type { AnalyticsTrackEvents } from '$declarations/orbiter/orbiter.did';
 	import { i18n } from '$lib/stores/i18n.store';
+	import { formatCompactNumber } from '$lib/utils/number.utils.js';
 
 	interface Props {
 		trackEvents: AnalyticsTrackEvents;
@@ -8,14 +9,13 @@
 
 	let { trackEvents }: Props = $props();
 
-	let total: Array<[string, number]> = $state([]);
+	let allEvents = $derived(trackEvents.total);
 
-	$effect(() => {
-		const { total: t } = trackEvents;
-		t.sort(([keyA, _], [keyB, __]) => keyA.localeCompare(keyB));
+	let events: Array<[string, number]> = $derived(
+		allEvents.toSorted(([keyA, _], [keyB, __]) => keyA.localeCompare(keyB))
+	);
 
-		total = t;
-	});
+	let total = $derived(allEvents.reduce((acc, [_, count]) => acc + count, 0));
 </script>
 
 <div class="table-container">
@@ -23,15 +23,19 @@
 		<thead>
 			<tr>
 				<th> {$i18n.analytics.tracked_events} </th>
-				<th> {$i18n.analytics.count} </th>
+				<th class="count"> {$i18n.analytics.count} </th>
 			</tr>
 		</thead>
 
 		<tbody>
-			{#each total as [name, count] (name)}
+			{#each events as [name, count] (name)}
+				{@const ratio = total > 0 ? `--ratio: ${(count * 100) / total}%` : undefined}
+
 				<tr>
 					<td>{name}</td>
-					<td>{count}</td>
+					<td class="count"
+						><span style={ratio} class="progress">{formatCompactNumber(count)}</span></td
+					>
 				</tr>
 			{/each}
 		</tbody>
@@ -41,5 +45,31 @@
 <style lang="scss">
 	.table-container {
 		margin: var(--padding-4x) 0;
+	}
+
+	.progress {
+		position: relative;
+		width: 100%;
+		display: inline-block;
+
+		&:before {
+			content: '';
+
+			position: absolute;
+			top: 0;
+			right: calc(var(--padding) * -1);
+
+			display: block;
+
+			background: rgba(var(--color-primary-rgb), 0.2);
+			border-radius: var(--border-radius);
+
+			width: calc(var(--ratio) + var(--padding));
+			height: 100%;
+		}
+	}
+
+	.count {
+		text-align: right;
 	}
 </style>

c370493cd0ddbd6e81981bf0ca86614ca6607fe1
David Dal Busco <david.dalbusco@outlook.com>
2025-05-05 19:08:37 +0200

    fix(frontend): cap analytics periodicity if hours pagination bigger than selection

--- src/frontend/src/lib/utils/orbiter.utils.ts
+++ src/frontend/src/lib/utils/orbiter.utils.ts
@@ -1,7 +1,7 @@
 import type { Orbiter } from '$declarations/mission_control/mission_control.did';
 import type { PageViewsParams, PageViewsPeriods } from '$lib/types/orbiter';
 import { metadataName } from '$lib/utils/metadata.utils';
-import { eachHourOfInterval } from 'date-fns';
+import { addHours, differenceInHours, eachHourOfInterval } from 'date-fns';
 
 export const orbiterName = ({ metadata }: Orbiter): string => metadataName(metadata);
 
@@ -12,19 +12,25 @@ export const buildAnalyticsPeriods = ({
 }): PageViewsPeriods => {
 	const { from, to, periodicity } = params;
 
-	const days = eachHourOfInterval(
+	const end = to ?? new Date();
+
+	// We need to cap the periodicity because eachHourOfInterval returns empty if for example the periodicity is a mont, 720h, but there are less hours between the two dates
+	const periodEndHours = addHours(from, periodicity);
+	const cappedPeriodicity = periodEndHours.getTime() > end.getTime() ? differenceInHours(end, from) : periodicity;
+
+	const hours = eachHourOfInterval(
 		{
 			start: from,
 			end: to ?? new Date()
 		},
-		{ step: periodicity }
+		{ step: cappedPeriodicity }
 	);
 
 	const periods = [];
-	for (let i = 0; i <= days.length - 2; i++) {
+	for (let i = 0; i <= hours.length - 2; i++) {
 		periods.push({
-			from: days[i],
-			to: days[i + 1]
+			from: hours[i],
+			to: hours[i + 1]
 		});
 	}
 

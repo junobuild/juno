544d63bdd565d51075d17f57b36be43b95088802
David Dal Busco <david.dalbusco@outlook.com>
2025-05-06 08:36:25 +0200

    feat(frontend): extract canister info components

---
+++ src/frontend/src/lib/components/canister/CanisterDailyConsumption.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import CanisterValue from '$lib/components/canister/CanisterValue.svelte';
+	import { i18n } from '$lib/stores/i18n.store';
+	import type { CanisterDataInfo, CanisterSyncStatus } from '$lib/types/canister';
+	import { formatTCycles } from '$lib/utils/cycles.utils.js';
+
+	interface Props {
+		canister: CanisterDataInfo | undefined;
+		sync: CanisterSyncStatus | undefined;
+	}
+
+	let { canister, sync }: Props = $props();
+
+	let idleCyclesBurnedPerDay: bigint | undefined = $derived(canister?.idleCyclesBurnedPerDay);
+</script>
+
+<div class="consumption">
+	<CanisterValue {sync}>
+		{#snippet label()}
+			{$i18n.canisters.daily_consumption}
+		{/snippet}
+		<span>
+			{formatTCycles(idleCyclesBurnedPerDay ?? 0n)}T <small>cycles</small>
+		</span>
+	</CanisterValue>
+</div>
+
+<style lang="scss">
+	.consumption {
+		min-height: calc(48px + var(--padding-2_5x));
+	}
+</style>
---
+++ src/frontend/src/lib/components/canister/CanisterQueries.svelte
@@ -0,0 +1,78 @@
+<script lang="ts">
+	import { nonNullish } from '@dfinity/utils';
+	import CanisterValue from '$lib/components/canister/CanisterValue.svelte';
+	import { i18n } from '$lib/stores/i18n.store';
+	import type { CanisterDataInfo, CanisterSyncStatus } from '$lib/types/canister';
+	import { formatBytes, formatNumber } from '$lib/utils/number.utils.js';
+
+	interface Props {
+		canister: CanisterDataInfo | undefined;
+		sync: CanisterSyncStatus | undefined;
+	}
+
+	let { canister, sync }: Props = $props();
+
+	let numInstructionsTotal: bigint | undefined = $derived(
+		canister?.queryStats?.numInstructionsTotal
+	);
+
+	let numCallsTotal: bigint | undefined = $derived(canister?.queryStats?.numCallsTotal);
+
+	let responsePayloadBytesTotal: bigint | undefined = $derived(
+		canister?.queryStats?.responsePayloadBytesTotal
+	);
+
+	let requestPayloadBytesTotal: bigint | undefined = $derived(
+		canister?.queryStats?.requestPayloadBytesTotal
+	);
+</script>
+
+<div class="queries">
+	<CanisterValue {sync} rows={4}>
+		{#snippet label()}
+			{$i18n.canisters.queries}
+		{/snippet}
+
+		<p>
+			{nonNullish(numCallsTotal)
+				? formatNumber(Number(numCallsTotal), {
+						minFraction: 0,
+						maxFraction: 0,
+						notation: 'compact'
+					})
+				: '???'} <small>{$i18n.canisters.calls}</small>
+		</p>
+		<p>
+			{nonNullish(numInstructionsTotal)
+				? formatNumber(Number(numInstructionsTotal), {
+						minFraction: 0,
+						maxFraction: 0,
+						notation: 'compact'
+					})
+				: '???'}
+			<small>{$i18n.canisters.instructions}</small>
+		</p>
+		<p>
+			{nonNullish(requestPayloadBytesTotal) ? formatBytes(Number(requestPayloadBytesTotal)) : '???'}
+			<small>{$i18n.canisters.requests}</small>
+		</p>
+		<p>
+			{nonNullish(responsePayloadBytesTotal)
+				? formatBytes(Number(responsePayloadBytesTotal))
+				: '???'}
+			<small>{$i18n.canisters.responses}</small>
+		</p>
+	</CanisterValue>
+</div>
+
+<style lang="scss">
+	p {
+		&:not(:last-of-type) {
+			margin: 0 0 var(--padding-0_25x);
+		}
+	}
+
+	.queries {
+		min-height: calc(126px + var(--padding-2_5x));
+	}
+</style>

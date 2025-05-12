e76571a4d6f3ac0f1980715e109043138442522a
David Dal Busco <david.dalbusco@outlook.com>
2025-05-12 09:07:22 +0200

    test: request

--- src/tests/specs/observatory/observatory.ping.spec.ts
+++ src/tests/specs/observatory/observatory.ping.spec.ts
@@ -1,19 +1,21 @@
 import type { _SERVICE as ObservatoryActor } from '$declarations/observatory/observatory.did';
 import { idlFactory as idlFactorObservatory } from '$declarations/observatory/observatory.factory.did';
 import { Ed25519KeyIdentity } from '@dfinity/identity';
-import { nowInBigIntNanoSeconds } from '@dfinity/utils';
+import { assertNonNullish, nowInBigIntNanoSeconds } from '@dfinity/utils';
 import { type Actor, PocketIc } from '@hadronous/pic';
 import { inject } from 'vitest';
 import { mockMissionControlId } from '../../../frontend/tests/mocks/modules.mock';
+import { toBodyJson } from '../../utils/orbiter-test.utils';
 import { tick } from '../../utils/pic-tests.utils';
 import { OBSERVATORY_WASM_PATH } from '../../utils/setup-tests.utils';
-import { toBodyJson } from '../../utils/orbiter-test.utils';
 
 describe('Observatory > Ping', () => {
 	let pic: PocketIc;
 	let actor: Actor<ObservatoryActor>;
 
 	const controller = Ed25519KeyIdentity.generate();
+
+	const EMAIL_API_KEY = 'test-key';
 
 	beforeAll(async () => {
 		pic = await PocketIc.create(inject('PIC_URL'));
@@ -33,7 +35,7 @@ describe('Observatory > Ping', () => {
 		const { set_env } = actor;
 
 		await set_env({
-			email_api_key: ['test-key']
+			email_api_key: [EMAIL_API_KEY]
 		});
 	});
 
@@ -64,15 +66,34 @@ describe('Observatory > Ping', () => {
 
 		await tick(pic);
 
-		const pendingHttpsOutcalls = await pic.getPendingHttpsOutcalls();
+		const [pendingHttpOutCall] = await pic.getPendingHttpsOutcalls();
 
-		console.log(pendingHttpsOutcalls);
+		assertNonNullish(pendingHttpOutCall);
 
-		const pendingGoogleSearchOutcall = pendingHttpsOutcalls[0];
+		const { requestId, subnetId, url, headers: headersArray, body } = pendingHttpOutCall;
 
+		expect(url).toEqual(
+			'https://europe-west6-juno-observatory.cloudfunctions.net/observatory/notifications/email'
+		);
+
+		const headers = headersArray.reduce<Record<string, string>>(
+			(acc, [key, value]) => ({ ...acc, [key]: value }),
+			{}
+		);
+
+		expect(headers['Content-Type']).toEqual('application/json');
+		expect(headers['authorization']).toEqual(`Bearer ${EMAIL_API_KEY}`);
+
+		// e.g. rdmx6-jaaaa-aaaaa-aaadq-cai___1620328630000000105___747495116
+		const idempotencyKey = headers['idempotency-key'].split('___');
+		expect(idempotencyKey[0]).toEqual(mockMissionControlId.toText());
+		expect(BigInt(idempotencyKey[1])).toBeGreaterThan(0n);
+		expect(Number(idempotencyKey[1])).toBeGreaterThan(0);
+
+		// Finalize
 		await pic.mockPendingHttpsOutcall({
-			requestId: pendingGoogleSearchOutcall.requestId,
-			subnetId: pendingGoogleSearchOutcall.subnetId,
+			requestId,
+			subnetId,
 			response: {
 				type: 'success',
 				body: toBodyJson({}),

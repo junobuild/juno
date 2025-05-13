b30aa03f265fefb732c0c61c31ff313035a88641
David Dal Busco <david.dalbusco@outlook.com>
2025-05-13 07:53:59 +0200

    test: improve monitoring test case with notification assertion (#1566)
    * test: improve monitoring test case with notification assertion
    
    * test: with and without api key
    
    * chore: fmt

--- src/tests/specs/mission-control/mission-control.monitoring.notifications.spec.ts
+++ src/tests/specs/mission-control/mission-control.monitoring.notifications.spec.ts
@@ -6,7 +6,10 @@ import type {
 	MonitoringStartConfig
 } from '$declarations/mission_control/mission_control.did';
 import { idlFactory as idlFactorMissionControl } from '$declarations/mission_control/mission_control.factory.did';
-import type { _SERVICE as ObservatoryActor } from '$declarations/observatory/observatory.did';
+import type {
+	NotifyStatus,
+	_SERVICE as ObservatoryActor
+} from '$declarations/observatory/observatory.did';
 import { idlFactory as idlFactorObservatory } from '$declarations/observatory/observatory.factory.did';
 import { Ed25519KeyIdentity } from '@dfinity/identity';
 import type { Principal } from '@dfinity/principal';
@@ -17,19 +20,29 @@ import { CONSOLE_ID } from '../../constants/console-tests.constants';
 import { OBSERVATORY_ID } from '../../constants/observatory-tests.constants';
 import { deploySegments, initMissionControls } from '../../utils/console-tests.utils';
 import { setupMissionControlModules } from '../../utils/mission-control-tests.utils';
+import {
+	assertNotificationHttpsOutcalls,
+	DEPOSITED_CYCLES_TEMPLATE_HTML,
+	DEPOSITED_CYCLES_TEMPLATE_TEXT,
+	mockObservatoryProxyBearerKey
+} from '../../utils/observatory-tests.utils';
 import { tick } from '../../utils/pic-tests.utils';
 import { CONSOLE_WASM_PATH, OBSERVATORY_WASM_PATH } from '../../utils/setup-tests.utils';
 
 describe('Mission Control > Notifications', () => {
 	let pic: PocketIc;
-	let actor: Actor<ConsoleActor>;
+
+	let consoleActor: Actor<ConsoleActor>;
 
 	const controller = Ed25519KeyIdentity.generate();
 
 	let observatoryId: Principal;
+	let observatoryActor: Actor<ObservatoryActor>;
 
 	beforeAll(async () => {
 		pic = await PocketIc.create(inject('PIC_URL'));
+
+		await pic.setTime(new Date('2025-05-12T07:53:19+00:00').getTime());
 
 		const { actor: c } = await pic.setupCanister<ConsoleActor>({
 			idlFactory: idlFactorConsole,
@@ -38,12 +51,22 @@ describe('Mission Control > Notifications', () => {
 			targetCanisterId: CONSOLE_ID
 		});
 
-		actor = c;
-		actor.setIdentity(controller);
+		consoleActor = c;
+		consoleActor.setIdentity(controller);
 
-		await deploySegments(actor);
+		const { update_rate_config } = consoleActor;
 
-		const { canisterId } = await pic.setupCanister<ObservatoryActor>({
+		const config = {
+			max_tokens: 1_000n,
+			time_per_token_ns: 60_000_000_000n / 1000n // 60ns per token
+		};
+
+		await update_rate_config({ Satellite: null }, config);
+		await update_rate_config({ MissionControl: null }, config);
+
+		await deploySegments(consoleActor);
+
+		const { canisterId, actor: oActor } = await pic.setupCanister<ObservatoryActor>({
 			idlFactory: idlFactorObservatory,
 			wasm: OBSERVATORY_WASM_PATH,
 			sender: controller.getPrincipal(),
@@ -51,7 +74,11 @@ describe('Mission Control > Notifications', () => {
 		});
 
 		observatoryId = canisterId;
+		observatoryActor = oActor;
 
+		observatoryActor.setIdentity(controller);
+
+		// The random seed generator init is deferred
 		await tick(pic);
 	});
 
@@ -59,37 +86,25 @@ describe('Mission Control > Notifications', () => {
 		await pic?.tearDown();
 	});
 
-	it('should notify observatory', { timeout: 600000 }, async () => {
+	const setup = async (): Promise<{
+		missionControlId: Principal;
+		satelliteId: Principal;
+		missionControlActor: Actor<MissionControlActor>;
+	}> => {
+		// Prevent "Rate limit reached, try again later."
+		await pic.advanceTime(100);
+
 		// Init a mission control
-		const [user] = await initMissionControls({ actor, pic, length: 1 });
+		const [user] = await initMissionControls({ actor: consoleActor, pic, length: 1 });
 
-		actor.setIdentity(user);
+		consoleActor.setIdentity(user);
 
-		const { get_user_mission_control_center } = actor;
+		const { get_user_mission_control_center } = consoleActor;
 		const missionControl = await get_user_mission_control_center();
 
 		const missionControlId = fromNullable(fromNullable(missionControl)?.mission_control_id ?? []);
 
 		assertNonNullish(missionControlId);
-
-		// Assert no notification have been submitted to the Observatory yet
-		const observatoryActor = pic.createActor<ObservatoryActor>(idlFactorObservatory, observatoryId);
-
-		observatoryActor.setIdentity(controller);
-
-		const { get_notify_status } = observatoryActor;
-
-		const status = await get_notify_status({
-			segment_id: toNullable(),
-			from: toNullable(),
-			to: toNullable()
-		});
-
-		expect(status).toEqual({
-			failed: 0n,
-			pending: 0n,
-			sent: 0n
-		});
 
 		// Spin some modules to monitor
 		const missionControlActor = pic.createActor<MissionControlActor>(
@@ -107,9 +122,8 @@ describe('Mission Control > Notifications', () => {
 			missionControlId
 		});
 
-		// Set an email, enable notification, attach the module that was created and start monitoring
-		const { update_and_start_monitoring, set_metadata, set_config, set_satellite } =
-			missionControlActor;
+		// Set an email, enable notification, attach the module that was created
+		const { set_metadata, set_config, set_satellite } = missionControlActor;
 
 		await set_metadata([['email', 'test@test.com']]);
 
@@ -129,71 +143,160 @@ describe('Mission Control > Notifications', () => {
 
 		await set_satellite(satelliteId, []);
 
-		const missionControlCurrentCycles = await pic.getCyclesBalance(missionControlId);
-		const satelliteCurrentCycles = await pic.getCyclesBalance(satelliteId);
+		return { missionControlId, satelliteId, missionControlActor };
+	};
 
-		const satelliteStrategy: CyclesMonitoringStrategy = {
-			BelowThreshold: {
-				// This way the satellite already requires cycles
-				min_cycles: BigInt(satelliteCurrentCycles) + 100_000_000_000n,
-				fund_cycles: 100_000n
-			}
-		};
+	const assertObservatoryStatus = async (notifyStatus: NotifyStatus) => {
+		const { get_notify_status } = observatoryActor;
 
-		const missionControlStrategy: CyclesMonitoringStrategy = {
-			BelowThreshold: {
-				// This way the mission control can consume some of its cycles already requires cycles
-				min_cycles: BigInt(Math.min(missionControlCurrentCycles - 100_000_000_000, 100_000)),
-				fund_cycles: 100_000n
-			}
-		};
-
-		const config: MonitoringStartConfig = {
-			cycles_config: [
-				{
-					satellites_strategy: toNullable({
-						ids: [satelliteId],
-						strategy: satelliteStrategy
-					}),
-					orbiters_strategy: toNullable(),
-					mission_control_strategy: toNullable(missionControlStrategy)
-				}
-			]
-		};
-
-		await update_and_start_monitoring(config);
-
-		// One pending notification should be registered in the Observatory
-		await vi.waitFor(async () => {
-			await pic.tick();
-
-			const statusPending = await get_notify_status({
-				segment_id: toNullable(),
-				from: toNullable(),
-				to: toNullable()
-			});
-
-			expect(statusPending).toEqual({
-				failed: 0n,
-				pending: 1n,
-				sent: 0n
-			});
+		const status = await get_notify_status({
+			segment_id: toNullable(),
+			from: toNullable(),
+			to: toNullable()
 		});
 
-		// The notification should fail because no api key for the proxy is registered
-		await vi.waitFor(async () => {
-			await pic.tick();
+		expect(status).toEqual(notifyStatus);
+	};
 
-			const statusFailed = await get_notify_status({
-				segment_id: toNullable(),
-				from: toNullable(),
-				to: toNullable()
+	describe('Deposited cycles', () => {
+		let missionControlActor: Actor<MissionControlActor>;
+
+		let satelliteId: Principal;
+
+		let config: MonitoringStartConfig;
+
+		beforeEach(async () => {
+			const { missionControlId, satelliteId: satId, missionControlActor: mActor } = await setup();
+
+			satelliteId = satId;
+
+			missionControlActor = mActor;
+
+			const missionControlCurrentCycles = await pic.getCyclesBalance(missionControlId);
+			const satelliteCurrentCycles = await pic.getCyclesBalance(satelliteId);
+
+			const satelliteStrategy: CyclesMonitoringStrategy = {
+				BelowThreshold: {
+					// This way the satellite already requires cycles
+					min_cycles: BigInt(satelliteCurrentCycles) + 100_000_000_000n,
+					fund_cycles: 140_000n
+				}
+			};
+
+			const missionControlStrategy: CyclesMonitoringStrategy = {
+				BelowThreshold: {
+					// This way the mission control can consume some of its cycles already requires cycles
+					min_cycles: BigInt(Math.min(missionControlCurrentCycles - 100_000_000_000, 100_000)),
+					fund_cycles: 100_000n
+				}
+			};
+
+			config = {
+				cycles_config: [
+					{
+						satellites_strategy: toNullable({
+							ids: [satelliteId],
+							strategy: satelliteStrategy
+						}),
+						orbiters_strategy: toNullable(),
+						mission_control_strategy: toNullable(missionControlStrategy)
+					}
+				]
+			};
+		});
+
+		describe('Observatory without Email API key', () => {
+			beforeEach(async () => {
+				const { set_env } = observatoryActor;
+
+				await set_env({
+					email_api_key: []
+				});
 			});
 
-			expect(statusFailed).toEqual({
-				failed: 1n,
-				pending: 0n,
-				sent: 0n
+			it(
+				'should notify deposited cycles but observatory cannot send notification ',
+				{ timeout: 600000 },
+				async () => {
+					await assertObservatoryStatus({
+						failed: 0n,
+						pending: 0n,
+						sent: 0n
+					});
+
+					// Start monitoring
+					const { update_and_start_monitoring } = missionControlActor;
+
+					await update_and_start_monitoring(config);
+
+					// One pending notification should be registered in the Observatory
+					await vi.waitFor(async () => {
+						await pic.tick();
+
+						await assertObservatoryStatus({
+							failed: 0n,
+							pending: 1n,
+							sent: 0n
+						});
+					});
+
+					// The notification should fail because no api key for the proxy is registered
+					await vi.waitFor(async () => {
+						await pic.tick();
+
+						await assertObservatoryStatus({
+							failed: 1n,
+							pending: 0n,
+							sent: 0n
+						});
+					});
+				}
+			);
+		});
+
+		describe('Observatory with Email API key', () => {
+			beforeEach(async () => {
+				const { set_env } = observatoryActor;
+
+				await set_env({
+					email_api_key: [mockObservatoryProxyBearerKey]
+				});
+			});
+
+			it('should notify deposited cycles', { timeout: 600000 }, async () => {
+				// Start monitoring
+				const { update_and_start_monitoring } = missionControlActor;
+
+				await update_and_start_monitoring(config);
+
+				// One pending notification should be registered in the Observatory
+				await vi.waitFor(async () => {
+					await pic.tick();
+
+					await assertObservatoryStatus({
+						failed: 1n,
+						pending: 1n,
+						sent: 0n
+					});
+				});
+
+				await assertNotificationHttpsOutcalls({
+					templateText: DEPOSITED_CYCLES_TEMPLATE_TEXT,
+					templateHtml: DEPOSITED_CYCLES_TEMPLATE_HTML,
+					templateTitle: `🚀 {{cycles}} T Cycles Deposited on Your Satellite`,
+					moduleName: 'Satellite',
+					url: `https://console.juno.build/satellite/?s=${satelliteId.toText()}`,
+					expectedIdempotencyKeySegmentId: satelliteId,
+					expectedCycles: '0.00000014',
+					pic
+				});
+
+				// The notification should have been sent
+				await assertObservatoryStatus({
+					failed: 1n,
+					pending: 0n,
+					sent: 1n
+				});
 			});
 		});
 	});

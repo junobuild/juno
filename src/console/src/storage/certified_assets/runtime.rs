961164dbca23acdf33d201be31dcaef1a43be72a
David Dal Busco <david.dalbusco@outlook.com>
2025-05-15 09:17:50 +0200

    refactor: rename prefix

--- src/console/src/storage/certified_assets/runtime.rs
+++ src/console/src/storage/certified_assets/runtime.rs
@@ -1,6 +1,6 @@
 use crate::storage::strategy_impls::{CdnHeap, StorageState};
-use junobuild_cdn::init_certified_assets as init_runtime_certified_assets;
+use junobuild_cdn::init_certified_assets as cdn_init_certified_assets;
 
 pub fn init_certified_assets() {
-    init_runtime_certified_assets(&CdnHeap, &StorageState);
+    cdn_init_certified_assets(&CdnHeap, &StorageState);
 }

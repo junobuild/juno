15be0f1ecb8d95cd8056e4a11096bea95846a34a
David Dal Busco <david.dalbusco@outlook.com>
2025-05-16 13:48:36 +0200

    chore: no dev

--- src/console/src/storage/certified_assets/runtime.rs
+++ src/console/src/storage/certified_assets/runtime.rs
@@ -1,6 +1,5 @@
 use crate::storage::strategy_impls::{CdnHeap, StorageState};
-use junobuild_cdn::init_certified_assets as cdn_init_certified_assets;
 
 pub fn init_certified_assets() {
-    cdn_init_certified_assets(&CdnHeap, &StorageState);
+    junobuild_cdn::init_certified_assets(&CdnHeap, &StorageState);
 }

dc6f661ddd76ecfaf94036bc100ca11f91a06ac5
David Dal Busco <david.dalbusco@outlook.com>
2025-05-16 12:40:28 +0200

    chore: fmt

--- src/libs/collections/src/errors.rs
+++ src/libs/collections/src/errors.rs
@@ -1,13 +1,17 @@
 // Collection {} is reserved and cannot be modified.
-pub const JUNO_COLLECTIONS_ERROR_MODIFY_RESERVED_COLLECTION: &str = "juno.collections.error.modify_reserved_collection";
+pub const JUNO_COLLECTIONS_ERROR_MODIFY_RESERVED_COLLECTION: &str =
+    "juno.collections.error.modify_reserved_collection";
 // The collection name matches a system collection.
 pub const JUNO_COLLECTIONS_ERROR_RESERVED_NAME: &str = "juno.collections.error.reserved_name";
 // Collection {} is reserved.
-pub const JUNO_COLLECTIONS_ERROR_RESERVED_COLLECTION: &str = "juno.collections.error.reserved_collection";
+pub const JUNO_COLLECTIONS_ERROR_RESERVED_COLLECTION: &str =
+    "juno.collections.error.reserved_collection";
 // Rate config cannot be disabled.
-pub const JUNO_COLLECTIONS_ERROR_RATE_CONFIG_ENABLED: &str = "juno.collections.error.rate_config_enabled";
+pub const JUNO_COLLECTIONS_ERROR_RATE_CONFIG_ENABLED: &str =
+    "juno.collections.error.rate_config_enabled";
 // Collection starting with {} cannot be deleted
-pub const JUNO_COLLECTIONS_ERROR_DELETE_PREFIX_RESERVED: &str = "juno.collections.error.prefix_deletion";
+pub const JUNO_COLLECTIONS_ERROR_DELETE_PREFIX_RESERVED: &str =
+    "juno.collections.error.prefix_deletion";
 // The "{}" collection in {} is not empty
 pub const JUNO_COLLECTIONS_ERROR_COLLECTION_NOT_EMPTY: &str = "juno.collections.error.not_empty";
 // Collection "{}" not found in {}.
--- src/libs/collections/src/lib.rs
+++ src/libs/collections/src/lib.rs
@@ -1,8 +1,8 @@
 pub mod assert;
 pub mod constants;
+mod errors;
 mod impls;
 pub mod msg;
 pub mod store;
 pub mod types;
 pub mod utils;
-mod errors;

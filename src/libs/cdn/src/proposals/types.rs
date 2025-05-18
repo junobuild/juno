070905556509b4fe0d42914843202660b2fa024d
David Dal Busco <david.dalbusco@outlook.com>
2025-05-18 17:52:35 +0200

    feat: move proposalsstable to cdn for reusability

--- src/libs/cdn/src/proposals/types.rs
+++ src/libs/cdn/src/proposals/types.rs
@@ -1,7 +1,11 @@
-use crate::proposals::ProposalId;
+use crate::proposals::{Proposal, ProposalId, ProposalKey};
 use candid::{CandidType, Deserialize};
+use ic_stable_structures::StableBTreeMap;
 use junobuild_shared::types::core::Hash;
+use junobuild_shared::types::memory::Memory;
 use serde::Serialize;
+
+pub type ProposalsStable = StableBTreeMap<ProposalKey, Proposal, Memory>;
 
 #[derive(Debug)]
 pub enum CommitProposalError {

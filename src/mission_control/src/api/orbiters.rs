87b6826d3265ebee0783e3a05bc37dbb851694d8
David Dal Busco <david.dalbusco@outlook.com>
2025-05-18 16:02:27 +0200

    feat: redo as on main

--- src/mission_control/src/api/orbiters.rs
+++ src/mission_control/src/api/orbiters.rs
@@ -1,7 +1,11 @@
 use crate::controllers::orbiter::{delete_orbiter_controllers, set_orbiter_controllers};
 use crate::guards::caller_is_user_or_admin_controller;
-use crate::segments::orbiter::{attach_orbiter, delete_orbiter, detach_orbiter};
-use crate::segments::store::get_orbiters;
+use crate::segments::orbiter::{
+    attach_orbiter, create_orbiter as create_orbiter_console,
+    create_orbiter_with_config as create_orbiter_with_config_console, delete_orbiter,
+    detach_orbiter,
+};
+use crate::segments::store::{get_orbiters, set_orbiter_metadata as set_orbiter_metadata_store};
 use crate::types::interface::CreateCanisterConfig;
 use crate::types::state::{Orbiter, Orbiters};
 use ic_cdk::trap;
@@ -16,14 +20,14 @@ fn list_orbiters() -> Orbiters {
 
 #[update(guard = "caller_is_user_or_admin_controller")]
 async fn create_orbiter(name: Option<String>) -> Orbiter {
-    crate::segments::orbiter::create_orbiter(&name)
+    create_orbiter_console(&name)
         .await
         .unwrap_or_else(|e| trap(&e))
 }
 
 #[update(guard = "caller_is_user_or_admin_controller")]
 async fn create_orbiter_with_config(config: CreateCanisterConfig) -> Orbiter {
-    crate::segments::orbiter::create_orbiter_with_config(&config)
+    create_orbiter_with_config_console(&config)
         .await
         .unwrap_or_else(|e| trap(&e))
 }
@@ -44,8 +48,7 @@ async fn unset_orbiter(orbiter_id: OrbiterId) {
 
 #[update(guard = "caller_is_user_or_admin_controller")]
 fn set_orbiter_metadata(orbiter_id: OrbiterId, metadata: Metadata) -> Orbiter {
-    crate::segments::store::set_orbiter_metadata(&orbiter_id, &metadata)
-        .unwrap_or_else(|e| trap(&e))
+    set_orbiter_metadata_store(&orbiter_id, &metadata).unwrap_or_else(|e| trap(&e))
 }
 
 #[update(guard = "caller_is_user_or_admin_controller")]

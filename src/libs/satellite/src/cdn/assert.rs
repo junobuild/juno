7dfab017e086f887bad69ac3d1d2ff4e951b1cef
David Dal Busco <david.dalbusco@outlook.com>
2025-05-26 12:00:14 +0200

    refactor(satellite): move asset keys assertion

--- src/libs/satellite/src/cdn/assert.rs
+++ src/libs/satellite/src/cdn/assert.rs
@@ -1,6 +1,9 @@
 use crate::cdn::constants::{CDN_JUNO_COLLECTION_KEY, CDN_JUNO_COLLECTION_PATH};
-use junobuild_cdn::storage::errors::JUNO_CDN_STORAGE_ERROR_INVALID_COLLECTION;
+use junobuild_cdn::storage::errors::{
+    JUNO_CDN_STORAGE_ERROR_INVALID_COLLECTION, JUNO_CDN_STORAGE_ERROR_INVALID_RELEASES_PATH,
+};
 use junobuild_collections::types::core::CollectionKey;
+use junobuild_shared::regex::build_regex;
 use junobuild_storage::types::state::FullPath;
 
 // TODO: storage module uses cdn because of this reference. Refactor modules.
@@ -9,10 +12,28 @@ pub fn assert_cdn_asset_keys(
     full_path: &FullPath,
     collection: &CollectionKey,
 ) -> Result<(), String> {
-    if full_path.starts_with(CDN_JUNO_COLLECTION_PATH) && collection != CDN_JUNO_COLLECTION_KEY {
+    match collection {
+        CDN_JUNO_COLLECTION_KEY => assert_releases_keys(full_path),
+        _ => {
+            if full_path.starts_with(CDN_JUNO_COLLECTION_PATH) {
+                return Err(format!(
+                    "{} ({} - {})",
+                    JUNO_CDN_STORAGE_ERROR_INVALID_COLLECTION, full_path, collection
+                ));
+            }
+
+            Ok(())
+        }
+    }
+}
+
+fn assert_releases_keys(full_path: &FullPath) -> Result<(), String> {
+    let re = build_regex(r"^/_juno/releases/satellite[^/]*\.wasm\.gz$")?;
+
+    if !re.is_match(full_path) {
         return Err(format!(
-            "{} ({} - {})",
-            JUNO_CDN_STORAGE_ERROR_INVALID_COLLECTION, full_path, collection
+            "{} ({})",
+            JUNO_CDN_STORAGE_ERROR_INVALID_RELEASES_PATH, full_path
         ));
     }
 

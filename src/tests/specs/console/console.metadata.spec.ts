c337f0fa3a8896eb28371696cdbd3027e4e56969
David Dal Busco <david.dalbusco@outlook.com>
2025-05-26 11:47:40 +0200

    feat: error label for reserved asset

--- src/tests/specs/console/console.metadata.spec.ts
+++ src/tests/specs/console/console.metadata.spec.ts
@@ -4,6 +4,7 @@ import { AnonymousIdentity } from '@dfinity/agent';
 import { Ed25519KeyIdentity } from '@dfinity/identity';
 import { fromNullable, toNullable } from '@dfinity/utils';
 import { PocketIc, type Actor } from '@hadronous/pic';
+import { JUNO_STORAGE_ERROR_RESERVED_ASSET } from '@junobuild/errors';
 import { beforeAll, describe, expect, inject } from 'vitest';
 import { uploadFile } from '../../utils/cdn-tests.utils';
 import { deploySegments } from '../../utils/console-tests.utils';
@@ -118,7 +119,7 @@ describe('Console > Metadata', () => {
 					},
 					proposalId
 				)
-			).rejects.toThrow('/releases/metadata.json is a reserved asset.');
+			).rejects.toThrow(`${JUNO_STORAGE_ERROR_RESERVED_ASSET} (/releases/metadata.json)`);
 		});
 	});
 });

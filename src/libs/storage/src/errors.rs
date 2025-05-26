c961e76ec7f2d441909029c08400cdce4a692bc7
David Dal Busco <david.dalbusco@outlook.com>
2025-05-26 13:47:39 +0200

    feat: error label for reserved asset (#1630)

--- src/libs/storage/src/errors.rs
+++ src/libs/storage/src/errors.rs
@@ -6,3 +6,4 @@ pub const JUNO_STORAGE_ERROR_CANNOT_READ_ASSET: &str = "juno.storage.error.canno
 // Asset path must be prefixed with collection key. e.g. collection #releases or releases must match a path /releases/something
 pub const JUNO_STORAGE_ERROR_UPLOAD_PATH_COLLECTION_PREFIX: &str =
     "juno.storage.error.upload_path_collection_prefix";
+pub const JUNO_STORAGE_ERROR_RESERVED_ASSET: &str = "juno.storage.error.reserved_asset";

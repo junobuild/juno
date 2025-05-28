be0b0c7d86e72f2c8c8943ace9edc81667bb7dd9
David Dal Busco <david.dalbusco@outlook.com>
2025-05-28 08:46:01 +0200

    refactor: rename controller_can_write for write scope (#1641)
    * refactor: rename is_controller for write scope
    
    * refactor: rename intent

--- src/libs/satellite/src/user/usage/assert.rs
+++ src/libs/satellite/src/user/usage/assert.rs
@@ -9,7 +9,7 @@ use crate::SetDoc;
 use junobuild_collections::assert::collection::is_system_collection;
 use junobuild_collections::constants::db::COLLECTION_USER_USAGE_KEY;
 use junobuild_collections::types::core::CollectionKey;
-use junobuild_shared::controllers::is_controller;
+use junobuild_shared::controllers::controller_can_write;
 use junobuild_shared::types::state::{Controllers, UserId};
 use junobuild_utils::decode_doc_data;
 // ---------------------------------------------------------
@@ -59,7 +59,7 @@ fn increment_and_assert_usage(
     }
 
     // We only collect usage for users
-    if is_controller(caller, controllers) {
+    if controller_can_write(caller, controllers) {
         return Ok(());
     }
 

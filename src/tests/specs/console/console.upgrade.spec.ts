e7b19157aada74df5db2dca88917e361221a7605
David Dal Busco <david.dalbusco@outlook.com>
2025-06-04 17:18:04 +0200

    feat: proper 0-0-14 did files

--- src/tests/specs/console/console.upgrade.spec.ts
+++ src/tests/specs/console/console.upgrade.spec.ts
@@ -1,9 +1,9 @@
+import type { _SERVICE as ConsoleActor } from '$declarations/console/console.did';
+import { idlFactory as idlFactorConsole } from '$declarations/console/console.factory.did';
 import type { _SERVICE as ConsoleActor_0_0_14 } from '$declarations/deprecated/console-0-0-14.did';
 import { idlFactory as idlFactoryConsole_0_0_14 } from '$declarations/deprecated/console-0-0-14.factory.did';
 import type { _SERVICE as ConsoleActor_0_0_8 } from '$declarations/deprecated/console-0-0-8-patch1.did';
 import { idlFactory as idlFactorConsole_0_0_8 } from '$declarations/deprecated/console-0-0-8-patch1.factory.did';
-import type { _SERVICE as ConsoleActor } from '$declarations/orbiter/orbiter.did';
-import { idlFactory as idlFactorConsole } from '$declarations/orbiter/orbiter.factory.did';
 import type { Identity } from '@dfinity/agent';
 import { Ed25519KeyIdentity } from '@dfinity/identity';
 import type { Principal } from '@dfinity/principal';
@@ -11,8 +11,9 @@ import { assertNonNullish } from '@dfinity/utils';
 import { PocketIc, type Actor } from '@hadronous/pic';
 import { afterEach, beforeEach, describe, expect, inject } from 'vitest';
 import {
+	deploySegments,
 	initMissionControls,
-	installReleases,
+	installReleasesWithDeprecatedFlow,
 	testSatelliteExists
 } from '../../utils/console-tests.utils';
 import { tick } from '../../utils/pic-tests.utils';
@@ -55,6 +56,43 @@ describe('Console > Upgrade', () => {
 		});
 	};
 
+	const testUsers = async ({
+		actor,
+		users
+	}: {
+		users: Identity[];
+		actor: Actor<ConsoleActor_0_0_8 | ConsoleActor_0_0_14 | ConsoleActor>;
+	}) => {
+		const { list_user_mission_control_centers } = actor;
+
+		const missionControls = await list_user_mission_control_centers();
+
+		expect(missionControls).toHaveLength(users.length);
+
+		for (const user of users) {
+			expect(
+				missionControls.find(([key]) => key.toText() === user.getPrincipal().toText())
+			).not.toBeUndefined();
+		}
+	};
+
+	const updateRateConfig = async ({
+		actor
+	}: {
+		actor: Actor<ConsoleActor_0_0_8 | ConsoleActor_0_0_14 | ConsoleActor>;
+	}) => {
+		const { update_rate_config } = actor;
+
+		const config = {
+			max_tokens: 100n,
+			time_per_token_ns: 60n
+		};
+
+		await update_rate_config({ Satellite: null }, config);
+		await update_rate_config({ Orbiter: null }, config);
+		await update_rate_config({ MissionControl: null }, config);
+	};
+
 	describe('v0.0.8 -> v0.0.9', () => {
 		beforeEach(async () => {
 			pic = await PocketIc.create(inject('PIC_URL'));
@@ -71,37 +109,10 @@ describe('Console > Upgrade', () => {
 			canisterId = cId;
 			actor.setIdentity(controller);
 
-			await installReleases(actor);
+			await installReleasesWithDeprecatedFlow(actor);
 
-			await updateRateConfig();
+			await updateRateConfig({ actor });
 		});
-
-		const updateRateConfig = async () => {
-			const { update_rate_config } = actor;
-
-			const config = {
-				max_tokens: 100n,
-				time_per_token_ns: 60n
-			};
-
-			await update_rate_config({ Satellite: null }, config);
-			await update_rate_config({ Orbiter: null }, config);
-			await update_rate_config({ MissionControl: null }, config);
-		};
-
-		const testUsers = async (users: Identity[]) => {
-			const { list_user_mission_control_centers } = actor;
-
-			const missionControls = await list_user_mission_control_centers();
-
-			expect(missionControls).toHaveLength(users.length);
-
-			for (const user of users) {
-				expect(
-					missionControls.find(([key]) => key.toText() === user.getPrincipal().toText())
-				).not.toBeUndefined();
-			}
-		};
 
 		describe('Heap state', () => {
 			it(
@@ -117,7 +128,7 @@ describe('Console > Upgrade', () => {
 
 					actor.setIdentity(controller);
 
-					await testUsers(originalUsers);
+					await testUsers({ users: originalUsers, actor });
 
 					await testSatelliteExists({
 						users: originalUsers,
@@ -130,7 +141,7 @@ describe('Console > Upgrade', () => {
 					// Moving from 0.0.8 to 0.0.9 we are on purpose deprecating previous ways to hold wasm in memory
 					// await testReleases(actor);
 
-					await testUsers(originalUsers);
+					await testUsers({ users: originalUsers, actor });
 
 					await testSatelliteExists({
 						users: originalUsers,
@@ -160,6 +171,10 @@ describe('Console > Upgrade', () => {
 			actor = c;
 			canisterId = cId;
 			actor.setIdentity(controller);
+
+			await updateRateConfig({ actor });
+
+			await deploySegments(actor);
 		});
 
 		it('should preserve controllers even if scope enum is extended', async () => {
@@ -222,6 +237,44 @@ describe('Console > Upgrade', () => {
 			newActor.setIdentity(controller);
 
 			await assertControllers(newActor);
+		});
+
+		describe('Clear stable memory of proposals', () => {
+			it(
+				'should still list mission controls even if we alterate other memory IDs',
+				{
+					timeout: 120000
+				},
+				async () => {
+					// We need to advance time for the rate limiter
+					await pic.advanceTime(1000);
+
+					const originalUsers = await initMissionControls({ actor, pic, length: 3 });
+
+					actor.setIdentity(controller);
+
+					await testUsers({ users: originalUsers, actor });
+
+					await testSatelliteExists({
+						users: originalUsers,
+						actor,
+						pic
+					});
+
+					await upgrade();
+
+					const newActor = pic.createActor<ConsoleActor>(idlFactorConsole, canisterId);
+					newActor.setIdentity(controller);
+
+					await testUsers({ users: originalUsers, actor: newActor });
+
+					await testSatelliteExists({
+						users: originalUsers,
+						actor,
+						pic
+					});
+				}
+			);
 		});
 	});
 });

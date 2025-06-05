0418564bfccf114e5d54afd9ddb2a6f86d89a313
David Dal Busco <david.dalbusco@outlook.com>
2025-06-05 09:24:21 +0200

    feat(storage): more error labels (#1664)

--- src/libs/storage/src/errors.rs
+++ src/libs/storage/src/errors.rs
@@ -10,3 +10,12 @@ pub const JUNO_STORAGE_ERROR_CANNOT_READ_ASSET: &str = "juno.storage.error.canno
 pub const JUNO_STORAGE_ERROR_UPLOAD_PATH_COLLECTION_PREFIX: &str =
     "juno.storage.error.upload_path_collection_prefix";
 pub const JUNO_STORAGE_ERROR_RESERVED_ASSET: &str = "juno.storage.error.reserved_asset";
+pub const JUNO_STORAGE_ERROR_BATCH_NOT_FOUND: &str = "juno.storage.error.batch_not_found";
+pub const JUNO_STORAGE_ERROR_CHUNK_NOT_FOUND: &str = "juno.storage.error.chunk_not_found";
+pub const JUNO_STORAGE_ERROR_CHUNK_NOT_INCLUDED_IN_BATCH: &str =
+    "juno.storage.error.chunk_not_included_in_batch";
+pub const JUNO_STORAGE_ERROR_CHUNK_TO_COMMIT_NOT_FOUND: &str =
+    "juno.storage.error.chunk_to_commit_not_found";
+// Asset exceed max allowed size
+pub const JUNO_STORAGE_ERROR_ASSET_MAX_ALLOWED_SIZE: &str =
+    "juno.storage.error.asset_max_allowed_size";

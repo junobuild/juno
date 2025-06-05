dffbd2839e68f5e218973929a29dba32eb4923d6
David Dal Busco <david.dalbusco@outlook.com>
2025-06-05 15:07:23 +0200

    chore: redo after merge

--- src/libs/satellite/src/rules/store.rs
+++ src/libs/satellite/src/rules/store.rs
@@ -1,8 +1,7 @@
 use crate::assets::storage::store::assert_assets_collection_empty_store;
 use crate::db::store::{delete_collection_store, init_collection_store};
 use crate::memory::internal::STATE;
-use crate::storage::store::assert_assets_collection_empty_store;
-use junobuild_collections::store::{del_rule, filter_rules, set_rule};
+use junobuild_collections::store::{del_rule, set_rule};
 use junobuild_collections::types::core::CollectionKey;
 use junobuild_collections::types::interface::{DelRule, SetRule};
 use junobuild_collections::types::rules::{Memory, Rule};

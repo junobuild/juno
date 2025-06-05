61a40afc32390b10efb3b7f0801687861896e788
David Dal Busco <david.dalbusco@outlook.com>
2025-06-05 16:51:17 +0200

    feat: let's make a breaking change

--- src/libs/collections/src/store.rs
+++ src/libs/collections/src/store.rs
@@ -3,13 +3,22 @@ use crate::assert::rules::{
     assert_system_collection_delete_permission, assert_system_collection_set_permission,
     assert_write_version,
 };
+use crate::constants::core::SYS_COLLECTION_PREFIX;
 use crate::types::core::CollectionKey;
-use crate::types::interface::{DelRule, SetRule};
+use crate::types::interface::{DelRule, ListRulesParams, ListRulesResults, SetRule};
 use crate::types::rules::{Rule, Rules};
 
 // ---------------------------------------------------------
 // Rules
 // ---------------------------------------------------------
+
+pub fn filter_rules(rules: &Rules) -> Vec<(CollectionKey, Rule)> {
+    rules
+        .clone()
+        .into_iter()
+        .filter(|(path, _rules)| (path.starts_with(|c| c != SYS_COLLECTION_PREFIX)))
+        .collect()
+}
 
 pub fn set_rule(
     collection: CollectionKey,
@@ -51,4 +60,35 @@ pub fn del_rule(
     rules.remove(&collection);
 
     Ok(())
+}
+
+pub fn list_rules(params: &ListRulesParams, rules: &Rules) -> ListRulesResults {
+    let include_system = params
+        .matcher
+        .as_ref()
+        .map(|m| m.include_system)
+        .unwrap_or(false);
+
+    if include_system {
+        let items_length = rules.len();
+
+        return ListRulesResults {
+            items: rules
+                .iter()
+                .map(|(key, rule)| (key.clone(), rule.clone()))
+                .collect(),
+            items_length,
+            matches_length: items_length,
+        };
+    }
+
+    let items = filter_rules(rules);
+
+    let items_length = items.len();
+
+    ListRulesResults {
+        items,
+        items_length,
+        matches_length: items_length,
+    }
 }

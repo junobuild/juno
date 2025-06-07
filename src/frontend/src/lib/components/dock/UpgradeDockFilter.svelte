b154ffb108212230208d7f36d514e42c612f55cf
David Dal Busco <david.dalbusco@outlook.com>
2025-06-07 10:58:50 +0200

    feat: wip

--- src/frontend/src/lib/components/dock/UpgradeDockFilter.svelte
+++ src/frontend/src/lib/components/dock/UpgradeDockFilter.svelte
@@ -3,6 +3,22 @@
 	import Toolbar from '$lib/components/ui/Toolbar.svelte';
 	import Value from '$lib/components/ui/Value.svelte';
 	import { i18n } from '$lib/stores/i18n.store';
+	import { satellitesStore } from '$lib/derived/satellites.derived';
+	import type { SatellitePickerProps } from '$lib/components/satellites/SatellitesPicker.svelte';
+	import { satelliteName } from '$lib/utils/satellite.utils';
+
+	let satellites = $derived<SatellitePickerProps['satellites']>(
+		Object.entries($satellitesStore ?? []).reduce<SatellitePickerProps['satellites']>(
+			(acc, [satelliteId, satellite]) => [
+				...acc,
+				{
+					satelliteId,
+					satName: satelliteName(satellite)
+				}
+			],
+			[]
+		)
+	);
 </script>
 
 <Toolbar>

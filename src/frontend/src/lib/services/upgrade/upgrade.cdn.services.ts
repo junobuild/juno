aa95a6a96e0d7b9b0f1a5d16f36a2b0fd22f0534
David Dal Busco <david.dalbusco@outlook.com>
2025-06-11 14:24:07 +0200

    feat(frontend): prevent functions downgrade (#1692)
    * feat(frontend): prevent functions downgrade
    
    * feat: update msg
    
    * feat: improve check

--- src/frontend/src/lib/services/upgrade/upgrade.cdn.services.ts
+++ src/frontend/src/lib/services/upgrade/upgrade.cdn.services.ts
@@ -1,17 +1,24 @@
+import { instantSatelliteVersion } from '$lib/services/feature.services';
 import { downloadWasmFromDevCdn } from '$lib/services/upgrade/upgrade.download.services';
 import { i18n } from '$lib/stores/i18n.store';
 import { toasts } from '$lib/stores/toasts.store';
 import type { Wasm } from '$lib/types/upgrade';
+import { i18nFormat } from '$lib/utils/i18n.utils';
 import { mapJunoPackageMetadata } from '$lib/utils/version.utils';
 import { readWasmMetadata } from '$lib/utils/wasm.utils';
+import type { Principal } from '@dfinity/principal';
 import { isNullish } from '@dfinity/utils';
+import { checkUpgradeVersion } from '@junobuild/admin';
 import type { Asset } from '@junobuild/storage';
+import { compare } from 'semver';
 import { get } from 'svelte/store';
 
 export const prepareWasmUpgrade = async ({
-	asset
+	asset,
+	satelliteId
 }: {
 	asset: Asset;
+	satelliteId: Principal;
 }): Promise<{ result: 'success'; wasm: Wasm } | { result: 'error'; err?: unknown }> => {
 	try {
 		const result = await downloadWasmFromDevCdn({ asset });
@@ -36,6 +43,47 @@ export const prepareWasmUpgrade = async ({
 			});
 
 			return { result: 'error' };
+		}
+
+		const currentVersion = instantSatelliteVersion({ satelliteId });
+
+		if (isNullish(currentVersion)) {
+			toasts.error({
+				text: get(i18n).errors.missing_satellite_version
+			});
+
+			return { result: 'error' };
+		}
+
+		const { current: selectedVersion } = metadata;
+
+		const isDowngrade = compare(currentVersion, selectedVersion) < 0;
+		if (isDowngrade) {
+			toasts.error({
+				text: get(i18n).errors.invalid_version_cannot_downgrade
+			});
+
+			return { result: 'error' };
+		}
+
+		const { canUpgrade } = checkUpgradeVersion({ currentVersion, selectedVersion });
+		if (!canUpgrade) {
+			toasts.error({
+				text: i18nFormat(get(i18n).errors.upgrade_requires_iterative_version, [
+					{
+						placeholder: '{0}',
+						value: get(i18n).satellites.satellite
+					},
+					{
+						placeholder: '{1}',
+						value: currentVersion
+					},
+					{
+						placeholder: '{2}',
+						value: selectedVersion
+					}
+				])
+			});
 		}
 
 		return {

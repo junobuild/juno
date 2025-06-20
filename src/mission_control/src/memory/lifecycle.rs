a401ab63ab13d1a0be842a0187043fb06f9d4498
David Dal Busco <david.dalbusco@outlook.com>
2025-06-20 09:32:39 +0200

    feat(mctrl): remove upgrade state (#1709)

--- src/mission_control/src/memory/lifecycle.rs
+++ src/mission_control/src/memory/lifecycle.rs
@@ -2,7 +2,6 @@ use crate::memory::manager::{get_memory_upgrades, init_runtime_state, init_stabl
 use crate::monitoring::monitor::defer_restart_monitoring;
 use crate::random::defer_init_random_seed;
 use crate::types::state::{HeapState, State};
-use crate::upgrade::types::upgrade::UpgradeState;
 use ciborium::{from_reader, into_writer};
 use ic_cdk::api::call::{arg_data, ArgDecoderConfig};
 use ic_cdk_macros::{init, post_upgrade, pre_upgrade};
@@ -41,16 +40,10 @@ fn post_upgrade() {
     let memory = get_memory_upgrades();
     let state_bytes = read_post_upgrade(&memory);
 
-    // TODO: replace UpgradeState by State after release
-    let state: UpgradeState = from_reader(&*state_bytes)
+    let state: State = from_reader(&*state_bytes)
         .expect("Failed to decode the state of the mission control in post_upgrade hook.");
 
-    STATE.with(|s| {
-        *s.borrow_mut() = State {
-            stable: state.stable,
-            heap: HeapState::from(&state.heap),
-        }
-    });
+    STATE.with(|s| *s.borrow_mut() = state);
 
     init_runtime_state();
 

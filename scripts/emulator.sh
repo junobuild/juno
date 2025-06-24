141b77cb56cd445502a847a78b2d93219136e645
David Dal Busco <david.dalbusco@outlook.com>
2025-06-24 18:44:39 +0200

    chore(emulator): replace docker compose by docker run (#1728)

---
+++ scripts/emulator.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+CONTAINER_NAME="juno-console"
+
+if docker ps -a -q -f name="^/${CONTAINER_NAME}$" | grep -q .; then
+  if [ "$(docker inspect -f '{{.State.Running}}' "$CONTAINER_NAME")" = "true" ]; then
+    echo "✋  Container '$CONTAINER_NAME' is already running..."
+  else
+    echo "▶️  Starting the emulator..."
+    docker start -a -i "$CONTAINER_NAME"
+  fi
+else
+  echo "🚀  Running new emulator..."
+
+  docker run -it \
+    --name "$CONTAINER_NAME" \
+    -p 5987:5987 \
+    -p 5999:5999 \
+    -v juno_console:/juno/.juno \
+    -v "$(pwd)/target/deploy:/juno/target/deploy" \
+    junobuild/console:latest
+fi

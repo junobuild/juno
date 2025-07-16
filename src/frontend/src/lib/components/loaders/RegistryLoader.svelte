3a77b67944c8782e18efe81e85813a1943074b0a
David Dal Busco <david.dalbusco@outlook.com>
2025-07-16 19:11:26 +0200

    feat: wip

--- src/frontend/src/lib/components/loaders/RegistryLoader.svelte
+++ src/frontend/src/lib/components/loaders/RegistryLoader.svelte
@@ -1,100 +1,45 @@
 <script lang="ts">
-	import type { Principal } from '@dfinity/principal';
-	import { debounce, isNullish } from '@dfinity/utils';
-	import { compare } from 'semver';
-	import { onDestroy, onMount, type Snippet } from 'svelte';
-	import { MISSION_CONTROL_v0_0_14 } from '$lib/constants/version.constants';
-	import { missionControlIdDerived } from '$lib/derived/mission-control.derived';
-	import { orbiterNotLoaded } from '$lib/derived/orbiter.derived';
-	import { satellitesNotLoaded } from '$lib/derived/satellites.derived';
-	import { missionControlVersion } from '$lib/derived/version.derived';
-	import {
-		RegistryWorker
-	} from '$lib/services/workers/worker.registry.services';
-	import { i18n } from '$lib/stores/i18n.store';
-	import { toasts } from '$lib/stores/toasts.store';
-	import type { CanisterSegment } from '$lib/types/canister';
+    import type { Principal } from '@dfinity/principal';
+    import { debounce } from '@dfinity/utils';
+    import { onDestroy, onMount, type Snippet } from 'svelte';
+    import { orbiterNotLoaded } from '$lib/derived/orbiter.derived';
+    import { satellitesNotLoaded } from '$lib/derived/satellites.derived';
+    import { RegistryWorker } from '$lib/services/workers/worker.registry.services';
+    import type { CanisterSegment } from '$lib/types/canister';
 
-	interface Props {
-		children: Snippet;
-		segments: CanisterSegment[];
-	}
+    interface Props {
+        children: Snippet;
+        segments: CanisterSegment[];
+    }
 
-	let { children, segments }: Props = $props();
+    let { children, segments, satellites: _ }: Props = $props();
 
-	let worker = $state<RegistryWorker | undefined>();
+    let worker = $state<RegistryWorker | undefined>(undefined);
 
-	onMount(async () => (worker = await RegistryWorker.init()));
-    onDestroy(() => worker?.terminate())
+    onMount(async () => (worker = await RegistryWorker.init()));
+    onDestroy(() => worker?.terminate());
 
-	const debounceStart = debounce(() => {
-		if (isNullish($missionControlIdDerived)) {
-			return;
-		}
+    const debounceStart = debounce(() =>
+        worker?.startCyclesTimer({
+            segments
+        })
+    );
 
-		if (isNullish($missionControlVersion)) {
-			return;
-		}
+    $effect(() => {
+        if ($satellitesNotLoaded) {
+            return;
+        }
 
-		worker?.startMonitoringTimer({
-			segments,
-			missionControlId: $missionControlIdDerived,
-			withMonitoringHistory:
-				compare($missionControlVersion.current ?? '0.0.0', MISSION_CONTROL_v0_0_14) >= 0
-		});
-	});
+        if ($orbiterNotLoaded) {
+            return;
+        }
 
-	$effect(() => {
-		worker?.stopMonitoringTimer();
+        if (segments.length === 0) {
+            return;
+        }
 
-		// We wait until mission control is loaded
-		if (isNullish($missionControlIdDerived)) {
-			return;
-		}
-
-		// We wait for the version for backwards compatibility because we only want to query the monitoring history for up-to-date mission control
-		if (isNullish($missionControlVersion)) {
-			return;
-		}
-
-		// Likewise for satellites
-		if ($satellitesNotLoaded) {
-			return;
-		}
-
-		// And orbiter
-		if ($orbiterNotLoaded) {
-			return;
-		}
-
-		// This way we now the list of segments is derived completely
-		if (segments.length === 0) {
-			return;
-		}
-
-		debounceStart();
-	});
-
-	onDestroy(() => worker?.stopMonitoringTimer());
-
-	const restartCycles = ({ detail: { canisterId: _ } }: CustomEvent<{ canisterId: Principal }>) => {
-		if (segments.length === 0) {
-			return;
-		}
-
-		if (isNullish($missionControlIdDerived)) {
-			toasts.error({
-				text: $i18n.errors.no_mission_control
-			});
-			return;
-		}
-
-		// TODO: we can potentially restart only the specified canister but, for now and simplicity reason, the worker just restart fully that's why we are passing all the segments.
-		worker?.restartMonitoringTimer({
-			segments,
-			missionControlId: $missionControlIdDerived
-		});
-	};
+        debounceStart();
+    });
 </script>
 
 {@render children()}

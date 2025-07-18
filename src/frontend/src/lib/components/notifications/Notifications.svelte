c0a8a39f0c275d33e085ecb1d85ec8d50ac3e46e
David Dal Busco <david.dalbusco@outlook.com>
2025-07-18 07:09:02 +0200

    feat: init popover

--- src/frontend/src/lib/components/notifications/Notifications.svelte
+++ src/frontend/src/lib/components/notifications/Notifications.svelte
@@ -2,15 +2,33 @@
 	import IconNotifications from '$lib/components/icons/IconNotifications.svelte';
 	import ButtonIcon from '$lib/components/ui/ButtonIcon.svelte';
 	import { authSignedIn } from '$lib/derived/auth.derived';
+	import Popover from "$lib/components/ui/Popover.svelte";
 
-	const onclick = () => {};
+	let button: HTMLButtonElement | undefined = $state();
+	let visible: boolean = $state(false);
+
+	const close = () => (visible = false);
+
+	const onclick = () => visible = true;
 </script>
 
 {#if $authSignedIn}
-	<ButtonIcon {onclick}>
+	<ButtonIcon {onclick}  bind:button>
 		{#snippet icon()}
 			<IconNotifications size="16px" />
 		{/snippet}
 		Notifications
 	</ButtonIcon>
 {/if}
+
+<Popover bind:visible anchor={button} direction="rtl">
+	<div class="container">
+		No notifications at the moment.
+	</div>
+</Popover>
+
+<style lang="scss">
+	@use '../../styles/mixins/overlay';
+
+	@include overlay.popover-container;
+</style>

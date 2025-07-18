50c13256af0bfb7f1137e79193c9965e8ff397ff
David Dal Busco <david.dalbusco@outlook.com>
2025-07-18 12:23:48 +0200

    feat(frontend): all navbar buttons aligned

--- src/frontend/src/lib/components/ui/ButtonMenu.svelte
+++ src/frontend/src/lib/components/ui/ButtonMenu.svelte
@@ -1,22 +1,24 @@
 <script lang="ts">
 	import IconClose from '$lib/components/icons/IconClose.svelte';
 	import IconMenu from '$lib/components/icons/IconMenu.svelte';
+	import ButtonIcon from '$lib/components/ui/ButtonIcon.svelte';
 	import { i18n } from '$lib/stores/i18n.store';
 	import { layoutMenuOpen } from '$lib/stores/layout-menu.store';
 </script>
 
-<button
-	data-tid="menu-toggle"
-	class="menu"
-	onclick={() => layoutMenuOpen.set(!$layoutMenuOpen)}
-	aria-label={$layoutMenuOpen ? $i18n.core.close : $i18n.core.menu}
->
-	{#if $layoutMenuOpen}
-		<IconClose />
-	{:else}
-		<IconMenu />
-	{/if}
-</button>
+<div class="menu">
+	<ButtonIcon onclick={() => layoutMenuOpen.set(!$layoutMenuOpen)}>
+		{#snippet icon()}
+			{#if $layoutMenuOpen}
+				<IconClose size="16px" />
+			{:else}
+				<IconMenu size="16px" />
+			{/if}
+		{/snippet}
+
+		{$layoutMenuOpen ? $i18n.core.close : $i18n.core.menu}
+	</ButtonIcon>
+</div>
 
 <style lang="scss">
 	@use '../../styles/mixins/media';
@@ -27,12 +29,6 @@
 		// On large screen the menu is sticky therefore it cannot be opened or closed
 		@include media.min-width(xlarge) {
 			display: none;
-		}
-
-		:global(svg) {
-			width: 28px;
-			height: 28px;
-			min-width: 28px;
 		}
 	}
 </style>

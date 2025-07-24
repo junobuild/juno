980431586f4f543898ed39f738a26c583035de7e
David Dal Busco <david.dalbusco@outlook.com>
2025-07-24 07:07:20 +0200

    test: more assertions

--- src/tests/specs/satellite/stock/satellite.allowed-callers.spec.ts
+++ src/tests/specs/satellite/stock/satellite.allowed-callers.spec.ts
@@ -1,5 +1,6 @@
 import type {
 	AuthenticationConfig,
+	Doc,
 	_SERVICE as SatelliteActor
 } from '$declarations/satellite/satellite.did';
 import { idlFactory as idlFactorSatellite } from '$declarations/satellite/satellite.factory.did';
@@ -8,6 +9,7 @@ import { Ed25519KeyIdentity } from '@dfinity/identity';
 import { type Actor, PocketIc } from '@dfinity/pic';
 import type { Principal } from '@dfinity/principal';
 import { fromNullable, toNullable } from '@dfinity/utils';
+import { JUNO_AUTH_ERROR_CALLER_NOT_ALLOWED, JUNO_DATASTORE_ERROR_USER_NOT_ALLOWED } from '@junobuild/errors';
 import { toArray } from '@junobuild/utils';
 import { nanoid } from 'nanoid';
 import { beforeAll, describe, expect, inject } from 'vitest';
@@ -105,7 +107,7 @@ describe('Satellite > Allowed Callers', () => {
 		await set_auth_config(config);
 	};
 
-	describe("Without auth config", () => {
+	describe('Without auth config', () => {
 		const collection = 'test_allowed_callers_without_config';
 
 		describe('Datastore', () => {
@@ -142,7 +144,7 @@ describe('Satellite > Allowed Callers', () => {
 					});
 				});
 
-				const assertAllowed = async ({actorIdentity}: {actorIdentity?: Identity} = {}) => {
+				const assertAllowed = async ({ actorIdentity }: { actorIdentity?: Identity } = {}) => {
 					actor.setIdentity(actorIdentity ?? user);
 					const { get_doc } = actor;
 
@@ -150,16 +152,52 @@ describe('Satellite > Allowed Callers', () => {
 					const doc = fromNullable(result);
 
 					expect(doc).not.toBeUndefined();
-				}
+				};
 
 				it('should get document if no config', async () => {
 					await assertAllowed();
 				});
 			});
-		});
-	})
 
-	describe("With auth config", () => {
+			describe('get many documents', () => {
+				let docKey: string;
+
+				beforeEach(async () => {
+					await createUser(user);
+
+					const { set_doc } = actor;
+
+					docKey = nanoid();
+
+					await set_doc(collection, docKey, {
+						data: await toArray({
+							hello: 'world'
+						}),
+						description: toNullable(),
+						version: toNullable()
+					});
+				});
+
+				const assertAllowed = async ({ actorIdentity }: { actorIdentity?: Identity } = {}) => {
+					actor.setIdentity(actorIdentity ?? user);
+
+					const { get_many_docs } = actor;
+
+					const result = await get_many_docs([[collection, docKey]]);
+
+					const doc = fromNullable(result[0][1]);
+
+					expect(doc).not.toBeUndefined();
+				};
+
+				it('should get documents if no config', async () => {
+					await assertAllowed();
+				});
+			});
+		});
+	});
+
+	describe('With auth config', () => {
 		const collection = 'test_allowed_callers';
 
 		describe('Datastore', () => {
@@ -210,7 +248,7 @@ describe('Satellite > Allowed Callers', () => {
 					expect(doc).toBeUndefined();
 				});
 
-				const assertAllowed = async ({actorIdentity}: {actorIdentity?: Identity} = {}) => {
+				const assertAllowed = async ({ actorIdentity }: { actorIdentity?: Identity } = {}) => {
 					actor.setIdentity(actorIdentity ?? user);
 					const { get_doc } = actor;
 
@@ -218,7 +256,7 @@ describe('Satellite > Allowed Callers', () => {
 					const doc = fromNullable(result);
 
 					expect(doc).not.toBeUndefined();
-				}
+				};
 
 				it('should get document if no rules', async () => {
 					await assertAllowed();
@@ -233,9 +271,132 @@ describe('Satellite > Allowed Callers', () => {
 				it('should get document if controller', async () => {
 					await setSomeAllowedCaller();
 
-					await assertAllowed({actorIdentity: controller});
+					await assertAllowed({ actorIdentity: controller });
+				});
+			});
+
+			describe('get many documents', () => {
+				let docKey: string;
+
+				beforeEach(async () => {
+					await resetAuthConfig();
+
+					await createUser(user);
+
+					const { set_doc } = actor;
+
+					docKey = nanoid();
+
+					await set_doc(collection, docKey, {
+						data: await toArray({
+							hello: 'world'
+						}),
+						description: toNullable(),
+						version: toNullable()
+					});
+				});
+
+				it('should not get documents if not allowed', async () => {
+					await setSomeAllowedCaller();
+
+					actor.setIdentity(user);
+					const { get_many_docs } = actor;
+
+					const result = await get_many_docs([[collection, docKey]]);
+
+					const doc = fromNullable(result[0][1]);
+
+					expect(doc).toBeUndefined();
+				});
+
+				const assertAllowed = async ({ actorIdentity }: { actorIdentity?: Identity } = {}) => {
+					actor.setIdentity(actorIdentity ?? user);
+
+					const { get_many_docs } = actor;
+
+					const result = await get_many_docs([[collection, docKey]]);
+
+					const doc = fromNullable(result[0][1]);
+
+					expect(doc).not.toBeUndefined();
+				};
+
+				it('should get documents if no rules', async () => {
+					await assertAllowed();
+				});
+
+				it('should get documents if empty allowed callers', async () => {
+					await setEmptyAllowedCallers();
+
+					await assertAllowed();
+				});
+
+				it('should get documents if controller', async () => {
+					await setSomeAllowedCaller();
+
+					await assertAllowed({ actorIdentity: controller });
+				});
+			});
+
+			describe('set document', () => {
+				const createDoc = async ({
+					actorIdentity
+				}: { actorIdentity?: Identity } = {}): Promise<Doc> => {
+					actor.setIdentity(actorIdentity ?? user);
+
+					const { set_doc } = actor;
+
+					return await set_doc(collection, nanoid(), {
+						data: await toArray({
+							hello: 'world'
+						}),
+						description: toNullable(),
+						version: toNullable()
+					});
+				};
+
+				beforeEach(async () => {
+					await resetAuthConfig();
+
+					await createUser(user);
+				});
+
+				it('should not set document if not allowed', async () => {
+					const doc = await createDoc();
+
+					expect(doc).not.toBeUndefined();
+
+					await setSomeAllowedCaller();
+
+					await expect(createDoc()).rejects.toThrow(JUNO_AUTH_ERROR_CALLER_NOT_ALLOWED);
+				});
+
+				const assertAllowed = async (params: { actorIdentity?: Identity } = {}) => {
+					const doc = await createDoc(params);
+
+					expect(doc).not.toBeUndefined();
+				};
+
+				it('should get documents if no rules', async () => {
+					await assertAllowed();
+				});
+
+				it('should get documents if empty allowed callers', async () => {
+					await createDoc();
+
+					await setEmptyAllowedCallers();
+
+					await assertAllowed();
+				});
+
+				it('should get documents if controller', async () => {
+					await createDoc();
+
+					await setSomeAllowedCaller();
+
+					await assertAllowed({ actorIdentity: controller });
 				});
 			});
 		});
-	})
+	});
 });

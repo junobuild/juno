bf4269e7d471d12dde433e6462b0dfd80d8c0753
David Dal Busco <david.dalbusco@outlook.com>
2025-08-08 09:22:25 +0200

    chore: handle The Wasm code for the upgrade is identical to the code currently installed. No upgrade is necessary.

--- scripts/console.upgrade.mjs
+++ scripts/console.upgrade.mjs
@@ -1,9 +1,21 @@
 #!/usr/bin/env node
 
+import { UpgradeCodeUnchangedError } from '@junobuild/admin';
 import { CONSOLE_ID } from './constants.mjs';
 import { upgrade } from './module.upgrade.mjs';
 
-await upgrade({
-	sourceFilename: 'console.wasm.gz',
-	canisterId: CONSOLE_ID
-});
+try {
+	await upgrade({
+		sourceFilename: 'console.wasm.gz',
+		canisterId: CONSOLE_ID
+	});
+} catch (err) {
+	// In the CI, it can happen that we are using the newest Docker image
+	// and no changes have been yet developed to the Console.
+	if (err instanceof UpgradeCodeUnchangedError) {
+		console.warn(err.message);
+		return;
+	}
+
+	throw err;
+}

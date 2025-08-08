023488f73f7625f9a191a656d67bcce2aebf47a2
David Dal Busco <david.dalbusco@outlook.com>
2025-08-08 13:04:40 +0200

    test: keep it simple for now

--- src/e2e/create-satellite.spec.ts
+++ src/e2e/create-satellite.spec.ts
@@ -1,30 +1,19 @@
 import { testWithII } from '@dfinity/internet-identity-playwright';
 import { expect } from '@playwright/test';
-import type { BrowserContextOptions } from 'playwright-core';
 import { initTestSuite } from './utils/init.utils';
 
 const getConsolePage = initTestSuite();
 
-const themes: BrowserContextOptions['colorScheme'][] = ['light', 'dark'];
+testWithII('should create a satellite', async () => {
+	const consolePage = getConsolePage();
 
-themes.forEach((colorScheme) => {
-	testWithII.describe(`${colorScheme} mode`, () => {
-		testWithII.use({
-			colorScheme
-		});
+	await consolePage.createSatellite();
+});
 
-		testWithII('should create a satellite', async () => {
-			const consolePage = getConsolePage();
+testWithII('should visite newly create satellite', async () => {
+	const consolePage = getConsolePage();
 
-			await consolePage.createSatellite();
-		});
+	const satellitePage = await consolePage.visitSatellite();
 
-		testWithII('should visite newly create satellite', async () => {
-			const consolePage = getConsolePage();
-
-			const satellitePage = await consolePage.visitSatellite();
-
-			await expect(satellitePage).toHaveScreenshot({ fullPage: true });
-		});
-	});
+	await expect(satellitePage).toHaveScreenshot({ fullPage: true });
 });

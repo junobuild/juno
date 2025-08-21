ee1b0b74a67601ea1e1be53f31f799f3011a8eca
David Dal Busco <david.dalbusco@outlook.com>
2025-08-21 12:09:50 +0200

    chore: fmt

--- src/libs/auth/src/strategies.rs
+++ src/libs/auth/src/strategies.rs
@@ -3,6 +3,7 @@ use ic_certification::Hash;
 
 pub trait AuthHeapStrategy {
     fn with_auth_state<R>(&self, f: impl FnOnce(&Option<AuthenticationHeapState>) -> R) -> R;
+
     fn with_auth_state_mut<R>(
         &self,
         f: impl FnOnce(&mut Option<AuthenticationHeapState>) -> R,

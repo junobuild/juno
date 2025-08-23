5f8d833820332a2203e1337f5a31d9e11be9d38f
David Dal Busco <david.dalbusco@outlook.com>
2025-08-23 10:44:25 +0200

    test(satellite): manually shard test upgrades

---
+++ src/tests/specs/satellite/stock/upgrade/satellite.upgrade-v0-0-22.spec.ts
@@ -0,0 +1,141 @@
+import type { _SERVICE as SatelliteActor_0_0_21 } from '$declarations/deprecated/satellite-0-0-21.did';
+import { idlFactory as idlFactorSatellite_0_0_21 } from '$declarations/deprecated/satellite-0-0-21.factory.did';
+import type { _SERVICE as SatelliteActor } from '$declarations/satellite/satellite.did';
+import { idlFactory as idlFactorSatellite } from '$declarations/satellite/satellite.factory.did';
+import { Ed25519KeyIdentity } from '@dfinity/identity';
+import { PocketIc, type Actor } from '@dfinity/pic';
+import type { Principal } from '@dfinity/principal';
+import { assertNonNullish, fromNullable, toNullable } from '@dfinity/utils';
+import { inject } from 'vitest';
+import { upgradeSatelliteVersion } from '../../../../utils/satellite-upgrade-tests.utils';
+import { controllersInitArgs, downloadSatellite } from '../../../../utils/setup-tests.utils';
+
+describe('Satellite > Upgrade > v0.0.22', () => {
+	let pic: PocketIc;
+	let canisterId: Principal;
+
+	const controller = Ed25519KeyIdentity.generate();
+
+	afterEach(async () => {
+		await pic?.tearDown();
+	});
+
+	let actor: Actor<SatelliteActor_0_0_21>;
+
+	beforeEach(async () => {
+		pic = await PocketIc.create(inject('PIC_URL'));
+
+		const destination = await downloadSatellite('0.0.21');
+
+		const { actor: c, canisterId: cId } = await pic.setupCanister<SatelliteActor_0_0_21>({
+			idlFactory: idlFactorSatellite_0_0_21,
+			wasm: destination,
+			arg: controllersInitArgs(controller),
+			sender: controller.getPrincipal()
+		});
+
+		actor = c;
+		canisterId = cId;
+		actor.setIdentity(controller);
+	});
+
+	it('should still migrate rules with none max_changes_per_users', async () => {
+		const { set_rule } = actor;
+
+		const collection = 'test_migration';
+
+		await set_rule({ Db: null }, collection, {
+			memory: toNullable({ Heap: null }),
+			version: toNullable(),
+			max_size: toNullable(),
+			read: { Managed: null },
+			mutable_permissions: toNullable(),
+			write: { Public: null },
+			max_capacity: toNullable(),
+			rate_config: toNullable()
+		});
+
+		await upgradeSatelliteVersion({ version: '0.0.22', controller, pic, canisterId });
+
+		const newActor = pic.createActor<SatelliteActor>(idlFactorSatellite, canisterId);
+		newActor.setIdentity(controller);
+
+		const { get_rule } = newActor;
+
+		const result = await get_rule({ Db: null }, collection);
+
+		const rule = fromNullable(result);
+
+		assertNonNullish(rule);
+
+		const {
+			updated_at,
+			created_at,
+			memory,
+			mutable_permissions,
+			read,
+			write,
+			version,
+			max_changes_per_user
+		} = rule;
+
+		expect(memory).toEqual(toNullable({ Heap: null }));
+		expect(read).toEqual({ Managed: null });
+		expect(write).toEqual({ Public: null });
+		expect(mutable_permissions).toEqual([true]);
+		expect(created_at).toBeGreaterThan(0n);
+		expect(updated_at).toBeGreaterThan(0n);
+		expect(fromNullable(version)).toEqual(1n);
+
+		expect(fromNullable(max_changes_per_user)).toBeUndefined();
+	});
+
+	it('should create a rule for collection #user-usage', async () => {
+		const collection = '#user-usage';
+
+		const { get_rule: getRuleBefore } = actor;
+
+		const beforeUpgrade = await getRuleBefore({ Db: null }, collection);
+
+		expect(fromNullable(beforeUpgrade)).toBeUndefined();
+
+		await upgradeSatelliteVersion({ version: '0.0.22', controller, pic, canisterId });
+
+		const newActor = pic.createActor<SatelliteActor>(idlFactorSatellite, canisterId);
+		newActor.setIdentity(controller);
+
+		const { get_rule: getRuleAfter } = newActor;
+
+		const afterUpgrade = await getRuleAfter({ Db: null }, collection);
+
+		const rule = fromNullable(afterUpgrade);
+
+		assertNonNullish(rule);
+
+		const {
+			updated_at,
+			created_at,
+			memory,
+			mutable_permissions,
+			read,
+			write,
+			version,
+			max_changes_per_user,
+			max_capacity,
+			max_size,
+			rate_config
+		} = rule;
+
+		expect(memory).toEqual(toNullable({ Stable: null }));
+		expect(read).toEqual({ Controllers: null });
+		expect(write).toEqual({ Controllers: null });
+		expect(mutable_permissions).toEqual([false]);
+		expect(created_at).toBeGreaterThan(0n);
+		expect(updated_at).toBeGreaterThan(0n);
+		expect(fromNullable(version)).toBeUndefined();
+		expect(fromNullable(max_changes_per_user)).toBeUndefined();
+		expect(fromNullable(max_capacity)).toBeUndefined();
+		expect(fromNullable(max_size)).toBeUndefined();
+		expect(fromNullable(rate_config)).toBeUndefined();
+	});
+});
---
+++ src/tests/utils/satellite-upgrade-tests.utils.ts
@@ -0,0 +1,100 @@
+import type { _SERVICE as SatelliteActor_0_0_16 } from '$declarations/deprecated/satellite-0-0-16.did';
+import type { _SERVICE as SatelliteActor_0_0_17 } from '$declarations/deprecated/satellite-0-0-17.did';
+import type { _SERVICE as SatelliteActor_0_0_21 } from '$declarations/deprecated/satellite-0-0-21.did';
+import type { Identity } from '@dfinity/agent';
+import { Ed25519KeyIdentity } from '@dfinity/identity';
+import type { Actor, PocketIc } from '@dfinity/pic';
+import type { Principal } from '@dfinity/principal';
+import { toNullable } from '@dfinity/utils';
+import { toArray } from '@junobuild/utils';
+import { tick } from './pic-tests.utils';
+import { downloadSatellite, SATELLITE_WASM_PATH } from './setup-tests.utils';
+
+export const upgradeSatelliteVersion = async ({
+	version,
+	pic,
+	canisterId,
+	controller
+}: {
+	version: string;
+	pic: PocketIc;
+	controller: Identity;
+	canisterId: Principal;
+}) => {
+	await tick(pic);
+
+	const destination = await downloadSatellite(version);
+
+	await pic.upgradeCanister({
+		canisterId,
+		wasm: destination,
+		sender: controller.getPrincipal()
+	});
+};
+
+export const upgradeSatellite = async ({
+	pic,
+	canisterId,
+	controller
+}: {
+	pic: PocketIc;
+	controller: Identity;
+	canisterId: Principal;
+}) => {
+	await tick(pic);
+
+	await pic.upgradeCanister({
+		canisterId,
+		wasm: SATELLITE_WASM_PATH,
+		sender: controller.getPrincipal()
+	});
+};
+
+export const initUsers = async (actor: Actor<SatelliteActor_0_0_16>): Promise<Identity[]> => {
+	const { set_doc } = actor;
+
+	const user1 = Ed25519KeyIdentity.generate();
+
+	await set_doc('#user', user1.getPrincipal().toText(), {
+		data: await toArray({
+			provider: 'internet_identity'
+		}),
+		description: toNullable(),
+		updated_at: toNullable()
+	});
+
+	const user2 = Ed25519KeyIdentity.generate();
+
+	await set_doc('#user', user2.getPrincipal().toText(), {
+		data: await toArray({
+			provider: 'internet_identity'
+		}),
+		description: toNullable(),
+		updated_at: toNullable()
+	});
+
+	return [user1, user2];
+};
+
+export const testUsers = async ({
+	users,
+	actor
+}: {
+	users: Identity[];
+	actor: Actor<SatelliteActor_0_0_16 | SatelliteActor_0_0_17 | SatelliteActor_0_0_21>;
+}) => {
+	const { list_docs } = actor;
+
+	const { items } = await list_docs('#user', {
+		matcher: toNullable(),
+		order: toNullable(),
+		owner: toNullable(),
+		paginate: toNullable()
+	});
+
+	expect(users).toHaveLength(users.length);
+
+	for (const user of users) {
+		expect(items.find(([key]) => key === user.getPrincipal().toText())).not.toBeUndefined();
+	}
+};

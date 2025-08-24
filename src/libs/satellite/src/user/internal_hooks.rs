5c1e074186f4d22805ecff935c21a278108506ba
David Dal Busco <david.dalbusco@outlook.com>
2025-08-24 19:59:43 +0200

    Revert "refactor(satellite): refactor user internal clean up hooks (#1885)" (#1886)
    This reverts commit d0d07a71c9eddd70599c54bb83793ca280e6bf76.

--- src/libs/satellite/src/user/internal_hooks.rs
+++ src/libs/satellite/src/user/internal_hooks.rs
@@ -1,11 +1,7 @@
 use crate::db::types::state::DocContext;
-use crate::user::usage::delete_user_usage;
+use crate::user::usage::internal_hooks::invoke_delete_user_usage;
 use crate::Doc;
-use ic_cdk::trap;
-use ic_cdk_timers::set_timer;
 use junobuild_collections::constants::db::COLLECTION_USER_KEY;
-use junobuild_shared::types::state::UserId;
-use std::time::Duration;
 
 pub fn invoke_on_delete_user(doc: &DocContext<Option<Doc>>) {
     invoke_deletion(doc);
@@ -21,14 +17,6 @@ fn invoke_deletion(doc: &DocContext<Option<Doc>>) {
     }
 
     if let Some(user) = &doc.data {
-        invoke_cleanup_user(&user.owner);
+        invoke_delete_user_usage(&user.owner);
     }
-}
-
-fn invoke_cleanup_user(user_id: &UserId) {
-    let user_id = *user_id;
-
-    set_timer(Duration::ZERO, move || {
-        delete_user_usage(&user_id).unwrap_or_else(|e| trap(&e))
-    });
 }

679b3a40c8d5fe12f1efbd164520c792e63c33e6
David Dal Busco <david.dalbusco@outlook.com>
2025-08-25 10:27:50 +0200

    refactor(satellite): rename for collection context

--- src/libs/satellite/src/db/assert.rs
+++ src/libs/satellite/src/db/assert.rs
@@ -8,7 +8,7 @@ use crate::hooks::db::{invoke_assert_delete_doc, invoke_assert_set_doc};
 use crate::types::store::{AssertContext, StoreContext};
 use crate::user::core::assert::{
     assert_user_collection_caller_key, assert_user_collection_data, assert_user_is_not_banned,
-    assert_user_write_permission,
+    assert_user_collection_write_permission,
 };
 use crate::user::usage::assert::{
     assert_user_usage_collection_data, increment_and_assert_db_usage,
@@ -74,7 +74,7 @@ pub fn assert_set_doc(
 
     assert_user_collection_caller_key(caller, collection, key, current_doc)?;
     assert_user_collection_data(collection, value)?;
-    assert_user_write_permission(caller, controllers, collection, current_doc)?;
+    assert_user_collection_write_permission(caller, controllers, collection, current_doc)?;
 
     assert_write_permission(caller, controllers, current_doc, &rule.write)?;
 

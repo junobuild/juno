d267d38cf9f05b1ad750a6e1d3beb029a4c455d7
David Dal Busco <david.dalbusco@outlook.com>
2025-08-25 08:53:03 +0200

    test: webauthn set

---
+++ src/tests/specs/satellite/stock/satellite.user-webauthn.spec.ts
@@ -0,0 +1,220 @@
+import type { _SERVICE as SatelliteActor } from '$declarations/satellite/satellite.did';
+import { idlFactory as idlFactorSatellite } from '$declarations/satellite/satellite.factory.did';
+import { SignIdentity } from '@dfinity/agent';
+import { Ed25519KeyIdentity } from '@dfinity/identity';
+import { type Actor, PocketIc } from '@dfinity/pic';
+import {
+	arrayBufferToUint8Array,
+	arrayOfNumberToUint8Array,
+	assertNonNullish,
+	fromNullable,
+	toNullable
+} from '@dfinity/utils';
+import {
+	JUNO_DATASTORE_ERROR_USER_WEBAUTHN_CALLER_KEY,
+	JUNO_DATASTORE_ERROR_USER_WEBAUTHN_CANNOT_UPDATE,
+	JUNO_DATASTORE_ERROR_USER_WEBAUTHN_INVALID_DATA
+} from '@junobuild/errors';
+import { fromArray, toArray } from '@junobuild/utils';
+import { nanoid } from 'nanoid';
+import { inject } from 'vitest';
+import { controllersInitArgs, SATELLITE_WASM_PATH } from '../../../utils/setup-tests.utils';
+
+describe('Satellite > User Webauthn', () => {
+	let pic: PocketIc;
+	let actor: Actor<SatelliteActor>;
+
+	const controller = Ed25519KeyIdentity.generate();
+
+	const createUserWebAuthn = async ({
+		publicKey,
+		credentialId
+	}: {
+		publicKey: Uint8Array;
+		credentialId: string;
+	}) => {
+		const { set_doc } = actor;
+
+		await set_doc('#user-webauthn', credentialId, {
+			data: await toArray({
+				publicKey
+			}),
+			description: toNullable(),
+			version: toNullable()
+		});
+	};
+
+	beforeAll(async () => {
+		pic = await PocketIc.create(inject('PIC_URL'));
+
+		const { actor: c } = await pic.setupCanister<SatelliteActor>({
+			idlFactory: idlFactorSatellite,
+			wasm: SATELLITE_WASM_PATH,
+			arg: controllersInitArgs(controller),
+			sender: controller.getPrincipal()
+		});
+
+		actor = c;
+
+		actor.setIdentity(controller);
+	});
+
+	afterAll(async () => {
+		await pic?.tearDown();
+	});
+
+	describe('Set', () => {
+		let user: SignIdentity;
+		let userPublicKey: Uint8Array;
+		let credentialId: string;
+
+		beforeEach(() => {
+			user = Ed25519KeyIdentity.generate();
+			actor.setIdentity(user);
+
+			userPublicKey = arrayBufferToUint8Array(user.getPublicKey().toDer());
+
+			credentialId = nanoid();
+		});
+
+		describe('#user-webauthn', () => {
+			it('should create user-webauthn with public key', async () => {
+				await createUserWebAuthn({ publicKey: userPublicKey, credentialId });
+
+				const { get_doc } = actor;
+
+				const doc = fromNullable(await get_doc('#user-webauthn', credentialId));
+
+				expect(doc).not.toBeUndefined();
+
+				const data = await fromArray(doc?.data ?? []);
+
+				expect((data as { publicKey: Uint8Array }).publicKey).toEqual(userPublicKey);
+			});
+
+			it('should not create a user-webauthn with invalid additional data fields', async () => {
+				const { set_doc } = actor;
+
+				const data = await toArray({
+					publicKey: userPublicKey,
+					unknown: 'field'
+				});
+
+				await expect(
+					set_doc('#user-webauthn', credentialId, {
+						data,
+						description: toNullable(),
+						version: toNullable()
+					})
+				).rejects.toThrow(
+					new RegExp(
+						`${JUNO_DATASTORE_ERROR_USER_WEBAUTHN_INVALID_DATA}: unknown field \`unknown\`, expected \`publicKey\``,
+						'i'
+					)
+				);
+			});
+
+			it('should not create a user-webauthn without public key', async () => {
+				const { set_doc } = actor;
+
+				const data = await toArray({});
+
+				await expect(
+					set_doc('#user-webauthn', credentialId, {
+						data,
+						description: toNullable(),
+						version: toNullable()
+					})
+				).rejects.toThrow(
+					new RegExp(
+						`${JUNO_DATASTORE_ERROR_USER_WEBAUTHN_INVALID_DATA}: missing field \`publicKey\` at line 1 column 2`,
+						'i'
+					)
+				);
+			});
+
+			it('should not update user-webauthn', async () => {
+				await createUserWebAuthn({ publicKey: userPublicKey, credentialId });
+
+				const { set_doc } = actor;
+
+				await expect(
+					set_doc('#user-webauthn', credentialId, {
+						data: await toArray({
+							publicKey: userPublicKey
+						}),
+						description: toNullable(),
+						version: toNullable(1n)
+					})
+				).rejects.toThrow(JUNO_DATASTORE_ERROR_USER_WEBAUTHN_CANNOT_UPDATE);
+			});
+		});
+
+		describe('#user-webauthn-index', () => {
+			it('should create index for user-webauthn', async () => {
+				await createUserWebAuthn({ publicKey: userPublicKey, credentialId });
+
+				actor.setIdentity(controller);
+				const { get_doc } = actor;
+
+				const doc = fromNullable(
+					await get_doc('#user-webauthn-index', user.getPrincipal().toText())
+				);
+
+				expect(doc).not.toBeUndefined();
+			});
+
+			it('should use field description to store the credential id in the index', async () => {
+				await createUserWebAuthn({ publicKey: userPublicKey, credentialId });
+
+				actor.setIdentity(controller);
+				const { get_doc } = actor;
+
+				const doc = fromNullable(
+					await get_doc('#user-webauthn-index', user.getPrincipal().toText())
+				);
+
+				assertNonNullish(doc);
+
+				expect(fromNullable(doc.description)).toEqual(credentialId);
+			});
+
+			it('should create index for user-webauthn with empty data', async () => {
+				await createUserWebAuthn({ publicKey: userPublicKey, credentialId });
+
+				actor.setIdentity(controller);
+				const { get_doc } = actor;
+
+				const doc = fromNullable(
+					await get_doc('#user-webauthn-index', user.getPrincipal().toText())
+				);
+
+				expect(doc).not.toBeUndefined();
+
+				expect(doc?.data).toBeInstanceOf(Uint8Array);
+				expect(doc?.data).toHaveLength(0);
+			});
+
+			it('should not create user-webauthn with different public key', async () => {
+				// Some other public key
+				const publicKey = arrayOfNumberToUint8Array([1, 3, 5, 7, 8, 9]);
+
+				await expect(createUserWebAuthn({ publicKey, credentialId })).rejects.toThrow(
+					JUNO_DATASTORE_ERROR_USER_WEBAUTHN_CALLER_KEY
+				);
+			});
+
+			it('should not return webauthn index for non controller', async () => {
+				await createUserWebAuthn({ publicKey: userPublicKey, credentialId });
+
+				const { get_doc } = actor;
+
+				const doc = fromNullable(
+					await get_doc('#user-webauthn-index', user.getPrincipal().toText())
+				);
+
+				expect(doc).toBeUndefined();
+			});
+		});
+	});
+});

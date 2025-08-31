c68f7348f2c4590a0a6bfa7c2affafeced515bb8
David Dal Busco <david.dalbusco@outlook.com>
2025-08-31 20:40:07 +0200

    feat: skip empty icons

--- scripts/webauthn.aaguids.mjs
+++ scripts/webauthn.aaguids.mjs
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 
+import { notEmptyString } from '@dfinity/utils';
 import { downloadFromURL } from '@junobuild/cli-tools';
 import { writeFile } from 'node:fs/promises';
 import { join } from 'node:path';
 
-const AAGUIDS_JSON_FILE = '/passkeydeveloper/passkey-authenticator-aaguids/refs/heads/main/aaguid.json';
+const AAGUIDS_JSON_FILE =
+	'/passkeydeveloper/passkey-authenticator-aaguids/refs/heads/main/aaguid.json';
 
 const data = await downloadFromURL({
 	hostname: 'raw.githubusercontent.com',
@@ -19,7 +21,7 @@ const STATIC_DEST_FOLDER = join(process.cwd(), 'src/frontend/static/aaguids');
 const ENV_DEST_FOLDER = join(process.cwd(), 'src/frontend/src/lib/env');
 
 const saveIcon = async ({ aaguid, theme, base64Svg }) => {
-	const svg = atob((base64Svg ?? "").replace(/data:image\/svg\+xml;base64,/, ''));
+	const svg = atob(base64Svg.replace(/data:image\/svg\+xml;base64,/, ''));
 
 	const dest = join(STATIC_DEST_FOLDER, `${aaguid}-${theme}.svg`);
 
@@ -30,8 +32,12 @@ const saveIcons = async ({ aaguid, value }) => {
 	const { icon_dark, icon_light } = value;
 
 	await Promise.all([
-		saveIcon({ aaguid, theme: 'dark', base64Svg: icon_dark }),
-		saveIcon({ aaguid, theme: 'light', base64Svg: icon_light })
+		...(notEmptyString(icon_dark)
+			? [saveIcon({ aaguid, theme: 'dark', base64Svg: icon_dark })]
+			: []),
+		...(notEmptyString(icon_light)
+			? [saveIcon({ aaguid, theme: 'light', base64Svg: icon_light })]
+			: [])
 	]);
 };
 
@@ -40,15 +46,12 @@ const entries = Object.entries(json);
 await Promise.all(entries.map(([key, value]) => saveIcons({ aaguid: key, value })));
 
 const destJson = join(ENV_DEST_FOLDER, 'aaguids.json');
-const cleanJson = entries.reduce(
-	((acc, [key, value]) => {
-		const { name } = value;
+const cleanJson = entries.reduce((acc, [key, value]) => {
+	const { name } = value;
 
-		return {
-			...acc,
-			[key]: name
-		};
-	}),
-	{}
-);
+	return {
+		...acc,
+		[key]: name
+	};
+}, {});
 await writeFile(destJson, JSON.stringify(cleanJson, null, 2), 'utf-8');

69ae26d6c3f4829f4db11ff4c70d37b907cc2e36
David Dal Busco <david.dalbusco@outlook.com>
2025-09-01 13:05:48 +0200

    feat(satellite): aaguid in user data (#1903)
    * feat(satellite): aaguid in user data
    
    * feat: camelCase
    
    * feat: assertion for aaguid moved and provider data

--- src/libs/satellite/src/user/core/types.rs
+++ src/libs/satellite/src/user/core/types.rs
@@ -2,10 +2,11 @@ pub mod state {
     use serde::{Deserialize, Serialize};
 
     #[derive(Serialize, Deserialize)]
-    #[serde(deny_unknown_fields)]
+    #[serde(deny_unknown_fields, rename_all = "camelCase")]
     pub struct UserData {
         pub provider: Option<AuthProvider>,
         pub banned: Option<BannedReason>,
+        pub provider_data: Option<ProviderData>,
     }
 
     #[derive(Serialize, Deserialize)]
@@ -21,5 +22,18 @@ pub mod state {
     #[serde(rename_all = "snake_case")]
     pub enum BannedReason {
         Indefinite,
+    }
+
+    #[derive(Serialize, Deserialize)]
+    #[serde(rename_all = "snake_case")]
+    pub enum ProviderData {
+        #[serde(rename = "webauthn")]
+        WebAuthn(WebAuthnData),
+    }
+
+    #[derive(Serialize, Deserialize)]
+    #[serde(rename_all = "camelCase", deny_unknown_fields)]
+    pub struct WebAuthnData {
+        pub aaguid: Option<Vec<u8>>,
     }
 }

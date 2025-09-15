5b678a9445f8ea864166a0ff8117a83919ef92f1
David Dal Busco <david.dalbusco@outlook.com>
2025-09-15 19:44:50 +0200

    feat: use unwrap_or_trap

--- src/observatory/src/notify.rs
+++ src/observatory/src/notify.rs
@@ -2,8 +2,8 @@ use crate::http::send::send_notification;
 use crate::store::stable::insert_notification;
 use crate::types::state::Notification;
 use ic_cdk::futures::spawn_017_compat;
-use ic_cdk::trap;
 use ic_cdk_timers::set_timer;
+use junobuild_shared::ic::UnwrapOrTrap;
 use junobuild_shared::types::interface::NotifyArgs;
 use std::time::Duration;
 
@@ -12,7 +12,7 @@ pub fn store_and_defer_notification(notify_args: &NotifyArgs) {
         &notify_args.segment.id,
         &Notification::from_args(notify_args),
     )
-    .unwrap_or_else(|e| trap(&e));
+    .unwrap_or_trap();
 
     set_timer(Duration::ZERO, || spawn_017_compat(send_notification(key)));
 }

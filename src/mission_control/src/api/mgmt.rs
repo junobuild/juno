562c7944cb490ed45a756bbdd1c8f612e72c5d55
David Dal Busco <david.dalbusco@outlook.com>
2025-09-15 19:43:51 +0200

    feat: use unwrap_or_trap

--- src/mission_control/src/api/mgmt.rs
+++ src/mission_control/src/api/mgmt.rs
@@ -6,9 +6,9 @@ use crate::user::store::{
     set_metadata as set_metadata_store,
 };
 use candid::Principal;
-use ic_cdk::trap;
 use ic_cdk_macros::{query, update};
 use ic_ledger_types::Tokens;
+use junobuild_shared::ic::UnwrapOrTrap;
 use junobuild_shared::mgmt::cmc::top_up_canister;
 use junobuild_shared::mgmt::ic::deposit_cycles as deposit_cycles_shared;
 use junobuild_shared::types::interface::DepositCyclesArgs;
@@ -43,12 +43,10 @@ fn get_settings() -> Option<MissionControlSettings> {
 async fn top_up(canister_id: Principal, amount: Tokens) {
     top_up_canister(&canister_id, &amount)
         .await
-        .unwrap_or_else(|e| trap(&e));
+        .unwrap_or_trap();
 }
 
 #[update(guard = "caller_is_user_or_admin_controller")]
 async fn deposit_cycles(args: DepositCyclesArgs) {
-    deposit_cycles_shared(args)
-        .await
-        .unwrap_or_else(|e| trap(&e))
+    deposit_cycles_shared(args).await.unwrap_or_trap()
 }

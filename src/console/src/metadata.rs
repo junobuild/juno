f08431047c516201742e0d32f363038dd45f9552
David Dal Busco <david.dalbusco@outlook.com>
2025-09-17 17:53:06 +0200

    refactor(storage): create types for update well-known assets

--- src/console/src/metadata.rs
+++ src/console/src/metadata.rs
@@ -5,10 +5,11 @@ use crate::store::heap::{get_releases_metadata, set_releases_metadata};
 use junobuild_cdn::proposals::SegmentsDeploymentOptions;
 use junobuild_shared::ic::api::id;
 use junobuild_storage::constants::ASSET_ENCODING_NO_COMPRESSION;
-use junobuild_storage::types::store::{Asset, AssetEncoding, AssetKey};
+use junobuild_storage::types::store::{Asset, AssetKey};
 use junobuild_storage::utils::{
     create_asset_with_content, insert_encoding_into_asset, map_content_type_headers,
 };
+use junobuild_storage::well_known::types::WellKnownAsset;
 use serde_json::to_string;
 
 pub fn update_releases_metadata(options: &SegmentsDeploymentOptions) -> Result<(), String> {
@@ -55,10 +56,7 @@ fn update_releases_metadata_asset() -> Result<(), String> {
     Ok(())
 }
 
-fn map_releases_metadata_asset(
-    metadata: &str,
-    existing_asset: Option<Asset>,
-) -> (Asset, AssetEncoding) {
+fn map_releases_metadata_asset(metadata: &str, existing_asset: Option<Asset>) -> WellKnownAsset {
     let key = AssetKey {
         name: RELEASES_METADATA_JSON.to_string(),
         full_path: RELEASES_METADATA_JSON.to_string(),

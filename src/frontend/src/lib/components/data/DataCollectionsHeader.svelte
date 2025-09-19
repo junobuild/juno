842dea547df4ef6fca5183112090bafc9da031b4
David Dal Busco <david.dalbusco@outlook.com>
2025-09-19 16:30:54 +0200

    chore: lint

--- src/frontend/src/lib/components/data/DataCollectionsHeader.svelte
+++ src/frontend/src/lib/components/data/DataCollectionsHeader.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { nonNullish } from '@dfinity/utils';
 	import type { Snippet } from 'svelte';
-	import { fade } from 'svelte/transition';
 	import DataActions from '$lib/components/data/DataActions.svelte';
 
 	interface Props {

882a2d83ab3566e2b1205686415c214291642da4
David Dal Busco <david.dalbusco@outlook.com>
2025-09-19 08:47:36 +0200

    feat(cdn): new implementation CdnCommitAssets

--- src/libs/cdn/src/strategies.rs
+++ src/libs/cdn/src/strategies.rs
@@ -1,9 +1,11 @@
 use crate::proposals::{Proposal, ProposalsStable};
 use crate::storage::{ProposalAssetsStable, ProposalContentChunksStable};
-use junobuild_collections::types::rules::Rules;
+use junobuild_collections::types::core::CollectionKey;
+use junobuild_collections::types::rules::{Rule, Rules};
 use junobuild_shared::types::domain::CustomDomains;
 use junobuild_storage::types::config::StorageConfig;
-use junobuild_storage::types::state::AssetsHeap;
+use junobuild_storage::types::state::{AssetsHeap, FullPath};
+use junobuild_storage::types::store::{Asset, AssetEncoding};
 
 pub trait CdnHeapStrategy {
     fn with_assets<R>(&self, f: impl FnOnce(&AssetsHeap) -> R) -> R;
@@ -35,4 +37,25 @@ pub trait CdnStableStrategy {
 pub trait CdnWorkflowStrategy {
     fn pre_commit_assets(&self, proposal: &Proposal) -> Result<(), String>;
     fn post_commit_assets(&self, proposal: &Proposal) -> Result<(), String>;
+}
+
+pub trait CdnCommitAssetsStrategy {
+    fn insert_asset(
+        &self,
+        collection: &CollectionKey,
+        full_path: &FullPath,
+        asset: &Asset,
+        rule: &Rule,
+    );
+
+    fn insert_asset_encoding(
+        &self,
+        full_path: &FullPath,
+        encoding_type: &str,
+        encoding: &AssetEncoding,
+        asset: &mut Asset,
+        rule: &Rule,
+    );
+
+    fn delete_assets(&self, collection: &CollectionKey) -> Result<(), String>;
 }

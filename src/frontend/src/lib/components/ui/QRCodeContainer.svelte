c9f0c5aa124c067719b8f2ca557376d8e0c4e873
David Dal Busco <david.dalbusco@outlook.com>
2025-09-22 10:40:51 +0200

    feat(frontend): replace QR-code generator library (#1986)
    * feat(frontend): replace QR-code generator library
    
    * chore: lint

--- src/frontend/src/lib/components/ui/QRCodeContainer.svelte
+++ src/frontend/src/lib/components/ui/QRCodeContainer.svelte
@@ -1,22 +1,17 @@
 <script lang="ts">
-	import { quintOut, circOut } from 'svelte/easing';
+	import { quintOut } from 'svelte/easing';
 	import { slide } from 'svelte/transition';
 	import QRCode from '$lib/components/ui/QRCode.svelte';
 
 	interface Props {
-		ariaLabel?: string | undefined;
 		value: string;
 	}
 
-	let { ariaLabel = undefined, value }: Props = $props();
+	let { value }: Props = $props();
 </script>
 
-<div
-	class="qrcode"
-	in:slide={{ delay: 0, duration: 150, easing: quintOut, axis: 'y' }}
-	out:slide={{ delay: 0, duration: 100, easing: circOut, axis: 'y' }}
->
-	<QRCode {ariaLabel} {value} />
+<div class="qrcode" in:slide={{ delay: 0, duration: 150, easing: quintOut, axis: 'y' }}>
+	<QRCode {value} />
 </div>
 
 <style lang="scss">
@@ -24,9 +19,9 @@
 
 	.qrcode {
 		min-width: calc(var(--padding) * 30);
-		padding: var(--padding-2x);
 
 		aspect-ratio: 1;
+		overflow: hidden;
 
 		@include shadow.card-action;
 	}

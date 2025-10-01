92c4467a2da0dee8b2d69fe61f898fdc3ddb7c8f
Vansh <77244334+vansh2408@users.noreply.github.com>
2025-10-01 15:16:46 +0530

    feat(wallet): display amount in USD on send review screen (#2007)
    * feat(wallet): display amount in USD on send review screen
    
    * Changed token type from TokenAmountV2 to bigint
    
    * Update SendTokensAmount.svelte
    
    Fix a small typo. It was on me when I provided the snippet in my previous review.
    
    ---------
    
    Co-authored-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/lib/components/tokens/SendTokensAmount.svelte
+++ src/frontend/src/lib/components/tokens/SendTokensAmount.svelte
@@ -2,7 +2,8 @@
 	import { nonNullish, type TokenAmountV2 } from '@dfinity/utils';
 	import Value from '$lib/components/ui/Value.svelte';
 	import { i18n } from '$lib/stores/i18n.store';
-	import { formatICP } from '$lib/utils/icp.utils';
+	import { formatICP, formatICPToUsd } from '$lib/utils/icp.utils';
+	import { icpToUsd } from '$lib/derived/exchange.derived';
 
 	interface Props {
 		token: TokenAmountV2 | undefined;
@@ -18,5 +19,16 @@
 
 	<p>
 		{#if nonNullish(token)}<span>{formatICP(token.toE8s())} <small>ICP</small></span>{/if}
+
+		{#if nonNullish($icpToUsd) && nonNullish(token)}
+			<span class="usd">{formatICPToUsd({ icp: token.toE8s(), icpToUsd: $icpToUsd })}</span>
+		{/if}
 	</p>
 </Value>
+
+<style lang="scss">
+	.usd {
+		display: block;
+		font-size: var(--font-size-small);
+	}
+</style>

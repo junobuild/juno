6f509d5fd1b14cf382ad3a64b12ffa5a886dcdca
David Dal Busco <david.dalbusco@outlook.com>
2025-10-03 16:49:43 +0200

    chore(e2e): testIds enabled for Skylab

--- src/frontend/src/lib/utils/test.utils.ts
+++ src/frontend/src/lib/utils/test.utils.ts
@@ -1,7 +1,7 @@
-import { isDev, isNotSkylab } from '$lib/env/app.env';
+import { isDev } from '$lib/env/app.env';
 import type { TestId } from '$lib/types/test-id';
 import { nonNullish } from '@dfinity/utils';
 
 export const testId = (testId: TestId | undefined): { ['data-tid']?: string } => ({
-	...(isDev() && isNotSkylab() && nonNullish(testId) && { 'data-tid': testId })
+	...(isDev() && nonNullish(testId) && { 'data-tid': testId })
 });

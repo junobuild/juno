b8f189a97325efa8b1421fa8f2f4eb57a98fc3b4
David Dal Busco <david.dalbusco@outlook.com>
2025-10-08 17:00:00 +0200

    chore: merge main

--- src/declarations/_factory.ts
+++ src/declarations/_factory.ts
@@ -49,7 +49,6 @@ import type { _SERVICE as SatelliteActor } from '$declarations/satellite/satelli
 import type { _SERVICE as SputnikActor } from '$declarations/sputnik/sputnik.did';
 import type { _SERVICE as ICActor } from '@dfinity/ic-management/dist/candid/ic-management';
 
-
 export {
 	idlFactoryCertifiedConsole,
 	idlFactoryConsole,

c6a5b99772433a890d913b507c49957d542f37c2
David Dal Busco <david.dalbusco@outlook.com>
2025-10-09 06:38:03 +0200

    test(e2e): fix analytics creation wizard

--- src/e2e/create-analytics.spec.ts
+++ src/e2e/create-analytics.spec.ts
@@ -1,22 +1,10 @@
 import { testWithII } from '@dfinity/internet-identity-playwright';
-import { expect } from '@playwright/test';
-import { testIds } from '$lib/constants/test-ids.constants';
 import { initTestSuite } from './utils/init.utils';
 
 const getConsolePage = initTestSuite();
 
-testWithII('should create Analytics via wizard', async () => {
+testWithII('should create analytics', async () => {
 	const consolePage = getConsolePage();
 
 	await consolePage.createAnalytics();
-});
-
-testWithII('should verify Analytics creation success', async () => {
-	const consolePage = getConsolePage();
-
-	// Navigate back to Analytics page to verify creation
-	await consolePage.page.goto('/analytics');
-
-	// Take screenshot to verify the Analytics page after creation
-	await expect(consolePage.page).toHaveScreenshot({ fullPage: true });
 });

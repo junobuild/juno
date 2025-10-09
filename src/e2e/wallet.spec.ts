b7bb11a27910505cc7d89fc1955ebade2f615504
David Dal Busco <david.dalbusco@outlook.com>
2025-10-09 06:31:33 +0200

    chore: fmt

--- src/e2e/wallet.spec.ts
+++ src/e2e/wallet.spec.ts
@@ -5,7 +5,7 @@ const getConsolePage = initTestSuite();
 
 testWithII('should have wallet balance equal 55.0001 ICP when user click on Get ICP', async () => {
 	testWithII.setTimeout(60_000);
-	
+
 	const consolePage = getConsolePage();
 
 	await consolePage.getICP({ balance: '55.0001 ICP' });

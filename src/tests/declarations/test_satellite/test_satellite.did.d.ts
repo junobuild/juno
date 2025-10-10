1270300216ad5dfb202c487f96fd091098f8758f
David Dal Busco <david.dalbusco@outlook.com>
2025-10-10 13:13:01 +0200

    feat(core): update did generated with latest didc

--- src/tests/declarations/test_satellite/test_satellite.did.d.ts
+++ src/tests/declarations/test_satellite/test_satellite.did.d.ts
@@ -375,7 +375,6 @@ export interface _SERVICE {
 	get_many_assets: ActorMethod<[Array<[string, string]>], Array<[string, [] | [AssetNoContent]]>>;
 	get_many_docs: ActorMethod<[Array<[string, string]>], Array<[string, [] | [Doc]]>>;
 	get_proposal: ActorMethod<[bigint], [] | [Proposal]>;
-	get_random: ActorMethod<[], Result>;
 	get_rule: ActorMethod<[CollectionType, string], [] | [Rule]>;
 	get_storage_config: ActorMethod<[], StorageConfig>;
 	http_request: ActorMethod<[HttpRequest], HttpResponse>;
@@ -410,6 +409,7 @@ export interface _SERVICE {
 	switch_storage_system_memory: ActorMethod<[], undefined>;
 	upload_asset_chunk: ActorMethod<[UploadChunk], UploadChunkResult>;
 	upload_proposal_asset_chunk: ActorMethod<[UploadChunk], UploadChunkResult>;
+	get_random: ActorMethod<[], Result>;
 }
 export declare const idlFactory: IDL.InterfaceFactory;
 export declare const init: (args: { IDL: typeof IDL }) => IDL.Type[];

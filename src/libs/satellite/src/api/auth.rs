76e46be7c7a15fe8d8cc76e0bd298436a97217c6
David Dal Busco <david.dalbusco@outlook.com>
2025-10-11 15:28:27 +0200

    feat: return result

--- src/libs/satellite/src/api/auth.rs
+++ src/libs/satellite/src/api/auth.rs
@@ -4,12 +4,15 @@ use junobuild_auth::types::interface::{
     GetDelegationResponse, OpenIdGetDelegationArgs, OpenIdPrepareDelegationArgs,
     PrepareDelegationResponse,
 };
-use junobuild_shared::ic::UnwrapOrTrap;
 
-pub fn prepare_delegation(args: &OpenIdPrepareDelegationArgs) -> PrepareDelegationResponse {
-    delegation::openid_prepare_delegation(args, &AuthCertificate).unwrap_or_trap()
+pub fn openid_prepare_delegation(
+    args: &OpenIdPrepareDelegationArgs,
+) -> Result<PrepareDelegationResponse, String> {
+    delegation::openid_prepare_delegation(args, &AuthCertificate)
 }
 
-pub fn get_delegation(args: &OpenIdGetDelegationArgs) -> GetDelegationResponse {
-    delegation::openid_get_delegation(args, &AuthCertificate).unwrap_or_trap()
+pub fn openid_get_delegation(
+    args: &OpenIdGetDelegationArgs,
+) -> Result<GetDelegationResponse, String> {
+    delegation::openid_get_delegation(args, &AuthCertificate)
 }

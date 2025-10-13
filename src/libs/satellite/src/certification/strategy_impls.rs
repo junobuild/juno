3b123cb07d28528ea5f4e1d2f0b4ea6aa8f0b9f2
David Dal Busco <david.dalbusco@outlook.com>
2025-10-13 10:49:43 +0200

    feat(auth): init certificate strategy

--- src/libs/satellite/src/certification/strategy_impls.rs
+++ src/libs/satellite/src/certification/strategy_impls.rs
@@ -1,7 +1,20 @@
 use crate::certification::cert::update_certified_data;
 use ic_certification::HashTree;
 use junobuild_auth::runtime::pruned_labeled_sigs_root_hash_tree;
+use junobuild_auth::strategies::AuthCertificateStrategy;
 use junobuild_storage::strategies::StorageCertificateStrategy;
+
+/// The consumer of junobuild_auth and junobuild_storage is responsible for implementing
+/// the functions that update the certified data. This way, both libraries remain unaware
+/// of each other.
+
+pub struct AuthCertificate;
+
+impl AuthCertificateStrategy for AuthCertificate {
+    fn update_certified_data(&self) {
+        update_certified_data()
+    }
+}
 
 pub struct StorageCertificate;
 

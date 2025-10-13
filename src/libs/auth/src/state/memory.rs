9230a09a5e7caecfa22c4db8e11a9c18cee8c7c9
David Dal Busco <david.dalbusco@outlook.com>
2025-10-13 07:49:39 +0200

    feat(auth): add signatures runtime state

---
+++ src/libs/auth/src/state/memory.rs
@@ -0,0 +1,6 @@
+use crate::types::runtime_state::State;
+use std::cell::RefCell;
+
+thread_local! {
+    pub(super) static STATE: RefCell<State> = RefCell::default();
+}
---
+++ src/libs/auth/src/state/runtime.rs
@@ -0,0 +1,16 @@
+use crate::state::services::read_state;
+use ic_canister_sig_creation::signature_map::LABEL_SIG;
+use ic_certification::hash_tree::HashTree;
+use ic_certification::{labeled_hash, pruned, Hash};
+
+fn sigs_root_hash() -> Hash {
+    read_state(|state| state.runtime.sigs.root_hash())
+}
+
+pub fn labeled_sigs_root_hash() -> Hash {
+    labeled_hash(LABEL_SIG, &sigs_root_hash())
+}
+
+pub fn pruned_labeled_sigs_root_hash_tree() -> HashTree<Vec<u8>> {
+    pruned(labeled_hash(LABEL_SIG, &sigs_root_hash()))
+}

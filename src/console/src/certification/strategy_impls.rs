9c1d341d971ce92934c7803abe17fbd21fb0c034
David Dal Busco <david.dalbusco@outlook.com>
2025-10-13 13:52:47 +0200

    docs(auth): move comment to abstract fn

--- src/console/src/certification/strategy_impls.rs
+++ src/console/src/certification/strategy_impls.rs
@@ -10,9 +10,6 @@ impl StorageCertificateStrategy for StorageCertificate {
         update_certified_data();
     }
 
-    // We use this function to access the auth signatures root hash
-    // in the storage crate when we build an HTTP response because the
-    // tree is a fork of both assets and signatures.
     fn get_pruned_labeled_sigs_root_hash_tree(&self) -> HashTree {
         pruned_labeled_sigs_root_hash_tree()
     }

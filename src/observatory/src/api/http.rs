cc6973f898a2bbd198884bdac15631882120927e
David Dal Busco <david.dalbusco@outlook.com>
2025-10-20 11:35:27 +0200

    chore: merge conflict

--- src/observatory/src/api/http.rs
+++ src/observatory/src/api/http.rs
@@ -3,6 +3,6 @@ use ic_cdk::management_canister::{HttpRequestResult, TransformArgs};
 use ic_cdk_macros::query;
 
 #[query(hidden = true)]
-fn transform(raw: TransformArgs) -> HttpResponse {
+fn transform(raw: TransformArgs) -> HttpRequestResult {
     transform_response(raw)
 }

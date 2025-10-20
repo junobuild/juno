b85f2594d4db63a897b1cefccc3f865dd8e5115d
David Dal Busco <david.dalbusco@outlook.com>
2025-10-20 11:27:23 +0200

    fix: expose

--- src/observatory/src/lib.rs
+++ src/observatory/src/lib.rs
@@ -13,5 +13,10 @@ mod types;
 
 use crate::types::interface::GetNotifications;
 use crate::types::interface::NotifyStatus;
+use crate::types::state::Env;
 use ic_cdk_macros::export_candid;
+use junobuild_shared::types::interface::NotifyArgs;
+use junobuild_shared::types::interface::{DeleteControllersArgs, SetControllersArgs};
+use junobuild_shared::types::state::Controllers;
+
 export_candid!();

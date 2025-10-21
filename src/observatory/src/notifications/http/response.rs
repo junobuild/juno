883b45223b1330cb964445dcbdeda885d80ca2a6
David Dal Busco <david.dalbusco@outlook.com>
2025-10-21 12:25:59 +0200

    refactor: rename

--- src/observatory/src/notifications/http/response.rs
+++ src/observatory/src/notifications/http/response.rs
@@ -1,6 +1,6 @@
 use ic_cdk::management_canister::{HttpRequestResult, TransformArgs};
 
-pub fn transform_response(raw: TransformArgs) -> HttpRequestResult {
+pub fn transform_post_email(raw: TransformArgs) -> HttpRequestResult {
     HttpRequestResult {
         status: raw.response.status.clone(),
         body: raw.response.body.clone(),

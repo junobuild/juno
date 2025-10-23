14b0062ee15421a3653f989ef8529e7af010bcae
David Dal Busco <david.dalbusco@outlook.com>
2025-10-23 09:46:34 +0200

    feat(satellite): use with pattern for accessing authentication on heap

--- src/libs/satellite/src/memory/state/services/raw.rs
+++ src/libs/satellite/src/memory/state/services/raw.rs
@@ -1,5 +1,9 @@
 use crate::memory::state::STATE;
-use crate::types::state::{RuntimeState, State};
+use crate::types::state::{HeapState, RuntimeState, State};
+
+fn read_state<R>(f: impl FnOnce(&State) -> R) -> R {
+    STATE.with(|cell| f(&cell.borrow()))
+}
 
 fn mutate_state<R>(f: impl FnOnce(&mut State) -> R) -> R {
     STATE.with(|cell| f(&mut cell.borrow_mut()))
@@ -7,4 +11,12 @@ fn mutate_state<R>(f: impl FnOnce(&mut State) -> R) -> R {
 
 pub fn mutate_runtime_state<R>(f: impl FnOnce(&mut RuntimeState) -> R) -> R {
     mutate_state(|state| f(&mut state.runtime))
+}
+
+pub fn read_heap_state<R>(f: impl FnOnce(&HeapState) -> R) -> R {
+    read_state(|state| f(&state.heap))
+}
+
+pub fn mutate_heap_state<R>(f: impl FnOnce(&mut HeapState) -> R) -> R {
+    mutate_state(|state| f(&mut state.heap))
 }

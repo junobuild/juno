4111bf8292d083482d51dab7544906f070f176eb
David Dal Busco <david.dalbusco@outlook.com>
2025-10-23 10:18:12 +0200

    feat: unsafe header

--- src/libs/auth/src/openid/jwt/jwks.rs
+++ src/libs/auth/src/openid/jwt/jwks.rs
@@ -5,10 +5,8 @@ use crate::openid::jwt::types::errors::JwtFindProviderError;
 use crate::openid::types::provider::OpenIdProvider;
 
 #[derive(Clone, Deserialize)]
-struct UnsafeHeaderClaims {
-    pub iss: Option<String>,
-    pub sub: Option<String>,
-    pub aud: Option<String>,
+struct UnsafeHeader {
+    pub kid: Option<String>,
 }
 
 pub fn unsafe_jwks_has_kid(provider: &OpenIdProvider, jwt: &str) {
@@ -16,7 +14,7 @@ pub fn unsafe_jwks_has_kid(provider: &OpenIdProvider, jwt: &str) {
     decode_jwt_header(jwt).map_err(JwtFindProviderError::from)?;
 
     // 2) Decode the payload (⚠️ no signature validation)
-    let token_data = dangerous::insecure_decode::<UnsafeClaims>(jwt)
+    let token_data = dangerous::insecure_decode::<UnsafeHeader>(jwt)
         .map_err(|e| JwtFindProviderError::BadSig(e.to_string()))?;
 
     let kid = token_data.claims.kid.ok_or(VerifyOpenidCredentialsError::MissingKid)?;

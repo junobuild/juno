bae81a79631cfc0ab6460c6f8085e85b60a3a9a1
David Dal Busco <david.dalbusco@outlook.com>
2025-10-27 07:35:24 +0100

    chore: run cargo test on satellite

--- scripts/cargo.tests.sh
+++ scripts/cargo.tests.sh
@@ -9,3 +9,4 @@ export RUSTFLAGS='--cfg getrandom_backend="custom"'
 cargo test -p junobuild-auth --lib --tests "${EXTRA_ARGS[@]}"
 cargo test -p junobuild-shared --lib --tests "${EXTRA_ARGS[@]}"
 cargo test -p junobuild-utils --lib --tests "${EXTRA_ARGS[@]}"
+cargo test -p junobuild-satellite --lib --tests "${EXTRA_ARGS[@]}"

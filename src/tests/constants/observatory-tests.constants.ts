079ae984617f9d56cf74b825dff9b07be5f40afa
David Dal Busco <david.dalbusco@outlook.com>
2025-10-28 17:45:16 +0100

    feat(observatory): anonymous cannot get certificate (#2154)

--- src/tests/constants/observatory-tests.constants.ts
+++ src/tests/constants/observatory-tests.constants.ts
@@ -3,4 +3,6 @@ import { Principal } from '@dfinity/principal';
 export const CALLER_NOT_CONTROLLER_OBSERVATORY_MSG =
 	'Caller is not a controller of the observatory.';
 
+export const CALLER_NOT_ANONYMOUS_MSG = 'Anonymous caller is not allowed.';
+
 export const OBSERVATORY_ID = Principal.fromText('klbfr-lqaaa-aaaak-qbwsa-cai');

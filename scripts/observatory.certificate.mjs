d8d03539f0e8896ff6dd89c2ccf20ae0149279b5
David Dal Busco <david.dalbusco@outlook.com>
2025-10-29 06:48:09 +0100

    chore(scripts): print last update

--- scripts/observatory.certificate.mjs
+++ scripts/observatory.certificate.mjs
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 
+import { fromNullable, isNullish } from '@dfinity/utils';
 import { observatoryActorIC, observatoryActorLocal } from './actor.mjs';
 import { targetMainnet } from './utils.mjs';
+
+const fromBigIntNanoSeconds = (nanoseconds) => new Date(Number(nanoseconds / 1_000_000n));
 
 const getGoogleCertificate = async (mainnet) => {
 	const { get_openid_certificate } = await (mainnet
@@ -11,6 +14,13 @@ const getGoogleCertificate = async (mainnet) => {
 	const certificate = await get_openid_certificate({ provider: { Google: null } });
 
 	console.log('📥 Google certificate:', certificate);
+
+	const cert = fromNullable(certificate);
+	if (isNullish(cert)) {
+		return;
+	}
+
+	console.log('🔄 Last update:', fromBigIntNanoSeconds(cert.updated_at));
 };
 
 const mainnet = targetMainnet();

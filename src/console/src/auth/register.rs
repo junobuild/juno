a43644d4bbd5fbc7b39ea6415aba0a03214cfbf2
David Dal Busco <david.dalbusco@outlook.com>
2025-10-30 17:26:40 +0100

    feat: in progress

--- src/console/src/auth/register.rs
+++ src/console/src/auth/register.rs
@@ -1,5 +1,5 @@
 use crate::factory::mission_control::init_user_mission_control;
-use crate::types::state::MissionControl;
+use crate::types::state::{MissionControl, Provider};
 use candid::Principal;
 use junobuild_auth::delegation::types::UserKey;
 use junobuild_auth::openid::types::interface::OpenIdCredential;
@@ -15,6 +15,27 @@ pub async fn register_mission_control(
 
     let current_mission_control = get_mission_control(&user_id)?;
 
+    let existing_provider_data: Option<&OpenIdData> = match &current_mission_control {
+        None => None, // A new user
+        Some(mission_control) => match mission_control.provider.as_ref() {
+            Some(Provider::Google(provider_data)) => Some(provider_data),
+            _ => return Err(JUNO_DATASTORE_ERROR_USER_PROVIDER_GOOGLE_INVALID_DATA.to_string()),
+        },
+    };
+
+    // If the credential data are unchanged and the mission control already exists, we can return it
+    // without any updates.
+    if let Some(current_mission_control) =
+        &current_mission_control
+    {
+        if let Some(existing_provider) = current_mission_control.provider.as_ref() {
+            let new_provider_data = OpenIdData::from(credential);
+
+            if *existing_provider_data == new_provider_data {
+                return Ok(current_user.clone());
+            }
+        }
+    }
 
     // TODO: init mission control with jwt info
 

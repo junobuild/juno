6edb310d1b8db0c9fa8639cb1e1921e0d920b08a
David Dal Busco <david.dalbusco@outlook.com>
2025-10-30 19:15:22 +0100

    test: add data and controller

--- src/tests/specs/console/auth/console.auth.mission-control.spec.ts
+++ src/tests/specs/console/auth/console.auth.mission-control.spec.ts
@@ -1,8 +1,12 @@
-import type { ConsoleActor } from '$declarations';
+import {
+	type ConsoleActor,
+	idlFactoryMissionControl,
+	type MissionControlActor
+} from '$declarations';
 import type { MissionControl } from '$declarations/console/console.did';
 import type { DelegationIdentity } from '@dfinity/identity';
 import type { Actor, PocketIc } from '@dfinity/pic';
-import { assertNonNullish, fromNullable } from '@dfinity/utils';
+import { assertNonNullish, fromNullable, toNullable } from '@dfinity/utils';
 import { authenticateAndMakeIdentity } from '../../../utils/auth-identity-tests.utils';
 import { setupConsoleAuth, type TestSession } from '../../../utils/auth-tests.utils';
 import { makeJwt, type MockOpenIdJwt } from '../../../utils/jwt-tests.utils';
@@ -19,18 +23,12 @@ describe('Satellite > Auth > Mission Control', () => {
 	let mockIdentity: DelegationIdentity;
 
 	const mockUserData = {
-		provider: 'google',
-		banned: null,
-		providerData: {
-			openid: {
-				email: 'user@example.com',
-				name: 'Hello World',
-				givenName: 'Hello',
-				familyName: 'World',
-				picture: null,
-				locale: null
-			}
-		}
+		email: toNullable('user@example.com'),
+		name: toNullable('Hello World'),
+		given_name: toNullable('Hello'),
+		family_name: toNullable('World'),
+		picture: toNullable(),
+		locale: toNullable()
 	};
 
 	beforeAll(async () => {
@@ -76,8 +74,28 @@ describe('Satellite > Auth > Mission Control', () => {
 			return;
 		}
 
-		const { Google: data } = provider;
+		const {
+			OpenId: { data, provider: provider_name }
+		} = provider;
+		expect('Google' in provider_name).toBeTruthy();
+
 		expect(data).toEqual(mockUserData);
+
+		// Should be a controller
+		const missionControlId = fromNullable(missionControl.mission_control_id);
+
+		assertNonNullish(missionControlId);
+
+		const micActor = pic.createActor<MissionControlActor>(
+			idlFactoryMissionControl,
+			missionControlId
+		);
+		micActor.setIdentity(identity);
+
+		const { get_user } = micActor;
+
+		const user = await get_user();
+		expect(user.toText()).toEqual(missionControl.owner.toText());
 	});
 
 	// TODO assert controller
@@ -86,9 +104,9 @@ describe('Satellite > Auth > Mission Control', () => {
 		await pic.advanceTime(1000 * 30); // 30s for cooldown guard
 		await tick(pic);
 
-		const { authenticate_user } = consoleActor;
+		const { authenticate } = consoleActor;
 
-		const result = await authenticate_user({
+		const result = await authenticate({
 			OpenId: {
 				jwt: mockJwt.jwt,
 				session_key: session.publicKey,
@@ -106,9 +124,22 @@ describe('Satellite > Auth > Mission Control', () => {
 
 		expect(missionControl.owner.toText()).toEqual(mockIdentity.getPrincipal().toText());
 
-		// TODO
-		// const data = await fromArray(user.data);
-		// expect(data).toEqual(mockUserData);
+		const provider = fromNullable(missionControl.provider);
+
+		assertNonNullish(provider);
+
+		if ('InternetIdentity' in provider) {
+			expect(true).toBeFalsy();
+
+			return;
+		}
+
+		const {
+			OpenId: { data, provider: provider_name }
+		} = provider;
+		expect('Google' in provider_name).toBeTruthy();
+
+		expect(data).toEqual(mockUserData);
 	});
 
 	it('should update user data', async () => {
@@ -129,9 +160,9 @@ describe('Satellite > Auth > Mission Control', () => {
 			privateKey: mockJwt.privateKey
 		});
 
-		const { authenticate_user } = consoleActor;
+		const { authenticate } = consoleActor;
 
-		const result = await authenticate_user({
+		const result = await authenticate({
 			OpenId: {
 				jwt: newJwt,
 				session_key: session.publicKey,
@@ -149,20 +180,27 @@ describe('Satellite > Auth > Mission Control', () => {
 
 		expect(missionControl.owner.toText()).toEqual(mockIdentity.getPrincipal().toText());
 
-		// TODO
-		// const data = await fromArray(user.data);
-		//
-		// expect(data).toEqual({
-		// 	...mockUserData,
-		// 	providerData: {
-		// 		openid: {
-		// 			...mockUserData.providerData.openid,
-		// 			name: updatePayload.name,
-		// 			givenName: updatePayload.given_name,
-		// 			familyName: updatePayload.family_name,
-		// 			email: updatePayload.email
-		// 		}
-		// 	}
-		// });
+		const provider = fromNullable(missionControl.provider);
+
+		assertNonNullish(provider);
+
+		if ('InternetIdentity' in provider) {
+			expect(true).toBeFalsy();
+
+			return;
+		}
+
+		const {
+			OpenId: { data, provider: provider_name }
+		} = provider;
+		expect('Google' in provider_name).toBeTruthy();
+
+		expect(data).toEqual({
+			...mockUserData,
+			name: toNullable(updatePayload.name),
+			given_name: toNullable(updatePayload.given_name),
+			family_name: toNullable(updatePayload.family_name),
+			email: toNullable(updatePayload.email)
+		});
 	});
 });

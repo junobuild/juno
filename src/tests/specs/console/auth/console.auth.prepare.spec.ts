96e9fd536c0614e45e5c47b30814146da60b2fdb
David Dal Busco <david.dalbusco@outlook.com>
2025-10-30 07:11:43 +0100

    test: prepare

---
+++ src/tests/specs/console/auth/console.auth.prepare.spec.ts
@@ -0,0 +1,37 @@
+import { type ConsoleActor } from '$declarations';
+import { Ed25519KeyIdentity } from '@dfinity/identity';
+import type { Actor, PocketIc } from '@dfinity/pic';
+import { setupConsole } from '../../../utils/console-tests.utils';
+import { testAuthPrepareDelegation } from '../../../utils/auth-assertions-prepare-delegation-tests.utils';
+import { mockCertificateDate } from '../../../mocks/jwt.mocks';
+
+describe('Console > Authentication', async () => {
+	let pic: PocketIc;
+
+	let actor: Actor<ConsoleActor>;
+	let controller: Ed25519KeyIdentity;
+
+	beforeAll(async () => {
+		const {
+			actor: a,
+			pic: p,
+			controller: cO
+		} = await setupConsole({
+			dateTime: mockCertificateDate
+		});
+
+		pic = p;
+		actor = a;
+		controller = cO;
+	});
+
+	afterAll(async () => {
+		await pic?.tearDown();
+	});
+
+	testAuthPrepareDelegation({
+		pic: () => pic,
+		actor: () => actor,
+		controller: () => controller
+	});
+});

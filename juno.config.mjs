f078e2bc9f5184a68a3bc8aa0703331a3841282f
David Dal Busco <david.dalbusco@outlook.com>
2025-11-02 19:53:42 +0100

    docs: review comment

--- juno.config.mjs
+++ juno.config.mjs
@@ -1,19 +1,19 @@
 import { defineConsoleConfig } from '@junobuild/config';
 
 /** @type {import('@junobuild/config').JunoConsoleConfig} */
-export default defineConsoleConfig(({mode}) => ({
+export default defineConsoleConfig(({ mode }) => ({
 	id: 'cokmz-oiaaa-aaaal-aby6q-cai',
 	source: 'build',
-	...(mode === "development" && {
+	...(mode === 'development' && {
 		authentication: {
 			google: {
-				clientId: "974645854757-h6ndt2o4kqv8o63512ssdlf20gnqjb8m.apps.googleusercontent.com",
+				clientId: '974645854757-h6ndt2o4kqv8o63512ssdlf20gnqjb8m.apps.googleusercontent.com',
 				delegation: {
-					// Similarly to the identities derived by Internet Identity, we allow those
-					// to target any canisters on the Internet Computer.
+					// Like identities derived by Internet Identity, those derived with OpenID
+					// are allowed to interact with any canister on the Internet Computer.
 					allowedTargets: null
 				}
 			}
 		}
-	}),
+	})
 }));

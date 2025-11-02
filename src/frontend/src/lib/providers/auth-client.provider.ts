0c20cd4397c09cf39863278f419686fd7c033c18
David Dal Busco <david.dalbusco@outlook.com>
2025-11-02 13:02:23 +0100

    feat(frontend): use custom storage for auth-client and pattern (#2181)

--- src/frontend/src/lib/providers/auth-client.provider.ts
+++ src/frontend/src/lib/providers/auth-client.provider.ts
@@ -1,20 +1,42 @@
 import { AuthClient, IdbStorage, KEY_STORAGE_KEY } from '@dfinity/auth-client';
 
-export const createAuthClient = (): Promise<AuthClient> =>
-	AuthClient.create({
-		idleOptions: {
-			disableIdle: true,
-			disableDefaultIdleCallback: true
-		}
-	});
+class AuthClientProvider {
+	// We use a dedicated storage for the auth client to better manage it, e.g. clear it for a new login.
+	// It is similar as the default fallback option of AgentJS.
+	readonly #storage: IdbStorage;
 
-export const resetAuthClient = async (): Promise<AuthClient> => {
-	// Since AgentJS persists the identity key in IndexedDB by default,
-	// it can be tampered with and affect the next login with Internet Identity.
-	// To ensure each session starts clean and safe, we clear the stored keys before creating a new AuthClient
-	// in case the user is not authenticated.
-	const storage = new IdbStorage();
-	await storage.remove(KEY_STORAGE_KEY);
+	constructor() {
+		this.#storage = new IdbStorage();
+	}
 
-	return await createAuthClient();
-};
+	createAuthClient = (): Promise<AuthClient> =>
+		AuthClient.create({
+			storage: this.#storage,
+			idleOptions: {
+				disableIdle: true,
+				disableDefaultIdleCallback: true
+			}
+		});
+
+	/**
+	 * Since icp-js-core persists identity keys in IndexedDB by default,
+	 * they could be tampered with and affect the next login.
+	 * To ensure each session starts clean and safe, we clear the stored keys before creating a new AuthClient.
+	 */
+	safeCreateAuthClient = async (): Promise<AuthClient> => {
+		await this.#storage.remove(KEY_STORAGE_KEY);
+		return await this.createAuthClient();
+	};
+
+	get storage(): IdbStorage {
+		return this.#storage;
+	}
+}
+
+const {
+	storage: __test_only_auth_client_storage__,
+	createAuthClient,
+	safeCreateAuthClient
+} = new AuthClientProvider();
+
+export { __test_only_auth_client_storage__, createAuthClient, safeCreateAuthClient };

ff15c8e60d5815198deef3f49ded5f377bbca079
David Dal Busco <david.dalbusco@outlook.com>
2025-11-02 20:20:31 +0100

    feat(frontend): sign-in with Google (#2184)
    * feat(frontend): sign-in with Google
    
    * feat: redirect
    
    * docs: review comment
    
    * chore: lint
    
    * build: reinstall config next
    
    * build: use next

--- juno.config.mjs
+++ juno.config.mjs
@@ -1,7 +1,19 @@
 import { defineConsoleConfig } from '@junobuild/config';
 
 /** @type {import('@junobuild/config').JunoConsoleConfig} */
-export default defineConsoleConfig({
+export default defineConsoleConfig(({ mode }) => ({
 	id: 'cokmz-oiaaa-aaaal-aby6q-cai',
-	source: 'build'
-});
+	source: 'build',
+	...(mode === 'development' && {
+		authentication: {
+			google: {
+				clientId: '974645854757-h6ndt2o4kqv8o63512ssdlf20gnqjb8m.apps.googleusercontent.com',
+				delegation: {
+					// Like identities derived by Internet Identity, those derived with OpenID
+					// are allowed to interact with any canister on the Internet Computer.
+					allowedTargets: null
+				}
+			}
+		}
+	})
+}));
--- src/frontend/src/lib/components/core/SignInGoogle.svelte
+++ src/frontend/src/lib/components/core/SignInGoogle.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
 	import IconGoogle from '$lib/components/icons/IconGoogle.svelte';
 	import { testIds } from '$lib/constants/test-ids.constants';
-	import { signInWithII } from '$lib/services/auth/auth.services';
+	import { signInWithGoogle } from '$lib/services/auth/auth.services';
 	import { isBusy } from '$lib/stores/busy.store';
 	import { i18n } from '$lib/stores/i18n.store';
 	import { testId } from '$lib/utils/test.utils';
 </script>
 
-<button {...testId(testIds.auth.signIn)} disabled={$isBusy} onclick={signInWithII}
+<button {...testId(testIds.auth.signIn)} disabled={$isBusy} onclick={signInWithGoogle}
 	><IconGoogle size="20px" />
 	<span>{$i18n.sign_in.google}</span></button
 >

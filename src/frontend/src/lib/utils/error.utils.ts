1ea558cb15556beec30b20ef3cb42158b6c6d675
David Dal Busco <david.dalbusco@outlook.com>
2025-11-03 07:58:06 +0100

    feat: print error cause

--- src/frontend/src/lib/utils/error.utils.ts
+++ src/frontend/src/lib/utils/error.utils.ts
@@ -1,2 +1,12 @@
+import { jsonReplacer, nonNullish } from '@dfinity/utils';
+
 export const errorDetailToString = (err: unknown): string | undefined =>
-	typeof err === 'string' ? err : err instanceof Error ? err.message : undefined;
+	typeof err === 'string'
+		? err
+		: err instanceof Error
+			? nonNullish(err.cause)
+				? `${err.message}: ${
+						err.cause instanceof Error ? err.cause.message : JSON.stringify(err.cause, jsonReplacer)
+					}`
+				: err.message
+			: undefined;

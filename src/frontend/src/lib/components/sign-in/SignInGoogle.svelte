df95864e168d4deeae5e6d67571c271b23d57b98
David Dal Busco <david.dalbusco@outlook.com>
2025-11-03 18:21:54 +0100

    test(frontend): make test id for sign-in with II unique

--- src/frontend/src/lib/components/sign-in/SignInGoogle.svelte
+++ src/frontend/src/lib/components/sign-in/SignInGoogle.svelte
@@ -2,15 +2,13 @@
 	import { nonNullish } from '@dfinity/utils';
 	import IconGoogle from '$lib/components/icons/IconGoogle.svelte';
 	import { GOOGLE_CLIENT_ID } from '$lib/constants/app.constants';
-	import { testIds } from '$lib/constants/test-ids.constants';
 	import { signInWithGoogle } from '$lib/services/auth/auth.openid.services';
 	import { isBusy } from '$lib/stores/busy.store';
 	import { i18n } from '$lib/stores/i18n.store';
-	import { testId } from '$lib/utils/test.utils';
 </script>
 
 {#if nonNullish(GOOGLE_CLIENT_ID)}
-	<button {...testId(testIds.auth.signIn)} disabled={$isBusy} onclick={signInWithGoogle}
+	<button disabled={$isBusy} onclick={signInWithGoogle}
 		><IconGoogle size="20px" />
 		<span>{$i18n.sign_in.google}</span></button
 	>

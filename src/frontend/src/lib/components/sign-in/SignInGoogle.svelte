5175a51f76524335a4587797e482a11e7a2abeb0
David Dal Busco <david.dalbusco@outlook.com>
2025-11-03 17:46:41 +0100

    feat(frontend): no Google sign-in button if no client ID (#2195)
    * feat(frontend): no Google sign-in button if no client ID
    
    * chore: lint

--- src/frontend/src/lib/components/sign-in/SignInGoogle.svelte
+++ src/frontend/src/lib/components/sign-in/SignInGoogle.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+	import { nonNullish } from '@dfinity/utils';
 	import IconGoogle from '$lib/components/icons/IconGoogle.svelte';
+	import { GOOGLE_CLIENT_ID } from '$lib/constants/app.constants';
 	import { testIds } from '$lib/constants/test-ids.constants';
 	import { signInWithGoogle } from '$lib/services/auth/auth.openid.services';
 	import { isBusy } from '$lib/stores/busy.store';
@@ -7,10 +9,12 @@
 	import { testId } from '$lib/utils/test.utils';
 </script>
 
-<button {...testId(testIds.auth.signIn)} disabled={$isBusy} onclick={signInWithGoogle}
-	><IconGoogle size="20px" />
-	<span>{$i18n.sign_in.google}</span></button
->
+{#if nonNullish(GOOGLE_CLIENT_ID)}
+	<button {...testId(testIds.auth.signIn)} disabled={$isBusy} onclick={signInWithGoogle}
+		><IconGoogle size="20px" />
+		<span>{$i18n.sign_in.google}</span></button
+	>
+{/if}
 
 <style lang="scss">
 	button {

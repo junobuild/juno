3592215ca313c5c3ee4ecfacf347f8995ed3108f
David Dal Busco <david.dalbusco@outlook.com>
2025-11-03 11:51:17 +0100

    feat(frontend): mission control in store, not just id

--- src/frontend/src/lib/derived/mission-control.derived.ts
+++ src/frontend/src/lib/derived/mission-control.derived.ts
@@ -1,14 +1,16 @@
-import { missionControlIdCertifiedStore } from '$lib/stores/mission-control.store';
+import { missionControlCertifiedStore } from '$lib/stores/mission-control.store';
+import { fromNullable } from '@dfinity/utils';
 import { derived } from 'svelte/store';
 
 // TODO: find a better name but, I don't want to use missionControlId because it would clashes with the properties called missionControlId
 export const missionControlIdDerived = derived(
-	[missionControlIdCertifiedStore],
-	([$missionControlDataStore]) => $missionControlDataStore?.data
+	[missionControlCertifiedStore],
+	([$missionControlDataStore]) =>
+		fromNullable($missionControlDataStore?.data.mission_control_id ?? [])
 );
 
 export const missionControlIdLoaded = derived(
-	[missionControlIdCertifiedStore],
+	[missionControlCertifiedStore],
 	([$missionControlIdCertifiedStore]) => $missionControlIdCertifiedStore !== undefined
 );
 

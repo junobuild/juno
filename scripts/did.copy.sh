79d8c2dded0eb6052157e7e8432ee1fcd9d59d3d
David Dal Busco <david.dalbusco@outlook.com>
2025-11-07 09:54:14 +0100

    feat: ic-management did

---
+++ scripts/did.copy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# ic-management to declarations
+cp node_modules/@dfinity/ic-management/dist/candid/ic-management.d.ts src/declarations/ic-management/ic-management.did.d.ts
+cp node_modules/@dfinity/ic-management/dist/candid/ic-management.certified.idl.js src/declarations/ic-management/ic-management.factory.certified.did.js
+cp node_modules/@dfinity/ic-management/dist/candid/ic-management.idl.js src/declarations/ic-management/ic-management.factory.did.js
+
+# mjs
+cp src/declarations/console/console.factory.did.js src/declarations/console/console.factory.did.mjs
+cp src/declarations/satellite/satellite.factory.did.js src/declarations/satellite/satellite.factory.did.mjs
+cp src/declarations/observatory/observatory.factory.did.js src/declarations/observatory/observatory.factory.did.mjs
+cp src/declarations/satellite/satellite.factory.did.js src/declarations/satellite/satellite.factory.did.mjs
+cp src/declarations/orbiter/orbiter.factory.did.js src/declarations/orbiter/orbiter.factory.did.mjs
+cp src/declarations/ic-management/ic-management.factory.did.js src/declarations/ic-management/ic-management.factory.did.mjs

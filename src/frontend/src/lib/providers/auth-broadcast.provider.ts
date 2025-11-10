8f706e3b32b12354e8dfcdbca6b4cde3624e1980
David Dal Busco <david.dalbusco@outlook.com>
2025-11-10 15:53:18 +0100

    docs: comment

--- src/frontend/src/lib/providers/auth-broadcast.provider.ts
+++ src/frontend/src/lib/providers/auth-broadcast.provider.ts
@@ -1,5 +1,9 @@
 import { isNullish, notEmptyString } from '@dfinity/utils';
 
+// We broadcast an object that contains the message and an emitter
+// because we want to ignore the message in the tab that emitted its.
+// This is useful for example to avoid to reload the auth store if already
+// synced (since the postMessage happens after login).
 interface BroadcastData {
 	msg: string;
 	emitterId: string;

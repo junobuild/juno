147d7c6adf3425316a4bb4a7d88ca067c4bbf1e9
David Dal Busco <david.dalbusco@outlook.com>
2025-11-11 07:14:30 +0100

    docs: review

--- src/frontend/src/lib/schemas/auth-broadcast.schema.ts
+++ src/frontend/src/lib/schemas/auth-broadcast.schema.ts
@@ -1,9 +1,9 @@
 import * as z from 'zod';
 
-// We broadcast an object that contains the message and an emitter
-// because we want to ignore the message in the tab that emitted its.
-// This is useful for example to avoid to reload the auth store if already
-// synced (since the postMessage happens after login).
+// We broadcast an object containing both the message and the emitter.
+// This allows us to ignore the message in the tab that originally sent it.
+// For example, it prevents reloading the auth store if it's already synced,
+// since the postMessage is triggered after login.
 export const BroadcastDataSchema = z.strictObject({
 	msg: z.literal('authClientLoginSuccess'),
 	emitterId: z.string().nonempty()

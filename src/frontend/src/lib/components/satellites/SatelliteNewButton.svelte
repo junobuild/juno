5aaeaaa1976eed13b63fe56b110f29d82f98acaf
David Dal Busco <david.dalbusco@outlook.com>
2025-11-20 09:37:18 +0100

    test(e2e): fix missing testId (#2270)
    * test(e2e): fix missing testId
    
    * chore: fmt

--- src/frontend/src/lib/components/satellites/SatelliteNewButton.svelte
+++ src/frontend/src/lib/components/satellites/SatelliteNewButton.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
+	import { testIds } from '$lib/constants/test-ids.constants';
 	import { missionControlIdDerived } from '$lib/derived/mission-control.derived';
 	import { initSatelliteWizard } from '$lib/services/wizard.services';
 	import { authStore } from '$lib/stores/auth.store';
 	import { i18n } from '$lib/stores/i18n.store';
+	import { testId } from '$lib/utils/test.utils';
 
 	const createSatellite = async () => {
 		await initSatelliteWizard({
@@ -12,7 +14,7 @@
 	};
 </script>
 
-<button class="primary" onclick={createSatellite}>
+<button class="primary" onclick={createSatellite} {...testId(testIds.createSatellite.launch)}>
 	{$i18n.satellites.launch}
 </button>
 

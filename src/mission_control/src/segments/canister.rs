b61a6e9283a3b8a1b6b5d9324a324c8facf539f3
David Dal Busco <david.dalbusco@outlook.com>
2025-11-28 12:21:58 +0100

    feat(mctrl): migrate get fee call to Call::bounded_wait (#2288)
    * feat(mctrl): migrate get fee call to Call::bounded_wait
    
    * chore: remove print

--- src/mission_control/src/segments/canister.rs
+++ src/mission_control/src/segments/canister.rs
@@ -3,14 +3,16 @@ use crate::user::store::get_user;
 use candid::Principal;
 use ic_cdk::api::call::CallResult;
 use ic_cdk::call;
+use ic_cdk::call::Call;
 use ic_ledger_types::{BlockIndex, Tokens};
 use junobuild_shared::constants_shared::{IC_TRANSACTION_FEE_ICP, MEMO_CANISTER_CREATE};
 use junobuild_shared::env::CONSOLE;
 use junobuild_shared::ic::api::id;
+use junobuild_shared::ic::DecodeCandid;
 use junobuild_shared::ledger::icp::{transfer_payment, SUB_ACCOUNT};
 use junobuild_shared::mgmt::ic::{delete_segment, stop_segment};
 use junobuild_shared::types::interface::{DepositCyclesArgs, GetCreateCanisterFeeArgs};
-use junobuild_shared::types::state::UserId;
+use junobuild_shared::types::state::{UserId};
 use std::future::Future;
 
 pub async fn create_canister<F, Fut, T>(
@@ -27,30 +29,28 @@ where
 
     let args = GetCreateCanisterFeeArgs { user };
 
-    let fee: CallResult<(Option<Tokens>,)> = call(console, fee_method, (args,)).await;
+    let fee = Call::bounded_wait(console, fee_method)
+        .with_arg(args)
+        .await
+        .decode_candid::<Option<Tokens>>()?;
 
     match fee {
-        Err((_, message)) => Err(["Cannot fetch creation fee.", &message].join(" - ")),
-        Ok((fee,)) => {
-            match fee {
-                // If no fee provided, the creation of the satellite is probably for free
-                None => create_and_save(user, config.clone(), None).await,
-                Some(fee) => {
-                    // If a free is set, transfer the requested fee to the console
-                    let block_index = transfer_payment(
-                        &console,
-                        &SUB_ACCOUNT,
-                        MEMO_CANISTER_CREATE,
-                        fee,
-                        IC_TRANSACTION_FEE_ICP,
-                    )
-                    .await
-                    .map_err(|e| format!("failed to call ledger: {e:?}"))?
-                    .map_err(|e| format!("ledger transfer error {e:?}"))?;
+        // If no fee provided, the creation of the satellite is probably for free
+        None => create_and_save(user, config.clone(), None).await,
+        Some(fee) => {
+            // If a free is set, transfer the requested fee to the console
+            let block_index = transfer_payment(
+                &console,
+                &SUB_ACCOUNT,
+                MEMO_CANISTER_CREATE,
+                fee,
+                IC_TRANSACTION_FEE_ICP,
+            )
+            .await
+            .map_err(|e| format!("failed to call ledger: {e:?}"))?
+            .map_err(|e| format!("ledger transfer error {e:?}"))?;
 
-                    create_and_save(user, config.clone(), Some(block_index)).await
-                }
-            }
+            create_and_save(user, config.clone(), Some(block_index)).await
         }
     }
 }

731a3b2b423f27686f3042abe7614eb857ee2c2b
David Dal Busco <david.dalbusco@outlook.com>
2025-11-28 15:49:46 +0100

    feat(observatory): migrate mission control to Call::bounded_wait (#2294)
    * feat(observatory): migrate mission control to Call::bounded_wait
    
    * test: update expectation text

--- src/tests/specs/observatory/observatory.notify.spec.ts
+++ src/tests/specs/observatory/observatory.notify.spec.ts
@@ -98,7 +98,7 @@ describe('Observatory > Notify', () => {
 		it('should throw errors if caller is not a mission control', async () => {
 			const { notify } = observatoryActor;
 
-			await expect(notify(mockNotifyArgs)).rejects.toThrow('Mission control is unknown.');
+			await expect(notify(mockNotifyArgs)).rejects.toThrow('User does not have a mission control center.');
 		});
 
 		it('should throw errors if caller is not observatory', async () => {

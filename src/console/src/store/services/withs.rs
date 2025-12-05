9e9f33224cb58d82e7d909f4c6a6a297be575d4f
David Dal Busco <david.dalbusco@outlook.com>
2025-12-05 07:04:41 +0100

    feat(console): use with_payments pattern

--- src/console/src/store/services/withs.rs
+++ src/console/src/store/services/withs.rs
@@ -1,4 +1,6 @@
 use crate::store::services::raw::{mutate_heap_state, read_heap_state};
+use crate::store::{mutate_stable_state, read_stable_state};
+use crate::types::state::PaymentsStable;
 use junobuild_auth::state::types::state::AuthenticationHeapState;
 use junobuild_storage::types::config::StorageConfig;
 use junobuild_storage::types::state::AssetsHeap;
@@ -30,4 +32,15 @@ pub fn with_auth<R>(f: impl FnOnce(&Option<AuthenticationHeapState>) -> R) -> R 
 
 pub fn with_auth_mut<R>(f: impl FnOnce(&mut Option<AuthenticationHeapState>) -> R) -> R {
     mutate_heap_state(|state| f(&mut state.authentication))
+}
+
+pub fn with_payments<R>(f: impl FnOnce(&PaymentsStable) -> R) -> R {
+    read_stable_state(|state| {
+        let payments = &state.payments;
+        f(&payments)
+    })
+}
+
+pub fn with_payments_mut<R>(f: impl FnOnce(&mut PaymentsStable) -> R) -> R {
+    mutate_stable_state(|state| f(&mut state.payments))
 }

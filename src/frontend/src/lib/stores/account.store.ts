477976c9ee5f888042a092d986fc25b88dc3485b
David Dal Busco <david.dalbusco@outlook.com>
2025-12-05 17:09:41 +0100

    chore: fmt

--- src/frontend/src/lib/stores/account.store.ts
+++ src/frontend/src/lib/stores/account.store.ts
@@ -1,4 +1,4 @@
-import { initCertifiedStore } from '$lib/stores/_certified.store';
 import type { ConsoleDid } from '$declarations';
+import { initCertifiedStore } from '$lib/stores/_certified.store';
 
 export const accountCertifiedStore = initCertifiedStore<ConsoleDid.Account>();

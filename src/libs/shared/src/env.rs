1d56ca16e22d7e272cc69fbbeea8d76d4621b3ff
David Dal Busco <david.dalbusco@outlook.com>
2025-12-06 15:04:30 +0100

    refactor(shared): rename ledger const into icp_ledger

--- src/libs/shared/src/env.rs
+++ src/libs/shared/src/env.rs
@@ -8,7 +8,7 @@ pub const OBSERVATORY: &str = "klbfr-lqaaa-aaaak-qbwsa-cai";
 
 // Mainnet: ryjl3-tyaaa-aaaaa-aaaba-cai
 // Local: ryjl3-tyaaa-aaaaa-aaaba-cai
-pub const LEDGER: &str = "ryjl3-tyaaa-aaaaa-aaaba-cai";
+pub const ICP_LEDGER: &str = "ryjl3-tyaaa-aaaaa-aaaba-cai";
 
 // Mainnet: rkp4c-7iaaa-aaaaa-aaaca-cai
 // Local: rkp4c-7iaaa-aaaaa-aaaca-cai

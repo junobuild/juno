a84fae06f72056091e3d3d9765b3f8f174d85250
David Dal Busco <david.dalbusco@outlook.com>
2025-12-17 12:14:10 +0100

    feat(console): mission control fee (#2371)
    * feat(console): mission control fee
    
    * chore: fmt
    
    * test: fees still there after migration

--- src/console/src/store/heap/fees.rs
+++ src/console/src/store/heap/fees.rs
@@ -11,12 +11,30 @@ pub fn get_orbiter_fee() -> Tokens {
     with_fees(|fees| fees.orbiter.fee)
 }
 
+pub fn get_mission_control_fee() -> Result<Tokens, String> {
+    with_fees(|fees| get_mission_control_fee_impl(fees))
+}
+
+fn get_mission_control_fee_impl(fees: &Fees) -> Result<Tokens, String> {
+    let fee = &fees.mission_control;
+
+    if let Some(fee) = fee {
+        return Ok(fee.fee);
+    }
+
+    Err("No fees defined for Mission Control".to_string())
+}
+
 pub fn set_create_satellite_fee(fee: &Tokens) {
     with_fees_mut(|fees| set_satellite_fee(fee, fees))
 }
 
 pub fn set_create_orbiter_fee(fee: &Tokens) {
     with_fees_mut(|fees| set_orbiter_fee(fee, fees))
+}
+
+pub fn set_create_mission_control_fee(fee: &Tokens) {
+    with_fees_mut(|fees| set_mission_control_fee(fee, fees))
 }
 
 fn set_satellite_fee(fee: &Tokens, state: &mut Fees) {
@@ -31,4 +49,11 @@ fn set_orbiter_fee(fee: &Tokens, state: &mut Fees) {
         fee: *fee,
         updated_at: time(),
     };
+}
+
+fn set_mission_control_fee(fee: &Tokens, state: &mut Fees) {
+    state.mission_control = Some(Fee {
+        fee: *fee,
+        updated_at: time(),
+    });
 }

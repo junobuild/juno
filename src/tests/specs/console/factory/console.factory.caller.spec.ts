8bff50e7c8710063054dace13b663e88c29f9a08
David Dal Busco <david.dalbusco@outlook.com>
2025-12-19 08:36:58 +0100

    chore: lint

--- src/tests/specs/console/factory/console.factory.caller.spec.ts
+++ src/tests/specs/console/factory/console.factory.caller.spec.ts
@@ -3,6 +3,7 @@ import type { Actor, PocketIc } from '@dfinity/pic';
 import { toNullable } from '@dfinity/utils';
 import type { Identity } from '@icp-sdk/core/agent';
 import { Ed25519KeyIdentity } from '@icp-sdk/core/identity';
+import type { Principal } from '@icp-sdk/core/principal';
 import { NO_ACCOUNT_ERROR_MSG } from '../../../constants/console-tests.constants';
 import { setupConsole } from '../../../utils/console-tests.utils';
 
@@ -34,10 +35,10 @@ describe('Console > Factory > Caller', () => {
 		await pic?.tearDown();
 	});
 
-	const createSatelliteWithConsole = async ({ user }: { user: Identity }) => {
+	const createSatelliteWithConsole = async ({ user }: { user: Identity }): Promise<Principal> => {
 		const { create_satellite } = actor;
 
-		return create_satellite({
+		return await create_satellite({
 			user: user.getPrincipal(),
 			block_index: toNullable(),
 			name: toNullable(),
@@ -46,10 +47,10 @@ describe('Console > Factory > Caller', () => {
 		});
 	};
 
-	const createOrbiterWithConsole = async ({ user }: { user: Identity }) => {
+	const createOrbiterWithConsole = async ({ user }: { user: Identity }): Promise<Principal> => {
 		const { create_orbiter } = actor;
 
-		return create_orbiter({
+		return await create_orbiter({
 			user: user.getPrincipal(),
 			block_index: toNullable(),
 			name: toNullable(),

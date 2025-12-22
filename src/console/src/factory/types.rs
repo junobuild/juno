0596b09fbe9944b66b54a9fc4fc302d0cd6506f0
David Dal Busco <david.dalbusco@outlook.com>
2025-12-22 12:01:57 +0100

    feat(console): if user has mission control, make it controller too (#2391)

--- src/console/src/factory/types.rs
+++ src/console/src/factory/types.rs
@@ -4,7 +4,7 @@ use junobuild_shared::types::state::{MissionControlId, UserId};
 
 #[derive(Clone)]
 pub enum CanisterCreator {
-    User(UserId), // The caller is the owner of the account. The caller comes from the Console UI.
+    User((UserId, Option<MissionControlId>)), // The caller is the owner of the account. The caller comes from the Console UI.
     MissionControl((MissionControlId, UserId)), // The caller is a mission control
 }
 

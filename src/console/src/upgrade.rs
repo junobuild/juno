1fa08eb4d58bce2461c64ca1f10da6189efb0081
David Dal Busco <david.dalbusco@outlook.com>
2026-01-06 12:28:23 +0100

    fix: upgrade

--- src/console/src/upgrade.rs
+++ src/console/src/upgrade.rs
@@ -1,12 +1,12 @@
 use crate::constants::CANISTER_CREATION_FEE_CYCLES;
-use crate::fees::{init_factory_fees, set_factory_fee};
+use crate::fees::init_factory_fees;
 use crate::rates::init::init_factory_rates;
-use crate::rates::set_factory_rate;
 use crate::store::mutate_heap_state;
-use crate::types::interface::FeesArgs;
-use crate::types::state::{HeapState};
+use crate::types::state::{FactoryFee, FactoryFees, FactoryRate, FactoryRates, HeapState};
+use ic_cdk::api::time;
 use junobuild_shared::ic::api::print;
 use junobuild_shared::rate::constants::DEFAULT_RATE_CONFIG;
+use junobuild_shared::rate::types::RateTokens;
 use junobuild_shared::types::state::SegmentKind;
 
 pub fn upgrade_init_factory_fees_and_rates() {
@@ -19,18 +19,49 @@ fn upgrade_init_factory_fees_and_rates_impl(state: &mut HeapState) {
 }
 
 pub fn upgrade_init_canister_fees_and_rates() {
-    mutate_heap_state(|state| upgrade_init_factory_fees_and_rates_impl(state));
+    mutate_heap_state(|state| {
+        upgrade_init_factory_fees_and_rates_impl(state);
 
-    // Init fee
-    let fee = FeesArgs {
+        upgrade_canister_fees(&mut state.factory_fees)
+            .unwrap_or_else(|err| print(format!("Error upgrading the Canister fee: {:?}", err)));
+
+        upgrade_canister_rates(&mut state.factory_rates)
+            .unwrap_or_else(|err| print(format!("Error upgrading the Canister rate: {:?}", err)));
+    });
+}
+
+fn upgrade_canister_fees(factory_fees: &mut Option<FactoryFees>) -> Result<(), String> {
+    let fees = factory_fees
+        .as_mut()
+        .ok_or_else(|| "Factory fees not initialized".to_string())?;
+
+    let fee = FactoryFee {
         fee_cycles: CANISTER_CREATION_FEE_CYCLES,
         fee_icp: None,
+        updated_at: time(),
     };
 
-    set_factory_fee(&SegmentKind::Canister, &fee)
-        .unwrap_or_else(|err| print(format!("Error upgrading the Canister fee: {:?}", err)));
+    fees.insert(SegmentKind::Canister, fee);
 
-    // Init rate
-    set_factory_rate(&SegmentKind::Canister, &DEFAULT_RATE_CONFIG)
-        .unwrap_or_else(|err| print(format!("Error upgrading the Canister rate: {:?}", err)));
+    Ok(())
+}
+
+fn upgrade_canister_rates(factory_rates: &mut Option<FactoryRates>) -> Result<(), String> {
+    let rates = factory_rates
+        .as_mut()
+        .ok_or_else(|| "Factory rates not initialized".to_string())?;
+
+    let tokens: RateTokens = RateTokens {
+        tokens: 1,
+        updated_at: time(),
+    };
+
+    let rate = FactoryRate {
+        config: DEFAULT_RATE_CONFIG,
+        tokens: tokens.clone(),
+    };
+
+    rates.insert(SegmentKind::Canister, rate);
+
+    Ok(())
 }

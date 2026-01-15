be17bb27550cb51272a5f359d5d6a44fbf9a6288
David Dal Busco <david.dalbusco@outlook.com>
2026-01-15 09:28:37 +0100

    chore(scripts): print ledger cycles balance of the console

--- scripts/constants.mjs
+++ scripts/constants.mjs
@@ -3,5 +3,6 @@ import { Principal } from '@icp-sdk/core/principal';
 export const CONSOLE_ID = Principal.fromText('cokmz-oiaaa-aaaal-aby6q-cai');
 export const OBSERVATORY_ID = Principal.fromText('klbfr-lqaaa-aaaak-qbwsa-cai');
 
-export const LEDGER_ID = Principal.fromText('ryjl3-tyaaa-aaaaa-aaaba-cai');
+export const ICP_LEDGER_ID = Principal.fromText('ryjl3-tyaaa-aaaaa-aaaba-cai');
+export const CYCLES_LEDGER_ID = Principal.fromText('um5iw-rqaaa-aaaaq-qaaba-cai');
 export const CMC_ID = Principal.fromText('rkp4c-7iaaa-aaaaa-aaaca-cai');

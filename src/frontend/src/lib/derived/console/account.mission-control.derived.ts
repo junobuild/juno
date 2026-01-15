60e54bc580940dabf75fd7352144e1d1815489b8
David Dal Busco <david.dalbusco@outlook.com>
2026-01-15 11:44:19 +0100

    feat(frontend): load segments once at boot time

--- src/frontend/src/lib/derived/console/account.mission-control.derived.ts
+++ src/frontend/src/lib/derived/console/account.mission-control.derived.ts
@@ -1,8 +1,26 @@
 import { accountCertifiedStore } from '$lib/stores/account.store';
+import type { MissionControlCertifiedId } from '$lib/types/mission-control';
 import type { Option } from '$lib/types/utils';
 import { fromNullable, isNullish } from '@dfinity/utils';
 import type { Principal } from '@icp-sdk/core/principal';
 import { derived } from 'svelte/store';
+
+export const missionControlCertifiedId = derived(
+	[accountCertifiedStore],
+	([$accountCertifiedStore]): MissionControlCertifiedId => {
+		if (isNullish($accountCertifiedStore)) {
+			return undefined;
+		}
+
+		const missionControlId =
+			fromNullable($accountCertifiedStore.data?.mission_control_id ?? []) ?? null;
+
+		return {
+			data: missionControlId,
+			certified: $accountCertifiedStore.certified
+		};
+	}
+);
 
 export const missionControlId = derived(
 	[accountCertifiedStore],

6dc39a4178688263a50321d5de60ad9acf8f825a
David Dal Busco <david.dalbusco@outlook.com>
2026-01-29 11:15:57 +0100

    feat(auth)!: rename get_providers to get_auth_providers

--- src/libs/satellite/src/auth/authenticate.rs
+++ src/libs/satellite/src/auth/authenticate.rs
@@ -6,12 +6,12 @@ use crate::types::interface::{Authentication, AuthenticationError, Authenticatio
 use junobuild_auth::delegation::types::{
     GetDelegationResult, OpenIdGetDelegationArgs, OpenIdPrepareDelegationArgs,
 };
-use junobuild_auth::state::get_providers;
+use junobuild_auth::state::get_auth_providers;
 
 pub async fn openid_authenticate(
     args: &OpenIdPrepareDelegationArgs,
 ) -> Result<AuthenticationResult, String> {
-    let providers = get_providers(&AuthHeap)?;
+    let providers = get_auth_providers(&AuthHeap)?;
 
     // We assert early on to avoid generating a delegation which won't be used
     // in case the user cannot be created. We also want to prevent creating delegations
@@ -37,7 +37,7 @@ pub async fn openid_authenticate(
 pub fn openid_get_delegation(
     args: &OpenIdGetDelegationArgs,
 ) -> Result<GetDelegationResult, String> {
-    let providers = get_providers(&AuthHeap)?;
+    let providers = get_auth_providers(&AuthHeap)?;
 
     let result = delegation::openid_get_delegation(args, &providers);
 

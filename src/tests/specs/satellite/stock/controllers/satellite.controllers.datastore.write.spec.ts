1f45730d1e391ddce28ccfa76701eb13e44fb8a5
David Dal Busco <david.dalbusco@outlook.com>
2026-02-05 17:13:57 +0100

    feat(shared): ephemeral access keys - assertion on expires_at (#2572)

---
+++ src/tests/specs/satellite/stock/controllers/satellite.controllers.datastore.write.spec.ts
@@ -0,0 +1,172 @@
+import { idlFactorySatellite, type SatelliteActor, type SatelliteDid } from '$declarations';
+import { toBigIntNanoSeconds } from '$lib/utils/date.utils';
+import { type Actor, PocketIc } from '@dfinity/pic';
+import { assertNonNullish, fromNullable, toNullable } from '@dfinity/utils';
+import { Ed25519KeyIdentity } from '@icp-sdk/core/identity';
+import { JUNO_DATASTORE_ERROR_CANNOT_WRITE } from '@junobuild/errors';
+import { inject } from 'vitest';
+import { CONTROLLER_METADATA } from '../../../../constants/controller-tests.constants';
+import { mockListParams } from '../../../../mocks/list.mocks';
+import { tick } from '../../../../utils/pic-tests.utils';
+import { createDoc as createDocUtils } from '../../../../utils/satellite-doc-tests.utils';
+import { controllersInitArgs, SATELLITE_WASM_PATH } from '../../../../utils/setup-tests.utils';
+
+describe.each([
+	{ title: 'heap', memory: { Heap: null } },
+	{ title: 'stable', memory: { Stable: null } }
+])('Satellite > Controllers > Datastore $title > Write', ({ memory }) => {
+	let pic: PocketIc;
+	let actor: Actor<SatelliteActor>;
+
+	const controller = Ed25519KeyIdentity.generate();
+
+	const TEST_COLLECTION = 'test';
+
+	const currentDate = new Date(2021, 6, 10, 0, 0, 0, 0);
+
+	const createDoc = (): Promise<string> =>
+		createDocUtils({
+			actor,
+			collection: TEST_COLLECTION
+		});
+
+	beforeAll(async () => {
+		pic = await PocketIc.create(inject('PIC_URL'));
+
+		await pic.setTime(currentDate.getTime());
+
+		const { actor: c } = await pic.setupCanister<SatelliteActor>({
+			idlFactory: idlFactorySatellite,
+			wasm: SATELLITE_WASM_PATH,
+			arg: controllersInitArgs(controller),
+			sender: controller.getPrincipal()
+		});
+
+		actor = c;
+
+		actor.setIdentity(controller);
+
+		const setRule: SatelliteDid.SetRule = {
+			memory: toNullable(memory),
+			max_size: toNullable(),
+			read: { Managed: null },
+			mutable_permissions: toNullable(),
+			write: { Managed: null },
+			version: toNullable(),
+			max_capacity: toNullable(),
+			rate_config: toNullable(),
+			max_changes_per_user: toNullable()
+		};
+
+		const { set_rule } = actor;
+		await set_rule({ Db: null }, TEST_COLLECTION, setRule);
+	});
+
+	afterAll(async () => {
+		await pic?.tearDown();
+	});
+
+	const generateController = async (futureMilliseconds?: number) => {
+		actor.setIdentity(controller);
+
+		const testController = Ed25519KeyIdentity.generate();
+
+		const { set_controllers } = actor;
+
+		await set_controllers({
+			controller: {
+				...CONTROLLER_METADATA,
+				scope: { Write: null },
+				expires_at: [
+					toBigIntNanoSeconds(new Date((await pic.getTime()) + (futureMilliseconds ?? 0)))
+				]
+			},
+			controllers: [testController.getPrincipal()]
+		});
+
+		actor.setIdentity(testController);
+	};
+
+	it('should throw on set document', async () => {
+		await generateController();
+
+		await pic.advanceTime(100);
+
+		await expect(createDoc()).rejects.toThrowError(JUNO_DATASTORE_ERROR_CANNOT_WRITE);
+	});
+
+	it('should return empty on get document', async () => {
+		const futureMilliseconds = 10_000;
+
+		await generateController(futureMilliseconds);
+
+		const key = await createDoc();
+
+		const { get_doc } = actor;
+
+		expect(fromNullable(await get_doc(TEST_COLLECTION, key))).not.toBeUndefined();
+
+		await pic.advanceTime(futureMilliseconds + 1);
+		await tick(pic);
+
+		await expect(get_doc(TEST_COLLECTION, key)).resolves.toEqual([]);
+	});
+
+	it('should throw on delete document', async () => {
+		const futureMilliseconds = 10_000;
+
+		await generateController(futureMilliseconds);
+
+		const key = await createDoc();
+
+		await pic.advanceTime(futureMilliseconds + 1);
+		await tick(pic);
+
+		const { del_doc } = actor;
+
+		await expect(del_doc(TEST_COLLECTION, key, { version: [1n] })).rejects.toThrowError(
+			JUNO_DATASTORE_ERROR_CANNOT_WRITE
+		);
+	});
+
+	it('should throw on update document', async () => {
+		const futureMilliseconds = 10_000;
+
+		await generateController(futureMilliseconds);
+
+		const key = await createDoc();
+
+		const { get_doc } = actor;
+
+		const doc = fromNullable(await get_doc(TEST_COLLECTION, key));
+
+		assertNonNullish(doc);
+
+		await pic.advanceTime(futureMilliseconds + 1);
+		await tick(pic);
+
+		const { set_doc } = actor;
+
+		await expect(
+			set_doc(TEST_COLLECTION, key, {
+				...doc,
+				version: doc.version
+			})
+		).rejects.toThrowError(JUNO_DATASTORE_ERROR_CANNOT_WRITE);
+	});
+
+	it('should return empty on list documents', async () => {
+		const futureMilliseconds = 10_000;
+
+		await generateController(futureMilliseconds);
+
+		const { list_docs } = actor;
+
+		expect((await list_docs(TEST_COLLECTION, mockListParams)).items_length).toBeGreaterThan(0n);
+
+		await pic.advanceTime(futureMilliseconds + 1);
+		await tick(pic);
+
+		expect((await list_docs(TEST_COLLECTION, mockListParams)).items_length).toEqual(0n);
+	});
+});
---
+++ src/tests/specs/satellite/stock/controllers/satellite.controllers.guards.spec.ts
@@ -0,0 +1,278 @@
+import { idlFactorySatellite, type SatelliteActor } from '$declarations';
+import { toBigIntNanoSeconds } from '$lib/utils/date.utils';
+import { type Actor, PocketIc } from '@dfinity/pic';
+import { toNullable } from '@dfinity/utils';
+import { Ed25519KeyIdentity } from '@icp-sdk/core/identity';
+import {
+	JUNO_AUTH_ERROR_NOT_CONTROLLER,
+	JUNO_AUTH_ERROR_NOT_WRITE_CONTROLLER
+} from '@junobuild/errors';
+import { inject } from 'vitest';
+import { CONTROLLER_METADATA } from '../../../../constants/controller-tests.constants';
+import { mockListProposalsParams } from '../../../../mocks/list.mocks';
+import { controllersInitArgs, SATELLITE_WASM_PATH } from '../../../../utils/setup-tests.utils';
+
+describe('Satellite > Controllers > Guards', () => {
+	let pic: PocketIc;
+	let actor: Actor<SatelliteActor>;
+
+	const controller = Ed25519KeyIdentity.generate();
+
+	const TEST_COLLECTION = 'test';
+
+	const currentDate = new Date(2021, 6, 10, 0, 0, 0, 0);
+
+	beforeAll(async () => {
+		pic = await PocketIc.create(inject('PIC_URL'));
+
+		await pic.setTime(currentDate.getTime());
+
+		const { actor: c } = await pic.setupCanister<SatelliteActor>({
+			idlFactory: idlFactorySatellite,
+			wasm: SATELLITE_WASM_PATH,
+			arg: controllersInitArgs(controller),
+			sender: controller.getPrincipal()
+		});
+
+		actor = c;
+
+		actor.setIdentity(controller);
+	});
+
+	afterAll(async () => {
+		await pic?.tearDown();
+	});
+
+	describe.each([
+		{ title: 'write', scope: { Write: null } },
+		{ title: 'submit', scope: { Submit: null } }
+	])('Caller is valid controller $title', ({ scope }) => {
+		const testController = Ed25519KeyIdentity.generate();
+
+		beforeAll(async () => {
+			actor.setIdentity(controller);
+
+			const { set_controllers } = actor;
+
+			await set_controllers({
+				controller: {
+					...CONTROLLER_METADATA,
+					scope,
+					expires_at: [toBigIntNanoSeconds(currentDate)]
+				},
+				controllers: [testController.getPrincipal()]
+			});
+
+			actor.setIdentity(testController);
+
+			await pic.advanceTime(100);
+		});
+
+		it('should throw on get_proposal', async () => {
+			const { get_proposal } = actor;
+
+			await expect(get_proposal(123n)).rejects.toThrowError(JUNO_AUTH_ERROR_NOT_CONTROLLER);
+		});
+
+		it('should throw on list_proposals', async () => {
+			const { list_proposals } = actor;
+
+			await expect(list_proposals(mockListProposalsParams)).rejects.toThrowError(
+				JUNO_AUTH_ERROR_NOT_CONTROLLER
+			);
+		});
+
+		it('should throw on count_proposals', async () => {
+			const { count_proposals } = actor;
+
+			await expect(count_proposals()).rejects.toThrowError(JUNO_AUTH_ERROR_NOT_CONTROLLER);
+		});
+
+		it('should throw on init_proposal', async () => {
+			const { init_proposal } = actor;
+
+			await expect(
+				init_proposal({
+					AssetsUpgrade: {
+						clear_existing_assets: toNullable()
+					}
+				})
+			).rejects.toThrowError(JUNO_AUTH_ERROR_NOT_CONTROLLER);
+		});
+
+		it('should throw on submit_proposal', async () => {
+			const { submit_proposal } = actor;
+
+			await expect(submit_proposal(123n)).rejects.toThrowError(JUNO_AUTH_ERROR_NOT_CONTROLLER);
+		});
+
+		it('should throw on init_proposal_asset_upload', async () => {
+			const { init_proposal_asset_upload } = actor;
+
+			await expect(
+				init_proposal_asset_upload(
+					{
+						collection: TEST_COLLECTION,
+						description: toNullable(),
+						encoding_type: [],
+						full_path: '/test',
+						name: '/test',
+						token: toNullable()
+					},
+					123n
+				)
+			).rejects.toThrowError(JUNO_AUTH_ERROR_NOT_CONTROLLER);
+		});
+
+		it('should throw on init_proposal_many_assets_upload', async () => {
+			const { init_proposal_many_assets_upload } = actor;
+
+			await expect(
+				init_proposal_many_assets_upload(
+					[
+						{
+							collection: TEST_COLLECTION,
+							description: toNullable(),
+							encoding_type: [],
+							full_path: '/test',
+							name: '/test',
+							token: toNullable()
+						}
+					],
+					123n
+				)
+			).rejects.toThrowError(JUNO_AUTH_ERROR_NOT_CONTROLLER);
+		});
+
+		it('should throw on upload_proposal_asset_chunk', async () => {
+			const { upload_proposal_asset_chunk } = actor;
+
+			await expect(
+				upload_proposal_asset_chunk({
+					batch_id: 123n,
+					content: Uint8Array.from([1, 2]),
+					order_id: [0n]
+				})
+			).rejects.toThrowError(JUNO_AUTH_ERROR_NOT_CONTROLLER);
+		});
+
+		it('should throw on commit_proposal_asset_upload', async () => {
+			const { commit_proposal_asset_upload } = actor;
+
+			await expect(
+				commit_proposal_asset_upload({
+					batch_id: 12n,
+					chunk_ids: [1n],
+					headers: []
+				})
+			).rejects.toThrowError(JUNO_AUTH_ERROR_NOT_CONTROLLER);
+		});
+
+		it('should throw on commit_proposal_many_assets_upload', async () => {
+			const { commit_proposal_many_assets_upload } = actor;
+
+			await expect(
+				commit_proposal_many_assets_upload([
+					{
+						batch_id: 12n,
+						chunk_ids: [1n],
+						headers: []
+					}
+				])
+			).rejects.toThrowError(JUNO_AUTH_ERROR_NOT_CONTROLLER);
+		});
+
+		it('should throw on memory_size', async () => {
+			const { memory_size } = actor;
+
+			await expect(memory_size()).rejects.toThrowError(JUNO_AUTH_ERROR_NOT_CONTROLLER);
+		});
+	});
+
+	describe('Caller with write', () => {
+		const testController = Ed25519KeyIdentity.generate();
+
+		beforeAll(async () => {
+			actor.setIdentity(controller);
+
+			const { set_controllers } = actor;
+
+			await set_controllers({
+				controller: {
+					...CONTROLLER_METADATA,
+					scope: { Write: null },
+					expires_at: [toBigIntNanoSeconds(currentDate)]
+				},
+				controllers: [testController.getPrincipal()]
+			});
+
+			actor.setIdentity(testController);
+
+			await pic.advanceTime(100);
+		});
+
+		it('should throw on del_docs', async () => {
+			const { del_docs } = actor;
+
+			await expect(del_docs(TEST_COLLECTION)).rejects.toThrowError(
+				JUNO_AUTH_ERROR_NOT_WRITE_CONTROLLER
+			);
+		});
+
+		it('should throw on count_collection_docs', async () => {
+			const { count_collection_docs } = actor;
+
+			await expect(count_collection_docs(TEST_COLLECTION)).rejects.toThrowError(
+				JUNO_AUTH_ERROR_NOT_WRITE_CONTROLLER
+			);
+		});
+
+		it('should throw on reject_proposal', async () => {
+			const { reject_proposal } = actor;
+
+			await expect(
+				reject_proposal({
+					proposal_id: 1123n,
+					sha256: Uint8Array.from([1, 2])
+				})
+			).rejects.toThrowError(JUNO_AUTH_ERROR_NOT_WRITE_CONTROLLER);
+		});
+
+		it('should throw on commit_proposal', async () => {
+			const { commit_proposal } = actor;
+
+			await expect(
+				commit_proposal({
+					proposal_id: 1123n,
+					sha256: Uint8Array.from([1, 2])
+				})
+			).rejects.toThrowError(JUNO_AUTH_ERROR_NOT_WRITE_CONTROLLER);
+		});
+
+		it('should throw on delete_proposal_assets', async () => {
+			const { delete_proposal_assets } = actor;
+
+			await expect(
+				delete_proposal_assets({
+					proposal_ids: [1n]
+				})
+			).rejects.toThrowError(JUNO_AUTH_ERROR_NOT_WRITE_CONTROLLER);
+		});
+
+		it('should throw on del_assets', async () => {
+			const { del_assets } = actor;
+
+			await expect(del_assets(TEST_COLLECTION)).rejects.toThrowError(
+				JUNO_AUTH_ERROR_NOT_WRITE_CONTROLLER
+			);
+		});
+
+		it('should throw on count_collection_assets', async () => {
+			const { count_collection_assets } = actor;
+
+			await expect(count_collection_assets(TEST_COLLECTION)).rejects.toThrowError(
+				JUNO_AUTH_ERROR_NOT_WRITE_CONTROLLER
+			);
+		});
+	});
+});

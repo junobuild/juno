c5779fcdeeefc64db8756815b3c1f52ff62271cd
David Dal Busco <david.dalbusco@outlook.com>
2026-02-05 10:33:51 +0100

    test: guards

--- src/tests/specs/satellite/stock/controllers/satellite.controllers.guards.spec.ts
+++ src/tests/specs/satellite/stock/controllers/satellite.controllers.guards.spec.ts
@@ -9,6 +9,7 @@ import {
 } from '@junobuild/errors';
 import { inject } from 'vitest';
 import { CONTROLLER_METADATA } from '../../../../constants/controller-tests.constants';
+import { mockListProposalsParams } from '../../../../mocks/list.mocks';
 import { controllersInitArgs, SATELLITE_WASM_PATH } from '../../../../utils/setup-tests.utils';
 
 describe.each([
@@ -89,6 +90,120 @@ describe.each([
 			const { get_proposal } = actor;
 
 			await expect(get_proposal(123n)).rejects.toThrowError(JUNO_AUTH_ERROR_NOT_CONTROLLER);
+		});
+
+		it('should throw on list_proposals', async () => {
+			const { list_proposals } = actor;
+
+			await expect(list_proposals(mockListProposalsParams)).rejects.toThrowError(
+				JUNO_AUTH_ERROR_NOT_CONTROLLER
+			);
+		});
+
+		it('should throw on count_proposals', async () => {
+			const { count_proposals } = actor;
+
+			await expect(count_proposals()).rejects.toThrowError(JUNO_AUTH_ERROR_NOT_CONTROLLER);
+		});
+
+		it('should throw on init_proposal', async () => {
+			const { init_proposal } = actor;
+
+			await expect(
+				init_proposal({
+					AssetsUpgrade: {
+						clear_existing_assets: toNullable()
+					}
+				})
+			).rejects.toThrowError(JUNO_AUTH_ERROR_NOT_CONTROLLER);
+		});
+
+		it('should throw on submit_proposal', async () => {
+			const { submit_proposal } = actor;
+
+			await expect(submit_proposal(123n)).rejects.toThrowError(JUNO_AUTH_ERROR_NOT_CONTROLLER);
+		});
+
+		it('should throw on init_proposal_asset_upload', async () => {
+			const { init_proposal_asset_upload } = actor;
+
+			await expect(
+				init_proposal_asset_upload(
+					{
+						collection: TEST_COLLECTION,
+						description: toNullable(),
+						encoding_type: [],
+						full_path: '/test',
+						name: '/test',
+						token: toNullable()
+					},
+					123n
+				)
+			).rejects.toThrowError(JUNO_AUTH_ERROR_NOT_CONTROLLER);
+		});
+
+		it('should throw on init_proposal_many_assets_upload', async () => {
+			const { init_proposal_many_assets_upload } = actor;
+
+			await expect(
+				init_proposal_many_assets_upload(
+					[
+						{
+							collection: TEST_COLLECTION,
+							description: toNullable(),
+							encoding_type: [],
+							full_path: '/test',
+							name: '/test',
+							token: toNullable()
+						}
+					],
+					123n
+				)
+			).rejects.toThrowError(JUNO_AUTH_ERROR_NOT_CONTROLLER);
+		});
+
+		it('should throw on upload_proposal_asset_chunk', async () => {
+			const { upload_proposal_asset_chunk } = actor;
+
+			await expect(
+				upload_proposal_asset_chunk({
+					batch_id: 123n,
+					content: Uint8Array.from([1, 2]),
+					order_id: [0n]
+				})
+			).rejects.toThrowError(JUNO_AUTH_ERROR_NOT_CONTROLLER);
+		});
+
+		it('should throw on commit_proposal_asset_upload', async () => {
+			const { commit_proposal_asset_upload } = actor;
+
+			await expect(
+				commit_proposal_asset_upload({
+					batch_id: 12n,
+					chunk_ids: [1n],
+					headers: []
+				})
+			).rejects.toThrowError(JUNO_AUTH_ERROR_NOT_CONTROLLER);
+		});
+
+		it('should throw on commit_proposal_many_assets_upload', async () => {
+			const { commit_proposal_many_assets_upload } = actor;
+
+			await expect(
+				commit_proposal_many_assets_upload([
+					{
+						batch_id: 12n,
+						chunk_ids: [1n],
+						headers: []
+					}
+				])
+			).rejects.toThrowError(JUNO_AUTH_ERROR_NOT_CONTROLLER);
+		});
+
+		it('should throw on memory_size', async () => {
+			const { memory_size } = actor;
+
+			await expect(memory_size()).rejects.toThrowError(JUNO_AUTH_ERROR_NOT_CONTROLLER);
 		});
 	});
 

9dd4f4c0254adfab84d6dc377325ab1d751a6f64
David Dal Busco <david.dalbusco@outlook.com>
2026-02-10 21:44:54 +0100

    test: wip

---
+++ src/tests/mocks/automation.mocks.ts
@@ -0,0 +1,12 @@
+import {
+	type ConsoleActor,
+	idlFactoryObservatory,
+	type ObservatoryActor,
+	type SatelliteActor,
+	type SatelliteDid
+} from '$declarations';
+
+export const mockRepositoryKey: SatelliteDid.RepositoryKey = {
+	owner: 'junobuild',
+	name: 'juno'
+};

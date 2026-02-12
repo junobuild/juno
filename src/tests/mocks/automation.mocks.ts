3405612b0a9be08d6fef227374c7469e3afe91a6
David Dal Busco <david.dalbusco@outlook.com>
2026-02-12 07:12:58 +0100

    test: target observatory

--- src/tests/mocks/automation.mocks.ts
+++ src/tests/mocks/automation.mocks.ts
@@ -1,4 +1,10 @@
-import type { SatelliteDid } from '$declarations';
+import {
+	type ConsoleActor,
+	idlFactoryObservatory,
+	type ObservatoryActor,
+	type SatelliteActor,
+	type SatelliteDid
+} from '$declarations';
 
 export const mockRepositoryKey: SatelliteDid.RepositoryKey = {
 	owner: 'junobuild',

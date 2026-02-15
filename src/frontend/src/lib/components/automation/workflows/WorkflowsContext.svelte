fc72ea6da2b708561ff3e387e02d9b27ebdecb00
David Dal Busco <david.dalbusco@outlook.com>
2026-02-15 11:29:11 +0100

    feat(frontend): use interface

--- src/frontend/src/lib/components/automation/workflows/WorkflowsContext.svelte
+++ src/frontend/src/lib/components/automation/workflows/WorkflowsContext.svelte
@@ -3,11 +3,10 @@
 	import UsersTable from '$lib/components/auth/Users.svelte';
 	import ListContext from '$lib/components/list/ListContext.svelte';
 	import type { ListDocsFn } from '$lib/services/satellite/_list-docs.services';
-	import { listUsers } from '$lib/services/satellite/user/users.services';
 	import { i18n } from '$lib/stores/app/i18n.store';
-	import type { User as UserType } from '$lib/types/user';
-	import type {Workflow} from "$lib/types/workflow";
-	import {listWorkflows} from "$lib/services/satellite/automation/workflows.services";
+	import type { Workflow } from '$lib/types/workflow';
+	import { listWorkflows } from '$lib/services/satellite/automation/workflows.services';
+	import Workflows from "$lib/components/automation/workflows/Workflows.svelte";
 
 	interface Props {
 		satelliteId: Principal;
@@ -17,10 +16,10 @@
 
 	const listFn: ListDocsFn<Workflow> = listWorkflows;
 
-	let listContextRef = $state<ListContext<UserType> | undefined>();
+	let listContextRef = $state<ListContext<Workflow> | undefined>();
 	const reload = () => listContextRef?.reload();
 </script>
 
 <ListContext bind:this={listContextRef} errorLabel={$i18n.errors.load_users} {listFn} {satelliteId}>
-	<UsersTable {reload} {satelliteId} />
+	<Workflows {reload} {satelliteId} />
 </ListContext>
--- src/frontend/src/lib/services/satellite/automation/workflows.services.ts
+++ src/frontend/src/lib/services/satellite/automation/workflows.services.ts
@@ -1,16 +1,12 @@
-import { listDocs, type ListDocsResult } from '$lib/services/satellite/_list-docs.services';
-import type { OptionIdentity } from '$lib/types/itentity';
-import type { ListParams } from '$lib/types/list';
+import {
+	listDocs,
+	type ListDocsParams,
+	type ListDocsResult
+} from '$lib/services/satellite/_list-docs.services';
 import type { Workflow, WorkflowKey } from '$lib/types/workflow';
 import { toKeyWorkflow } from '$lib/utils/workflow.utils';
-import type { Principal } from '@icp-sdk/core/principal';
 
-export const listWorkflows = async (
-	params: Pick<ListParams, 'startAfter' | 'filter' | 'order'> & {
-		satelliteId: Principal;
-		identity: OptionIdentity;
-	}
-): Promise<ListDocsResult<Workflow>> => {
+export const listWorkflows = async (params: ListDocsParams): Promise<ListDocsResult<Workflow>> => {
 	const { items, matches_length, items_length } = await listDocs({
 		...params,
 		collection: '#automation-workflow'

28ddcaf75694c432a7900f9ed4d43ccca48424bd
David Dal Busco <david.dalbusco@outlook.com>
2026-02-19 09:14:31 +0100

    feat(frontend): config automation disabled in skylab

--- src/frontend/src/lib/components/satellites/automation/AutomationNew.svelte
+++ src/frontend/src/lib/components/satellites/automation/AutomationNew.svelte
@@ -2,6 +2,8 @@
 	import { i18n } from '$lib/stores/app/i18n.store';
 	import type { Satellite } from '$lib/types/satellite';
 	import { emit } from '$lib/utils/events.utils';
+	import { isSkylab } from '$lib/env/app.env';
+	import { toasts } from '$lib/stores/app/toasts.store';
 
 	interface Props {
 		satellite: Satellite;
@@ -10,6 +12,11 @@
 	let { satellite }: Props = $props();
 
 	const createAutomation = () => {
+		if (isSkylab()) {
+			toasts.warn($i18n.automation.warn_skylab);
+			return;
+		}
+
 		emit({
 			message: 'junoModal',
 			detail: {

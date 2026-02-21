71720b598e0485f9426643abb01c695cbee2a07d
David Dal Busco <david.dalbusco@outlook.com>
2026-02-21 15:41:07 +0100

    feat(frontend): split satellite overview

--- src/frontend/src/lib/types/i18n.d.ts
+++ src/frontend/src/lib/types/i18n.d.ts
@@ -347,6 +347,7 @@ interface I18nSatellites {
 	create_satellite_price: string;
 	loading_satellites: string;
 	overview: string;
+	metadata: string;
 	id: string;
 	stock_version: string;
 	extended_version: string;

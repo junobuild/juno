fdbee72d76bfc469df8af4a73b50409797d1ea8f
David Dal Busco <david.dalbusco@outlook.com>
2026-02-25 11:49:23 +0100

    feat: iterate

--- src/sputnik/build.rs
+++ src/sputnik/build.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     // Re-run a build if env variable DEV_SCRIPT_PATH changes.
@@ -12,9 +13,8 @@ fn main() {
 
     println!("cargo:rustc-env=DEV_SCRIPT_PATH={script_path}");
 
-    let default_custom_functions_path = "generated.rs";
-    let custom_functions_path = env::var("DEV_CUSTOM_FUNCTIONS_PATH")
-        .unwrap_or_else(|_| default_custom_functions_path.to_string());
+    let source = env::var("DEV_CUSTOM_FUNCTIONS_PATH")
+        .unwrap_or_else(|_| "resources/functions.rs".to_string());
 
-    println!("cargo:rustc-env=DEV_CUSTOM_FUNCTIONS_PATH={custom_functions_path}");
+    fs::copy(&source, "src/generated.rs").expect("Failed to copy generated.rs");
 }

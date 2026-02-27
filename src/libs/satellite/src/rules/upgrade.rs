b06fc54d66baab0ec2fbc164cd1ce227b0c7db26
David Dal Busco <david.dalbusco@outlook.com>
2026-02-27 09:23:25 +0100

    feat(satellite): keep system collection upgrades (#2639)
    * feat(satellite): keep system collection upgrades
    
    * feat: use rules
    
    * feat: juno release
    
    * feat: storage withs
    
    * fix: storage

--- src/libs/satellite/src/rules/upgrade.rs
+++ src/libs/satellite/src/rules/upgrade.rs
@@ -1,80 +1,148 @@
-use crate::memory::state::STATE;
+use crate::assets::constants::{
+    CDN_JUNO_RELEASES_COLLECTION_KEY, COLLECTION_RELEASES_DEFAULT_RULE,
+};
+use crate::memory::state::services::{
+    with_db_rules, with_db_rules_mut, with_storage_rules, with_storage_rules_mut,
+};
 use ic_cdk::api::time;
 use junobuild_collections::constants::db::{
     COLLECTION_AUTOMATION_TOKEN_DEFAULT_RULE, COLLECTION_AUTOMATION_TOKEN_KEY,
     COLLECTION_AUTOMATION_WORKFLOW_DEFAULT_RULE, COLLECTION_AUTOMATION_WORKFLOW_KEY,
+    COLLECTION_LOG_DEFAULT_RULE, COLLECTION_LOG_KEY, COLLECTION_USER_USAGE_DEFAULT_RULE,
+    COLLECTION_USER_USAGE_KEY,
 };
+use junobuild_collections::constants::db::{
+    COLLECTION_USER_WEBAUTHN_DEFAULT_RULE, COLLECTION_USER_WEBAUTHN_INDEX_DEFAULT_RULE,
+    COLLECTION_USER_WEBAUTHN_INDEX_KEY, COLLECTION_USER_WEBAUTHN_KEY,
+};
+use junobuild_collections::types::core::CollectionKey;
+use junobuild_collections::types::interface::SetRule;
 use junobuild_collections::types::rules::Rule;
 
 // ---------------------------------------------------------
 // One time upgrade
 // ---------------------------------------------------------
 
-pub fn init_automation_collections() {
+pub fn init_system_collections() {
+    init_db_collections();
+
+    init_storage_collections();
+}
+
+fn init_db_collections() {
+    // Logs
+    init_log_collection();
+
+    // User usage
+    init_user_usage_collection();
+
+    // WebAuthn authentication
+    init_user_webauthn_collection();
+    init_user_webauthn_index_collection();
+
+    // Automation
     init_automation_token_collection();
     init_automation_workflow_collection();
 }
 
+fn init_storage_collections() {
+    init_juno_releases_collection();
+}
+
+fn init_log_collection() {
+    init_db_collection(&COLLECTION_LOG_KEY.to_string(), COLLECTION_LOG_DEFAULT_RULE);
+}
+
+fn init_user_usage_collection() {
+    init_db_collection(
+        &COLLECTION_USER_USAGE_KEY.to_string(),
+        COLLECTION_USER_USAGE_DEFAULT_RULE,
+    );
+}
+
+fn init_user_webauthn_collection() {
+    init_db_collection(
+        &COLLECTION_USER_WEBAUTHN_KEY.to_string(),
+        COLLECTION_USER_WEBAUTHN_DEFAULT_RULE,
+    );
+}
+
+fn init_user_webauthn_index_collection() {
+    init_db_collection(
+        &COLLECTION_USER_WEBAUTHN_INDEX_KEY.to_string(),
+        COLLECTION_USER_WEBAUTHN_INDEX_DEFAULT_RULE,
+    );
+}
+
 fn init_automation_token_collection() {
-    let col = STATE.with(|state| {
-        let rules = &state.borrow_mut().heap.db.rules;
-        rules.get(COLLECTION_AUTOMATION_TOKEN_KEY).cloned()
-    });
+    init_db_collection(
+        &COLLECTION_AUTOMATION_TOKEN_KEY.to_string(),
+        COLLECTION_AUTOMATION_TOKEN_DEFAULT_RULE,
+    );
+}
+
+fn init_automation_workflow_collection() {
+    init_db_collection(
+        &COLLECTION_AUTOMATION_WORKFLOW_KEY.to_string(),
+        COLLECTION_AUTOMATION_WORKFLOW_DEFAULT_RULE,
+    );
+}
+
+fn init_db_collection(collection: &CollectionKey, default_rule: SetRule) {
+    let col = with_db_rules(|rules| rules.get(collection).cloned());
 
     if col.is_none() {
-        STATE.with(|state| {
-            let rules = &mut state.borrow_mut().heap.db.rules;
-
+        with_db_rules_mut(|rules| {
             let now = time();
 
             let rule = Rule {
-                read: COLLECTION_AUTOMATION_TOKEN_DEFAULT_RULE.read,
-                write: COLLECTION_AUTOMATION_TOKEN_DEFAULT_RULE.write,
-                memory: COLLECTION_AUTOMATION_TOKEN_DEFAULT_RULE.memory,
-                mutable_permissions: COLLECTION_AUTOMATION_TOKEN_DEFAULT_RULE.mutable_permissions,
-                max_size: COLLECTION_AUTOMATION_TOKEN_DEFAULT_RULE.max_size,
-                max_capacity: COLLECTION_AUTOMATION_TOKEN_DEFAULT_RULE.max_capacity,
-                max_changes_per_user: COLLECTION_AUTOMATION_TOKEN_DEFAULT_RULE.max_changes_per_user,
+                read: default_rule.read,
+                write: default_rule.write,
+                memory: default_rule.memory,
+                mutable_permissions: default_rule.mutable_permissions,
+                max_size: default_rule.max_size,
+                max_capacity: default_rule.max_capacity,
+                max_changes_per_user: default_rule.max_changes_per_user,
                 created_at: now,
                 updated_at: now,
-                version: COLLECTION_AUTOMATION_TOKEN_DEFAULT_RULE.version,
-                rate_config: COLLECTION_AUTOMATION_TOKEN_DEFAULT_RULE.rate_config,
+                version: default_rule.version,
+                rate_config: default_rule.rate_config,
             };
 
-            rules.insert(COLLECTION_AUTOMATION_TOKEN_KEY.to_string(), rule.clone());
+            rules.insert(collection.to_string(), rule.clone());
         });
     }
 }
 
-fn init_automation_workflow_collection() {
-    let col = STATE.with(|state| {
-        let rules = &state.borrow_mut().heap.db.rules;
-        rules.get(COLLECTION_AUTOMATION_WORKFLOW_KEY).cloned()
-    });
+fn init_juno_releases_collection() {
+    init_storage_collection(
+        &CDN_JUNO_RELEASES_COLLECTION_KEY.to_string(),
+        COLLECTION_RELEASES_DEFAULT_RULE,
+    );
+}
+
+fn init_storage_collection(collection: &CollectionKey, default_rule: SetRule) {
+    let col = with_storage_rules(|rules| rules.get(collection).cloned());
 
     if col.is_none() {
-        STATE.with(|state| {
-            let rules = &mut state.borrow_mut().heap.db.rules;
-
+        with_storage_rules_mut(|rules| {
             let now = time();
 
             let rule = Rule {
-                read: COLLECTION_AUTOMATION_WORKFLOW_DEFAULT_RULE.read,
-                write: COLLECTION_AUTOMATION_WORKFLOW_DEFAULT_RULE.write,
-                memory: COLLECTION_AUTOMATION_WORKFLOW_DEFAULT_RULE.memory,
-                mutable_permissions: COLLECTION_AUTOMATION_WORKFLOW_DEFAULT_RULE
-                    .mutable_permissions,
-                max_size: COLLECTION_AUTOMATION_WORKFLOW_DEFAULT_RULE.max_size,
-                max_capacity: COLLECTION_AUTOMATION_WORKFLOW_DEFAULT_RULE.max_capacity,
-                max_changes_per_user: COLLECTION_AUTOMATION_WORKFLOW_DEFAULT_RULE
-                    .max_changes_per_user,
+                read: default_rule.read,
+                write: default_rule.write,
+                memory: default_rule.memory,
+                mutable_permissions: default_rule.mutable_permissions,
+                max_size: default_rule.max_size,
+                max_capacity: default_rule.max_capacity,
+                max_changes_per_user: default_rule.max_changes_per_user,
                 created_at: now,
                 updated_at: now,
-                version: COLLECTION_AUTOMATION_WORKFLOW_DEFAULT_RULE.version,
-                rate_config: COLLECTION_AUTOMATION_WORKFLOW_DEFAULT_RULE.rate_config,
+                version: default_rule.version,
+                rate_config: default_rule.rate_config,
             };
 
-            rules.insert(COLLECTION_AUTOMATION_WORKFLOW_KEY.to_string(), rule.clone());
+            rules.insert(collection.to_string(), rule.clone());
         });
     }
 }

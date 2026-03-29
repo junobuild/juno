90bf5b2d0824f44088cbe6f0b6054191eb4bc767
David Dal Busco <david.dalbusco@outlook.com>
2026-03-29 11:15:58 +0200

    chore: fmt

--- src/libs/satellite/src/assets/storage/services.rs
+++ src/libs/satellite/src/assets/storage/services.rs
@@ -1,7 +1,7 @@
+use crate::assets::storage::certified_assets::all::certify_all_assets;
+use crate::assets::storage::store::set_config_store;
 use junobuild_storage::types::config::StorageConfig;
 use junobuild_storage::types::interface::SetStorageConfig;
-use crate::assets::storage::store::set_config_store;
-use crate::assets::storage::certified_assets::all::certify_all_assets;
 
 pub fn apply_storage_config(config: &SetStorageConfig) -> Result<StorageConfig, String> {
     let stored_config = set_config_store(config)?;

7fab2577580d9726a43db98e28df69ca1aa7fb9f
David Dal Busco <david.dalbusco@outlook.com>
2026-03-29 14:17:51 +0200

    feat: transform

--- src/tests/fixtures/test_sputnik/resources/apis/ic-cdk/ic-cdk-http-request.ts
+++ src/tests/fixtures/test_sputnik/resources/apis/ic-cdk/ic-cdk-http-request.ts
@@ -1,8 +1,9 @@
-import { defineUpdate } from '@junobuild/functions';
+import { defineQuery, defineUpdate } from '@junobuild/functions';
 import {
 	HttpRequestArgsSchema,
 	httpRequest as httpRequestLib,
-	HttpRequestResultSchema
+	HttpRequestResultSchema,
+	TransformArgsSchema
 } from '@junobuild/functions/ic-cdk';
 
 export const httpRequest = defineUpdate({
@@ -10,5 +11,22 @@ export const httpRequest = defineUpdate({
 	result: HttpRequestResultSchema,
 	handler: async (args) => {
 		return await httpRequestLib(args);
+	}
+});
+
+export const myHttpTransform = defineQuery({
+	hidden: true,
+	args: TransformArgsSchema,
+	result: HttpRequestResultSchema,
+	handler: async (raw) => {
+		const response = raw.response;
+
+		console.log('myHttpTransform', response);
+
+		return {
+			status: response.status,
+			body: response.body,
+			headers: []
+		};
 	}
 });

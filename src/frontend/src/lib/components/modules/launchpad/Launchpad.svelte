eee166f4e6acdaf37b03e020ad9935b891d6abf6
David Dal Busco <david.dalbusco@outlook.com>
2026-05-26 13:59:13 +0900

    feat(frontend): remove getting started for free call to action

--- src/frontend/src/lib/components/modules/launchpad/Launchpad.svelte
+++ src/frontend/src/lib/components/modules/launchpad/Launchpad.svelte
@@ -1,28 +1,18 @@
 <script lang="ts">
-	import { nonNullish } from '@dfinity/utils';
 	import LaunchpadGuard from '$lib/components/modules/launchpad/LaunchpadGuard.svelte';
 	import LaunchpadHeader from '$lib/components/modules/launchpad/LaunchpadHeader.svelte';
 	import LaunchpadSegments from '$lib/components/modules/launchpad/LaunchpadSegments.svelte';
 	import LaunchpadToolbar from '$lib/components/modules/launchpad/LaunchpadToolbar.svelte';
-	import { account } from '$lib/derived/console/account.derived';
 	import { satellites } from '$lib/derived/satellites.derived';
 
 	let filter = $state('');
-
-	// If the user was never updated, they never received credits.
-	// We do this check to e.g. not display the getting started banner on Skylab.
-	let userNoFirstCredits = $derived(
-		nonNullish($account) &&
-			$account.created_at === $account.updated_at &&
-			$account.credits.e8s === 0n
-	);
 
 	let withoutGreetingsReturningLabel = $derived(($satellites?.length ?? 0n) === 0n);
 </script>
 
 <LaunchpadGuard>
 	<section>
-		<LaunchpadHeader userGettingStarted={userNoFirstCredits} {withoutGreetingsReturningLabel}>
+		<LaunchpadHeader {withoutGreetingsReturningLabel}>
 			<LaunchpadToolbar bind:filter />
 		</LaunchpadHeader>
 

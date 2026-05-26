40df359861210d8945b90ef9957fc3fbca044564
David Dal Busco <david.dalbusco@outlook.com>
2026-05-26 14:13:27 +0900

    chore: fmt

--- src/frontend/src/lib/components/app/core/Banner.svelte
+++ src/frontend/src/lib/components/app/core/Banner.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	import IconSpaceStation from '$lib/components/icons/IconSpaceStation.svelte';
+	import IconWarning from '$lib/components/icons/IconWarning.svelte';
 	import Html from '$lib/components/ui/Html.svelte';
 	import { i18n } from '$lib/stores/app/i18n.store';
-	import IconWarning from '$lib/components/icons/IconWarning.svelte';
 
 	interface Props {
 		display: 'dev' | 'maintenance';

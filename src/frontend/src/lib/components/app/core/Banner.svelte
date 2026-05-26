bb96e5a64dba37db4a4f73e67269b2fb139f31fb
David Dal Busco <david.dalbusco@outlook.com>
2026-05-26 14:15:45 +0900

    feat(frontend): display a maintenance banner warning (#2801)
    * feat(frontend): display a maintenance banner warning
    
    * chore: fmt

---
+++ src/frontend/src/lib/components/app/core/Banner.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import IconSpaceStation from '$lib/components/icons/IconSpaceStation.svelte';
+	import IconWarning from '$lib/components/icons/IconWarning.svelte';
+	import Html from '$lib/components/ui/Html.svelte';
+	import { i18n } from '$lib/stores/app/i18n.store';
+
+	interface Props {
+		display: 'dev' | 'maintenance';
+	}
+
+	let { display }: Props = $props();
+</script>
+
+<aside class="banner" class:warn={display === 'maintenance'}>
+	{#if display === 'dev'}
+		<IconSpaceStation /> <strong>Dev Console</strong>
+	{:else}
+		<IconWarning size="18" /> <Html text={$i18n.core.maintenance}></Html>
+	{/if}
+</aside>
+
+<style lang="scss">
+	.banner {
+		height: var(--banner-height);
+
+		background: var(--color-tertiary);
+		color: var(--color-tertiary-contrast);
+
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+		gap: var(--padding);
+
+		&.warn {
+			background: var(--color-warning);
+			color: var(--color-warning-contrast);
+		}
+	}
+</style>
